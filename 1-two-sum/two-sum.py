082ad710ac6bc266bb7ef8c175c28da229f34659
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-14 10:42:03 -0500

    Time: 0 ms (100.00%) | Memory: 13.3 MB (48.91%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,10 +5,18 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # Brute force solution:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        # # Brute force solution:
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
 
+        # More optimal solution:
+        complement_index_map = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if nums[i] in complement_index_map:
+                return [complement_index_map[nums[i]] , i]
+            else:
+                complement_index_map[complement] = i
         
