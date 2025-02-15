e0ad97a7a33b5d2f8ccadac15c4b38c4797e4fb7
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-15 08:55:24 -0500

    Time: 55 ms (79.55%) | Memory: 12.6 MB (59.09%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -7,17 +7,17 @@ class Solution(object):
         :rtype: List[bool]
         """
         # Brute force solution
-        # result = []
-        # for i in range(len(l)):
-        #     curr_list = nums[l[i]:r[i]+1]
-        #     curr_list.sort()
-        #     curr_bool = True
-        #     for i in range(2, len(curr_list)):
-        #         if curr_list[i]-curr_list[i-1] != curr_list[1] - curr_list[0]:
-        #             curr_bool = False
-        #             break
-        #     result.append(curr_bool)
-        # return result
+        result = []
+        for i in range(len(l)):
+            curr_list = nums[l[i]:r[i]+1]
+            curr_list.sort()
+            curr_bool = True
+            for i in range(2, len(curr_list)):
+                if curr_list[i]-curr_list[i-1] != curr_list[1] - curr_list[0]:
+                    curr_bool = False
+                    break
+            result.append(curr_bool)
+        return result
         
         # Form a hash set
 
