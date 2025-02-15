fd61b9c97b3faa0b6a18e886a4ad9b2ce9aaf513
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-15 08:01:24 -0500

    Time: 50 ms (93.18%) | Memory: 12.5 MB (59.09%) - LeetSync

--- 1752-arithmetic-subarrays/arithmetic-subarrays.py
+++ 1752-arithmetic-subarrays/arithmetic-subarrays.py
@@ -9,14 +9,11 @@ class Solution(object):
         # Brute force solution
         result = []
         for i in range(len(l)):
-            left_index = l[i]
-            right_index = r[i]+1
-            curr_list = nums[left_index:right_index]
+            curr_list = nums[l[i]:r[i]+1]
             curr_list.sort()
-            initial_difference = curr_list[1] - curr_list[0]
             curr_bool = True
             for i in range(2, len(curr_list)):
-                if curr_list[i]-curr_list[i-1] != initial_difference:
+                if curr_list[i]-curr_list[i-1] != curr_list[1] - curr_list[0]:
                     curr_bool = False
                     break
             result.append(curr_bool)
