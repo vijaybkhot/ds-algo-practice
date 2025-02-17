1c63fb6eb7ed8358b050e2944a9028dbfd1cc4f9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-17 14:12:54 -0500

    Time: 31 ms (69.13%) | Memory: 20.1 MB (83.97%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,29 +4,46 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        if len(nums)==0 or len(nums)==1:
-            return nums
-            
-        # Initialize arrays
-        prefix = [1] * len(nums)
-        postfix = [1] * len(nums)
-        output = [1] * len(nums)
+        # # Solution using separate prefix and postfix arrays
+        # if len(nums)==0 or len(nums)==1:
+        #     return nums
 
-        # Fill prefix product array
-        prefix[0] = nums[0]
-        for i in range(1, len(prefix)):
-            prefix[i] = prefix[i-1] * nums[i]
+        # # Initialize arrays
+        # prefix = [1] * len(nums)
+        # postfix = [1] * len(nums)
+        # output = [1] * len(nums)
+
+        # # Fill prefix product array
+        # prefix[0] = nums[0]
+        # for i in range(1, len(prefix)):
+        #     prefix[i] = prefix[i-1] * nums[i]
         
-        # Fill postfix product array
-        postfix[-1] = nums[-1]
-        for i in range(len(nums)-2,0,-1):
-            postfix[i] = postfix[i+1] * nums[i]
+        # # Fill postfix product array
+        # postfix[-1] = nums[-1]
+        # for i in range(len(nums)-2,0,-1):
+        #     postfix[i] = postfix[i+1] * nums[i]
         
-        # calculate output using prefix and postfix
-        output[0] = postfix[1]
-        output[-1] = prefix[-2]
-        for i in range(1, len(output)-1):
-            output[i] = prefix[i-1] * postfix[i+1]
+        # # calculate output using prefix and postfix
+        # output[0] = postfix[1]
+        # output[-1] = prefix[-2]
+        # for i in range(1, len(output)-1):
+        #     output[i] = prefix[i-1] * postfix[i+1]
+
+        # return output
+
+        # Solution without separate prefix and postfix array
+        output = [1] * len(nums)
+        # calculate prefix and store in the output
+        for i in range(1, len(nums)):
+            prefix_product = nums[i-1]*output[i-1] 
+            output[i] = prefix_product
+        
+        # calculate output in the reverse order:
+        postfix = 1
+        for i in range(len(nums)-2, -1, -1):
+            postfix = postfix * nums[i+1]
+            output[i] = postfix * output[i]
+            
 
         return output
 
