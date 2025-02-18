39ae85f07f9e678849f3f53a11ade7f0afc5d043
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-18 11:52:51 -0500

    Time: 15 ms (32.00%) | Memory: 13.7 MB (31.25%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -4,26 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # Solution using hash map - O(n) time and space complexity
-        majority_number = int(math.floor(len(nums)/2))
-        frequency_map = {}
-        for i in range(len(nums)):
-            frequency_map[nums[i]] = frequency_map.get(nums[i], 0) + 1
-            if frequency_map[nums[i]] > majority_number:
-                return nums[i]
+        # # Solution using hash map - O(n) time and space complexity
+        # majority_number = int(math.floor(len(nums)/2))
+        # frequency_map = {}
+        # for i in range(len(nums)):
+        #     frequency_map[nums[i]] = frequency_map.get(nums[i], 0) + 1
+        #     if frequency_map[nums[i]] > majority_number:
+        #         return nums[i]
         
         # Boyer-Moore Voting Algorithm: O(n) time and constant space complexity
         count = 0
         candidate = None
-
         for num in nums:
             if count == 0:
                 candidate = num
-            else:
-                if num == candidate:
-                    count += 1
-                else:
-                    count -= 1
+            count += (1 if num == candidate else -1)
         return candidate
 
         
