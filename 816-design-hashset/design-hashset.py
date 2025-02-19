6693853aee4743afc1b9f8b656b845cb121c65cc
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-19 15:03:01 -0500

    Time: 78 ms (55.30%) | Memory: 19.4 MB (33.38%) - LeetSync

--- 816-design-hashset/design-hashset.py
+++ 816-design-hashset/design-hashset.py
@@ -4,24 +4,23 @@ class ListNode(object):
         self.next = None
 
 class MyHashSet(object):
-
     def __init__(self):
         self.size = 10007
         self.set = [None] * self.size  
 
     def add(self, key):
         index = key % self.size
-        if self.set[index] is None:
+        cur = self.set[index]
+        if cur is None:
             self.set[index] = ListNode(key)
             return
-        cur = self.set[index]
-        while cur:
-            if cur.key == key:
-                return  # Key already exists
-            if cur.next is None:
-                break
+        if cur.key == key:
+            return
+        while cur.next:
+            if cur.next.key == key:
+                return
             cur = cur.next
-        cur.next = ListNode(key)  # Append at end
+        cur.next = ListNode(key)
 
     def remove(self, key):
         index = key % self.size
@@ -29,14 +28,14 @@ class MyHashSet(object):
         if cur is None:
             return
         if cur.key == key:
-            self.set[index] = cur.next  # Remove head
+            self.set[index] = cur.next  
             return
-        prev = None
-        while cur:
-            if cur.key == key:
-                prev.next = cur.next
+        while cur.next:
+            if cur.next.key == key:
+                cur.next = cur.next.next
                 return
-            prev, cur = cur, cur.next
+            cur = cur.next
+        return
 
     def contains(self, key):
         index = key % self.size
