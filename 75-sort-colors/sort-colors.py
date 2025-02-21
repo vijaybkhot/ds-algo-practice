dbc398db528daaee0405de69ff12a17addc60435
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-21 11:42:59 -0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.05%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,14 +4,61 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        # using insertion sort:
-        for i in range(1, len(nums)):
-            space = i
-            curr_elem = nums[space]
-            while space > 0 and nums[space-1] > curr_elem:
-                nums[space] = nums[space-1]
-                space = space - 1
-            nums[space] = curr_elem
+        # # using insertion sort:
+        # for i in range(1, len(nums)):
+        #     space = i
+        #     curr_elem = nums[space]
+        #     while space > 0 and nums[space-1] > curr_elem:
+        #         nums[space] = nums[space-1]
+        #         space = space - 1
+        #     nums[space] = curr_elem
+        # return nums
+
+        # Using merge sort: No use here. this is not in place sorting
+        # def merge(arr1, arr2):
+        #     i, j, sorted_arr = 0, 0, []
+        #     while i < len(arr1) and j < len(arr2):
+        #         if arr1[i] <= arr2[j]:
+        #             sorted_arr.append(arr1[i])
+        #             i += 1
+        #         else:
+        #             sorted_arr.append(arr2[j])
+        #             j += 1
+
+        #     sorted_arr.extend(arr1[i:])
+        #     sorted_arr.extend(arr2[j:])
+        #     return sorted_arr
+        
+        # def merge_sort(arr):
+        #     if len(arr) <= 1:
+        #         return arr
+        #     mid = len(arr)//2
+        #     left = arr[:mid]
+        #     right = arr[mid:]
+        #     left = merge_sort(left)
+        #     right = merge_sort(right)
+        #     return merge(left, right)
+        
+
+        # Bucket sort: in place sorting
+        if not nums:
+            return nums
+        max_elem = max(nums)
+        count = [0] * (max_elem + 1)
+
+        for num in nums:
+            count[num] += 1
+
+        j = 0
+        for i in range(len(count)):
+            while count[i] > 0:
+                nums[j] = i  
+                j += 1
+                count[i] -= 1
         return nums
+
+
+            
+
 
         
