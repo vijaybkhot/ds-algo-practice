0089738c02af3054f047826d68346bda8da769c0
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-23 12:58:50 -0500

    Time: 6 ms (87.00%) | Memory: 14.2 MB (66.43%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -5,23 +5,47 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        nums.sort()
-        left = 0
-        right = 1
+        # # Brute force solution using sorting
+        # nums.sort()
+        # left = 0
+        # right = 1
+        # diff_pair = 0
+        # while left < len(nums) and right < len(nums):
+        #     right = left + 1
+        #     curr_left = nums[left]
+        #     possible_right = curr_left + k
+        #     for i in range(right, len(nums)):
+        #         if nums[i] == possible_right:
+        #             diff_pair += 1
+        #             break
+        #     left += 1
+        #     while left < len(nums) and nums[left] == curr_left:
+        #         left += 1
+        
+        # return diff_pair
+
+        # Efficient solution using hash map
+        # Create a hash map to count frequency of each number in the array
+        
         diff_pair = 0
-        while left < len(nums) and right < len(nums):
-            right = left + 1
-            curr_left = nums[left]
-            possible_right = curr_left + k
-            for i in range(right, len(nums)):
-                if nums[i] == possible_right:
+        hash_map = {}
+        for num in nums:
+            hash_map[num] = hash_map.get(num, 0) + 1
+        
+        # Get all keys in the map
+        unique_nums = hash_map.keys()
+        if k == 0:
+            for num in unique_nums:
+                if num in hash_map and hash_map[num] > 1:
                     diff_pair += 1
-                    break
-            left += 1
-            while left < len(nums) and nums[left] == curr_left:
-                left += 1
+            return diff_pair
+
+        for num in unique_nums:
+            if num + k in hash_map:
+                diff_pair += 1
         
         return diff_pair
+        
             
 
 
