f8e0aa3d460212b9504be0ee259dd85c61ecb349
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-23 11:59:31 -0500

    Time: 1890 ms (16.25%) | Memory: 13.4 MB (93.14%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.py
@@ -13,9 +13,6 @@ class Solution(object):
             right = left + 1
             curr_left = nums[left]
             possible_right = curr_left + k
-            # if nums[right] == curr_left:
-            #     while (nums[right] == curr_left) and (right < len(nums)):
-            #         right += 1
             for i in range(right, len(nums)):
                 if nums[i] == possible_right:
                     diff_pair += 1
