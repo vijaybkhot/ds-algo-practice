d92747f57a78c0b9292e5e04544b9ede33ccd34d
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-24 11:30:43 -0500

    Time: 81 ms (14.62%) | Memory: 31.4 MB (43.31%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -9,21 +9,18 @@ class Solution(object):
         # Edge cases
         if len(nums) < 2:
             return False
-        #Create a prefix sum array
-        prefix = [0]
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == 0 and nums[i-1] == 0:
-                return True
-            prefix.append(prefix[-1] + nums[i])
-        
             
         # Creating a hash map to store remainders
-        hash_map = {0:-1}
-        for i in range(1, len(prefix)):
-            curr_remainder = prefix[i] % k
-            if curr_remainder in hash_map and i > 1 and i - hash_map[curr_remainder] >= 2:
+        hash_map = {0: -1}
+        prefix = nums[0]
+        for i in range(len(nums)):
+            curr_remainder = prefix % k
+            if curr_remainder in hash_map and i - hash_map[curr_remainder] >= 2:
                 return True
             if curr_remainder not in hash_map:
                 hash_map[curr_remainder] = i
+            prefix = prefix + (nums[i+1] if i + 1 < len(nums) else 0)
+
+        return False
         
         return False
