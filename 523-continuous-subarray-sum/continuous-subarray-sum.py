c575e8e12a5c29e96322c7bb505a9fb6b655060b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-24 11:33:07 -0500

    Time: 60 ms (39.59%) | Memory: 31.6 MB (32.00%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -12,13 +12,13 @@ class Solution(object):
             
         # Creating a hash map to store remainders
         hash_map = {0: -1}
-        prefix = nums[0]
+        prefix = 0
         for i in range(len(nums)):
+            prefix = prefix + nums[i]
             curr_remainder = prefix % k
             if curr_remainder in hash_map and i - hash_map[curr_remainder] >= 2:
                 return True
             if curr_remainder not in hash_map:
                 hash_map[curr_remainder] = i
-            prefix = prefix + (nums[i+1] if i + 1 < len(nums) else 0)
 
         return False
