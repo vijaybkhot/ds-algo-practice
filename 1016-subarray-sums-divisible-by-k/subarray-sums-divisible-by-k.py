f45e59336dbc1fa9f5cfdc88605bc75f263f4413
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-25 10:58:57 -0500

    Time: 57 ms (6.44%) | Memory: 15.5 MB (5.42%) - LeetSync

---
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def subarraysDivByK(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        # edge cases
+        if not nums:
+            return 0
+        if len(nums)== 1:
+            if nums[0] % k != 0:
+                return 0
+            else:
+                return 1
+        
+        res = 0
+        # Remainder frequency hash map
+        remainder = {}
+        prefix = 0
+        for index, num in enumerate(nums, start=0):
+            prefix += num
+            curr_remainder = prefix % k
+            # if curr_remainder == 0:
+            #     res += 1
+            #     remainder.setdefault(curr_remainder, []).append(index)
+            # else:
+            #     remainder.setdefault(curr_remainder, []).append(index)
+            #     res += len(remainder[curr_remainder]) - 1
+            remainder.setdefault(curr_remainder, []).append(index)
+            res += len(remainder[curr_remainder]) - 1
+            if curr_remainder == 0:
+                res += 1
+                    
+        return res
+        
