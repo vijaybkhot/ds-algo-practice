35e36d1241237b9d8ad1671495e382f91bfd1660
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-25 12:17:06 -0500

    Time: 47 ms (83.38%) | Memory: 26.9 MB (82.58%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,17 +4,35 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # if not nums:
+        #     return 0
+        # res = []
+        # unique_nums = list(set(nums))
+        # unique_nums.sort()
+        # curr_sequence = 1
+        # for i in range(0, len(unique_nums)-1):
+        #     if unique_nums[i+1] == unique_nums[i] + 1:
+        #         curr_sequence += 1
+        #     else:
+        #         res.append(curr_sequence)
+        #         curr_sequence = 1
+        # res.append(curr_sequence)
+        # return max(res)
+
         if not nums:
             return 0
-        res = []
-        unique_nums = list(set(nums))
-        unique_nums.sort()
-        curr_sequence = 1
-        for i in range(0, len(unique_nums)-1):
-            if unique_nums[i+1] == unique_nums[i] + 1:
-                curr_sequence += 1
-            else:
-                res.append(curr_sequence)
-                curr_sequence = 1
-        res.append(curr_sequence)
-        return max(res)
+        longest_streak = 0
+        unique_nums = set(nums)
+        for num in unique_nums:
+            if num-1 not in unique_nums:
+                curr_streak = 1
+                curr_num = num
+
+                while curr_num + 1 in unique_nums:
+                    curr_num += 1
+                    curr_streak += 1
+                
+                if curr_streak > longest_streak:
+                    longest_streak = curr_streak
+                
+        return longest_streak
