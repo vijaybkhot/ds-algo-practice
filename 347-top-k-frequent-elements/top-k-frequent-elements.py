bdc22e16b1a3b4795af823f4042f355d684b1778
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-26 09:46:57 -0500

    Time: 15 ms (37.53%) | Memory: 20 MB (5.47%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,14 +5,39 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # # Sorting approach O(nlogn)
+        # def frequencyIndexMap(arr):
+        #     map = {}
+        #     for i in range(len(arr)):
+        #         map[arr[i]] = map.get(arr[i],0)+1
+        #     return map
+        
+        # freqMap = frequencyIndexMap(nums)
+        # ferqMapTupleArray = tuple(freqMap.items())
+        # sortedTupleArray = sorted(ferqMapTupleArray, key=lambda x: x[1],  reverse=True)
+        # return map(lambda x:x[0], sortedTupleArray[0:k])
+
+        # Bucket sort approach O(n)
         def frequencyIndexMap(arr):
             map = {}
             for i in range(len(arr)):
                 map[arr[i]] = map.get(arr[i],0)+1
             return map
-        
         freqMap = frequencyIndexMap(nums)
-        ferqMapTupleArray = tuple(freqMap.items())
-        sortedTupleArray = sorted(ferqMapTupleArray, key=lambda x: x[1],  reverse=True)
-        return map(lambda x:x[0], sortedTupleArray[0:k])
+
+        buckets = [[] for _ in range(len(nums)+1)]
+        for num, freq in freqMap.items():
+            buckets[freq].append(num)
+        
+        res = []
+        for i in range(len(buckets)-1, -1, -1):
+            for num in buckets[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+            
+        
+        
+
+        
         
