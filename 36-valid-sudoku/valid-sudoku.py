69002d1940b13414ee983219c0ad3ce855a3b039
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-27 14:05:36 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (16.49%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,47 +4,66 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
-        # Came up with this solution without any external help!
-        # Used a set instead of a count array
-        # Check 3 X 3 Matrices
-        for i in range(9):
-            row_lower = 3 * (i // 3)
-            row_upper = row_lower + 3
-            col_lower = (i % 3) * 3
-            col_upper = col_lower + 3
-            num_set = set()
-            for row in range(row_lower, row_upper):
-                for col in range(col_lower, col_upper):
-                    num = board[row][col]
-                    if num == '.':
-                        continue
-                    if num in num_set:
-                        return False
-                    num_set.add(num)
-        # Check rows:
-        for i in range(9):
-            row = i
-            num_set = set()
+        # # Came up with this solution without any external help!
+        # # Used a set instead of a count array
+        # # Check 3 X 3 Matrices
+        # for i in range(9):
+        #     row_lower = 3 * (i // 3)
+        #     row_upper = row_lower + 3
+        #     col_lower = (i % 3) * 3
+        #     col_upper = col_lower + 3
+        #     num_set = set()
+        #     for row in range(row_lower, row_upper):
+        #         for col in range(col_lower, col_upper):
+        #             num = board[row][col]
+        #             if num == '.':
+        #                 continue
+        #             if num in num_set:
+        #                 return False
+        #             num_set.add(num)
+        # # Check rows:
+        # for i in range(9):
+        #     row = i
+        #     num_set = set()
+        #     for col in range(9):
+        #         num = board[row][col]
+        #         if num == '.':
+        #             continue
+        #         if num in num_set:
+        #             return False
+        #         num_set.add(num) 
+
+        # # Check columns:
+        # for i in range(9):
+        #     col = i
+        #     num_set = set()
+        #     for row in range(9):
+        #         num = board[row][col]
+        #         if num == '.':
+        #             continue
+        #         if num in num_set:
+        #             return False
+        #         num_set.add(num) 
+
+        # return True
+
+        # Optimized solution
+        row_set = [set() for _ in range(9)]
+        col_set = [set() for _ in range(9)]
+        box_set = [set() for _ in range(9)]
+
+        for row in range(9):
             for col in range(9):
                 num = board[row][col]
                 if num == '.':
                     continue
-                if num in num_set:
+                box_index = (row // 3) * 3 + (col // 3)
+                if num in row_set[row] or num in col_set[col] or num in box_set[box_index]:
                     return False
-                num_set.add(num) 
-
-        # Check columns:
-        for i in range(9):
-            col = i
-            num_set = set()
-            for row in range(9):
-                num = board[row][col]
-                if num == '.':
-                    continue
-                if num in num_set:
-                    return False
-                num_set.add(num) 
-
+                row_set[row].add(num)
+                col_set[col].add(num)
+                box_set[box_index].add(num)
+        
         return True
 
 
