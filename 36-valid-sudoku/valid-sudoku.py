cb24b127e20dcb67faaf697ffd21b4393aa87036
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-27 14:01:08 -0500

    Time: 14 ms (23.66%) | Memory: 12.5 MB (55.01%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -4,41 +4,46 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
+        # Came up with this solution without any external help!
+        # Used a set instead of a count array
         # Check 3 X 3 Matrices
         for i in range(9):
             row_lower = 3 * (i // 3)
             row_upper = row_lower + 3
             col_lower = (i % 3) * 3
             col_upper = col_lower + 3
-            count_array = [0] * 10
+            num_set = set()
             for row in range(row_lower, row_upper):
                 for col in range(col_lower, col_upper):
-                    if board[row][col] == '.':
+                    num = board[row][col]
+                    if num == '.':
                         continue
-                    count_array[int(board[row][col])] += 1
-            if max(count_array) > 1:
-                return False
+                    if num in num_set:
+                        return False
+                    num_set.add(num)
         # Check rows:
         for i in range(9):
             row = i
-            count_array = [0] * 10
+            num_set = set()
             for col in range(9):
-                if board[row][col] == '.':
+                num = board[row][col]
+                if num == '.':
                     continue
-                count_array[int(board[row][col])] += 1
-            if max(count_array) > 1:
-                return False
+                if num in num_set:
+                    return False
+                num_set.add(num) 
 
         # Check columns:
         for i in range(9):
             col = i
-            count_array = [0] * 10
+            num_set = set()
             for row in range(9):
-                if board[row][col] == '.':
+                num = board[row][col]
+                if num == '.':
                     continue
-                count_array[int(board[row][col])] += 1
-            if max(count_array) > 1:
-                return False
+                if num in num_set:
+                    return False
+                num_set.add(num) 
 
         return True
 
