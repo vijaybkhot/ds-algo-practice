ab511bf3bdace369b3ca0f352f4588cdc176eb2b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-02-28 09:50:34 -0500

    Time: 1 ms (58.93%) | Memory: 13.6 MB (45.34%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -4,39 +4,48 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if len(prices) == 1:
-            return 0
+        # # First attempt - Efficient by unreadable
+        # if len(prices) == 1:
+        #     return 0
 
-        if len(prices) == 2:
-            if (prices[1] - prices[0]) > 0:
-                return prices[1] - prices[0]
-            else:
-                return 0
+        # if len(prices) == 2:
+        #     if (prices[1] - prices[0]) > 0:
+        #         return prices[1] - prices[0]
+        #     else:
+        #         return 0
 
-        prices.append(-1)
+        # prices.append(-1)
 
+        # profit = 0
+        # curr_profit = 0
+        # curr_stock = 0
+
+        # for i in range(len(prices) - 1):
+        #     if i == len(prices) - 2 and curr_stock > 0 and curr_profit > 0:
+        #         if (prices[i + 1] - prices[i] + curr_profit) > curr_profit:
+        #             curr_profit += prices[i + 1] - prices[i]
+        #             profit += curr_profit
+        #             curr_stock, curr_profit = 0, 0
+
+        #     if (prices[i + 1] - prices[i] + curr_profit) > curr_profit:
+        #         if curr_stock == 0:
+        #             curr_stock = prices[i]
+        #         curr_profit += prices[i + 1] - prices[i]
+        #     else:
+        #         if curr_profit > 0:
+        #             curr_stock = 0
+        #             profit += curr_profit
+        #             curr_profit = 0
+        #         else:
+        #             continue
+        # return profit
+
+        # More readable solution
         profit = 0
-        curr_profit = 0
-        curr_stock = 0
-
-        for i in range(len(prices) - 1):
-            if i == len(prices) - 2 and curr_stock > 0 and curr_profit > 0:
-                if (prices[i + 1] - prices[i] + curr_profit) > curr_profit:
-                    curr_profit += prices[i + 1] - prices[i]
-                    profit += curr_profit
-                    curr_stock, curr_profit = 0, 0
-
-            if (prices[i + 1] - prices[i] + curr_profit) > curr_profit:
-                if curr_stock == 0:
-                    curr_stock = prices[i]
-                curr_profit += prices[i + 1] - prices[i]
-            else:
-                if curr_profit > 0:
-                    curr_stock = 0
-                    profit += curr_profit
-                    curr_profit = 0
-                else:
-                    continue
+        for i in range(1, len(prices)):
+            if prices[i] - prices[i-1] > 0:
+                profit += prices[i] - prices[i-1]
+        
         return profit
 
 
