087b94ac317b134998295cdd606af8865998f7dd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-02 16:43:35 -0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (13.68%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,23 +7,43 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        nums3 = nums1[:]
-        i, j, k = 0, 0, 0
+        # # Solution using an extra array: O(n) time and space complexity
+        # nums3 = nums1[:]
+        # i, j, k = 0, 0, 0
 
-        while i < m and j < n:
-            if nums3[i] < nums2[j]:
-                nums1[k] = nums3[i]
-                k += 1
-                i += 1
+        # while i < m and j < n:
+        #     if nums3[i] < nums2[j]:
+        #         nums1[k] = nums3[i]
+        #         k += 1
+        #         i += 1
+        #     else:
+        #         nums1[k] = nums2[j]
+        #         k += 1
+        #         j += 1
+        # while i < m:
+        #     nums1[k] = nums3[i]
+        #     k += 1
+        #     i += 1
+        # while j < n:
+        #     nums1[k] = nums2[j]
+        #     k += 1
+        #     j += 1
+
+        # In-place merging without an extra array - O(n) time and O(1) space complexity
+
+        last = m + n -1
+
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[last] = nums1[m-1]
+                m -= 1
             else:
-                nums1[k] = nums2[j]
-                k += 1
-                j += 1
-        while i < m:
-            nums1[k] = nums3[i]
-            k += 1
-            i += 1
-        while j < n:
-            nums1[k] = nums2[j]
-            k += 1
-            j += 1
+                nums1[last] = nums2[n-1]
+                n -= 1
+            last -= 1
+        
+        while n > 0:
+            nums1[last] = nums2[n-1]
+            n -= 1
+            last -= 1
+
