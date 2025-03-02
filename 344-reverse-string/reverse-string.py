cdf062876cbae5f5ea379bc6d8e930b9cb3f884c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-02 08:23:40 -0500

    Time: 0 ms (100.00%) | Memory: 19.9 MB (35.19%) - LeetSync

---
+++ 344-reverse-string/reverse-string.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        left_p = 0
+        right_p = len(s)-1
+        while right_p > left_p:
+            s[left_p], s[right_p] = s[right_p], s[left_p]
+            left_p += 1
+            right_p -= 1
+        
+        return s
+        
