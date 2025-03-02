c56d1c55644b9e9adc98bcf1e2d1dbfffde81e8b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-02 08:37:56 -0500

    Time: 0 ms (100.00%) | Memory: 20.2 MB (19.73%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -4,12 +4,15 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        left_p = 0
-        right_p = len(s)-1
-        while right_p > left_p:
-            s[left_p], s[right_p] = s[right_p], s[left_p]
-            left_p += 1
-            right_p -= 1
+        # left_p = 0
+        # right_p = len(s)-1
+        # while right_p > left_p:
+        #     s[left_p], s[right_p] = s[right_p], s[left_p]
+        #     left_p += 1
+        #     right_p -= 1
         
-        return s
+        # return s
+
+        # Simpler python syntax to reverse in place
+        s[:] = s[::-1]
         
