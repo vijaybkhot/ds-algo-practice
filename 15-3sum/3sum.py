a68256f8c66dff1876fdaf3b86612039534b2a8e
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-03 09:55:46 -0500

    Time: 727 ms (63.07%) | Memory: 18.5 MB (41.42%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,26 +4,55 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+
+        # nums.sort()
+        # n = len(nums)
+        # res = []
+        # for i in range(n):
+        #     curr_num = nums[i]
+        #     curr_target = -curr_num
+        #     left, right = i + 1, n-1
+        #     curr_triplet = []
+        #     while left < right:
+        #         if nums[left] + nums[right] == curr_target:
+        #             curr_triplet = [nums[i], nums[left], nums[right]]
+        #             if curr_triplet in res:
+        #                 curr_triplet = []
+        #             if len(curr_triplet) == 3:
+        #                 res.append(curr_triplet)
+        #             left += 1
+        #             right -= 1 
+        #         elif nums[left] + nums[right] < curr_target:
+        #             left += 1
+        #         else:
+        #             right -= 1
+        
+        # return res
+
+        # Optimize above appraoch
         nums.sort()
         n = len(nums)
         res = []
+        triplet_first = set()
         for i in range(n):
             curr_num = nums[i]
+            if curr_num in triplet_first:
+                continue
             curr_target = -curr_num
             left, right = i + 1, n-1
             curr_triplet = []
             while left < right:
                 if nums[left] + nums[right] == curr_target:
                     curr_triplet = [nums[i], nums[left], nums[right]]
-                    if curr_triplet in res:
-                        curr_triplet = []
                     if len(curr_triplet) == 3:
                         res.append(curr_triplet)
                     left += 1
-                    right -= 1 
+                    while nums[left] == nums[left-1] and left < right:
+                        left += 1
                 elif nums[left] + nums[right] < curr_target:
                     left += 1
                 else:
                     right -= 1
+            triplet_first.add(curr_num)
         
         return res
