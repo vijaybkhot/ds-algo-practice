d6044a7d97ad055fb904deb1d207d659495b10bb
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-03 09:58:15 -0500

    Time: 725 ms (63.89%) | Memory: 18.4 MB (41.42%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -29,14 +29,41 @@ class Solution(object):
         
         # return res
 
-        # Optimize above appraoch
+        # # Optimize above approach. Used a set to store first elem in each triplets, to avoid duplicates
+        # nums.sort()
+        # n = len(nums)
+        # res = []
+        # triplet_first = set()
+        # for i in range(n):
+        #     curr_num = nums[i]
+        #     if curr_num in triplet_first:
+        #         continue
+        #     curr_target = -curr_num
+        #     left, right = i + 1, n-1
+        #     curr_triplet = []
+        #     while left < right:
+        #         if nums[left] + nums[right] == curr_target:
+        #             curr_triplet = [nums[i], nums[left], nums[right]]
+        #             if len(curr_triplet) == 3:
+        #                 res.append(curr_triplet)
+        #             left += 1
+        #             while nums[left] == nums[left-1] and left < right:
+        #                 left += 1
+        #         elif nums[left] + nums[right] < curr_target:
+        #             left += 1
+        #         else:
+        #             right -= 1
+        #     triplet_first.add(curr_num)
+        
+        # return res
+
+         # Optimize above approach. without using set
         nums.sort()
         n = len(nums)
         res = []
-        triplet_first = set()
         for i in range(n):
             curr_num = nums[i]
-            if curr_num in triplet_first:
+            if i > 0 and curr_num == nums[i-1]:
                 continue
             curr_target = -curr_num
             left, right = i + 1, n-1
@@ -52,7 +79,7 @@ class Solution(object):
                 elif nums[left] + nums[right] < curr_target:
                     left += 1
                 else:
-                    right -= 1
-            triplet_first.add(curr_num)
-        
+                    right -= 1      
+                      
         return res
+
