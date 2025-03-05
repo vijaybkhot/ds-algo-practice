9694d6a8e600fde23bda1bfa1318e745af7de029
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-05 10:54:19 -0500

    Time: 83 ms (10.31%) | Memory: 16.5 MB (73.46%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -10,24 +10,23 @@ class Solution(object):
         left, right = 0, len(people)-1
 
         while left <= right:
-            if left < right:
-                if people[left] + people[right] <= limit:
-                    left += 1
-                    right -= 1
-                    total_boats += 1
-                    continue
-                elif people[right] <= limit:
-                    right -= 1
-                    total_boats += 1
-                else:
-                    left += 1
-                    total_boats += 1
-            elif left == right:
+            if left == right:
                 if people[left] <= limit:
                     total_boats += 1
                     break
 
-        
+            if people[left] + people[right] <= limit:
+                left += 1
+                right -= 1
+                total_boats += 1
+                continue
+            elif people[right] <= limit:
+                right -= 1
+                total_boats += 1
+            else:
+                left += 1
+                total_boats += 1
+           
         return total_boats
                     
                 
