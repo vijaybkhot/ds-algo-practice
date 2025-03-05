772a7c1e5e2c2bedce42f39f81061753d9a3b6ad
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-05 10:57:55 -0500

    Time: 68 ms (80.04%) | Memory: 16.6 MB (43.64%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.py
+++ 917-boats-to-save-people/boats-to-save-people.py
@@ -5,44 +5,29 @@ class Solution(object):
         :type limit: int
         :rtype: int
         """
-        # # Solution without external help
-        # total_boats = 0
-        # people.sort()
-        # left, right = 0, len(people)-1
-
-        # while left <= right:
-        #     if left == right:
-        #         if people[left] <= limit:
-        #             total_boats += 1
-        #             break
-
-        #     if people[left] + people[right] <= limit:
-        #         left += 1
-        #         right -= 1
-        #         total_boats += 1
-        #         continue
-        #     elif people[right] <= limit:
-        #         right -= 1
-        #         total_boats += 1
-        #     else:
-        #         left += 1
-        #         total_boats += 1
-           
-        # return total_boats
-
-        # Refined solution:
+        # Solution without external help
         total_boats = 0
         people.sort()
-        left, right = 0, len(people) - 1
+        left, right = 0, len(people)-1
 
         while left <= right:
-            
+            if left == right:
+                if people[left] <= limit:
+                    total_boats += 1
+                    break
+
             if people[left] + people[right] <= limit:
-                left += 1  
-
-            right -= 1  
-            total_boats += 1  
-
+                left += 1
+                right -= 1
+                total_boats += 1
+                continue
+            elif people[right] <= limit:
+                right -= 1
+                total_boats += 1
+            else:
+                left += 1
+                total_boats += 1
+           
         return total_boats
                     
                 
