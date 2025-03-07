5a98e675bf0c5e38a032b22165bbc1b4725d8bcb
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-07 14:37:08 -0500

    Time: 143 ms (57.60%) | Memory: 24.5 MB (99.71%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -57,36 +57,28 @@ class Solution(object):
         max_sum = 0
 
         freq_map = {}
-        duplicate_counter = 0
         # Initialize first window
         curr_sum = 0
         for i in range(k):
             freq_map[nums[i]] = freq_map.get(nums[i], 0) + 1
             curr_sum += nums[i]
-            if freq_map[nums[i]] == 2:
-                duplicate_counter += 1
         
-        if duplicate_counter == 0:
+        if len(freq_map) == k:
             max_sum = curr_sum
         
         left = 0
         for right in range(k, len(nums)):
             # Remove left
-            left_num = nums[left]
             freq_map[nums[left]] = freq_map.get(nums[left]) - 1
-            curr_sum -= left_num
-            if freq_map[nums[left]] == 1:
-                duplicate_counter -= 1
+            curr_sum -= nums[left]
             if freq_map[nums[left]] == 0:
                 del freq_map[nums[left]]
             
             # Add right
             freq_map[nums[right]] = freq_map.get(nums[right], 0) + 1
             curr_sum += nums[right]
-            if freq_map[nums[right]] == 2:
-                duplicate_counter += 1
             
-            if duplicate_counter == 0:
+            if len(freq_map) == k:
                 max_sum = max(max_sum, curr_sum)
             
             left += 1
