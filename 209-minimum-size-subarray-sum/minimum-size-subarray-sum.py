3804f1f4c4c877232ca82281a7321e3f8f1e849e
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-07 20:11:24 -0500

    Time: 16 ms (36.87%) | Memory: 20.1 MB (55.97%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        minimal_length = len(nums) + 1
+        left = 0
+        curr_sum = 0
+        for right in range(len(nums)):
+            # Include right in widow
+            curr_sum += nums[right]
+
+            while True:
+                if (curr_sum - nums[left]) >= target:
+                    curr_sum -= nums[left]
+                    left += 1
+                else:
+                    break
+            
+            if curr_sum >= target:
+                minimal_length = min(minimal_length, (right-left+1))
+        
+        if minimal_length <= len(nums):
+            return minimal_length
+        else:
+            return 0
+
+        
