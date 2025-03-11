7d7d3732b99958a0c91405c2b9cfc306c1532a01
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-11 12:56:56 -0400

    Time: 10 ms (53.62%) | Memory: 13.3 MB (35.25%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -4,68 +4,51 @@ class Solution(object):
         :type asteroids: List[int]
         :rtype: List[int]
         """
+        # # Working solution without external help
         # stack = []
         
         # for i in range(len(asteroids)):
         #     if not stack:
         #         stack.append(asteroids[i])
         #         continue
-            
         #     num2 = asteroids[i]
         #     if (stack[-1] > 0 and num2 < 0):
         #         num1 = stack.pop()
-        #         if abs(num1) > abs(num2):
-        #             while True:
-        #             stack.append(num1)
-        #         elif abs(num1) < abs(num2):
-        #             while True:
-        #                 num1 = stack.pop()
-        #                 if abs(num1) > abs(num2):
-        #                     stack.append(num1)
-        #                     break
-        #                 elif abs(num1) < abs(num2):
+        #         while True:
+        #             if abs(num1) > abs(num2):
+        #                 stack.append(num1)
+        #                 break
+        #             elif abs(num1) < abs(num2):
+        #                 if stack and stack[-1] > 0:
+        #                     num1 = stack.pop()
         #                     continue
         #                 else:
-        #                     if num1 == num2:
-        #                         stack.append(num1)
-        #                         stack.append(num2)
+        #                     stack.append(num2)
         #                     break
-        #         else:
-        #             continue
+        #             else:
+        #                 if num1 == num2:
+        #                     stack.append(num1)
+        #                     stack.append(num2)
+        #                 break
         #     else:
         #         stack.append(num2)
         
         # return stack
 
+        # More readabale solution
         stack = []
-        
-        for i in range(len(asteroids)):
-            if not stack:
-                stack.append(asteroids[i])
-                continue
-            
-            num2 = asteroids[i]
-            if (stack[-1] > 0 and num2 < 0):
-                num1 = stack.pop()
-                while True:
-                    if abs(num1) > abs(num2):
-                        stack.append(num1)
-                        break
-                    elif abs(num1) < abs(num2):
-                        if stack and stack[-1] > 0:
-                            num1 = stack.pop()
-                            continue
-                        else:
-                            stack.append(num2)
-                            break
-                    else:
-                        if num1 == num2:
-                            stack.append(num1)
-                            stack.append(num2)
-                        break
-            else:
-                stack.append(num2)
-        
+        for a in asteroids:
+            while stack and stack[-1] > 0 and a < 0:
+                diff = a + stack[-1]
+                if diff > 0:
+                    a = 0
+                elif diff < 0:
+                    stack.pop()
+                else:
+                    a = 0
+                    stack.pop()
+            if a:
+                stack.append(a)
         return stack
 
             
