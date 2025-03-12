a12335e9af14715e5a3c93b59f1926d039800f3c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-11 21:45:03 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (84.03%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -5,17 +5,36 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        res = [-1] * len(nums2)
+        # Solution without external help
+        # Using monotonically decreasing stack approach
+        # res = [-1] * len(nums2)
+        # stack = []
+        # for index, num in enumerate(nums2):
+        #     while stack and stack[-1][0] < num:
+        #         stack_num, stack_idx = stack.pop()
+        #         res[stack_idx] = num
+        #     stack.append([num, index])
+        # output = []
+        # for num in nums1:
+        #     idx = nums2.index(num)
+        #     output.append(res[idx])
+        # return output
+
+        # More optimal solution.# Instead of using the index() method O(n), to get index of an element in array, We can create a index map to lookup index in O(1)
+        index_map = {}
+        for index, num in enumerate(nums2):
+            index_map[num] = index
+        
+        next_greater = [-1] * len(nums2)
         stack = []
         for index, num in enumerate(nums2):
             while stack and stack[-1][0] < num:
                 stack_num, stack_idx = stack.pop()
-                res[stack_idx] = num
+                next_greater[stack_idx] = num
             stack.append([num, index])
         output = []
         for num in nums1:
-            idx = nums2.index(num)
-            output.append(res[idx])
+            output.append(next_greater[index_map[num]])
         return output
 
         
