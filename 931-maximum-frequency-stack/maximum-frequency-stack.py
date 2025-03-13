68ede77009d49ce09b11020fa0b87ca552912cc7
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-13 11:11:20 -0400

    Time: 182 ms (22.73%) | Memory: 20.9 MB (96.21%) - LeetSync

--- 931-maximum-frequency-stack/maximum-frequency-stack.py
+++ 931-maximum-frequency-stack/maximum-frequency-stack.py
@@ -13,7 +13,6 @@ class FreqStack(object):
         """
         self.freq[val] = self.freq.get(val, 0) + 1
         curr_freq = self.freq[val]
-        prev_freq = curr_freq - 1
         self.group[curr_freq] = self.group.get(curr_freq, [])
         self.group[curr_freq].append(val)
         self.max_freq = max(self.max_freq, self.freq[val])
@@ -22,23 +21,6 @@ class FreqStack(object):
         """
         :rtype: int
         """
-        # # O(n) pop method
-        # i = len(self.stack)-1
-        # while i >= 0:
-        #     if self.freq[self.stack[i]] == self.max_freq:
-        #         most_freq = self.stack.pop(i)
-        #         self.freq[most_freq] = self.freq.get(most_freq) - 1
-        #         if self.freq[most_freq] == 0:
-        #             del self.freq[most_freq]
-        #         if self.freq:
-        #             self.max_freq = max(self.freq.values())
-        #         else:
-        #             self.max_freq = 0
-        #         return most_freq
-        #     i -= 1
-
-        # More optimal O(1) pop method
-
         if self.group[self.max_freq]:
             most_freq = self.group[self.max_freq].pop()
             if not self.group[self.max_freq]:
@@ -52,10 +34,4 @@ class FreqStack(object):
             
             return most_freq
 
-        
-
-
-# Your FreqStack object will be instantiated and called as such:
-# obj = FreqStack()
-# obj.push(val)
-# param_2 = obj.pop()
+    
