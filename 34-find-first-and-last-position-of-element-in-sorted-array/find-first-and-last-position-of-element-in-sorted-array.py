447925e874f8ac06cc5ff3fddd809237d66bdb0b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-15 10:40:20 -0400

    Time: 0 ms (100.00%) | Memory: 13.3 MB (29.98%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,24 +5,55 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # O(n) solution
-        result = []
-        for i in range(len(nums)):
-            if nums[i] == target and len(result)== 0:
-                result.append(i)
-            elif nums[i] == target and len(result)>= 1:
-                if(len(result)==1):
-                    result.append(i)
-                else:
-                    result[1] = i
+        # # O(n) solution
+        # result = []
+        # for i in range(len(nums)):
+        #     if nums[i] == target and len(result)== 0:
+        #         result.append(i)
+        #     elif nums[i] == target and len(result)>= 1:
+        #         if(len(result)==1):
+        #             result.append(i)
+        #         else:
+        #             result[1] = i
         
-        if len(result)== 1:
-            if nums[-1] == target:
-                result.append(len(nums)-1)
+        # if len(result)== 1:
+        #     if nums[-1] == target:
+        #         result.append(len(nums)-1)
+        #     else:
+        #         result.append(result[0])
+        # if len(result)==0:
+        #     result = [-1, -1]
+        
+        # return result
+
+        # O(log n) solution
+
+        low, high = 0, len(nums)-1
+        target_index = -1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] < target:
+                low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
             else:
-                result.append(result[0])
-        if len(result)==0:
-            result = [-1, -1]
+                target_index = mid
+                break
         
-        return result
+        if target_index == -1:
+            return [-1, -1]
+        
+        low_index, high_index = target_index, target_index
+        output = [low_index, high_index]
+        while low_index > -1 and nums[low_index] == target:
+            output[0] = low_index
+            low_index -= 1
+        
+        while high_index < len(nums) and nums[high_index] == target:
+            output[1] = high_index
+            high_index += 1
+        
+        return output
+        
+
 
