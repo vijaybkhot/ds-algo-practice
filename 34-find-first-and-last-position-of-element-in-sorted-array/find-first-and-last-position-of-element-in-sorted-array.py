cd9754cf4ba97456500cd76db943d172847011e4
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-15 10:51:20 -0400

    Time: 0 ms (100.00%) | Memory: 13.2 MB (29.98%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -26,34 +26,66 @@ class Solution(object):
         
         # return result
 
-        # O(log n) solution
+        # # O(log n) + O(n) solution
+        # low, high = 0, len(nums)-1
+        # target_index = -1
+        # while low <= high:
+        #     mid = (low + high) // 2
+        #     if nums[mid] < target:
+        #         low = mid + 1
+        #     elif nums[mid] > target:
+        #         high = mid - 1
+        #     else:
+        #         target_index = mid
+        #         break
+        
+        # if target_index == -1:
+        #     return [-1, -1]
+        
+        # low_index, high_index = target_index, target_index
+        # output = [low_index, high_index]
+        # while low_index > -1 and nums[low_index] == target:
+        #     output[0] = low_index
+        #     low_index -= 1
+        
+        # while high_index < len(nums) and nums[high_index] == target:
+        #     output[1] = high_index
+        #     high_index += 1
+        
+        # return output
+        
+        # Optimized O(log n) solution
 
-        low, high = 0, len(nums)-1
-        target_index = -1
-        while low <= high:
-            mid = (low + high) // 2
-            if nums[mid] < target:
-                low = mid + 1
-            elif nums[mid] > target:
-                high = mid - 1
-            else:
-                target_index = mid
-                break
+        def findFirstOccurrence(nums, target):
+            low, high = 0, len(nums)-1
+            first = -1
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] >= target:
+                    if nums[mid] == target:
+                        first = mid
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return first
         
-        if target_index == -1:
-            return [-1, -1]
+        def findLastOccurrence(nums, target):
+            low, high = 0, len(nums)-1
+            last = -1
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] <= target:
+                    if nums[mid] == target:
+                        last = mid
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return last
         
-        low_index, high_index = target_index, target_index
-        output = [low_index, high_index]
-        while low_index > -1 and nums[low_index] == target:
-            output[0] = low_index
-            low_index -= 1
-        
-        while high_index < len(nums) and nums[high_index] == target:
-            output[1] = high_index
-            high_index += 1
-        
-        return output
-        
+        first = findFirstOccurrence(nums, target)
+        last = findLastOccurrence(nums, target)
+        return [first, last]
+
+
 
 
