fe601e5f888788a3fa4b6bf03372e8840d09a129
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-17 10:07:50 -0400

    Time: 2 ms (19.25%) | Memory: 18.2 MB (6.86%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -9,14 +9,25 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        # Iterative solution
-        prev, curr = None, head
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-        return prev
+        # # Iterative solution
+        # prev, curr = None, head
+        # while curr:
+        #     nxt = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = nxt
+        # return prev
 
-
+        # Recursive solution
+        # Base case: if head is None or only one node, return head
+        if not head or not head.next:
+            return head
         
+        # Recursive call on the rest of the list
+        new_head = self.reverseList(head.next)
+        
+        # Reverse the current node's pointer
+        head.next.next = head
+        head.next = None
+        
+        return new_head
