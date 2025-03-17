dd9c435f319b28654b58c31b32580cefe4db46e6
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-17 11:54:35 -0400

    Time: 36 ms (64.06%) | Memory: 19.7 MB (27.16%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -10,15 +10,25 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        # Using a set to identify unique nodes
-        node_set = set()
-        curr = head
-        while curr:
-            if curr in node_set:
-                return True
-            else:
-                node_set.add(curr)
-                curr = curr.next
+        # # Using a set to identify unique nodes. O(n) time and O(n) space complexity
+        # node_set = set()
+        # curr = head
+        # while curr:
+        #     if curr in node_set:
+        #         return True
+        #     else:
+        #         node_set.add(curr)
+        #         curr = curr.next
         
-        return False
+        # return False
+
+        # Floyd’s Cycle Detection Algorithm (Tortoise and Hare). Fast and slow pointer
+        tortoise, hare = head, head
+        while True:
+            tortoise = tortoise.next if tortoise else None
+            hare = hare.next.next if hare and hare.next else None
+            if not hare or not tortoise:
+                return False
+            if tortoise == hare:
+                return True
         
