98a8eeb16c8c7f9e0b6e4d01c7588ff78f9f9f49
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-19 10:08:19 -0400

    Time: 0 ms (100.00%) | Memory: 12.8 MB (42.42%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -11,37 +11,65 @@ class Solution(object):
         :type right: int
         :rtype: Optional[ListNode]
         """
+        # # Multiple pass approach
+        # if left == right:
+        #     return head
+        # prev_count, left_node = 0, head
+        # for _ in range(left-1):
+        #     prev_count += 1
+        #     left_node = left_node.next
+
+        # remainder, right_node = None, left_node
+        # for _ in range(right-left):
+        #     right_node = right_node.next
+        #     remainder = right_node.next
+        
+        # prev, curr = None, left_node
+        # for _ in range(right - left +1):
+        #     tmp = curr.next
+        #     curr.next = prev
+        #     if _ == 0:
+        #         curr.next = remainder
+        #     prev = curr
+        #     curr = tmp
+        
+        # dummy = ListNode()
+        # tail = dummy
+        # tail.next = head
+        
+        # for _ in range(prev_count):
+        #     tail = tail.next
+        
+        # tail.next = None
+        # tail.next = prev
+        # return dummy.next
+
+        # One pass approach
         if left == right:
             return head
-        prev_count, left_node = 0, head
-        for _ in range(left-1):
-            prev_count += 1
-            left_node = left_node.next
-
-        remainder, right_node = None, left_node
-        for _ in range(right-left):
-            right_node = right_node.next
-            remainder = right_node.next
-        
-        prev, curr = None, left_node
-        for _ in range(right - left +1):
-            tmp = curr.next
-            curr.next = prev
-            if _ == 0:
-                curr.next = remainder
-            prev = curr
-            curr = tmp
         
         dummy = ListNode()
         tail = dummy
+
         tail.next = head
-        
-        for _ in range(prev_count):
+        left_node = head        
+        for _ in range(left-1):
             tail = tail.next
+            left_node = left_node.next
         
-        tail.next = None
+        right_node, prev, tmp = left_node, None, None
+        for _ in range(right - left + 1):
+            tmp = right_node.next
+            right_node.next = prev
+            prev = right_node
+            right_node = tmp
+        left_node.next = tmp
         tail.next = prev
         return dummy.next
+            
+            
+
+
 
 
         
