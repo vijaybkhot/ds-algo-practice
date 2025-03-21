e3f42c0a58af78460f086e5a19810dad61c7be4f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-21 10:33:08 -0400

    Time: 71 ms (39.20%) | Memory: 20.9 MB (43.42%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.py
+++ 41-first-missing-positive/first-missing-positive.py
@@ -27,31 +27,49 @@ class Solution(object):
         
         # return len(nums_set) + 1
 
-        # Optimal O(n) time and O(1) space complexity solution
-        # Replace negative numbers with 0
+        # # Optimal O(n) time and O(1) space complexity solution
+        # # Replace negative numbers with 0
+        # n = len(nums)
+        # for i in range(n):
+        #     if nums[i] < 0:
+        #         nums[i] = 0
+        
+        # # Loop over each number, and mark the index of that number negative. i.e. if 1 is present, make the number at index 0 negative
+        # # If a number is out of bounds of the array, skip it
+        # for i in range(n):      
+        #     num = abs(nums[i])
+        #     if 1 <= num <= n:
+        #         # Mark the index corresponding to the number as negative
+        #         if nums[num-1] > 0:
+        #             nums[num-1] = - nums[num-1]
+        #         elif nums[num-1] == 0:
+        #             nums[num-1] = -1 * (n+1)
+        
+        # # Now loop over the transformed array and the first index whose vlaue is positive is the answer
+        # for i in range(1, n+1):
+        #     if nums[i-1] >= 0:
+        #         return i
+        # return n + 1
+        
         n = len(nums)
         for i in range(n):
-            if nums[i] < 0:
-                nums[i] = 0
+            if nums[i] <= 0:
+                nums[i] = (n+2)
         
-        # Loop over each number, and mark the index of that number negative. i.e. if 1 is present, make the number at index 0 negative
-        # If a number is out of bounds of the array, skip it
-        for i in range(n):      
+        for i in range(n):
             num = abs(nums[i])
             if 1 <= num <= n:
-                # Mark the index corresponding to the number as negative
-                if nums[num-1] > 0:
-                    nums[num-1] = - nums[num-1]
-                elif nums[num-1] == 0:
-                    nums[num-1] = -1 * (n+1)
+                index = num-1
+                if nums[index] > 0:
+                    nums[index] = -nums[index]
         
-        # Now loop over the transformed array and the first index whose vlaue is positive is the answer
         for i in range(1, n+1):
-            if nums[i-1] >= 0:
+            index = i-1
+            if nums[index] > 0:
                 return i
         
-        return n + 1
-        
+        return n+1
+
 
 
                 
