db835cc60075e295a0448cdd50d4de09efb7e1f5
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-24 09:07:31 -0400

    Time: 156 ms (84.00%) | Memory: 48 MB (16.00%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def countDays(self, days, meetings):
+        """
+        :type days: int
+        :type meetings: List[List[int]]
+        :rtype: int
+        """
+        # Sort the meetings array based on the start day
+        meetings.sort(key=lambda x: x[0])
+        curr_day = 1
+        res = 0
+    
+        for start, end in meetings:
+            if start > curr_day:
+                res += start - curr_day
+            elif start < curr_day and end < curr_day:
+                continue
+            curr_day = end + 1
+
+        if curr_day < days+1:
+            res += days+1 - curr_day
+        return res
+            
+        
