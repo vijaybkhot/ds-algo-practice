0a87f80e0ab30257e026a1d378dbf33e881d07db
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-26 08:00:36 -0400

    Time: 161 ms (100.00%) | Memory: 32.6 MB (40.00%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.py
@@ -25,28 +25,12 @@ class Solution(object):
                 operations += abs(target - num) // x
             return operations
         
-        # arr.sort()
-        # left, right = arr[0], arr[-1]
-        # res = float('infinity')
-
-        # while left <= right:
-        #     mid = (left + right) // 2
-        #     ops = num_operations(mid)
-        #     if ops < res:
-        #         res = ops
-        #         right = mid - 1
-        #     else:
-        #         left = mid + 1
-
-        
         arr.sort()
         median = arr[len(arr) // 2]
         res = num_operations(median)
         
         return res
-        
-        return res
-
+    
                 
             
         
