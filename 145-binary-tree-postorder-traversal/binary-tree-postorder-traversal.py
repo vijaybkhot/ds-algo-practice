26576221337b20bc33b4f8640efa1349d90049e6
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-26 05:35:23 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (55.77%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -10,9 +10,27 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[int]
         """
-        return self.postorder(root, [])
+        # # Recursive solution
+        # return self.postorder(root, [])
+        
+        # Iterative solution
+        if not root:
+            return []
+        res = []
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+            
+        return res[::-1]
     
 
+    # Recursive function
     def postorder(self, root, res):
         if root:
             self.postorder(root.left, res)
