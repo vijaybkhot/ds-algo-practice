b4c92f557f5fb745d34c4ac90bb6a9cf7ccc03c8
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-26 05:39:16 -0400

    Time: 0 ms (100.00%) | Memory: 12.5 MB (55.77%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -14,18 +14,24 @@ class Solution(object):
         # return self.postorder(root, [])
         
         # Iterative solution
+        # [Left -> Right -> Root]
         if not root:
             return []
         res = []
         stack = [root]
 
+        # Add vals to res in the order, root -> Right -> Left
         while stack:
+            # Adding root to res
             node = stack.pop()
             res.append(node.val)
+            # Adding Left then Right to stack. So when we pop, the vals to res will be added in order Right -> Left
             if node.left:
                 stack.append(node.left)
             if node.right:
                 stack.append(node.right)
+        
+        # The final res will be in Root -> Right -> Left Order, which is exactly reverse of post order
             
         return res[::-1]
     
