c9ef9b1886ed980589e720d587ab13cd4a436f9a
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-28 06:25:33 -0400

    Time: 11 ms (95.44%) | Memory: 18 MB (84.10%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,30 +6,44 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-
-        def isSameTree(p, q):
-            if (p and not q) or (q and not p):
-                return False
-            if not p and not q:
-                return True
+        # # Recursively checking with helper function isSameTree: O(N*M) time complexity
+        # def isSameTree(p, q):
+        #     if (p and not q) or (q and not p):
+        #         return False
+        #     if not p and not q:
+        #         return True
             
-            if p.val == q.val and isSameTree(p.left, q.left) and isSameTree(p.right, q.right):
-                return True
+        #     if p.val == q.val and isSameTree(p.left, q.left) and isSameTree(p.right, q.right):
+        #         return True
             
-            return False
+        #     return False
 
 
-        if (not root and subRoot):
-            return False
-            
-        if not subRoot:
-            return True
+        # if (not root and subRoot):
+        #     return False
+
+        # if not subRoot:
+        #     return True
         
-        if isSameTree(root, subRoot):
-            return True
+        # if isSameTree(root, subRoot):
+        #     return True
         
         
-        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        # return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+        # Optimized approach using serialization of Tree: O(N+M) time complexity
+
+        def serialize(root):
+            if root == None:
+                return "$#"
+
+            return ("$" + str(root.val) + serialize(root.left) + serialize(root.right))  
+
+        
+        root_str = serialize(root)
+        sub_root_str = serialize(subRoot)
+
+        return sub_root_str in root_str
         
     
         
