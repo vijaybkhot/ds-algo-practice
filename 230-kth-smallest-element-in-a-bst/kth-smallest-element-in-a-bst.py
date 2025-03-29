a9254536fb5a14e9f0230b10436cf763b77f7b7b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-29 06:37:20 -0400

    Time: 0 ms (100.00%) | Memory: 21.1 MB (27.60%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,15 +6,36 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-
-        def inorder(root, arr):
-            if root:
-                inorder(root.left, arr)
-                arr.append(root.val)
-                inorder(root.right, arr)
-            return arr
         
-        sorted_bst_arr = inorder(root, [])
+        # # Solution using inorder traversal. Uses O(n) extra space
+        # def inorder(root, arr):
+        #     if root:
+        #         inorder(root.left, arr)
+        #         arr.append(root.val)
+        #         inorder(root.right, arr)
+        #     return arr
+        
+        # sorted_bst_arr = inorder(root, [])
 
-        return sorted_bst_arr[k-1]
+        # return sorted_bst_arr[k-1]
+
+        # Optimized solution for follow up question. Using iterative inorder traversal
+
+        stack = []
+        smallest = 0
+        curr = root
+
+        while curr or stack:
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            # Process leftmost node
+            curr = stack.pop()
+            smallest += 1
+            if smallest == k:
+                return curr.val
+            curr = curr.right
+            
+            
+
         
