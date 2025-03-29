3b622f3582f83dbe83db85179b60fcc9b3108679
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-29 11:39:42 -0400

    Time: 7 ms (32.61%) | Memory: 19.6 MB (83.88%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.py
@@ -6,27 +6,48 @@
 #         self.right = right
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return None
+        # # Iterative solution
+        # if not root:
+        #     return None
         
-        curr = root
-        stack = []
-        res = float('inf')
-        prev = None
+        # curr = root
+        # stack = []
+        # res = float('inf')
+        # prev = None
 
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
+        # while curr or stack:
+        #     while curr:
+        #         stack.append(curr)
+        #         curr = curr.left
 
-            curr = stack.pop()
-            if prev is not None:
-                res = min(res, abs(curr.val - prev.val))
+        #     curr = stack.pop()
+        #     if prev is not None:
+        #         res = min(res, abs(curr.val - prev.val))
 
-            prev = curr
-            curr = curr.right
+        #     prev = curr
+        #     curr = curr.right
         
-        return res
+        # return res
+
+        # Recursive solution
+        self.prev = None
+        self.res = float('inf')
+
+        def inorder(node):
+            if not node:
+                return
+            
+            inorder(node.left)
+            
+            if self.prev is not None:
+                self.res = min(self.res, abs(node.val - self.prev))
+            
+            self.prev = node.val  # Update prev
+            
+            inorder(node.right)
+
+        inorder(root)
+        return self.res
         
             
 
