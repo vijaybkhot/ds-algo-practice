cfa86d188b3c52f6a949091d6b13b38b396d57ca
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-30 06:44:11 -0400

    Time: 3 ms (91.15%) | Memory: 18 MB (14.39%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,22 +1,19 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        charHashMap = {}    # {char: [first_index, last_index]}
+        last_index = {}    # {char: [first_index, last_index]}
         res = []
         for index, char in enumerate(s):
-            if char not in charHashMap:
-                charHashMap[char] = [index, index]
-            else:
-                charHashMap[char] = [charHashMap[char][0], index]
+            last_index[char] = index
         
-        end = charHashMap[s[0]][1]
+        end = last_index[s[0]]
         start = 0
         for index, char in enumerate(s):
             if index == end:
                 res.append(end - start + 1)
                 start = end + 1
-                end = charHashMap[s[start]][1] if start < len(s) else index
+                end = last_index[s[start]] if start < len(s) else index
             else:
-                end = max(end, charHashMap[s[index]][1])
+                end = max(end, last_index[s[index]])
             
         return res
 
