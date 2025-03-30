ec9b1b12793f2c17a2505bb00c7eb23dcb53e4bd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-03-30 13:51:13 -0400

    Time: 2 ms (48.03%) | Memory: 18.3 MB (48.44%) - LeetSync

---
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        
+        # # This method only delete the leaf nodes at first instance. Not the subsequent possible target leaf nodes
+        # def deleteNode(root, key):
+        #     if root.left is None and root.right is None and root.val == key:
+        #         return None
+        #     else:
+        #         if root.left:
+        #             root.left = deleteNode(root.left, key)
+        #         if root.right:
+        #             root.right = deleteNode(root.right, key)
+        #     return root
+        
+        # return deleteNode(root, target)
+
+        # Using post order traversal
+        def dfs(root, key):
+            if root:
+                dfs(root.left, key)
+                dfs(root.right, key)
+                if root.left and root.left.left is None and root.left.right is None and root.left.val == key:
+                    root.left = None
+                if root.right and root.right.left is None and root.right.right is None and root.right.val == key:
+                    root.right = None
+            return root
+        
+        new_tree = dfs(root, target)
+        if new_tree.left is None and new_tree.right is None and new_tree.val == target:
+            return None
+        return new_tree
