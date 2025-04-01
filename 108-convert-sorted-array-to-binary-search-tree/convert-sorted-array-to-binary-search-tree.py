5f8eb0d49c608a6ea155553a038d0be83e09aae5
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-01 06:11:09 -0400

    Time: 3 ms (73.70%) | Memory: 19.2 MB (30.26%) - LeetSync

---
+++ 108-convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        n = len(nums)
+        root_val = nums[n//2]
+        root = TreeNode(root_val)
+
+        def buildTree(root, leftTree, rightTree):
+            if not leftTree and not rightTree:
+                return root
+            if leftTree:
+                n = len(leftTree)
+                left_root_index = n//2
+                left_root_node = TreeNode(leftTree[left_root_index])
+                root.left = left_root_node
+                newLeftTree = leftTree[:left_root_index]
+                newRightTree = leftTree[left_root_index+1:] if left_root_index+1 < n else []
+                buildTree(left_root_node, newLeftTree, newRightTree)
+            if rightTree:
+                n = len(rightTree)
+                right_root_index = n // 2
+                right_root_node = TreeNode(rightTree[right_root_index])
+                root.right = right_root_node
+                newLeftTree = rightTree[:right_root_index]
+                newRightTree = rightTree[right_root_index+1:] if right_root_index+1 < n else []
+                buildTree(right_root_node, newLeftTree, newRightTree)
+            return root
+        
+        return buildTree(root, nums[:n//2], nums[(n//2)+1:])
+                
+            
+        
