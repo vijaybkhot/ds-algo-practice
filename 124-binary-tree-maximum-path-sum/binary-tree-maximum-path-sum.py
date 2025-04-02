7d3cf180b4155dca99cc3ed6e5b650fff971dc0e
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-02 18:05:29 -0400

    Time: 18 ms (47.64%) | Memory: 23 MB (42.80%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -7,18 +7,34 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         
-        # First Attempt:
-        self.curr_max = float('-inf')
-        def dfs(node):
-            if node is None:
-                return 0
+        # # First Attempt:
+        # self.curr_max = float('-inf')
+        # def dfs(node):
+        #     if node is None:
+        #         return 0
                 
-            left_path_sum = dfs(node.left)
-            right_path_sum = dfs(node.right)
-            curr_path_sum = max(node.val, node.val + left_path_sum + right_path_sum, node.val + left_path_sum, node.val + right_path_sum)
-            self.curr_max = max(self.curr_max, curr_path_sum)
-            return_path_sum = max(node.val, node.val+left_path_sum, node.val + right_path_sum)
-            return return_path_sum
+        #     left_path_sum = dfs(node.left)
+        #     right_path_sum = dfs(node.right)
+        #     curr_path_sum = max(node.val, node.val + left_path_sum + right_path_sum, node.val + left_path_sum, node.val + right_path_sum)
+        #     self.curr_max = max(self.curr_max, curr_path_sum)
+        #     return_path_sum = max(node.val, node.val+left_path_sum, node.val + right_path_sum)
+        #     return return_path_sum
+        
+        # dfs(root)
+        # return self.curr_max
+
+        # More readable code:
+        self.curr_max = float('-inf')
+
+        def dfs(node):
+            if not node:
+                return 0
+            leftTreeSum = max(0, dfs(node.left))
+            rightTreeSum = max(0, dfs(node.right))
+            curr_node_sum = node.val + leftTreeSum + rightTreeSum
+            self.curr_max = max(self.curr_max, curr_node_sum)
+
+            return node.val + max(leftTreeSum, rightTreeSum)
         
         dfs(root)
         return self.curr_max
