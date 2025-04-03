04e527403d62a88c34f7051e5b7dfad4494352f3
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-03 07:12:48 -0400

    Time: 740 ms (5.01%) | Memory: 23.1 MB (5.54%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -8,19 +8,26 @@
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         self.res = None
-        val_set = set([p.val, q.val])
+
         def dfs(node):
-            if not node:
+            if not node or self.res:  # Stop recursion if LCA is found
                 return set()
+
             left_set = dfs(node.left)
             right_set = dfs(node.right)
-            
+
+            # If either left_set or right_set is None, treat it as an empty set
+            if left_set is None: left_set = set()
+            if right_set is None: right_set = set()
+
             merged_set = left_set | {node.val} | right_set
 
             if p.val in merged_set and q.val in merged_set and self.res is None:
-                self.res =  node
-                return set()
+                self.res = node
+                return None  # Signal to stop further recursion
+
             return merged_set
+
 
         dfs(root)
         return self.res
