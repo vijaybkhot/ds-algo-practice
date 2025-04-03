c5b0b82725cafb9e065bd02193ec40727f414972
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-03 07:42:02 -0400

    Time: 56 ms (47.09%) | Memory: 22.1 MB (71.40%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,27 +7,41 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        self.res = None
+        # # Approach using set - Inefficient
+        # self.res = None
+        # def dfs(node):
+        #     if not node or self.res:  # Stop recursion if LCA is found
+        #         return set()
 
-        def dfs(node):
-            if not node or self.res:  # Stop recursion if LCA is found
-                return set()
+        #     left_set = dfs(node.left)
+        #     right_set = dfs(node.right)
 
-            left_set = dfs(node.left)
-            right_set = dfs(node.right)
+        #     # If either left_set or right_set is None, treat it as an empty set
+        #     if left_set is None: left_set = set()
+        #     if right_set is None: right_set = set()
 
-            # If either left_set or right_set is None, treat it as an empty set
-            if left_set is None: left_set = set()
-            if right_set is None: right_set = set()
+        #     merged_set = left_set | {node.val} | right_set
 
-            merged_set = left_set | {node.val} | right_set
+        #     if p.val in merged_set and q.val in merged_set and self.res is None:
+        #         self.res = node
+        #         return None  # Signal to stop further recursion
 
-            if p.val in merged_set and q.val in merged_set and self.res is None:
-                self.res = node
-                return None  # Signal to stop further recursion
-
-            return merged_set
+        #     return merged_set
 
 
-        dfs(root)
-        return self.res
+        # dfs(root)
+        # return self.res
+
+        # Optimized approach
+
+        if not root or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if left and right:
+            return root
+        
+        return left or right
+        
