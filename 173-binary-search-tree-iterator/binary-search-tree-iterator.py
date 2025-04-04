39d888ba24761b878cc668b398ead24a5319889a
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-04 07:59:53 -0400

    Time: 7 ms (65.01%) | Memory: 25 MB (57.26%) - LeetSync

--- 173-binary-search-tree-iterator/binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.py
@@ -5,25 +5,44 @@
 #         self.left = left
 #         self.right = right
 class BSTIterator:
+    # # Inefficient O(n) Time approach
+    # def __init__(self, root: Optional[TreeNode]):
+    #     self.root = root
+    #     self.pointer = -1
+    #     self.inorder_arr = []
+    #     def inorder(node):
+    #         if node:
+    #             inorder(node.left)
+    #             self.inorder_arr.append(node.val)
+    #             inorder(node.right)
+    #     inorder(root)
+    #     self.size = len(self.inorder_arr)
 
+    # def next(self) -> int:
+    #     self.pointer += 1
+    #     return self.inorder_arr[self.pointer]        
+
+    # def hasNext(self) -> bool:
+    #     return self.pointer < self.size - 1
+
+    # Optimized O(h) approach
     def __init__(self, root: Optional[TreeNode]):
-        self.root = root
-        self.pointer = -1
-        self.inorder_arr = []
-        def inorder(node):
-            if node:
-                inorder(node.left)
-                self.inorder_arr.append(node.val)
-                inorder(node.right)
-        inorder(root)
-        self.size = len(self.inorder_arr)
+        self.stack = []
+        self._leftmost_inorder(root)
 
+    def _leftmost_inorder(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+            
     def next(self) -> int:
-        self.pointer += 1
-        return self.inorder_arr[self.pointer]        
+        top_node = self.stack.pop()
+        if top_node.right:
+            self._leftmost_inorder(top_node.right)
+        return top_node.val      
 
     def hasNext(self) -> bool:
-        return self.pointer < self.size - 1
+        return len(self.stack) > 0
         
 
 
