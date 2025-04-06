3b9092e2d2bdca6db158c7bc7a28d3baa956e4f2
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-06 14:50:23 -0400

    Time: 640 ms (5.01%) | Memory: 28.9 MB (17.68%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,10 +1,62 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for num in nums:
-            if len(heap) < k:
-                heapq.heappush(heap, num)
-            else:
-                heapq.heappushpop(heap, num)
+        # # Solution using built in heap data structure
+        # heap = []
+        # for num in nums:
+        #     if len(heap) < k:
+        #         heapq.heappush(heap, num)
+        #     else:
+        #         heapq.heappushpop(heap, num)
+        # return heap[0]
+        # Solution using custom heap
+        def heappush(heap, val, k):
+            heap.append(val)
+            bubble_up(heap, len(heap)-1)
+            if len(heap) > k:
+                remove_min(heap)
 
+        def bubble_up(heap, idx):
+            while idx > 0:
+                parent_idx = (idx - 1) // 2
+                if heap[parent_idx] <= heap[idx]:
+                    break
+                heap[parent_idx], heap[idx] = heap[idx], heap[parent_idx]
+                idx = parent_idx
+
+        def remove_min(heap):
+            if len(heap) == 1:
+                return heap.pop()
+            elif len(heap) == 0:
+                return None
+            root, last = heap[0], heap[-1]
+            heap.pop()
+            heap[0] = last
+            bubble_down(heap, 0)
+            return root
+        
+        def bubble_down(heap, idx):
+            n = len(heap)
+            while True:
+                left = (2 * idx) + 1
+                right = (2*idx) + 2
+                smallest = idx
+                if left < n and heap[left] < heap[smallest]:
+                    smallest = left
+                if right < n and heap[right] < heap[smallest]:
+                    smallest = right
+                if idx == smallest:
+                    break
+                heap[smallest], heap[idx] = heap[idx], heap[smallest]
+                idx = smallest
+        
+        heap = []
+
+        for num in nums:
+            heappush(heap, num, k)
+        
         return heap[0]
+            
+
+            
+            
+
