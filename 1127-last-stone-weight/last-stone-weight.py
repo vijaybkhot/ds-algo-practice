eb0b4c6c61794809665e8955b29ef79ccc7589ca
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-06 05:55:29 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (10.99%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -1,24 +1,41 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         
-        stones.sort()
+        # # Solution using sorting
+        # stones.sort()
+        # n = len(stones)
+        # while True:
+        #     if len(stones) > 1:
+        #        stone1 = stones.pop()
+        #        stone2 = stones.pop()
+        #        difference = abs(stone1 - stone2)
+        #        if difference > 0:
+        #         stones.append(difference)
+        #         if len(stones) > 1:
+        #             stones.sort()
+            
+        #     elif len(stones) <= 1:
+        #         break
         
-        n = len(stones)
+        # return 0 if not stones else stones[0]
+
+        # Solution using heap Data structure
+        heap = []
+        for num in stones:
+            heapq.heappush(heap, -num)
+        
         while True:
-            if len(stones) > 1:
-               stone1 = stones.pop()
-               stone2 = stones.pop()
-               difference = abs(stone1 - stone2)
-               if difference > 0:
-                stones.append(difference)
-                if len(stones) > 1:
-                    stones.sort()
-            
-            elif len(stones) <= 1:
+            if len(heap) > 1:
+                stone1 = -heapq.heappop(heap)
+                stone2 = -heapq.heappop(heap)
+                difference = abs(stone1 - stone2)
+                if difference > 0:
+                    heapq.heappush(heap, -difference)
+                    heapq.heapify(heap)
+
+            elif len(heap) <=1:
                 break
-        
-        return 0 if not stones else stones[0]
-            
+        return 0 if not heap else -heap[0]
         
 
         
