ab2ba49d1885c2fcc0eb644b7cab37b845b6452c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-06 07:43:19 -0400

    Time: 487 ms (5.62%) | Memory: 22 MB (89.03%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,7 +1,60 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        # Implement a priority queue to maintain a queue of length k
+        # # Initial attempt
+        # # Implement a priority queue to maintain a queue of length k
+        # def enque(q, point):
+        #     q.append(point)
+        #     bubble_up(q)
+        #     if len(q) > k:
+        #         remove_max(q)
 
+        # def bubble_up(q):
+        #     idx = len(q) - 1
+        #     while idx > 0:
+        #         parent_idx = (idx-1) // 2
+        #         child = (q[idx][0]**2 + q[idx][1]**2) ** (1/2) 
+        #         parent = (q[parent_idx][0]**2 + q[parent_idx][1]**2) ** (1/2)
+        #         if parent >= child:
+        #             break
+        #         q[idx], q[parent_idx] = q[parent_idx], q[idx]
+        #         idx = parent_idx  
+            
+        # def remove_max(q):
+        #     if len(q) == 1:
+        #         return q.pop()
+        #     if not q:
+        #         return None
+        #     q[0], q[-1] = q[-1], q[0]
+        #     root = q.pop()
+        #     bubble_down(q)
+        
+        # def bubble_down(q):
+        #     n = len(q)
+        #     idx = 0
+        #     while idx < n:
+        #         left_idx = (2*idx) + 1
+        #         right_idx = (2*idx) + 2
+        #         child = [(q[idx][0]**2 + q[idx][1]**2) ** (1/2), idx]
+        #         left = [(q[left_idx][0]**2 + q[left_idx][1]**2) ** (1/2) if left_idx < n else float('-inf'), left_idx]
+        #         right = [(q[right_idx][0]**2 + q[right_idx][1]**2) ** (1/2) if right_idx < n else float('-inf'), right_idx]
+        #         largest = child
+        #         if left[0] > largest[0]:
+        #             largest = left
+        #         if right[0] > largest[0]:
+        #             largest = right
+        #         if child[0] == largest[0]:
+        #             break
+        #         q[largest[1]], q[child[1]] = q[child[1]], q[largest[1]]
+        #         idx = largest[1]
+            
+        # queue = []
+        # for point in points:
+        #     enque(queue, point)
+                
+        # return queue
+
+        # Optimized attempt
+        # Implement a priority queue to maintain a queue of length k
         def enque(q, point):
             q.append(point)
             bubble_up(q)
@@ -12,8 +65,8 @@ class Solution:
             idx = len(q) - 1
             while idx > 0:
                 parent_idx = (idx-1) // 2
-                child = (q[idx][0]**2 + q[idx][1]**2) ** (1/2) 
-                parent = (q[parent_idx][0]**2 + q[parent_idx][1]**2) ** (1/2)
+                child = (q[idx][0]**2 + q[idx][1]**2)
+                parent = (q[parent_idx][0]**2 + q[parent_idx][1]**2)
                 if parent >= child:
                     break
                 q[idx], q[parent_idx] = q[parent_idx], q[idx]
@@ -34,9 +87,9 @@ class Solution:
             while idx < n:
                 left_idx = (2*idx) + 1
                 right_idx = (2*idx) + 2
-                child = [(q[idx][0]**2 + q[idx][1]**2) ** (1/2), idx]
-                left = [(q[left_idx][0]**2 + q[left_idx][1]**2) ** (1/2) if left_idx < n else float('-inf'), left_idx]
-                right = [(q[right_idx][0]**2 + q[right_idx][1]**2) ** (1/2) if right_idx < n else float('-inf'), right_idx]
+                child = [(q[idx][0]**2 + q[idx][1]**2), idx]
+                left = [(q[left_idx][0]**2 + q[left_idx][1]**2) if left_idx < n else float('-inf'), left_idx]
+                right = [(q[right_idx][0]**2 + q[right_idx][1]**2) if right_idx < n else float('-inf'), right_idx]
                 largest = child
                 if left[0] > largest[0]:
                     largest = left
@@ -52,4 +105,6 @@ class Solution:
             enque(queue, point)
                 
         return queue
+
+
 
