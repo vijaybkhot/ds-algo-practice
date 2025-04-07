36118dd80647cfbb91710e19e3ed47b57a9c3038
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-07 06:56:22 -0400

    Time: 126 ms (56.97%) | Memory: 19.3 MB (27.05%) - LeetSync

--- 621-task-scheduler/task-scheduler.py
+++ 621-task-scheduler/task-scheduler.py
@@ -20,35 +20,59 @@ class Solution:
         
         # return interval_count
 
-        # Using heap data structure
-        freq_map = defaultdict(int)
-        for task in tasks:
-            freq_map[task] += 1
-        heap = []
-        for task in freq_map:
-            heapq.heappush(heap, (-freq_map[task], task))
-        total_interval = 0
-        while heap:
-            task_stack = []
-            i = 0
-            last_iteration = False
-            if heap[0][0] == -1:
-                last_iteration = True
-            if not last_iteration:
-                while i <= n:
-                    if heap:
-                        task_stack.append(heapq.heappop(heap))
-                    i += 1
-                    total_interval += 1
-            else:
-                while heap:
-                    heapq.heappop(heap)
-                    total_interval += 1
-            while task_stack:
-                freq, task = task_stack.pop()
-                freq += 1
-                if freq < 0:
-                    heapq.heappush(heap, (freq, task))
-                    
-        return total_interval
+        # # Using heap data structure
+        # freq_map = defaultdict(int)
+        # for task in tasks:
+        #     freq_map[task] += 1
+        # heap = []
+        # for task in freq_map:
+        #     heapq.heappush(heap, (-freq_map[task], task))
+            
+        # total_interval = 0
+        # while heap:
+        #     task_stack = []
+        #     i = 0
+        #     last_iteration = False
+        #     if heap[0][0] == -1:
+        #         last_iteration = True
+        #     if not last_iteration:
+        #         while i <= n:
+        #             if heap:
+        #                 task_stack.append(heapq.heappop(heap))
+        #             i += 1
+        #             total_interval += 1
+        #     else:
+        #         while heap:
+        #             heapq.heappop(heap)
+        #             total_interval += 1
+        #     while task_stack:
+        #         freq, task = task_stack.pop()
+        #         freq += 1
+        #         if freq < 0:
+        #             heapq.heappush(heap, (freq, task))
+
+        # return total_interval
+
+        # More readable solution:
+        freq_map = Counter(tasks)
+        max_heap = [-freq for freq in freq_map.values()]
+        heapq.heapify(max_heap)
+
+        total_time = 0
+
+        while max_heap:
+            temp = []
+            for i in range(n+1):
+                if max_heap:
+                    freq = heapq.heappop(max_heap)
+                    if freq + 1 < 0:
+                        temp.append(freq + 1)
+                total_time += 1
+                if not max_heap and not temp:
+                    break
+            for freq in temp:
+                heapq.heappush(max_heap, freq)
+        
+        return total_time
+
 
