0fb86e262ad342fa8f1a8c66435d3410ecdcdf1c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-08 18:23:33 -0400

    Time: 3 ms (64.20%) | Memory: 18.5 MB (11.31%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -3,51 +3,38 @@ class Solution:
         curr_capacity = 0
         organized_trips = [(trip[1], trip[2],trip[0]) for trip in trips]
         organized_trips.sort(key=lambda x: x[0])
-        max_distance = trips[0][1]
-        for trip in organized_trips:
-            max_distance = max(max_distance, trip[1])
         
-        start, destination = trips[0][0], max_distance
         current_trip = 0
-        n = len(trips)
+        last_trip = len(trips)
         heap = []
 
+        def addToHeap(heap, current_trip, curr_capacity, to, cap):
+            heapq.heappush(heap, (to, cap))
+            current_trip += 1
+            curr_capacity += cap
+            return current_trip, curr_capacity
 
-        while current_trip < n:
-            if current_trip < n and not heap:
+        
+
+        while current_trip < last_trip:
+            if current_trip < last_trip and not heap:
                 frm, to, cap = organized_trips[current_trip]
-                heapq.heappush(heap, (to, cap))
-                current_trip += 1
-                curr_capacity += cap
+                current_trip, curr_capacity = addToHeap(heap, current_trip, curr_capacity, to, cap)
                 if curr_capacity > capacity:
                     return False
-                if current_trip >= n:
-                    break
+                continue
+               
             curr_frm, curr_dest, curr_cap = organized_trips[current_trip]
             if curr_frm >= heap[0][0]:
                 while heap and curr_frm >= heap[0][0]:
                     curr_capacity -= heap[0][1]
                     heapq.heappop(heap)
-                heapq.heappush(heap, (curr_dest, curr_cap))
-                current_trip += 1
-                curr_capacity += curr_cap
+                current_trip, curr_capacity = addToHeap(heap, current_trip, curr_capacity, curr_dest, curr_cap)
                 if curr_capacity > capacity:
                     return False
-                if current_trip >= n:
-                    break
             elif curr_frm < heap[0][0]:
-                heapq.heappush(heap, (curr_dest, curr_cap))
-                current_trip += 1
-                curr_capacity += curr_cap
+                current_trip, curr_capacity = addToHeap(heap, current_trip, curr_capacity, curr_dest, curr_cap)
                 if curr_capacity > capacity:
                     return False
-                if current_trip >= n:
-                    break
 
-        return True
-            
-            
-                
-
-        return True
-            
+        return True            
