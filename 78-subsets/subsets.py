316ef908b170167cf028d1de200fba0ccaeb2338
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-11 09:27:16 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (64.96%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,16 +1,33 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
 
-        def dfs(index, output):
-            if index == len(nums):
-                result.append(output)
-                return
-            include_subset = output + [nums[index]]
-            exclude_subset = output
-            dfs(index + 1, include_subset)
-            dfs(index + 1, exclude_subset)
+        # def dfs(index, output):
+        #     if index == len(nums):
+        #         result.append(output)
+        #         return
+        #     include_subset = output + [nums[index]]
+        #     exclude_subset = output
+        #     dfs(index + 1, include_subset)
+        #     dfs(index + 1, exclude_subset)
 
-        result = []
-        dfs(0, [])
-        return result
+        # result = []
+        # dfs(0, [])
+        # return result
+
+        # Solution using BFS
+        q = deque([[nums[0]], []]) if nums else deque([])
+        i = 1
+
+        while i < (len(nums)):
+            for _ in range(len(q)):
+                curr_path = q.popleft()
+                q.append(curr_path + [nums[i]])
+                q.append(curr_path)
+            i += 1
+        
+        return list(q)
+
+
+            
+            
         
