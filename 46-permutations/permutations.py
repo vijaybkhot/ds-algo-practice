3b4552ebd78d4c82f1c8287e3e916bda91129679
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-12 11:11:23 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (56.96%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,19 +1,35 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         
-        k = len(nums)
+        # k = len(nums)
+        # result = []
+        # def dfs(curr_path, remainder):
+        #     if len(curr_path) == k:
+        #         result.append(curr_path[:])
+        #         return
+        #     for i in range(len(remainder)):
+        #         curr_path.append(remainder[i])
+        #         new_remainder = remainder[:i]+remainder[i+1:] if len(remainder) > 1 else []
+        #         dfs(curr_path, new_remainder)
+        #         curr_path.pop()
+
+        # dfs([], nums)
+        # return result
+
+        # Solution II: Swapping in place
         result = []
+        k = len(nums)
 
-        def dfs(curr_path, remainder):
-            if len(curr_path) == k:
-                result.append(curr_path[:])
+        def dfs(start):
+            if start == len(nums):
+                result.append(nums[:])
                 return
-            for i in range(len(remainder)):
-                curr_path.append(remainder[i])
-                new_remainder = remainder[:i]+remainder[i+1:] if len(remainder) > 1 else []
-                dfs(curr_path, new_remainder)
-                curr_path.pop()
+            
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]     # Swap in place
+                dfs(start+1)
+                nums[start], nums[i] = nums[i], nums[start]     # Swap back - backtrack
         
-
-        dfs([], nums)
+        dfs(0)
         return result
+
