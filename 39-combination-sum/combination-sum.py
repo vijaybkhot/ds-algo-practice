19864ecdd97330dbb7d6b96bde0f29fcff5be758
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-12 09:04:37 -0400

    Time: 11 ms (59.00%) | Memory: 18 MB (20.37%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,22 +2,6 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
         result = []
-        # def dfs(start_index, current_combination, total):
-        #     if total == target:
-        #         result.append(current_combination.copy())
-        #         return
-        #     if start_index >= len(candidates) or total > target:
-        #         return
-
-        #     # Considering the current start_index
-        #     # Include current number
-        #     current_combination.append(candidates[start_index])
-        #     dfs(start_index, current_combination, total + candidates[start_index])
-        #     current_combination.pop()
-
-        #     # Exclude current number
-        #     dfs(start_index + 1, current_combination, total)
-
         def dfs(start_index, current_combination, total):
             if total == target:
                 result.append(current_combination.copy())
