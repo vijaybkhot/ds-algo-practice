207bc936f78db01c9fe443de8b8bac7466b4266b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-13 06:24:12 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (57.01%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
+        res = [[]]
+        nums.sort()
+
+        def dfs(start, path):
+            if path:
+                res.append(path[:])
+            if start >= len(nums):
+                return        
+            for i in range(start, len(nums)):
+                if i > start and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                dfs(i+1, path)
+                path.pop()
+            
+        dfs(0, [])
+        
+        return res
+            
+        
