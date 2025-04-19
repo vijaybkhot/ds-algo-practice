adec85bd2ad5cb6887534af508b2819bda6c4112
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-19 11:08:52 -0400

    Time: 2445 ms (23.87%) | Memory: 18.3 MB (10.48%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.py
+++ 37-sudoku-solver/sudoku-solver.py
@@ -61,11 +61,10 @@ class Solution:
         for row in range(len(board)):
             for col in range(len(board[0])):
                 if board[row][col] != ".":
-                    num = int(board[row][col])
                     grid = ((row // 3) * 3) + (col // 3)
-                    row_sets[row].add(num)
-                    col_sets[col].add(num)
-                    grid_sets[grid].add(num)
+                    row_sets[row].add(board[row][col])
+                    col_sets[col].add(board[row][col])
+                    grid_sets[grid].add(board[row][col])
         
         empty_cells = []
         for row in range(9):
@@ -84,20 +83,21 @@ class Solution:
             grid = ((row // 3) * 3) + (col // 3)
             
             for i in range(1, 10):
+                str_num = str(i)
                 grid = ((row // 3) * 3) + (col // 3)
-                if i not in row_sets[row] and i not in col_sets[col] and i not in grid_sets[grid]:
-                    board[row][col] = str(i)
-                    row_sets[row].add(i)
-                    col_sets[col].add(i)
-                    grid_sets[grid].add(i)
+                if str_num not in row_sets[row] and str_num not in col_sets[col] and str_num not in grid_sets[grid]:
+                    board[row][col] = str_num
+                    row_sets[row].add(str_num)
+                    col_sets[col].add(str_num)
+                    grid_sets[grid].add(str_num)
 
                     dfs(index+1)
 
                     if not self.complete:
                         board[row][col] = "."
-                        row_sets[row].remove(i)
-                        col_sets[col].remove(i)
-                        grid_sets[grid].remove(i)
+                        row_sets[row].remove(str_num)
+                        col_sets[col].remove(str_num)
+                        grid_sets[grid].remove(str_num)
         
         dfs(0)
                                 
