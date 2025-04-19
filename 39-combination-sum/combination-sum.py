1f2d28cf42e2d397b1647f9d1803ba93d85fe27b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-18 20:25:19 -0400

    Time: 7 ms (82.64%) | Memory: 17.9 MB (40.74%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -21,24 +21,43 @@ class Solution:
         # dfs(0, [], 0)
         # return result
 
-        ## Sorting and using a for loop to backtrack
+        # ## Sorting and using a for loop to backtrack
+        # result = []
+        # candidates.sort()  
 
-        result = []
-        candidates.sort()  # Sorting helps with early stopping
+        # def dfs(start_index, current_combination, remaining_target):
+        #     if remaining_target == 0:
+        #         result.append(current_combination[:])
+        #         return
 
-        def dfs(start_index, current_combination, remaining_target):
-            if remaining_target == 0:
-                result.append(current_combination[:])
+        #     for i in range(start_index, len(candidates)):
+        #         if candidates[i] > remaining_target:
+        #             break  # No point in continuing if candidate is too large
+
+        #         # Include candidates[i] and stay at i (can reuse the same number)
+        #         current_combination.append(candidates[i])
+        #         dfs(i, current_combination, remaining_target - candidates[i])
+        #         current_combination.pop()  # backtrack
+
+        # dfs(0, [], target)
+        # return result
+
+        res= []
+        candidates.sort()
+        def dfs(index, curr_path, total):
+            if total == target:
+                res.append(curr_path[:])
                 return
+            if total > target:
+                return
+            
+            for i in range(index, len(candidates)):
+                if candidates[i] > target:
+                    break
+                if total + candidates[i] <= target:
+                    curr_path.append(candidates[i])
+                    dfs(i, curr_path, total+candidates[i])
+                    curr_path.pop()
+        dfs(0, [], 0)
+        return res
 
-            for i in range(start_index, len(candidates)):
-                if candidates[i] > remaining_target:
-                    break  # No point in continuing if candidate is too large
-
-                # Include candidates[i] and stay at i (can reuse the same number)
-                current_combination.append(candidates[i])
-                dfs(i, current_combination, remaining_target - candidates[i])
-                current_combination.pop()  # backtrack
-
-        dfs(0, [], target)
-        return result
