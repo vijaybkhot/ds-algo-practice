a8d95ea7bce456ed98146e3c26d7c80ad86598ab
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-19 07:12:40 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (91.71%) - LeetSync

--- 140-word-break-ii/word-break-ii.py
+++ 140-word-break-ii/word-break-ii.py
@@ -1,27 +1,46 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
         
-        res = []
-        s_list = [char for char in s]
+        # # First attempt
+        # res = []
+        # s_list = [char for char in s]
 
+        # wordSet = set(wordDict)
+        
+        # def dfs(curr_index, start, path):
+        #     if curr_index == len(s)-1:
+        #         if ''.join(s_list[start:curr_index+1]) in wordSet:
+        #             path += ''.join(s_list[start:curr_index+1])
+        #             res.append(path)
+        #         return
+            
+
+        #     if ''.join(s_list[start:curr_index+1]) in wordSet:
+        #         dfs(curr_index+1, curr_index+1, path + ''.join(s_list[start:curr_index+1]) + ' ')
+
+        #     dfs(curr_index+1, start, path)
+        
+        # dfs(0, 0, "")
+        # return res
+
+        res = []
         wordSet = set(wordDict)
         
         def dfs(curr_index, start, path):
-            if curr_index == len(s):
-                if ''.join(s_list[start:curr_index]) in wordSet:
-                    path += ''.join(s_list[start:curr_index])
+            if curr_index == len(s)-1:
+                if ''.join(s[start:curr_index+1]) in wordSet:
+                    path += s[start:curr_index+1]
                     res.append(path)
                 return
             
 
-            if ''.join(s_list[start:curr_index+1]) in wordSet:
-                dfs(curr_index+1, curr_index+1, path + ''.join(s_list[start:curr_index+1]) + ' ')
+            if s[start:curr_index+1] in wordSet:
+                dfs(curr_index+1, curr_index+1, path + s[start:curr_index+1] + ' ')
 
             dfs(curr_index+1, start, path)
         
         dfs(0, 0, "")
         return res
-
 
             
 
