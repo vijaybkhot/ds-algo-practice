d89a751652e8da473e5b8827a1456a84162ff7cd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-20 08:05:48 -0400

    Time: 151 ms (20.97%) | Memory: 25 MB (14.60%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,20 +1,48 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        res = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    isLeft = grid[i][j-1] if j-1 > -1 else 0
-                    isRight = grid[i][j+1] if j+1 < len(grid[0]) else 0
-                    isTop = grid[i-1][j] if i-1 > -1 else 0
-                    isBottom = grid[i+1][j] if i+1 < len(grid) else 0
-                    isLeft = 1 if not isLeft else 0
-                    isRight = 1 if not isRight else 0
-                    isTop = 1 if not isTop else 0
-                    isBottom = 1 if not isBottom else 0
-                    res += (isLeft + isRight + isTop + isBottom)
+        # # Brute force solution
+        # res = 0
+        # for i in range(len(grid)):
+        #     for j in range(len(grid[0])):
+        #         if grid[i][j] == 1:
+        #             isLeft = grid[i][j-1] if j-1 > -1 else 0
+        #             isRight = grid[i][j+1] if j+1 < len(grid[0]) else 0
+        #             isTop = grid[i-1][j] if i-1 > -1 else 0
+        #             isBottom = grid[i+1][j] if i+1 < len(grid) else 0
+        #             isLeft = 1 if not isLeft else 0
+        #             isRight = 1 if not isRight else 0
+        #             isTop = 1 if not isTop else 0
+        #             isBottom = 1 if not isBottom else 0
+        #             res += (isLeft + isRight + isTop + isBottom)
 
-                    
+        # return res
         
-        return res
+        # DFS Solution
+        visited = set()
+
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+                return 1
+            if (i, j) in visited:
+                return 0
+            # left = dfs(i, j-1) if (i, j-1) not in visited else 0
+            # right = dfs(i, j+1) if (i, j+1) not in visited else 0
+            # top = dfs(i-1, j) if (i-1, j) not in visited else 0
+            # bottom = dfs(i+1, j) if (i+1, j) not in visited else 0
+            visited.add((i, j))
+            
+            return dfs(i, j-1) + dfs(i, j+1) + dfs(i-1, j) + dfs(i+1, j)
+        
+        i, j = 0, 0
+        for r in range(len(grid)):
+            if i and j:
+                break
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    i = r
+                    j = c
+                    break
+        
+        return dfs(i, j)
+        
         
