64f73155c7a41f0d46c689c32d6636010f788e02
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-20 13:22:05 -0400

    Time: 119 ms (36.42%) | Memory: 19.1 MB (36.81%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -20,26 +20,72 @@ class Solution:
         # DFS Solution
         visited = set()
 
-        def dfs(i, j):
-            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
-                return 1
-            if (i, j) in visited:
-                return 0
+        # def dfs(i, j):
+        #     if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+        #         return 1
+        #     if (i, j) in visited:
+        #         return 0
         
-            visited.add((i, j))
+        #     visited.add((i, j))
             
-            return dfs(i, j-1) + dfs(i, j+1) + dfs(i-1, j) + dfs(i+1, j)
+        #     return dfs(i, j-1) + dfs(i, j+1) + dfs(i-1, j) + dfs(i+1, j)
         
-        i, j = 0, 0
+        # i, j = 0, 0
+        # for r in range(len(grid)):
+        #     if i and j:
+        #         break
+        #     for c in range(len(grid[0])):
+        #         if grid[r][c] == 1:
+        #             i = r
+        #             j = c
+        #             break
+        
+        # return dfs(i, j)
+
+        visited = set()
+        self.res = 0
+        # BFS Solution:
+        def bfs(i, j):
+            q = deque([(i, j)])
+            while q:
+                r, c = q.popleft()
+                visited.add((r, c))
+                left, right, top, bottom = 1, 1, 1, 1
+                if c == 0 or grid[r][c-1]==0:
+                    self.res += 1
+                    left = None
+                if c == len(grid[0])-1 or grid[r][c+1]==0:
+                    self.res += 1
+                    right = None
+                if r == 0 or grid[r-1][c] == 0:
+                    self.res += 1
+                    top = None
+                if r == len(grid) - 1 or grid[r+1][c] == 0:
+                    self.res += 1
+                    bottom = None
+                if left and (r, c-1) not in visited:
+                    q.append((r, c-1))
+                    visited.add((r, c-1))
+                if right and (r, c+1) not in visited:
+                    q.append((r, c+1))
+                    visited.add((r, c+1))
+                if top and (r-1, c) not in visited:
+                    q.append((r-1, c))
+                    visited.add((r-1, c))
+                if bottom and (r+1, c) not in visited:
+                    q.append((r+1, c))
+                    visited.add((r+1, c))
+        
         for r in range(len(grid)):
-            if i and j:
-                break
             for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-                    i = r
-                    j = c
-                    break
+                    bfs(r, c)
+                    return self.res
         
-        return dfs(i, j)
+                
+                
+
+                
+                
         
         
