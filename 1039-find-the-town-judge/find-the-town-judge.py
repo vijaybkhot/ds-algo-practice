97e917685e41d16af88496ca823228682c71f5c9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-21 06:57:21 -0400

    Time: 11 ms (77.29%) | Memory: 20.8 MB (81.39%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,28 +1,34 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        # if len(trust) > n-1:
-        #     return -1
-        
-        # judge = trust[0][1]
-
-        # for i in range(1, len(trust)):
-        #     if trust[i][1] != judge:
-        #         return -1
+        # # First attempt:
+        # if n == 1 and not trust:
+        #     return 1
+        # trust_map = {}
+        # judge = -1
+        # for fromTrust, toTrust in trust:
+        #     if toTrust not in trust_map:
+        #         trust_map[toTrust] = [set(), set()]
+        #     if fromTrust not in trust_map:
+        #         trust_map[fromTrust] = [set(), set()]
+        #     trust_map[toTrust][0].add(fromTrust)
+        #     trust_map[fromTrust][1].add(toTrust)
+        # for person in trust_map:
+        #     if len(trust_map[person][0]) == n-1 and len(trust_map[person][1]) == 0:
+        #         judge = person
         
         # return judge
-        if n == 1 and not trust:
-            return 1
+
+        # First attempt:
         trust_map = {}
+        for person in range(1, n+1):
+            trust_map[person] = [0, 0]  # trustOut, trustIn
         judge = -1
-        for fromTrust, toTrust in trust:
-            if toTrust not in trust_map:
-                trust_map[toTrust] = [set(), set()]
-            if fromTrust not in trust_map:
-                trust_map[fromTrust] = [set(), set()]
-            trust_map[toTrust][0].add(fromTrust)
-            trust_map[fromTrust][1].add(toTrust)
+        for trustOut, trustIn in trust:
+            trust_map[trustOut][1] += 1
+            trust_map[trustIn][0] += 1
+            
         for person in trust_map:
-            if len(trust_map[person][0]) == n-1 and len(trust_map[person][1]) == 0:
+            if trust_map[person][0] == n-1 and trust_map[person][1] == 0:
                 judge = person
         
         return judge
