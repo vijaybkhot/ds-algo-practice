40abe7ae076ac2fb9c8fa44f1b728143ed201abd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-21 06:03:22 -0400

    Time: 7 ms (58.19%) | Memory: 18 MB (77.63%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,23 +1,40 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
 
-        result = []
-        k = len(nums)
+        # result = []
+        # k = len(nums)
 
+        # def dfs(start):
+        #     if start == k:
+        #         result.append(nums[:])
+        #         return
+        #     seen = set()
+        #     for i in range(start, len(nums)):
+        #         if nums[i] in seen:
+        #             continue
+        #         seen.add(nums[i])
+        #         nums[start], nums[i] = nums[i], nums[start]     # Swap in place
+        #         dfs(start+1)
+        #         nums[start], nums[i] = nums[i], nums[start]     # Swap back - backtrack
+
+        # nums.sort()
+        # dfs(0)
+        # return result
+
+        res = []
+        nums.sort()
         def dfs(start):
-            if start == k:
-                result.append(nums[:])
-                return
+            if start == len(nums):
+                res.append(nums[:])
             seen = set()
             for i in range(start, len(nums)):
                 if nums[i] in seen:
                     continue
                 seen.add(nums[i])
-                nums[start], nums[i] = nums[i], nums[start]     # Swap in place
+                nums[i], nums[start] = nums[start], nums[i]
                 dfs(start+1)
-                nums[start], nums[i] = nums[i], nums[start]     # Swap back - backtrack
-
-        nums.sort()
+                nums[i], nums[start] = nums[start], nums[i]
+        
         dfs(0)
-        return result
+        return res
         
