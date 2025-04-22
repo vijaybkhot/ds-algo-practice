cabaaf8d7f6167bafc0f2f98d91b29a32e3cfaec
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-22 07:24:19 -0400

    Time: 57 ms (6.60%) | Memory: 18 MB (82.75%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -11,20 +11,39 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
-        
-        old_to_new = {}
-        def dfs(curr_node):
-            if curr_node in old_to_new:
-                return old_to_new[curr_node]
-            
-            # Clone current Node
-            clone = Node(curr_node.val)
-            old_to_new[curr_node] = clone
 
-            # Recursively clone neighbors
-            for neighbor in curr_node.neighbors:
-                clone.neighbors.append(dfs(neighbor))
+        # # DFS Solution
+        # old_to_new = {}
+        # def dfs(curr_node):
+        #     if curr_node in old_to_new:
+        #         return old_to_new[curr_node]
             
-            return clone
+        #     # Clone current Node
+        #     clone = Node(curr_node.val)
+        #     old_to_new[curr_node] = clone
+
+        #     # Recursively clone neighbors
+        #     for neighbor in curr_node.neighbors:
+        #         clone.neighbors.append(dfs(neighbor))
+            
+        #     return clone
         
-        return dfs(node)
+        # return dfs(node)
+
+        # BFS Solution
+        old_to_new = {}
+        old_to_new[node] = Node(node.val)
+        queue = deque()
+        queue.append(node)
+
+        while queue:
+            curr_node = queue.popleft()
+
+            for neighbor in curr_node.neighbors:
+                if neighbor not in old_to_new:
+                    old_to_new[neighbor] = Node(neighbor.val)
+                    queue.append(neighbor)
+                # Append the clone neighbor to the neighbors list of the clone of the current node
+                old_to_new[curr_node].neighbors.append(old_to_new[neighbor])
+        
+        return old_to_new[node]
