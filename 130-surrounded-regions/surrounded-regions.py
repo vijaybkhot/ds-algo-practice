402a6dff585b6df104aa172110349128ac1a9d09
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-23 10:42:05 -0400

    Time: 5 ms (66.42%) | Memory: 22 MB (23.42%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,40 +3,75 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows, cols = len(board), len(board[0])
+        # # First attempt - Working but inefficient:
+        # # For each ‘O’, use DFS to check if it’s fully surrounded by ‘X’, and if so, capture the region by flipping all connected ‘O’s to ‘X’.
+        # rows, cols = len(board), len(board[0])
 
-        def is_surrounded(i, j):
-            visited = set()
-            surrounded = True
-            def dfs(row,col):
-                nonlocal surrounded
-                visited.add((row, col))
-                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    r, c = row+dr, col+dc
-                    if r < 0 or r >= rows or c < 0 or c >= cols:
-                        surrounded = False
-                        break
-                    if (r, c) not in visited and board[r][c] == "O":
-                        dfs(r, c)
+        # def is_surrounded(i, j):
+        #     visited = set()
+        #     surrounded = True
+        #     def dfs(row,col):
+        #         nonlocal surrounded
+        #         visited.add((row, col))
+        #         for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        #             r, c = row+dr, col+dc
+        #             if r < 0 or r >= rows or c < 0 or c >= cols:
+        #                 surrounded = False
+        #                 break
+        #             if (r, c) not in visited and board[r][c] == "O":
+        #                 dfs(r, c)
                     
-            dfs(i, j)
-            return surrounded
+        #     dfs(i, j)
+        #     return surrounded
         
-        def capture(i, j):
-            visited = set()
-            def dfs(row,col):
-                visited.add((row, col))
-                board[row][col] = "X"
-                for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                    r, c = row+dr, col+dc
-                    if 0<= r < rows and 0 <= c < cols and board[r][c] not in visited and board[r][c] == "O":
-                        dfs(r, c)
-            dfs(i, j)
+        # def capture(i, j):
+        #     visited = set()
+        #     def dfs(row,col):
+        #         visited.add((row, col))
+        #         board[row][col] = "X"
+        #         for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+        #             r, c = row+dr, col+dc
+        #             if 0<= r < rows and 0 <= c < cols and board[r][c] not in visited and board[r][c] == "O":
+        #                 dfs(r, c)
+        #     dfs(i, j)
+        
+        # for r in range(rows):
+        #     for c in range(cols):
+        #         if board[r][c] == "O" and is_surrounded(r, c):
+        #             capture(r, c)
+
+        # Using reverse dfs approach:
+        rows, cols = len(board), len(board[0])
+        visited = set()
+        def rev_dfs_identify_non_region(row, col):
+            if board[row][col] == "X" or board[row][col] == "T":
+                return
+            visited.add((row, col))
+            board[row][col] = "T"
+            for dr, dc in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+                r, c = row+dr, col+dc
+                if 0 <= r < rows and 0 <= c < cols and (r, c) not in visited and board[r][c] == "O":
+                    rev_dfs_identify_non_region(r, c)
+        
+        for r in range(rows):
+            rev_dfs_identify_non_region(r, 0)
+            rev_dfs_identify_non_region(r, cols-1)
+        
+        for c in range(cols):
+            rev_dfs_identify_non_region(0, c)
+            rev_dfs_identify_non_region(rows-1, c)
         
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == "O" and is_surrounded(r, c):
-                    capture(r, c)
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
+                    
+
+
+
+
             
 
             
