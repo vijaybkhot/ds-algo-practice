cc57e24b5f5095b36310062254b28372d7cfdbe4
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-24 06:02:59 -0400

    Time: 47 ms (72.49%) | Memory: 34.6 MB (19.71%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,27 +1,48 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
 
-        def isPalindrome(word):
-            # return word == word[::-1]
-            left, right = 0, len(word)-1
-            while left <= right:
-                if word[left] != word[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
+        # def isPalindrome(word):
+        #     # return word == word[::-1]
+        #     left, right = 0, len(word)-1
+        #     while left <= right:
+        #         if word[left] != word[right]:
+        #             return False
+        #         left += 1
+        #         right -= 1
+        #     return True
+        
+        # res = []
+        # def backtrack(start, path):
+        #     if start == len(s):
+        #         res.append(path[:])
+        #         return
+            
+        #     for i in range(start, len(s)):
+        #         if isPalindrome(s[start:i+1]):
+        #             path.append(s[start:i+1])
+        #             backtrack(i+1, path)
+        #             path.pop()
+
+        # backtrack(0, [])
+        # return res
+
+
+        def is_palindrome(word):
+            return word == word[::-1]
         
         res = []
-        def backtrack(start, path):
+        def dfs(start, path):
             if start == len(s):
                 res.append(path[:])
                 return
             
             for i in range(start, len(s)):
-                if isPalindrome(s[start:i+1]):
-                    path.append(s[start:i+1])
-                    backtrack(i+1, path)
+                left = s[start:i+1]
+                right = s[i+1:]
+                if is_palindrome(left):
+                    path.append(left)
+                    dfs(i+1, path)
                     path.pop()
-
-        backtrack(0, [])
+        dfs(0, [])
         return res
+            
