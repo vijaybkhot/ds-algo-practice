6efb259bf7c62ed12d0a721fb2180ebc121c7faf
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-25 09:49:27 -0400

    Time: 4 ms (68.67%) | Memory: 19.2 MB (50.43%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -3,28 +3,47 @@ class Solution:
 
         # Create graph
         graph = defaultdict(set)
+        inDegree = [0] * numCourses
         for a, b in prerequisites:
             graph[b].add(a)
+            inDegree[a] += 1
 
-        visited = [False] * numCourses
-        rec_stack = [False] * numCourses
+        # visited = [False] * numCourses
+        # rec_stack = [False] * numCourses
 
-        def dfs(node):
-            visited[node] = True
-            rec_stack[node] = True
+        # def dfs(node):
+        #     visited[node] = True
+        #     rec_stack[node] = True
+
+        #     for neighbor in graph[node]:
+        #         if not visited[neighbor]:
+        #             if dfs(neighbor):
+        #                 return True
+        #         elif rec_stack[neighbor]:
+        #             return True
+        #     rec_stack[node] = False
+        #     return False
+        
+        # for node in range(numCourses):
+        #     if not visited[node]:
+        #         if dfs(node):
+        #             return False
+        # return True
+
+        queue = deque()
+
+        for i in range(numCourses):
+            if inDegree[i] == 0:
+                queue.append(i)
+        topo_order = []
+        while queue:
+            node = queue.popleft()
+            topo_order.append(node)
 
             for neighbor in graph[node]:
-                if not visited[neighbor]:
-                    if dfs(neighbor):
-                        return True
-                elif rec_stack[neighbor]:
-                    return True
-            rec_stack[node] = False
-            return False
+                inDegree[neighbor] -= 1
+                if inDegree[neighbor] == 0:
+                    queue.append(neighbor)
         
-        for node in range(numCourses):
-            if not visited[node]:
-                if dfs(node):
-                    return False
-        return True
+        return True if len(topo_order) == numCourses else False
 
