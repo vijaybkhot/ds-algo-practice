a1b3a83342e4f695005d04d530ffcca00ffbbe10
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-25 10:16:11 -0400

    Time: 3 ms (77.20%) | Memory: 20.8 MB (5.89%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,28 +1,55 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # Topological sorting using Kahn's algorithm
+        # # Topological sorting using Kahn's algorithm
         graph = defaultdict(set)
         in_degree = [0] * numCourses
         for a, b in prerequisites:
             graph[b].add(a)
             in_degree[a] += 1
         
-        queue = deque()
+        # queue = deque()
 
-        for i in range(numCourses):
-            if in_degree[i] == 0:
-                queue.append(i)
+        # for i in range(numCourses):
+        #     if in_degree[i] == 0:
+        #         queue.append(i)
         
+        # topo_order = []
+
+        # while queue:
+        #     node = queue.popleft()
+        #     topo_order.append(node)
+
+        #     for neighbor in graph[node]:
+        #         in_degree[neighbor] -= 1
+        #         if in_degree[neighbor] == 0:
+        #             queue.append(neighbor)
+        
+        # return topo_order if len(topo_order) == numCourses else []
+
+        # Topological sorting using DFS
+        visited = set()
+        rec_stack = set()
+        hasCycle = False
         topo_order = []
+        def dfs(node):
+            nonlocal hasCycle
+            nonlocal topo_order
+            if not hasCycle:
+                visited.add(node)
+                rec_stack.add(node)
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        dfs(neighbor)
+                    elif neighbor in rec_stack:
+                        topo_order = []
+                        hasCycle = True
+                        return
+                if not hasCycle:
+                    topo_order.append(node)
+                    rec_stack.remove(node)
 
-        while queue:
-            node = queue.popleft()
-            topo_order.append(node)
-
-            for neighbor in graph[node]:
-                in_degree[neighbor] -= 1
-                if in_degree[neighbor] == 0:
-                    queue.append(neighbor)
+        for node in range(numCourses):
+            if node not in visited:
+                dfs(node)
         
-        return topo_order if len(topo_order) == numCourses else []
-        
+        return topo_order[::-1] if topo_order else []
