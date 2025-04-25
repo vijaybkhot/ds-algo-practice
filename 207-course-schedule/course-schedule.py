a4715b2e84025157b5d7492f110bb30ee3da5824
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-25 08:47:02 -0400

    Time: 5 ms (58.63%) | Memory: 20.8 MB (5.57%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -2,31 +2,29 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
 
         # Create graph
-        input_graph = defaultdict(set)
+        graph = defaultdict(set)
         for a, b in prerequisites:
-            input_graph[b].add(a)
+            graph[b].add(a)
 
-        def detect_cycle(graph):
-            visited = [False] * numCourses
-            rec_stack = [False] * numCourses
+        visited = [False] * numCourses
+        rec_stack = [False] * numCourses
 
-            def dfs(node):
-                visited[node] = True
-                rec_stack[node] = True
+        def dfs(node):
+            visited[node] = True
+            rec_stack[node] = True
 
-                for neighbor in graph[node]:
-                    if not visited[neighbor]:
-                        if dfs(neighbor):
-                            return True
-                    elif rec_stack[neighbor]:
+            for neighbor in graph[node]:
+                if not visited[neighbor]:
+                    if dfs(neighbor):
                         return True
-                rec_stack[node] = False
-                return False
-            
-            for node in range(numCourses):
-                if not visited[node]:
-                    if dfs(node):
-                        return False
-            return True
+                elif rec_stack[neighbor]:
+                    return True
+            rec_stack[node] = False
+            return False
+        
+        for node in range(numCourses):
+            if not visited[node]:
+                if dfs(node):
+                    return False
+        return True
 
-        return detect_cycle(input_graph)
