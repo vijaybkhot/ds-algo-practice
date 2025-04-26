9aa082103147b88bca14e2f7c2e54b0335d287a2
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-26 06:04:07 -0400

    Time: 11 ms (51.04%) | Memory: 17.6 MB (98.12%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,32 +1,62 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
+        # board = [['.']*n for row in range(n)]
+        # cols = set()
+        # pos_diagonals = set() # row + col
+        # neg_diagonals = set() # row - col
+        # self.count = 0 
+
+        # def dfs(row, board):
+        #     if row == n:
+        #         self.count += 1
+        #         return
+            
+        #     for col in range(n):
+        #         if col not in cols and row+col not in pos_diagonals and row-col not in neg_diagonals:
+        #             board[row][col] = 'Q'
+        #             cols.add(col)
+        #             pos_diagonals.add(row+col)
+        #             neg_diagonals.add(row-col)
+
+        #             dfs(row+1, board)
+
+        #             board[row][col] = '.'
+        #             cols.remove(col)
+        #             pos_diagonals.remove(row + col)
+        #             neg_diagonals.remove(row - col)
+        
+        # dfs(0, board)
+        # return self.count
+
         board = [['.']*n for row in range(n)]
         cols = set()
-        pos_diagonals = set() # row + col
-        neg_diagonals = set() # row - col
-        self.count = 0 
+        pos_dias = set()
+        neg_dias = set()
+        self.count = 0
 
-        def dfs(row, board):
+        def dfs(row):
             if row == n:
                 self.count += 1
                 return
             
             for col in range(n):
-                if col not in cols and row+col not in pos_diagonals and row-col not in neg_diagonals:
-                    board[row][col] = 'Q'
+                if col not in cols and (row+col) not in pos_dias and (row-col) not in neg_dias:
                     cols.add(col)
-                    pos_diagonals.add(row+col)
-                    neg_diagonals.add(row-col)
+                    pos_dias.add(row+col)
+                    neg_dias.add(row-col)
+                    
+                    dfs(row+1)
 
-                    dfs(row+1, board)
-
-                    board[row][col] = '.'
                     cols.remove(col)
-                    pos_diagonals.remove(row + col)
-                    neg_diagonals.remove(row - col)
+                    pos_dias.remove(row+col)
+                    neg_dias.remove(row-col)
         
-        dfs(0, board)
+        dfs(0)
+
         return self.count
+
+
+
 
                 
                     
