c99095e6b60de3362d22d1038bc57ab317e70431
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-26 10:01:36 -0400

    Time: 15 ms (11.30%) | Memory: 18.9 MB (6.98%) - LeetSync

---
+++ 684-redundant-connection/redundant-connection.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        indegree = defaultdict(int)
+        input_graph = defaultdict(set)
+
+        max_indegree = 0
+        max_indegree_node = None
+        remove = 0
+        
+        for index, (u, v) in enumerate(edges):
+            input_graph[u].add(v)
+            input_graph[v].add(u)
+            
+
+        def components(graph):
+            def dfs(node):
+                visited.add(node)
+                for neighbor in graph[node]:
+                    if neighbor not in visited:
+                        dfs(neighbor)
+
+            visited = set()
+            count = 0
+            for node in graph:
+                if node not in visited:
+                    count += 1
+                    dfs(node)
+            return count
+
+        for i in range(len(edges)-1, -1, -1):
+            u, v = edges[i]
+            input_graph[u].remove(v)
+            input_graph[v].remove(u)
+            if components(input_graph) == 1:
+                return edges[i]
+            input_graph[u].add(v)
+            input_graph[v].add(u)
