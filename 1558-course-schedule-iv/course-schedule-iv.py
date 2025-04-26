1fc2e1c0b6d22f38ff2c35a7ee1efe6c4ccbe6db
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-26 07:54:16 -0400

    Time: 39 ms (67.73%) | Memory: 21.1 MB (10.40%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,6 +1,6 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-
+        # Using Kahns algorithm:
         prereq = defaultdict(set)
         graph = defaultdict(set)
         indegree = defaultdict(int)
@@ -8,21 +8,34 @@ class Solution:
             graph[a].add(b)
             indegree[b] += 1
         
-        q = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
+        # q = deque()
+        # for i in range(numCourses):
+        #     if indegree[i] == 0:
+        #         q.append(i)
 
-        while q:
-            node = q.popleft()
+        # while q:
+        #     node = q.popleft()
+        #     for neighbor in graph[node]:
+        #         prereq[neighbor].update(prereq[node])
+        #         prereq[neighbor].add(node)
+        #         indegree[neighbor] -= 1
+        #         if indegree[neighbor] == 0:
+        #             q.append(neighbor)
+
+        visited = set()
+
+        def dfs(node):
+            if node in visited:
+                return
+            visited.add(node)
+
             for neighbor in graph[node]:
-                prereq[neighbor].update(prereq[node])
-                prereq[neighbor].add(node)
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    q.append(neighbor)
+                dfs(neighbor)
+                prereq[node].add(neighbor)
+                prereq[node].update(prereq[neighbor])
+        
+        for i in range(numCourses):
+            dfs(i)
 
-
-            
-        return [True if u in prereq[v] else False for u, v in queries]
+        return [True if v in prereq[u] else False for u, v in queries]
         
