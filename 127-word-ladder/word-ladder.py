871c8364975d560d48116df0705f37b30e2a6f80
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-29 10:37:34 -0400

    Time: 43 ms (96.26%) | Memory: 18.5 MB (98.93%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,5 +1,7 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
         char_sets = [set()] * len(beginWord)
         # Accumulate unique index wise character for all words in wordList
         for word in wordList:
@@ -7,27 +9,53 @@ class Solution:
                 char_sets[idx].add(char)
         
         wordSet = set(wordList)
-
+        wordSet.add(beginWord)
         visited = set()
+
         def getAdjacentWords(word):
             adj = []
             for i in range(len(beginWord)):
                 for char in char_sets[i]:
                     curr_word = word[:i] + char + word[i+1:]
-                    if curr_word not in visited and curr_word in wordSet:
-                        visited.add(curr_word)
+                    if curr_word in wordSet:
                         adj.append(curr_word)
             return adj
+        # # One directional BFS
+        # q = deque()
+        # q.append((beginWord, 0))
+        # dist = 0
+        # while q:
+        #     curr_word, curr_dist = q.popleft()
+        #     if curr_word == endWord:
+        #         return curr_dist+1
+        #     for nei in getAdjacentWords(curr_word):
+        #         q.append((nei, curr_dist+1))
+        
+        # return 0
 
-        q = deque()
-        q.append((beginWord, 0))
-        dist = 0
-        while q:
-            curr_word, curr_dist = q.popleft()
-            if curr_word == endWord:
-                return curr_dist+1
-            for nei in getAdjacentWords(curr_word):
-                q.append((nei, curr_dist+1))
+        # Bi-directional BFS
+        begin = {beginWord}
+        end = {endWord}
+        dist = 1
+
+        
+        while begin and end:
+            if len(begin) > len(end):
+                begin, end = end, begin
+
+            temp = set()
+            for word in begin:
+                visited.add(word)
+                for nei in getAdjacentWords(word):
+                    if nei in end:
+                        return dist + 1
+                    if nei not in visited:
+                        visited.add(nei)
+                        temp.add(nei)
+
+            begin = temp
+            dist += 1
+                        
         
         return 0
 
