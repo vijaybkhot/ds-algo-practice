f9950e7a6123d235cc2d2ea0e0d29d43dd5cd693
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-29 06:24:21 -0400

    Time: 85 ms (9.20%) | Memory: 24 MB (16.15%) - LeetSync

--- 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.py
@@ -1,4 +1,47 @@
 class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+        for num in nums:
+            self._insert(num)
+        
+    def _insert(self, val):
+        self.heap.append(val)
+        child = len(self.heap)-1
+        parent = (child - 1) // 2
+        while child > 0:
+            parent = (child - 1) // 2
+            if self.heap[child] >= self.heap[parent]:
+                break
+            self.heap[child], self.heap[parent] = self.heap[parent], self.heap[child]
+            child = parent
+            
+        while len(self.heap) > self.k:
+            self._remove_min()
+
+    def _remove_min(self):
+        self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
+        self.heap.pop()
+        # Bubble down:
+        parent = 0
+        while parent < len(self.heap):
+            largest = parent
+            left, right = (2 * parent) + 1, (2 * parent) + 2
+            if left < len(self.heap) and self.heap[left] < self.heap[largest]:
+                largest = left
+            if right < len(self.heap) and self.heap[right] < self.heap[largest]:
+                largest = right
+            if largest == parent:
+                break
+        
+            self.heap[largest], self.heap[parent] = self.heap[parent], self.heap[largest]
+            parent = largest
+            
+
+
+    def add(self, val: int) -> int:
+        self._insert(val)
+        return self.heap[0]
 
     # def __init__(self, k: int, nums: List[int]):
     #     self.k = k
@@ -47,19 +90,19 @@ class KthLargest:
     #         self.heap[idx], self.heap[smallest] = self.heap[smallest], self.heap[idx]
     #         idx = smallest
 
-    # Using built in data structure
-    def __init__(self, k: int, nums: list[int]):
-        self.k = k
-        self.heap = []
+    # # Using built in data structure
+    # def __init__(self, k: int, nums: list[int]):
+    #     self.k = k
+    #     self.heap = []
 
-        for num in nums:
-            self.add(num)  # use add() to build the heap correctly
+    #     for num in nums:
+    #         self.add(num)  # use add() to build the heap correctly
 
-    def add(self, val: int) -> int:
-        heapq.heappush(self.heap, val)
-        if len(self.heap) > self.k:
-            heapq.heappop(self.heap)  # remove smallest of k+1 elements
-        return self.heap[0]  # kth largest is at root of min-heap
+    # def add(self, val: int) -> int:
+    #     heapq.heappush(self.heap, val)
+    #     if len(self.heap) > self.k:
+    #         heapq.heappop(self.heap)  # remove smallest of k+1 elements
+    #     return self.heap[0]  # kth largest is at root of min-heap
 
 
         
