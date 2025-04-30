9d4a4322443d57721e0343d2691dcdb4d17ce1a6
Vijay Khot <vijaysinh.khot@gmail.com>
2025-04-30 06:37:37 -0400

    Time: 3 ms (9.49%) | Memory: 17.9 MB (10.37%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.py
+++ 1127-last-stone-weight/last-stone-weight.py
@@ -111,25 +111,77 @@ class Solution:
         # return 0 if not stones else stones[0]
 
 
-        stones_max = [-stone for stone in stones]
-        heapq.heapify(stones_max)
-        n = len(stones_max)
-        remaining = n
-        while remaining >= 2:
-            if len(stones_max) >= 2:
-                stone1 = -heapq.heappop(stones_max)
-                stone2 = -heapq.heappop(stones_max)
-                if stone1 > stone2:
-                    new_stone = stone1 - stone2
-                    heapq.heappush(stones_max, -new_stone)
-                    remaining -= 1
-                else:
-                    remaining -= 2
+        # stones_max = [-stone for stone in stones]
+        # heapq.heapify(stones_max)
+        # n = len(stones_max)
+        # remaining = n
+        # while remaining >= 2:
+        #     if len(stones_max) >= 2:
+        #         stone1 = -heapq.heappop(stones_max)
+        #         stone2 = -heapq.heappop(stones_max)
+        #         if stone1 > stone2:
+        #             new_stone = stone1 - stone2
+        #             heapq.heappush(stones_max, -new_stone)
+        #             remaining -= 1
+        #         else:
+        #             remaining -= 2
 
 
         
-        return -stones_max[0] if stones_max else 0
+        # return -stones_max[0] if stones_max else 0
 
+        def heapify(heap):
+            last_non_leaf = (len(heap)-1)//2
+            for i in range(last_non_leaf, -1, -1):
+                bubble_down(heap, i)
+            return heap
+
+        def insert(heap, val):
+            heap.append(val)
+            bubble_up(heap, len(heap)-1)
+            return heap
+
+        def bubble_up(heap, idx):
+            while idx > 0:
+                parent_idx = (idx-1) // 2
+                if heap[parent_idx] >= heap[idx]:
+                    break
+                heap[parent_idx], heap[idx] = heap[idx], heap[parent_idx]
+                idx = parent_idx
+            return heap
+        
+        def max_pop(heap):
+            heap[0], heap[-1] = heap[-1], heap[0]
+            max_val = heap.pop()
+            heap = bubble_down(heap, 0)
+            return heap, max_val
+        
+        def bubble_down(heap, idx):
+            while idx < len(heap):
+                largest = idx
+                left = (2 * idx) + 1
+                right = (2 * idx) + 2
+                if left < len(heap) and heap[left] > heap[largest]:
+                    largest = left
+                if right < len(heap) and heap[right] > heap[largest]:
+                    largest = right
+                if largest == idx:
+                    break
+                heap[largest], heap[idx] = heap[idx], heap[largest]
+                idx = largest
+            return heap
+
+        max_heap = heapify(stones)
+
+        while len(max_heap) > 1:
+            if len(max_heap) > 1:
+                max_heap, stone1 = max_pop(max_heap)
+                max_heap, stone2 = max_pop(max_heap)
+                if stone1 != stone2:
+                    new_stone = stone1 - stone2
+                    max_heap = insert(max_heap, new_stone)
+        
+        return max_heap[0] if max_heap else 0
 
 
 
