aaa411d2a1c42e864ba9357cd92463fb0e14d29b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-01 06:12:23 -0400

    Time: 90 ms (55.15%) | Memory: 28.6 MB (72.09%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -8,54 +8,66 @@ class Solution:
         #     else:
         #         heapq.heappushpop(heap, num)
         # return heap[0]
-        # Solution using custom heap
-        def heappush(heap, val):
-            heap.append(val)
-            bubble_up(heap, len(heap)-1)
+        # # Solution using custom heap
+        # def heappush(heap, val):
+        #     heap.append(val)
+        #     bubble_up(heap, len(heap)-1)
             
 
-        def bubble_up(heap, idx):
-            while idx > 0:
-                parent_idx = (idx - 1) // 2
-                if heap[parent_idx] <= heap[idx]:
-                    break
-                heap[parent_idx], heap[idx] = heap[idx], heap[parent_idx]
-                idx = parent_idx
+        # def bubble_up(heap, idx):
+        #     while idx > 0:
+        #         parent_idx = (idx - 1) // 2
+        #         if heap[parent_idx] <= heap[idx]:
+        #             break
+        #         heap[parent_idx], heap[idx] = heap[idx], heap[parent_idx]
+        #         idx = parent_idx
 
-        def remove_min(heap):
-            if len(heap) == 1:
-                return heap.pop()
-            elif len(heap) == 0:
-                return None
-            root, last = heap[0], heap[-1]
-            heap.pop()
-            heap[0] = last
-            bubble_down(heap, 0)
-            return root
+        # def remove_min(heap):
+        #     if len(heap) == 1:
+        #         return heap.pop()
+        #     elif len(heap) == 0:
+        #         return None
+        #     root, last = heap[0], heap[-1]
+        #     heap.pop()
+        #     heap[0] = last
+        #     bubble_down(heap, 0)
+        #     return root
         
-        def bubble_down(heap, idx):
-            n = len(heap)
-            while True:
-                left = (2 * idx) + 1
-                right = (2*idx) + 2
-                smallest = idx
-                if left < n and heap[left] < heap[smallest]:
-                    smallest = left
-                if right < n and heap[right] < heap[smallest]:
-                    smallest = right
-                if idx == smallest:
-                    break
-                heap[smallest], heap[idx] = heap[idx], heap[smallest]
-                idx = smallest
+        # def bubble_down(heap, idx):
+        #     n = len(heap)
+        #     while True:
+        #         left = (2 * idx) + 1
+        #         right = (2*idx) + 2
+        #         smallest = idx
+        #         if left < n and heap[left] < heap[smallest]:
+        #             smallest = left
+        #         if right < n and heap[right] < heap[smallest]:
+        #             smallest = right
+        #         if idx == smallest:
+        #             break
+        #         heap[smallest], heap[idx] = heap[idx], heap[smallest]
+        #         idx = smallest
         
+        # heap = []
+
+        # for num in nums:
+        #     heappush(heap, num)
+        #     if len(heap) > k:
+        #         remove_min(heap)
+        
+        # return heap[0]
+
+
+
         heap = []
-
         for num in nums:
-            heappush(heap, num)
+            heapq.heappush(heap, num)
             if len(heap) > k:
-                remove_min(heap)
+                heapq.heappop(heap)
         
         return heap[0]
+
+
             
 
             
