35006ac3bbf3e93c668de5514589edd1e1f0dd30
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-04 07:41:59 -0400

    Time: 31 ms (45.86%) | Memory: 19.5 MB (18.85%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,30 +1,23 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # Shortest path using Djikstras algorithm
+
         graph = defaultdict(list)
         for u, v, w in flights:
             graph[u].append((v, w))
 
-        # (price, node, stops)
         heap = [(0, src, 0)]
-
-        # Best price to reach a node with <= stops
         prices = dict()
 
         while heap:
             cost, node, stops = heapq.heappop(heap)
-
             if node == dst:
                 return cost
-
             if stops > k:
                 continue
-
-            for nei, price in graph[node]:
-                if (nei, stops) not in prices or cost + price < prices[(nei, stops)]:
-                    prices[(nei, stops)] = cost + price
-                    heapq.heappush(heap, (cost + price, nei, stops + 1))
-
+            for nei, nei_price in graph[node]:
+                if (nei, stops) not in prices or prices[(nei, stops)] > cost + nei_price:
+                    prices[(nei, stops)] = cost + nei_price
+                    heapq.heappush(heap, (cost + nei_price, nei, stops+1))
+        
         return -1
-            
-            
+
