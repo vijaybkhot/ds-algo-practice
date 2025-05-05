0d72979a78c7b5cb91ce63865314019ec2568702
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-05 06:20:28 -0400

    Time: 4 ms (19.17%) | Memory: 17.7 MB (74.36%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -14,30 +14,72 @@ class Solution:
         
         # return output
 
-        # Solution using heap
-        def addCharToRes(s, char, count, heap):
-            s += char
-            count += 1
-            if count < 0:
-                heapq.heappush(heap, (count, char))
-            return s
+        # # Solution using heap
+        # def addCharToRes(s, char, count, heap):
+        #     s += char
+        #     count += 1
+        #     if count < 0:
+        #         heapq.heappush(heap, (count, char))
+        #     return s
 
-        freq_count = Counter(s)
-        max_heap = [(-count, char) for char, count in freq_count.items()]
-        heapq.heapify(max_heap)
+        # freq_count = Counter(s)
+        # max_heap = [(-count, char) for char, count in freq_count.items()]
+        # heapq.heapify(max_heap)
         
-        res = ""
-        prev_char = ""
+        # res = ""
+        # prev_char = ""
 
-        while max_heap:
-            count1, char1 = heapq.heappop(max_heap)
-            if res and prev_char == char1:
-                if max_heap:
-                    count2, char2 = heapq.heappop(max_heap)
-                    res = addCharToRes(res, char2, count2, max_heap)
-                else:
-                    return ""
-            res = addCharToRes(res, char1, count1, max_heap)
-            prev_char = char1
+        # while max_heap:
+        #     count1, char1 = heapq.heappop(max_heap)
+        #     if res and prev_char == char1:
+        #         if max_heap:
+        #             count2, char2 = heapq.heappop(max_heap)
+        #             res = addCharToRes(res, char2, count2, max_heap)
+        #         else:
+        #             return ""
+        #     res = addCharToRes(res, char1, count1, max_heap)
+        #     prev_char = char1
+        
+        # return res
+
+
+        freq_count = defaultdict(int)
+        for char in s:
+            freq_count[char] += 1
+        
+        heap = [(-val, key) for key, val in freq_count.items()]
+        heapq.heapify(heap)
+
+        res = ""
+
+        while len(heap) > 1:
+            count1, char1 = heapq.heappop(heap)
+            count2, char2 = heapq.heappop(heap)
+            res += char1 + char2
+            count1 += 1
+            count2 += 1
+            if count1 < 0:
+                heapq.heappush(heap, (count1, char1))
+            if count2 < 0:
+                heapq.heappush(heap, (count2, char2))
+        
+        if heap:
+            if heap[0][0] < -1 or (res and res[-1] == heap[0][1]):
+                return ""
+            else:
+                res += heap[0][1]
         
         return res
+
+            
+            
+
+
+
+
+
+
+
+
+
+
