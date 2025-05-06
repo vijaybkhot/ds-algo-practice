e43f3b40e476950d24cbbe1cf2646985f6c63719
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-06 05:36:56 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.87%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.py
+++ 1304-longest-happy-string/longest-happy-string.py
@@ -1,8 +1,8 @@
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        max_heap = [(-a,'a'), (-b, 'b'), (-c, 'c')]
-        max_heap = list(filter(lambda x: x[0] != 0, max_heap))
-        heapq.heapify(max_heap)
+        # max_heap = [(-a,'a'), (-b, 'b'), (-c, 'c')]
+        # max_heap = list(filter(lambda x: x[0] != 0, max_heap))
+        # heapq.heapify(max_heap)
 
         # # First Attempt:
         # def addToOutput(count, char, output):
@@ -42,38 +42,60 @@ class Solution:
 
 
         # Simplified solution
-        def addToOutput(count, char, output):
-            output.append(char)
-            count += 1
-            return count, char, output
+        # if not max_heap:
+        #     return ""
 
-        if not max_heap:
-            return ""
-        output = []
-        # first_count, first_char = heapq.heappop(max_heap)
-        # output = [first_char]
-        # first_count += 1
-        # if first_count < 0:
-        #     heapq.heappush(max_heap, (first_count, first_char))
+        # output = []
 
-        while max_heap:
-            count1, char1 = heapq.heappop(max_heap)
-            if len(output) > 1 and output[-1] == char1 and output[-2] == char1:
-                if max_heap:
-                    count2, char2 = heapq.heappop(max_heap)
-                    output.append(char2)
-                    count2 += 1
-                    if count2 < 0:
-                        heapq.heappush(max_heap, (count2, char2))
-                else:
-                    break
-            else:
-                output.append(char1)
-                count1 += 1
-            if count1:
-                heapq.heappush(max_heap, (count1, char1))
+        # while max_heap:
+        #     count1, char1 = heapq.heappop(max_heap)
+        #     if len(output) > 1 and output[-1] == char1 and output[-2] == char1:
+        #         if max_heap:
+        #             count2, char2 = heapq.heappop(max_heap)
+        #             output.append(char2)
+        #             count2 += 1
+        #             if count2 < 0:
+        #                 heapq.heappush(max_heap, (count2, char2))
+        #         else:
+        #             break
+        #     output.append(char1)
+        #     count1 += 1
+        #     if count1:
+        #         heapq.heappush(max_heap, (count1, char1))
             
-        return ''.join(output)
+        # return ''.join(output)
+                
+        char_heap = []
+        if a > 0:
+            char_heap.append((-a, 'a'))
+        if b > 0:
+            char_heap.append((-b, 'b'))
+        if c > 0:
+            char_heap.append((-c, 'c'))
+
+        heapq.heapify(char_heap)
+
+        res = []
+        while char_heap:
+            count1, char1 = heapq.heappop(char_heap)
+            if len(res) > 1 and res[-1] == char1 and res[-2] == char1:
+                if len(char_heap)==0:
+                    break
+
+                count2, char2 = heapq.heappop(char_heap)
+                res.append(char2)
+                count2 += 1
+                if count2:
+                    heapq.heappush(char_heap, (count2, char2))
+                
+            res.append(char1)
+            count1 += 1
+                
+            if count1:
+                heapq.heappush(char_heap, (count1, char1))
+        
+        return ''.join(res)
+
                 
 
 
