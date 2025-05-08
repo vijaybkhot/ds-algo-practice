6a122c058a05014aebf99378fda9f2b70255a274
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-08 06:15:13 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.71%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -1,47 +1,17 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def postorderTraversal(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[int]
-        """
-        # # Recursive solution
-        # return self.postorder(root, [])
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         
-        # Iterative solution
-        # [Left -> Right -> Root]
-        if not root:
-            return []
+        def dfs(node):
+            if node:
+                dfs(node.left)
+                dfs(node.right)
+                res.append(node.val)
         res = []
-        stack = [root]
-
-        # Add vals to res in the order, root -> Right -> Left
-        while stack:
-            # Adding root to res
-            node = stack.pop()
-            res.append(node.val)
-            # Adding Left then Right to stack. So when we pop, the vals to res will be added in order Right -> Left
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-        
-        # The final res will be in Root -> Right -> Left Order, which is exactly reverse of post order
-            
-        return res[::-1]
-    
-
-    # Recursive function
-    def postorder(self, root, res):
-        if root:
-            self.postorder(root.left, res)
-            self.postorder(root.right, res)
-            res.append(root.val)
-        
+        dfs(root)
         return res
-    
