7ff511d7e355f7869cd761aee3de1f9224ef85d9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-11 07:28:32 -0400

    Time: 55 ms (11.35%) | Memory: 18.4 MB (6.35%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,25 +6,25 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        # def isSameTree(p, q):
-        #     if not p and not q:
-        #         return True
-        #     if p and not q or q and not p:
-        #         return False
-        #     if p.val != q.val:
-        #         return False
-        #     return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+        def isSameTree(p, q):
+            if not p and not q:
+                return True
+            if p and not q or q and not p:
+                return False
+            if p.val != q.val:
+                return False
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
         
-        # if not root and subRoot:
-        #     return False
+        if not root and subRoot:
+            return False
 
-        # if not subRoot:
-        #     return False
+        if not subRoot:
+            return True
         
-        # if isSameTree(root, subRoot):
-        #     return True
+        if isSameTree(root, subRoot):
+            return True
 
-        # return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
         # Serialize
         def serialize(root):
