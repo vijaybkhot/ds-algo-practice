01c7653f046dffa4e32913087816a9bb8b54ce70
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-11 07:00:23 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.35%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -6,13 +6,43 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if (p and not q) or (q and not p):
-            return False
         if not p and not q:
             return True
+
+        if p and not q or q and not p:
+            return False
+
+        if p.val != q.val:
+            return False
+        if p.left and not q.left or q.left and not p.left:
+            return False
+        if p.right and not q.right or q.right and not p.right:
+            return False
+
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if (p and not q) or (q and not p):
+        #     return False
+        # if not p and not q:
+        #     return True
         
-        if p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
-            return True 
+        # if p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):
+        #     return True 
         
-        return False
+        # return False
         
