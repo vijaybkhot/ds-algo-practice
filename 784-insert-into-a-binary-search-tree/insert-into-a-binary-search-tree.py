4a2d7e56141d70f82cbcabfaa3a34b04a00200ec
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-12 05:29:20 -0400

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.76%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.py
@@ -6,39 +6,65 @@
 #         self.right = right
 class Solution:
     def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        # Find the insert position
-        # if not root:
-        #     return TreeNode(val)
-            
-        # curr = root
-        # parent = None
-        # while curr:
-        #     if val < curr.val:
-        #         parent = curr
-        #         curr = curr.left
-        #     elif val > curr.val:
-        #         parent = curr
-        #         curr = curr.right
-
-        # if val < parent.val:
-        #     parent.left = TreeNode(val)
-        # else:
-        #     parent.right = TreeNode(val)
-        # return root
-
-        # More readable approach
         if not root:
             return TreeNode(val)
-        curr = root
-        while True:
+        parent, curr = None, root
+        while curr:
+            parent = curr
             if val < curr.val:
-                if not curr.left:
-                    curr.left = TreeNode(val)
-                    break
                 curr = curr.left
             else:
-                if not curr.right:
-                    curr.right = TreeNode(val)
-                    break
                 curr = curr.right
+        if val < parent.val:
+            parent.left = TreeNode(val)
+        else:
+            parent.right = TreeNode(val)
+        
         return root
+        
+
+
+
+
+
+
+
+
+
+
+#         # Find the insert position
+#         # if not root:
+#         #     return TreeNode(val)
+            
+#         # curr = root
+#         # parent = None
+#         # while curr:
+#         #     if val < curr.val:
+#         #         parent = curr
+#         #         curr = curr.left
+#         #     elif val > curr.val:
+#         #         parent = curr
+#         #         curr = curr.right
+
+#         # if val < parent.val:
+#         #     parent.left = TreeNode(val)
+#         # else:
+#         #     parent.right = TreeNode(val)
+#         # return root
+
+#         # More readable approach
+#         if not root:
+#             return TreeNode(val)
+#         curr = root
+#         while True:
+#             if val < curr.val:
+#                 if not curr.left:
+#                     curr.left = TreeNode(val)
+#                     break
+#                 curr = curr.left
+#             else:
+#                 if not curr.right:
+#                     curr.right = TreeNode(val)
+#                     break
+#                 curr = curr.right
+#         return root
