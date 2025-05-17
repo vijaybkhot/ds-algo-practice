e3108e8401a0a8403f75063ad86acccd9120b01c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-17 06:12:48 -0400

    Time: 4 ms (58.17%) | Memory: 19.6 MB (56.65%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.py
+++ 337-house-robber-iii/house-robber-iii.py
@@ -6,17 +6,40 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        
+
+
         def max_rob(node):
             if not node:
                 return [0, 0]
-            
-            leftPair = max_rob(node.left)
-            rightPair = max_rob(node.right)
+            left_money = max_rob(node.left) # [with left, without left]
+            right_money = max_rob(node.right) # [with right, without right]
 
-            withRoot = node.val + leftPair[1] + rightPair[1]
-            withoutRoot = max(leftPair) + max(rightPair)
+            with_root = node.val + left_money[1] + right_money[1]
+            without_root = max(left_money) + max(right_money)
 
-            return [withRoot, withoutRoot]
+            return with_root, without_root
         
         return max(max_rob(root))
+
+
+
+
+
+
+
+
+
+        
+#         def max_rob(node):
+#             if not node:
+#                 return [0, 0]
+            
+#             leftPair = max_rob(node.left)   # [withLeft, withoutLeft]
+#             rightPair = max_rob(node.right) # [withRight, withoutRight]
+
+#             withRoot = node.val + leftPair[1] + rightPair[1]
+#             withoutRoot = max(leftPair) + max(rightPair)
+
+#             return [withRoot, withoutRoot]
+        
+#         return max(max_rob(root))
