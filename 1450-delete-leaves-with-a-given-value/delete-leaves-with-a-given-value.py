2cd70351c994d5cf2c679f85f9531cd709e7e9fb
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-17 06:28:35 -0400

    Time: 0 ms (100.00%) | Memory: 18.5 MB (31.03%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -6,32 +6,59 @@
 #         self.right = right
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        def remove_leaf(node, target):
+            # if not node.right and not node.left:
+            #     if node.val == target:
+            #         return None
+            #     else:
+            #         return root
+            if node.left:
+                node.left = remove_leaf(node.left, target)
+            if node.right:
+                node.right = remove_leaf(node.right, target)
+            if not node.left and not node.right and node.val == target:
+                return None
+            else:
+                return node
         
-        # # This method only delete the leaf nodes at first instance. Not the subsequent possible target leaf nodes
-        # def deleteNode(root, key):
-        #     if root.left is None and root.right is None and root.val == key:
-        #         return None
-        #     else:
-        #         if root.left:
-        #             root.left = deleteNode(root.left, key)
-        #         if root.right:
-        #             root.right = deleteNode(root.right, key)
-        #     return root
-        
-        # return deleteNode(root, target)
+        return remove_leaf(root, target)
+            
 
-        # Using post order traversal
-        def dfs(root, key):
-            if root:
-                dfs(root.left, key)
-                dfs(root.right, key)
-                if root.left and root.left.left is None and root.left.right is None and root.left.val == key:
-                    root.left = None
-                if root.right and root.right.left is None and root.right.right is None and root.right.val == key:
-                    root.right = None
-            return root
+
+
+
         
-        new_tree = dfs(root, target)
-        if new_tree.left is None and new_tree.right is None and new_tree.val == target:
-            return None
-        return new_tree
+
+
+
+
+
+
+#         # # This method only delete the leaf nodes at first instance. Not the subsequent possible target leaf nodes
+#         # def deleteNode(root, key):
+#         #     if root.left is None and root.right is None and root.val == key:
+#         #         return None
+#         #     else:
+#         #         if root.left:
+#         #             root.left = deleteNode(root.left, key)
+#         #         if root.right:
+#         #             root.right = deleteNode(root.right, key)
+#         #     return root
+        
+#         # return deleteNode(root, target)
+
+#         # Using post order traversal
+#         def dfs(root, key):
+#             if root:
+#                 dfs(root.left, key)
+#                 dfs(root.right, key)
+#                 if root.left and root.left.left is None and root.left.right is None and root.left.val == key:
+#                     root.left = None
+#                 if root.right and root.right.left is None and root.right.right is None and root.right.val == key:
+#                     root.right = None
+#             return root
+        
+#         new_tree = dfs(root, target)
+#         if new_tree.left is None and new_tree.right is None and new_tree.val == target:
+#             return None
+#         return new_tree
