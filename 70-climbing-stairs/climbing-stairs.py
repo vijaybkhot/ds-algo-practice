d75c8e58d634b5454bb8e5edf8e00cc5adb45dae
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-18 07:16:01 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.50%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -19,15 +19,29 @@ class Solution:
         #     return memo[i]
         # return dfs(0)
 
-        # Dynamic Programming:
+        # # Dynamic Programming:
+        # if n <= 2:
+        #     return n
+
+        # dp = [0]*(n+1)
+        # dp[1], dp[2] = 1, 2
+
+        # for i in range(3, n+1):
+        #     dp[i] = dp[i-1] + dp[i-2]
+        
+        # return dp[n]
+
+        # Space optimized DP
         if n <= 2:
             return n
-            
-        dp = [0]*(n+1)
-        dp[1], dp[2] = 1, 2
+        
+        dp_1, dp_2 = 1, 2
 
         for i in range(3, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        
-        return dp[n]
+            dp_curr = dp_1 + dp_2
+            dp_1 = dp_2
+            dp_2 = dp_curr
+
+        return dp_2
+            
             
