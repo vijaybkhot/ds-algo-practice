10ec4fd26903cb89678bcd235d8ff92cb153cba7
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-18 07:12:45 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (78.02%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,14 +1,33 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        memo = [-1] * n
-
-        def dfs(i):
-            if i >= n:
-                return i == n
-            if memo[i] != -1:
-                return memo[i]
-            memo[i] = dfs(i + 1) + dfs(i + 2)
-            return memo[i]
+        # # Recursion:
+        # def dfs(i):
+        #     if i >=n:
+        #         return i == n
             
-        return dfs(0)
+        #     return dfs(i+1) + dfs(i+2)
+
+        ## Memoization
+        # memo = [-1] * n
+        # def dfs(i):
+        #     if i >= n:
+        #         return i == n
+        #     if memo[i] != -1:
+        #         return memo[i]
+        #     memo[i] = dfs(i + 1) + dfs(i + 2)
+        #     return memo[i]
+        # return dfs(0)
+
+        # Dynamic Programming:
+        if n <= 2:
+            return n
+            
+        dp = [0]*(n+1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        
+        return dp[n]
+            
