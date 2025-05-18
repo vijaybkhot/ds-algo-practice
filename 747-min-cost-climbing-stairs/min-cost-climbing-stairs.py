4dccf7174f3c6411558932dfe866cc9b1ecb4c85
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-18 11:19:12 -0400

    Time: 12 ms (5.26%) | Memory: 20.8 MB (7.19%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,55 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # Start = 0
+        n = len(cost)
+        # cost_0, cost_1 = cost[0], cost[1]
+        # i = 0
+        # while i < n:
+        #     cost_first = cost[i+1] if i+1 < n else 0
+        #     cost_second = cost[i+2] if i+2 < n else 0
+        #     if cost_first < cost_second:
+        #         cost_0 += cost_first
+        #         i += 1
+        #     else:
+        #         cost_0 += cost_second
+        #         i += 2
+        
+        # j = 1
+        # while j < len(cost):
+        #     cost_first = cost[j+1] if j+1 < n else 0
+        #     cost_second = cost[j+2] if j+2 < n else 0
+        #     if cost_first < cost_second:
+        #         cost_1 += cost_first
+        #         j += 1
+        #     else:
+        #         cost_1 += cost_second
+        #         j += 2
+            
+        # return min(cost_0, cost_1)
+
+        # # recursion - Brute
+        # res = sum(cost)
+        #         def dfs(i, curr_cost):
+        #     nonlocal res
+        #     if i >= n:
+        #         res = min(res, curr_cost)
+        #         return
+            
+        #     for j in range(1, 3):
+        #         new_cost = curr_cost + cost[i]
+        #         dfs(i+j, new_cost)
+        # dfs(0, 0)
+        # dfs(1, 0)
+        # return res
+
+        # Optimized lru cache recursion
+        n = len(cost)
+        
+        @lru_cache(maxsize=None)
+        def dfs(i):
+            if i >= n:
+                return 0
+            return cost[i] + min(dfs(i + 1), dfs(i + 2))
+
+        return min(dfs(0), dfs(1))
+        
