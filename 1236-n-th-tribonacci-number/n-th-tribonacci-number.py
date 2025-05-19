8ef4471b98996a15f83fd265be0649ec8b4a9dc8
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-19 10:20:55 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.63%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,15 +1,38 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif 1 <= n <= 2:
-            return 1
-        t_0, t_1, t_2 = 0, 1, 1
-        for i in range(3, n+1):
-            t_3 = t_0 + t_1 + t_2
-            t_0 = t_1
-            t_1 = t_2
-            t_2 = t_3
+
+        # # Simple recursion 
+        # def tribonacci(n):
+        #     if n == 0:
+        #         return 0
+        #     elif n == 1 or n == 2:
+        #         return 1
+        #     return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
         
-        return t_2
+        # return tribonacci(n)
+
+        # Memoized recursion 
+        @lru_cache(maxsize=None)
+        def tribonacci(n):
+            if n == 0:
+                return 0
+            elif n == 1 or n == 2:
+                return 1
+            return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
+        
+        return tribonacci(n)
+
+
+        # if n == 0:
+        #     return 0
+        # elif 1 <= n <= 2:
+        #     return 1
+        # t_0, t_1, t_2 = 0, 1, 1
+        # for i in range(3, n+1):
+        #     t_3 = t_0 + t_1 + t_2
+        #     t_0 = t_1
+        #     t_1 = t_2
+        #     t_2 = t_3
+        
+        # return t_2
         
