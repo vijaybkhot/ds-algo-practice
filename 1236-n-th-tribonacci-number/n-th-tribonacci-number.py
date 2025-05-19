be425d02d72934f6730cd7db374cc8b0ba5dfad6
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-19 10:26:03 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.94%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -11,16 +11,36 @@ class Solution:
         
         # return tribonacci(n)
 
-        # Memoized recursion 
-        @lru_cache(maxsize=None)
-        def tribonacci(n):
-            if n == 0:
-                return 0
-            elif n == 1 or n == 2:
-                return 1
-            return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
+        # # Library Memoized recursion 
+        # @lru_cache(maxsize=None)
+        # def tribonacci(n):
+        #     if n == 0:
+        #         return 0
+        #     elif n == 1 or n == 2:
+        #         return 1
+        #     return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
         
-        return tribonacci(n)
+        # return tribonacci(n)
+
+        # Manual memoization
+
+        if n == 0:
+            return 0
+        elif n == 1 or n == 2:
+            return 1
+
+        memo = [-1]*(n+1)
+        memo[0], memo[1], memo[2] = 0, 1, 1
+
+        def tribo(n):
+            if memo[n] != -1:
+                return memo[n]
+            
+            memo[n] = tribo(n-1) + tribo(n-2) + tribo(n-3)
+            return memo[n]
+        
+        return tribo(n)
+
 
 
         # if n == 0:
