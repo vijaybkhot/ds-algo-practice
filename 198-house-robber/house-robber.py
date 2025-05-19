4cdc2bdd7ac956f417aee020f15958a71f25e9ab
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-19 18:33:27 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.59%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -3,13 +3,25 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        nums_record = [(0, 0)]*(n+1)    # with_curr, without_curr
-        nums_record[0] = (nums[0], 0)
-        nums_record[1] = (nums[1], nums_record[0][0])
+        
+        # # Bottom-up Dynamic programming approach
+        # nums_record = [(0, 0)]*(n+1)    # with_curr, without_curr
+        # nums_record[0] = (nums[0], 0)
+        # nums_record[1] = (nums[1], nums_record[0][0])
+
+        # for i in range(2, n):
+        #     nums_record[i] = ((nums[i]+nums_record[i-1][1]), max(nums_record[i-1]))
+        
+        # return max(nums_record[n-1])
+
+        # Space optimized DP
+        max_num_0 = (nums[0], 0)
+        max_num_1 = (nums[1], nums[0])
 
         for i in range(2, n):
-            nums_record[i] = ((nums[i]+nums_record[i-1][1]), max(nums_record[i-1]))
+            curr_max_set = ((nums[i]+ max_num_1[1]), max(max_num_1))
+            max_num_1 = curr_max_set
         
-        return max(nums_record[n-1])
+        return max(max_num_1)
 
         
