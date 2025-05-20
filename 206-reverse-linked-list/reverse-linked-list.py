614da0747a2118ede29eefcd908142623450f5de
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-20 07:21:37 -0400

    Time: 0 ms (100.00%) | Memory: 18.8 MB (38.17%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -1,43 +1,16 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseList(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        # # Iterative solution
-        # prev, curr = None, head
-        # while curr:
-        #     nxt = curr.next
-        #     curr.next = prev
-        #     prev = curr
-        #     curr = nxt
-        # return prev
-
-        # Recursive solution
-        # # Base case: if head is None or only one node, return head
-        # if not head or not head.next:
-        #     return head
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev,curr = None, head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
         
-        # # Recursive call on the rest of the list
-        # new_head = self.reverseList(head.next)
         
-        # # Reverse the current node's pointer
-        # head.next.next = head
-        # head.next = None
-        
-        # return new_head
-
-        # Base case:
-        if not head or not head.next:
-            return head
-        
-        new_head = self.reverseList(head.next)
-        head.next.next = head
-        head.next = None
-
-        return new_head
