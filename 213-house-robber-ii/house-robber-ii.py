017e2242954aa5d42da603d06f10d03b370936dd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-20 11:38:13 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (37.15%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if not nums:
+            return 0
+        if n == 1:
+            return nums[0]
+        if n == 2:
+            return max(nums)
+
+        def rob_normal(houses):
+            if not houses:
+                return 0
+            if len(houses) == 1:
+                return houses[0]
+            
+            dp_0 = (houses[0], 0) #(with, without)
+            for i in range(1, len(houses)):
+                dp_curr = ((houses[i]+dp_0[1]), max(dp_0))
+                dp_0 = dp_curr
+            
+            return max(dp_0)
+
+        return max(rob_normal(nums[:-1]), rob_normal(nums[1:]))
