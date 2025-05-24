aa0a681c287bddf1779b4d833810806389744351
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-24 09:23:40 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (34.30%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -6,26 +6,48 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
 
+        # if not head:
+        #     return []
+        # if not head.next and n == 1:
+        #     return None
+
+        # tail = ListNode()
+        # tail.next = head
+        # count = 0
+        # curr = head
+        # while curr:
+        #     count += 1
+        #     curr = curr.next
+        # first_half = count - n
+        # curr, prev = head, tail
+        # for _ in range(first_half):
+        #     prev = curr
+        #     curr = curr.next
+
+        # prev.next = curr.next
+        # curr.next = None
+        # return tail.next
+
         if not head:
             return []
         if not head.next and n == 1:
             return None
+        
+        slow, fast = head, head
+        for _ in range(n):
+            fast = fast.next
+        
+        if not fast:
+            return head.next
+        
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        
+        deleted_node = slow.next
+        slow.next = slow.next.next
+        deleted_node.next = None
+        return head
 
-        tail = ListNode()
-        tail.next = head
-        count = 0
-        curr = head
-        while curr:
-            count += 1
-            curr = curr.next
-        first_half = count - n
-        curr, prev = head, tail
-        for _ in range(first_half):
-            prev = curr
-            curr = curr.next
-
-        prev.next = curr.next
-        curr.next = None
-        return tail.next
         
         
