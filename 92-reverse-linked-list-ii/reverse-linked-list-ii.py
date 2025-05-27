429eafdc4c480147a0887293be45d05fdf3df19b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-27 19:28:06 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (55.76%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -1,83 +1,48 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseBetween(self, head, left, right):
-        """
-        :type head: Optional[ListNode]
-        :type left: int
-        :type right: int
-        :rtype: Optional[ListNode]
-        """
-        # # Multiple pass approach
-        # if left == right:
-        #     return head
-        # prev_count, left_node = 0, head
-        # for _ in range(left-1):
-        #     prev_count += 1
-        #     left_node = left_node.next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head:
+            return None
 
-        # remainder, right_node = None, left_node
-        # for _ in range(right-left):
-        #     right_node = right_node.next
-        #     remainder = right_node.next
-        
-        # prev, curr = None, left_node
-        # for _ in range(right - left +1):
-        #     tmp = curr.next
-        #     curr.next = prev
-        #     if _ == 0:
-        #         curr.next = remainder
-        #     prev = curr
-        #     curr = tmp
-        
-        # dummy = ListNode()
-        # tail = dummy
-        # tail.next = head
-        
-        # for _ in range(prev_count):
-        #     tail = tail.next
-        
-        # tail.next = None
-        # tail.next = prev
-        # return dummy.next
-
-        # One pass approach
-        # Edge case: If left and right are the same, no need to reverse
         if left == right:
             return head
 
-        # Dummy node to handle edge cases where head might change
-        dummy = ListNode(0)
+        dummy = ListNode()
         dummy.next = head
-        tail = dummy
 
-        # Step 1: Traverse to the node just before the 'left' position
-        left_node = head
-        for _ in range(left - 1):
-            tail = tail.next
-            left_node = left_node.next
+        count = 1
+        curr, prev = head, dummy
+        while curr and count != left:
+            prev = curr
+            curr = curr.next
+            count += 1
         
-        # Step 2: Reverse the sublist from left to right
-        right_node, prev = left_node, None
-        for _ in range(right - left + 1):
-            temp = right_node.next  # Store next node
-            right_node.next = prev  # Reverse link
-            prev = right_node  # Move prev forward
-            right_node = temp  # Move current forward
+        if not curr:
+            return dummy.next
         
-        # Step 3: Reconnect the reversed sublist with the remaining list
-        left_node.next = right_node  # Connect end of reversed list to the remaining part
-        tail.next = prev  # Connect the previous part to the reversed sublist
+        tail1 = prev
+        head2 = curr
+        prev = None
 
-        # Return the updated list
+        while curr and count != right:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+            count += 1
+
+        if curr:
+            head3 = curr.next
+            curr.next = prev
+        if tail1:
+            tail1.next = curr
+        if head2:
+            head2.next = head3
+
         return dummy.next
-            
-
-
-
-
         
         
