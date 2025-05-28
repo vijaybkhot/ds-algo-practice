59293a43e120d1b4def63bfbbfde3c8020e09641
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-28 07:38:49 -0400

    Time: 16 ms (8.78%) | Memory: 18.6 MB (36.53%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.py
+++ 860-design-circular-queue/design-circular-queue.py
@@ -1,152 +1,53 @@
-class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.next = None
 
-class MyCircularQueue(object):
-    # # Array Implementation
-    # def __init__(self, k):
-    #     """
-    #     :type k: int
-    #     """
-    #     self.size = k
-    #     self.queue = [None] * k  
-    #     self.front = self.rear = -1 
-
-
-    # def enQueue(self, value):
-    #     """
-    #     :type value: int
-    #     :rtype: bool
-    #     """
-    #     if (self.rear + 1) % self.size == self.front:
-    #         return False
-    #     if self.front == -1:
-    #         self.front = self.rear = 0
-    #     else:
-    #         self.rear = (self.rear + 1) % self.size
-        
-    #     self.queue[self.rear] = value
-    #     return True
-
-    # def deQueue(self):
-    #     """
-    #     :rtype: bool
-    #     """
-    #     if self.front == -1:
-    #         return False
-
-    #     if self.front == self.rear:
-    #         self.front = self.rear = -1
-    #     else:
-    #         self.front = (self.front + 1) % self.size
-        
-    #     return True
-
-    # def Front(self):
-    #     """
-    #     :rtype: int
-    #     """
-    #     if self.front == -1:
-    #         return -1
-    #     return self.queue[self.front]
-
-    # def Rear(self):
-    #     """
-    #     :rtype: int
-    #     """
-    #     if self.front == -1:
-    #         return -1
-    #     return self.queue[self.rear]
-        
-
-    # def isEmpty(self):
-    #     """
-    #     :rtype: bool
-    #     """
-    #     return self.front == -1
-        
-
-    # def isFull(self):
-    #     """
-    #     :rtype: bool
-    #     """
-    #     return (self.rear + 1) % self.size == self.front
-    
-    # Linked List Implementation
-    def __init__(self, k):
-        """
-        :type k: int
-        """
+class MyCircularQueue:
+    def __init__(self, k: int):
+        self.queue = [None]*k
         self.size = k
-        self.front = self.rear = None
         self.count = 0
+        self.front = 0
+        self.rear = 0
+        
 
-    def enQueue(self, value):
-        """
-        :type value: int
-        :rtype: bool
-        """
-        if self.count == self.size:
+    def enQueue(self, value: int) -> bool:
+        if self.isFull():
             return False
-        newNode = Node(value)
 
-        if self.count == 0:
-            self.front = self.rear = newNode
-            self.rear.next = self.front
-        else:
-            self.rear.next = newNode
-            self.rear = self.rear.next
-            self.rear.next = self.front
-            
+        self.queue[self.rear] = value
+        self.rear = (self.rear + 1) % self.size
         self.count += 1
         return True
-    def deQueue(self):
-        """
-        :rtype: bool
-        """
-        if self.count == 0:
-            return False
+        
 
-        if self.front == self.rear:
-            self.front = self.rear = None
-        else:
-            self.front = self.front.next
-            self.rear.next = self.front
+    def deQueue(self) -> bool:
+        if self.isEmpty():
+            return False
+            
+        self.queue[self.front] = None
+      
+        self.front = (self.front + 1) % self.size
         
         self.count -= 1
         return True
-
-    def Front(self):
-        """
-        :rtype: int
-        """
-        if self.count == 0:
-            return -1
-
-        return self.front.value
-
-    def Rear(self):
-        """
-        :rtype: int
-        """
-        if self.count == 0:
-            return -1
         
-        return self.rear.value
-    
-    def isEmpty(self):
-        """
-        :rtype: bool
-        """
+
+    def Front(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.queue[self.front]
+        
+
+    def Rear(self) -> int:
+        if self.isEmpty():
+            return -1
+        return self.queue[(self.rear - 1 + self.size) % self.size]
+        
+
+    def isEmpty(self) -> bool:
         return self.count == 0
         
-
-    def isFull(self):
-        """
-        :rtype: bool
-        """
+    def isFull(self) -> bool:
         return self.count == self.size
+
         
 
 
