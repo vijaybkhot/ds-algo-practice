c7aefd2348c3cb2a4426057147bc564ddd7f1b4f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-28 06:18:37 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (55.76%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -5,41 +5,53 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        if not head:
-            return None
-
-        if left == right:
+        if not head or left == right:
             return head
 
-        dummy = ListNode()
+        dummy = ListNode(0)
         dummy.next = head
+        prev = dummy
 
-        count = 1
-        curr, prev = head, dummy
-        while curr and count != left:
-            prev = curr
-            curr = curr.next
-            count += 1
+        # while curr and count != left:
+        #     prev = curr
+        #     curr = curr.next
+        #     count += 1
         
-        tail1 = prev
-        head2 = curr
-        prev = None
+        # tail1 = prev
+        # head2 = curr
+        # prev = None
 
-        while curr and count != right:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-            count += 1
+        # while curr and count != right:
+        #     nxt = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = nxt
+        #     count += 1
 
-        if curr:
-            head3 = curr.next
-            curr.next = prev
-        if tail1:
-            tail1.next = curr
-        if head2:
-            head2.next = head3
+        # if curr:
+        #     head3 = curr.next
+        #     curr.next = prev
+        # if tail1:
+        #     tail1.next = curr
+        # if head2:
+        #     head2.next = head3
 
+        # return dummy.next
+
+        # More readable solution
+        for _ in range(left-1):
+            prev = prev.next
+        
+        # `start` is the first node to reverse
+        start = prev.next
+        then = start.next
+
+        for _ in range(right-left):
+            start.next = then.next
+            then.next = prev.next
+            prev.next = then
+            then = start.next
+        
         return dummy.next
         
         
