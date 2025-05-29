d58ca45ddcb4ae4fc8264b68a810c237ed7171cd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-29 18:03:04 -0400

    Time: 771 ms (51.27%) | Memory: 18.2 MB (56.44%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -48,19 +48,29 @@ class Solution:
 
         # return minCoins if minCoins < float('inf') else -1
 
-        # Memoized recursion:
-        memo = {0: 0}
-        def dfs(amount):
-            if amount in memo:
-                return memo[amount]
+        # # Memoized recursion: top-down DP
+        # memo = {0: 0}
+        # def dfs(amount):
+        #     if amount in memo:
+        #         return memo[amount]
         
             
-            res = float('inf')
-            for coin in coins:
-                if amount - coin >= 0:
-                    res = min(res, 1 + dfs(amount-coin))
-            memo[amount] = res
-            return res
+        #     res = float('inf')
+        #     for coin in coins:
+        #         if amount - coin >= 0:
+        #             res = min(res, 1 + dfs(amount-coin))
+        #     memo[amount] = res
+        #     return res
 
-        res = dfs(amount)
-        return res if res != float('inf') else -1
+        # res = dfs(amount)
+        # return res if res != float('inf') else -1
+
+        # Bottom-up DP
+        dp = [float('inf')]*(amount+1)
+        dp[0] = 0
+        for a in range(1, amount+1):
+            for coin in coins:
+                if a - coin >=0:
+                    dp[a] = min(dp[a], 1+dp[a-coin])
+        
+        return dp[amount] if dp[amount] != float('inf') else -1
