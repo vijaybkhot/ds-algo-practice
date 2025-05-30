0f544e6d25900ced8e5d44c9d41f97fbf5f1ad75
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-30 13:37:41 -0400

    Time: 2 ms (76.04%) | Memory: 17.6 MB (97.60%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -11,21 +11,35 @@ class Solution:
         # return start == end
 
 
-        wordSet = set(wordDict)
-        memo = set()
+        # # Top-down DP
+        # wordSet = set(wordDict)
+        # memo = set()
 
-        def dfs(start):
-            if start in memo:
-                return False
-            if start == len(s):
-                return True
+        # def dfs(start):
+        #     if start in memo:
+        #         return False
+        #     if start == len(s):
+        #         return True
 
-            for end in range(start + 1, len(s) + 1):
-                if s[start:end] in wordSet and dfs(end):
-                    return True
+        #     for end in range(start + 1, len(s) + 1):
+        #         if s[start:end] in wordSet and dfs(end):
+        #             return True
 
-            memo.add(start)  # mark this start as leading to a dead end
-            return False
+        #     memo.add(start)  # mark this start as leading to a dead end
+        #     return False
+
+        # Bottom-up DP
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        for i in range(len(s) - 1, -1, -1):
+            for w in wordDict:
+                if (i + len(w)) <= len(s) and s[i : i + len(w)] == w:
+                    dp[i] = dp[i + len(w)]
+                if dp[i]:
+                    break
+
+        return dp[0]
 
         return dfs(0)
 
