50083b8dc04f444260d91c32b4782d0f2bce1b97
Vijay Khot <vijaysinh.khot@gmail.com>
2025-05-31 08:08:53 -0400

    Time: 132 ms (66.19%) | Memory: 78.4 MB (55.67%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,147 +1,59 @@
-# class ListNode:
-#     def __init__(self, key=0, value=0, next=None, prev=None):
-#         self.key = key
-#         self.value = value
-#         self.next = next
-#         self.prev = prev
-
-# class LRUCache(object):
-
-#     def __init__(self, capacity):
-#         """
-#         :type capacity: int
-#         """
-#         self.size = capacity
-#         self.count = 0
-#         self.map = {}
-#         self.front, self.rear = None, None
-    
-#     def remove_node(self, node):
-#         """Helper function to remove a node from the doubly linked list"""
-#         if node.prev:
-#             node.prev.next = node.next
-#         if node.next:
-#             node.next.prev = node.prev
-#         if node == self.front:  # If it's the front, update self.front
-#             self.front = self.front.next
-#             if self.front:
-#                 self.front.prev = None
-#         if node == self.rear:  # If it's the rear, update self.rear
-#             self.rear = self.rear.prev
-#             if self.rear:
-#                 self.rear.next = None
-
-
-#     def get(self, key):
-#         """
-#         :type key: int
-#         :rtype: int
-#         """
-#         if key not in self.map:
-#             return -1
-        
-#         node = self.map[key]
-
-#         if node != self.rear:
-#             self.remove_node(node)
-#             self.rear.next = node
-#             node.prev = self.rear
-#             node.next = None
-#             self.rear = node
-
-#         return node.value
-
-
-#     def put(self, key, value):
-#         if key in self.map:
-#             node = self.map[key]
-#             node.value = value
-#             self.get(key)  # Move to the rear
-#             return
-
-#         new_node = ListNode(key=key, value=value)
-
-#         if self.count == self.size:
-#             # Cache is full: remove the least recently used node (front)
-#             del self.map[self.front.key]
-#             self.remove_node(self.front)
-#             self.count -= 1
-
-#         # Insert the new node at the rear
-#         if not self.front:
-#             self.front = self.rear = new_node
-#         else:
-#             self.rear.next = new_node
-#             new_node.prev = self.rear
-#             self.rear = new_node
-
-#         self.map[key] = new_node
-#         self.count += 1
-        
-
-# Simplified solution:
-
 class ListNode:
-    def __init__(self, key=0, value=0):
+    def __init__(self, key=0, val=0, nxt=None, prev=None):
         self.key = key
-        self.value = value
-        self.next = None
-        self.prev = None
+        self.val = val
+        self.next = nxt 
+        self.prev = prev
 
-class LRUCache(object):
+class LRUCache:
 
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
-        self.capacity = capacity
-        self.cache = {}  
-        self.head = ListNode() 
-        self.tail = ListNode()  
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.count = 0
+        self.dict = {}
+        self.head = ListNode()
+        self.tail = ListNode()
         self.head.next = self.tail
         self.tail.prev = self.head
-    
-    def _remove(self, node):
+        
+
+    def get(self, key: int) -> int:
+        if self.count == 0 or key not in self.dict:
+            return -1
+        node = self.dict[key]
         prev_node, next_node = node.prev, node.next
         prev_node.next, next_node.prev = next_node, prev_node
-    
-
-    def _insert_at_end(self, node):
-        prev, last = self.tail.prev, self.tail
-        prev.next = last.prev = node
-        node.prev, node.next = prev, last
+        head_node = self.head.next
+        node.next, node.prev = head_node, self.head
+        self.head.next, head_node.prev = node, node
+        return node.val
 
 
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        if key not in self.cache:
-            return -1
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict:
+            existing_node = self.dict[key]
+            existing_node.val = value
+            prev_node, next_node = existing_node.prev, existing_node.next
+            prev_node.next, next_node.prev = next_node, prev_node
+            head_node = self.head.next
+            self.head.next, head_node.prev = existing_node, existing_node
+            existing_node.next, existing_node.prev = head_node, self.head
+            return
+        # Remove the last node, if the cache is full
+        if self.count == self.size:
+            node_to_delete = self.tail.prev
+            node_to_delete.prev.next = self.tail
+            self.tail.prev = node_to_delete.prev
+            node_to_delete.next, node_to_delete.prev = None, None
+            del self.dict[node_to_delete.key]
+            self.count -= 1
+        new_node = ListNode(key=key, val=value)
+        head_node = self.head.next
+        head_node.prev, self.head.next = new_node, new_node
+        new_node.next, new_node.prev = head_node, self.head
+        self.dict[key] = new_node
+        self.count += 1
         
-        node = self.cache[key]
-        self._remove(node)
-        self._insert_at_end(node)
-
-        return node.value
-
-
-    def put(self, key, value):
-        if key in self.cache:
-            node = self.cache[key]
-            node.value = value
-            self._remove(node)
-            self._insert_at_end(node)
-        else:
-            if len(self.cache) == self.capacity:
-                lru_node = self.head.next
-                self._remove(lru_node)
-                del self.cache[lru_node.key]
-            new_node = ListNode(key=key, value=value)
-            self.cache[key] = new_node
-            self._insert_at_end(new_node)
-
 
 
 # Your LRUCache object will be instantiated and called as such:
