10a2b61b69e5277086fc2b2ba284fb1e4ccda7fb
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-01 08:13:34 -0400

    Time: 199 ms (24.85%) | Memory: 80.2 MB (35.46%) - LeetSync

--- 460-lfu-cache/lfu-cache.py
+++ 460-lfu-cache/lfu-cache.py
@@ -1,80 +1,102 @@
-from collections import defaultdict
-
 class ListNode:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+        self.use_count = 1  # For LFU
 
-    def __init__(self, val, prev=None, next=None):
-        self.val = val
-        self.prev = prev
-        self.next = next
-
-class LinkedList:
-
+class DoublyLinkedList:
     def __init__(self):
-        self.left = ListNode(0)
-        self.right = ListNode(0, self.left)
-        self.left.next = self.right
-        self.map = {}
+        self.head = ListNode()  # Dummy head
+        self.tail = ListNode()  # Dummy tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.count = 0  # Number of real nodes
+        self.key_map = {}  # key -> node
 
-    def length(self):
-        return len(self.map)
+    def insert_at_head(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+        self.key_map[node.key] = node
+        self.count += 1
 
-    def pushRight(self, val):
-        node = ListNode(val, self.right.prev, self.right)
-        self.map[val] = node
-        self.right.prev = node
-        node.prev.next = node
+    def remove_node(self, node):
+        prev_node, next_node = node.prev, node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+        del self.key_map[node.key]
+        self.count -= 1
 
-    def pop(self, val):
-        if val in self.map:
-            node = self.map[val]
-            next, prev = node.next, node.prev
-            next.prev = prev
-            prev.next = next
-            self.map.pop(val, None)
-    
-    def popLeft(self):
-        res = self.left.next.val
-        self.pop(self.left.next.val)
-        return res
-    
-    def update(self, val):
-        self.pop(val)
-        self.pushRight(val)
+    def pop_from_tail(self):
+        # Remove the least recently used node (before dummy tail)
+        if self.count == 0:
+            return None
+        lru_node = self.tail.prev
+        self.remove_node(lru_node)
+        return lru_node
+
+    def is_empty(self):
+        return self.count == 0
+
 
 class LFUCache:
 
-    def __init__(self, capacity):
-        self.cap = capacity
-        self.lfuCnt = 0
-        self.valMap = {}  # Map key -> val
-        self.countMap = defaultdict(int)  # Map key -> count
-        # Map count of key -> linkedlist
-        self.listMap = defaultdict(LinkedList)
+    def __init__(self, capacity: int):
+        self.size = capacity
+        self.count = 0
+        self.key_node_map = {}
+        self.frequency_nodes_map = defaultdict(DoublyLinkedList)
+        self.min_count = 0
+        
 
-    def counter(self, key):
-        cnt = self.countMap[key]
-        self.countMap[key] += 1
-        self.listMap[cnt].pop(key)
-        self.listMap[cnt + 1].pushRight(key)
-
-        if cnt == self.lfuCnt and self.listMap[cnt].length() == 0:
-            self.lfuCnt += 1
-
-    def get(self, key):
-        if key not in self.valMap:
+    def get(self, key: int) -> int:
+        if key not in self.key_node_map or self.size == 0:
             return -1
-        self.counter(key)
-        return self.valMap[key]
+        node = self.key_node_map[key]
+        old_freq = node.use_count
+        self.frequency_nodes_map[old_freq].remove_node(node)
+        
+        if self.frequency_nodes_map[old_freq].is_empty():
+            del self.frequency_nodes_map[old_freq]
+            if self.min_count == old_freq:
+                self.min_count += 1
 
-    def put(self, key, value):
-        if self.cap == 0:
+        # Increment frequency and insert into new freq list
+        node.use_count += 1
+        self.frequency_nodes_map[node.use_count].insert_at_head(node)
+
+        return node.value
+
+    def put(self, key: int, value: int) -> None:
+        if self.size == 0:
             return
         
-        if key not in self.valMap and len(self.valMap) == self.cap:
-            res = self.listMap[self.lfuCnt].popLeft()
-            self.valMap.pop(res)
-            self.countMap.pop(res)
+        if key in self.key_node_map:
+            node = self.key_node_map[key]
+            node.value = value
+            self.get(key)  # Reuses get() logic to update frequency
+            return
         
-        self.valMap[key] = value
-        self.counter(key)
-        self.lfuCnt = min(self.lfuCnt, self.countMap[key])
+        # Evict if full
+        if self.count == self.size:
+            evict_list = self.frequency_nodes_map[self.min_count]
+            evicted_node = evict_list.pop_from_tail()
+            del self.key_node_map[evicted_node.key]
+            self.count -= 1
+        
+        # Create and insert new node
+        new_node = ListNode(key, value)
+        self.key_node_map[key] = new_node
+        self.frequency_nodes_map[1].insert_at_head(new_node)
+        self.min_count = 1
+        self.count += 1
+        
+
+
+# Your LFUCache object will be instantiated and called as such:
+# obj = LFUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
