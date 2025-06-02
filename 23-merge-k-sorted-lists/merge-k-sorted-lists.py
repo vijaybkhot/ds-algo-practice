092814306c555372fc3cfd6b505503e3d08f657d
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-02 07:03:25 -0400

    Time: 1146 ms (11.51%) | Memory: 20 MB (97.65%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -1,85 +1,37 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[Optional[ListNode]]
-        :rtype: Optional[ListNode]
-        """
-        # # Brute Force approach - O(N*k) time complexity 
-        # dummy = ListNode()
-        # curr = dummy
-
-        # def countListLen(head):
-        #     node = head
-        #     count = 0
-        #     while node:
-        #         count += 1
-        #         node = node.next
-        #     return count
-        
-        # listsLen = list(map(lambda lst: countListLen(lst), lists))
-        # sum_len = sum(listsLen) if listsLen else 0
-        # for i in range(sum_len):
-        #     min_val = float('infinity')
-        #     min_node = None
-        #     # Get the minimum value node from all heads
-        #     for j in range(len(lists)):
-        #         if lists[j] and lists[j].val < min_val:
-        #             min_val = lists[j].val
-        #             min_node = j
-        #     curr.next = lists[min_node]
-        #     curr = curr.next
-        #     lists[min_node] = lists[min_node].next
-        
-        # curr.next = None
-        # return dummy.next
-
-        # Optimized approach
-        # Edge cases
-        if len(lists) == 0 or not lists:
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
             return None
-
-        def mergeTwoLists(list1, list2):
+        def mergeTwoLists(l1, l2):
             dummy = ListNode()
             tail = dummy
-
-            while list1 and list2:
-                if list1.val < list2.val:
-                    tail.next = list1
-                    list1 = list1.next
+            while l1 and l2:
+                if l1.val < l2.val:
+                    tail.next = l1
+                    l1 = l1.next
+                    tail = tail.next
                 else:
-                    tail.next = list2
-                    list2 = list2.next
-                tail = tail.next
-            
-            if list1:
-                tail.next = list1
-    
-            if list2:
-                tail.next = list2
+                    tail.next = l2
+                    l2 = l2.next
+                    tail = tail.next
+            if l1:
+                tail.next = l1
+            if l2:
+                tail.next = l2
             
             return dummy.next
-        
-        while len(lists) > 1:
-            merged_list = []
-            for i in range(0, len(lists), 2):
-                list1 = lists[i]
-                list2 = lists[i+1] if i+1 < len(lists) else None
-                merged_list.append(mergeTwoLists(list1, list2))
-            lists = merged_list
-        
-        return lists[0]
-
-
             
-                
-
-
-                
-
-
-        
+        dummy = ListNode()
+        tail = dummy
+        tail.next = lists[0]
+        tail = tail.next
+        for i in range(1, len(lists)):
+            merged_list = mergeTwoLists(tail, lists[i])
+            dummy.next = merged_list
+            tail = dummy.next
+        return dummy.next
