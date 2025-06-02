6d18ea5afdcf40b980de127e515b081ac2827748
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-02 17:48:37 -0400

    Time: 5912 ms (5.02%) | Memory: 33.4 MB (11.50%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -11,20 +11,34 @@ class Solution:
 
         self.min_count = n
         memo = {}
+        # def dfs(path, curr_sum):
+        #     if curr_sum in memo and path >= memo[curr_sum]:
+        #         return
+        #     if curr_sum == n:
+        #         self.min_count = min(self.min_count, path)
+        #         return
+            
+        #     if curr_sum > n:
+        #         return
+        
+        #     for num in squares:
+        #         dfs(path+1, curr_sum+num)
+
+        #     memo[curr_sum] = min(memo.get(curr_sum, float('inf')), path)
+
         def dfs(path, curr_sum):
-            if curr_sum in memo and path >= memo[curr_sum]:
-                return
             if curr_sum == n:
                 self.min_count = min(self.min_count, path)
                 return
-            
+
             if curr_sum > n:
                 return
-        
-            for num in squares:
-                dfs(path+1, curr_sum+num)
 
-            memo[curr_sum] = min(memo.get(curr_sum, float('inf')), path)
+            # Only explore deeper if this path to curr_sum is better (shorter)
+            if curr_sum not in memo or path < memo[curr_sum]:
+                memo[curr_sum] = path
+                for num in squares:
+                    dfs(path + 1, curr_sum + num)
         
         dfs(0, 0)
         return self.min_count
