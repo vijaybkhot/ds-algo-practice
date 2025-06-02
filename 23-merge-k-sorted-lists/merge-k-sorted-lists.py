706dc7c596e83973c28a91551bc856915fb64599
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-02 07:55:33 -0400

    Time: 7 ms (90.67%) | Memory: 20.3 MB (36.45%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -8,45 +8,38 @@ class Solution:
         if not lists:
             return None
         
-        # Appraoch - I: Using a function to merge two lists and iteratively merging two lists in the lists array
-        def mergeTwoLists(l1, l2):
-            dummy = ListNode()
-            tail = dummy
-            while l1 and l2:
-                if l1.val < l2.val:
-                    tail.next = l1
-                    l1 = l1.next
-                    tail = tail.next
-                else:
-                    tail.next = l2
-                    l2 = l2.next
-                    tail = tail.next
-            if l1:
-                tail.next = l1
-            if l2:
-                tail.next = l2
+        # # Appraoch - I: Using a function to merge two lists and iteratively merging two lists in the lists array
+        # def mergeTwoLists(l1, l2):
+        #     dummy = ListNode()
+        #     tail = dummy
+        #     while l1 and l2:
+        #         if l1.val < l2.val:
+        #             tail.next = l1
+        #             l1 = l1.next
+        #             tail = tail.next
+        #         else:
+        #             tail.next = l2
+        #             l2 = l2.next
+        #             tail = tail.next
+        #     if l1:
+        #         tail.next = l1
+        #     if l2:
+        #         tail.next = l2
             
-            return dummy.next
+        #     return dummy.next
             
-        dummy = ListNode()
-        tail = dummy
-        # tail.next = lists[0]
-        # tail = tail.next
-        # for i in range(1, len(lists)):
-        #     merged_list = mergeTwoLists(tail, lists[i])
-        #     dummy.next = merged_list
-        #     tail = dummy.next
-        # return dummy.next
+        # dummy = ListNode()
+        # tail = dummy
 
-        while len(lists) > 1:
-            merged_list = []
-            for i in range(0, len(lists), 2):
-                list1 = lists[i]
-                list2 = lists[i+1] if i+1 < len(lists) else None
-                merged_list.append(mergeTwoLists(list1, list2))
-            lists = merged_list
+        # while len(lists) > 1:
+        #     merged_list = []
+        #     for i in range(0, len(lists), 2):
+        #         list1 = lists[i]
+        #         list2 = lists[i+1] if i+1 < len(lists) else None
+        #         merged_list.append(mergeTwoLists(list1, list2))
+        #     lists = merged_list
         
-        return lists[0]
+        # return lists[0]
 
         # # Approach II: Counting the maximum length of a list and iterating that many number of times to merge all lists together
         # dummy = ListNode()
@@ -72,3 +65,21 @@ class Solution:
         # Approach III: Using Priority Queue
         pq = []
 
+        for idx, l in enumerate(lists):
+            if l:
+                heapq.heappush(pq, (l.val, idx))
+
+        dummy = ListNode()
+        tail = dummy
+        while pq:
+            val, idx = heapq.heappop(pq)
+            node = lists[idx]
+            if node:
+                tail.next = node
+                tail = tail.next
+                node = node.next
+                if node is not None:
+                    heapq.heappush(pq, (node.val, idx))
+                lists[idx] = node
+        
+        return dummy.next
