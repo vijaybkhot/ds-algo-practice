3c295f1dcb885e6b859f54012ed3de6042150a6c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-02 08:39:05 -0400

    Time: 1748 ms (36.62%) | Memory: 70.1 MB (26.10%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -2,6 +2,7 @@ class Solution:
     def countPrimes(self, n: int) -> int:
         if n == 0 or n == 1:
             return 0
+            
         is_prime = [True] * (n)
         is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime
 
