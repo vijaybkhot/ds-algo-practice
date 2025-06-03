6696d00113a5b13b28eaa4d45eefca268b07bcea
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-03 07:11:44 -0400

    Time: 2 ms (55.72%) | Memory: 17.9 MB (35.25%) - LeetSync

---
+++ 343-integer-break/integer-break.py
@@ -0,0 +1,16 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [0]*(n+1)
+        dp[1] = 1
+        dp[2] = 1  # (max_product, index)
+
+        for i in range(3, n+1):
+            l, r = 1, i-1
+            curr_max = float('-inf')
+            while l <= r:
+                curr_max = max(curr_max, max(dp[l], l) * max(dp[r], r))
+                l += 1
+                r -= 1
+            dp[i] = curr_max
+        return dp[n]
+
