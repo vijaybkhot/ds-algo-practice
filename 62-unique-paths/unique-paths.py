37b8f5ce64856bbfd2ac79ac7ac7076694d82e00
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-04 15:33:01 -0400

    Time: 1 ms (31.71%) | Memory: 17.7 MB (64.69%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,17 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # # Dynamic Programming bottom-up solution
-        # dp = [[0]*n for _ in range(m)]
-        # for i in range(m):
-        #     for j in range(n):
-        #         if i == m-1 or j == n-1:
-        #             dp[i][j] = 1
+        # Dynamic Programming bottom-up solution
+        dp = [[1]*n for _ in range(m)]
         
-        # for i in range(m-2, -1, -1):
-        #     for j in range(n-2, -1, -1):
-        #         dp[i][j] = dp[i+1][j]+dp[i][j+1]
         
-        # return dp[0][0]
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                dp[i][j] = dp[i+1][j]+dp[i][j+1]
+        
+        return dp[0][0]
 
         # # Recursive solution
         # def dfs(i, j):
