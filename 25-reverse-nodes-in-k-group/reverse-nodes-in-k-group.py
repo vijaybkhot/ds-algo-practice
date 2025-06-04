8658218c2cda5b57112625aa17a7f05abbb81c66
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-04 06:32:27 -0400

    Time: 5 ms (7.37%) | Memory: 18.5 MB (58.50%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -1,57 +1,47 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseKGroup(self, head, k):
-        """
-        :type head: Optional[ListNode]
-        :type k: int
-        :rtype: Optional[ListNode]
-        """
-        def countNodes(list_head):
-            curr = list_head
-            count = 0
-            while list_head:
-                count += 1
-                list_head = list_head.next
-            return count
-
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         dummy = ListNode()
         dummy.next = head
-        tail, curr = dummy, dummy.next
-        node_count = countNodes(head)
-        while curr:
-            prev = None
-            if node_count < k:
-                tail.next = curr
-                break
-            for _ in range(k):
-                if curr:
-                    tmp = curr.next
-                    curr.next = prev
-                    prev = curr
-                    curr = tmp
-                else:
-                    break
-        
+        curr = head
+        prev = dummy
+
+        def count_nodes(list_head: Optional[ListNode]):
+            curr = list_head
+            count = 0
+            while curr:
+                count += 1
+                curr = curr.next
+            return count
+
+        def reverse_k(list_head: Optional[ListNode], prev: Optional[ListNode], num_nodes: int):
+            curr = list_head
+            tail = ListNode()
+            tail.next = curr
+            new_prev = None
+            while curr and num_nodes:
+                num_nodes -= 1
+                nxt = curr.next
+                curr.next = new_prev
+                new_prev = curr
+                curr = nxt
             tail.next.next = curr
-            tail.next = prev
-            for _ in range(k):
-                tail = tail.next
-            node_count -= k
+            prev.next = new_prev
+            return tail.next
 
+        while curr:
+            if count_nodes(curr) < k:
+                break
+            prev = reverse_k(curr, prev, k)
+            curr = prev.next
+        
         return dummy.next
-            
 
 
 
 
-
-
-        
-
-
-
-        
+                
