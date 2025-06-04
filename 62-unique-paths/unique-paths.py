01d3245ca5002c6ba5090f0de9b9cd627558babd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-04 15:19:25 -0400

    Time: 3 ms (22.23%) | Memory: 18.2 MB (5.87%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,13 +1,38 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0]*n for _ in range(m)]
-        for i in range(m):
-            for j in range(n):
-                if i == m-1 or j == n-1:
-                    dp[i][j] = 1
+        # # Dynamic Programming bottom-up solution
+        # dp = [[0]*n for _ in range(m)]
+        # for i in range(m):
+        #     for j in range(n):
+        #         if i == m-1 or j == n-1:
+        #             dp[i][j] = 1
         
-        for i in range(m-2, -1, -1):
-            for j in range(n-2, -1, -1):
-                dp[i][j] = dp[i+1][j]+dp[i][j+1]
+        # for i in range(m-2, -1, -1):
+        #     for j in range(n-2, -1, -1):
+        #         dp[i][j] = dp[i+1][j]+dp[i][j+1]
         
-        return dp[0][0]
+        # return dp[0][0]
+
+        # # Recursive solution
+        # def dfs(i, j):
+        #     if i == m-1 or j == n-1:
+        #         return 1
+        #     curr_total = dfs(i+1, j) + dfs(i, j+1)
+        #     return curr_total
+        
+        # return dfs(0, 0)
+
+        # DP top down solution
+        dp = {}
+        def dfs(i, j):
+            if i == m-1 or j == n-1:
+                return 1
+            if (i, j) in dp:
+                return dp[(i, j)]
+            curr_total = dfs(i+1, j) + dfs(i, j+1)
+            dp[(i, j)] = curr_total
+            return curr_total
+        
+        
+        return dfs(0, 0)
+            
