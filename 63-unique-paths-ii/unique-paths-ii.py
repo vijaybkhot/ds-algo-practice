58fff3de349648c850c13901284f504508d3f534
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-04 16:52:08 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (10.91%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -10,12 +10,32 @@ class Solution:
                     obstacleGrid[i][j] = -1
                
 
-        for i in range(m - 1 if m > 1 else 0, -1, -1):
-            for j in range(n - 1, -1, -1):
-                if i == m-1 and j == n-1:
-                    obstacleGrid[i][j] = 1
-                else:
-                    right = obstacleGrid[i][j + 1] if (j + 1 < n and obstacleGrid[i][j + 1] != -1) else 0
-                    bottom = obstacleGrid[i + 1][j] if (i + 1 < m and obstacleGrid[i + 1][j] != -1) else 0
-                    obstacleGrid[i][j] = right + bottom if obstacleGrid[i][j] != -1 else 0
-        return obstacleGrid[0][0]
+        # # Bottom-up DP
+        # for i in range(m - 1 if m > 1 else 0, -1, -1):
+        #     for j in range(n - 1, -1, -1):
+        #         if i == m-1 and j == n-1:
+        #             obstacleGrid[i][j] = 1
+        #         else:
+        #             right = obstacleGrid[i][j + 1] if (j + 1 < n and obstacleGrid[i][j + 1] != -1) else 0
+        #             bottom = obstacleGrid[i + 1][j] if (i + 1 < m and obstacleGrid[i + 1][j] != -1) else 0
+        #             obstacleGrid[i][j] = right + bottom if obstacleGrid[i][j] != -1 else 0
+        # return obstacleGrid[0][0]
+
+        # Top-down DP
+        dp = {}
+        def dfs(i, j):
+            if i == m-1 and j == n-1:
+                return 1
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            if obstacleGrid[i][j] == -1:
+                obstacleGrid[i][j] = 0
+            else:
+                right = dfs(i, j+1) if (j+1 < n and obstacleGrid[i][j+1] != -1) else 0
+                bottom = dfs(i+1, j) if (i+1 < m and obstacleGrid[i+1][j] != -1) else 0
+                obstacleGrid[i][j] = right + bottom
+            dp[(i, j)] = obstacleGrid[i][j]
+            return obstacleGrid[i][j]
+            
+        return dfs(0, 0)
