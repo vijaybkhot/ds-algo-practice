9c82bb3ddcf4280f9f2b3a23fea0749095ae9f56
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-05 07:38:01 -0400

    Time: 58 ms (10.48%) | Memory: 28.2 MB (9.45%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,14 +1,30 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        # Bottom-up Dynamic programming:
-        m, n = len(grid), len(grid[0])
-
-        for i in range(m-1, -1, -1):
-            for j in range(n-1, -1, -1):
-                if i == m-1 and j == n-1:
-                    continue
-                bottom = float('inf') if i == m-1 else grid[i+1][j]
-                right = float('inf') if j+1 >= n else grid[i][j+1]
-                grid[i][j] = grid[i][j] + min(bottom, right)
+        # # Bottom-up Dynamic programming:
+        # m, n = len(grid), len(grid[0])
+        # for i in range(m-1, -1, -1):
+        #     for j in range(n-1, -1, -1):
+        #         if i == m-1 and j == n-1:
+        #             continue
+        #         bottom = float('inf') if i == m-1 else grid[i+1][j]
+        #         right = float('inf') if j+1 >= n else grid[i][j+1]
+        #         grid[i][j] = grid[i][j] + min(bottom, right)
                 
-        return grid[0][0]
+        # return grid[0][0]
+
+        # Top-down DP:
+        dp = {}
+        m, n = len(grid), len(grid[0])
+        def dfs(i, j):
+            if i == m-1 and j == n-1:
+                return grid[i][j]
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            bottom = float('inf') if i == m-1 else dfs(i+1, j)
+            right = float('inf') if j+1 >= n else dfs(i, j+1)
+            total = grid[i][j] + min(bottom, right)
+            dp[(i, j)] = total
+            return total
+        
+        return dfs(0, 0)
