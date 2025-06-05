063e111297ea2264a0e81d69ee1be3aafde568dc
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-05 07:59:55 -0400

    Time: 8 ms (95.73%) | Memory: 18.7 MB (94.51%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -12,19 +12,30 @@ class Solution:
                 
         # return grid[0][0]
 
-        # Top-down DP:
-        dp = {}
-        m, n = len(grid), len(grid[0])
-        def dfs(i, j):
-            if i == m-1 and j == n-1:
-                return grid[i][j]
-            if (i, j) in dp:
-                return dp[(i, j)]
+        # # Top-down DP: Recursion with caching
+        # dp = {}
+        # m, n = len(grid), len(grid[0])
+        # def dfs(i, j):
+        #     if i == m-1 and j == n-1:
+        #         return grid[i][j]
+        #     if (i, j) in dp:
+        #         return dp[(i, j)]
 
-            bottom = float('inf') if i == m-1 else dfs(i+1, j)
-            right = float('inf') if j+1 >= n else dfs(i, j+1)
-            total = grid[i][j] + min(bottom, right)
-            dp[(i, j)] = total
-            return total
+        #     bottom = float('inf') if i == m-1 else dfs(i+1, j)
+        #     right = float('inf') if j+1 >= n else dfs(i, j+1)
+        #     total = grid[i][j] + min(bottom, right)
+        #     dp[(i, j)] = total
+        #     return total
         
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+        ROWS, COLS = len(grid), len(grid[0])
+        dp = [float("inf")] * (COLS + 1)
+        dp[COLS - 1] = 0
+
+        for r in range(ROWS - 1, -1, -1):
+            for c in range(COLS - 1, -1, -1):
+                dp[c] = grid[r][c] + min(dp[c], dp[c + 1])
+
+        return dp[0]
+                
