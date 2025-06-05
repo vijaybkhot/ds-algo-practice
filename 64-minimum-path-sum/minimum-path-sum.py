10018d34df989d1f687e63356b30678b74115411
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-05 07:33:28 -0400

    Time: 20 ms (31.29%) | Memory: 20 MB (72.58%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -5,13 +5,10 @@ class Solution:
 
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
-                bottom = 0 if i == m-1 else grid[i+1][j]
-                right = 0 if j+1 >= n else grid[i][j+1]
-                if i == m-1:
-                    grid[i][j] = grid[i][j] + right
-                elif j == n-1:
-                    grid[i][j] = grid[i][j] + bottom
-                else:
-                    grid[i][j] = grid[i][j] + min(bottom, right)
-        
+                if i == m-1 and j == n-1:
+                    continue
+                bottom = float('inf') if i == m-1 else grid[i+1][j]
+                right = float('inf') if j+1 >= n else grid[i][j+1]
+                grid[i][j] = grid[i][j] + min(bottom, right)
+                
         return grid[0][0]
