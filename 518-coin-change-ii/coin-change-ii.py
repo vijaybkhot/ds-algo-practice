df97924a75a18e2497831dfaeae2a228259de425
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-06 14:50:53 -0400

    Time: 1149 ms (10.56%) | Memory: 274.3 MB (8.97%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+
+        dp = {}
+        def dfs(i, curr_sum):
+            if curr_sum == amount:
+                return 1
+            if i >= len(coins) or curr_sum > amount:
+                return 0
+            if (i, curr_sum) in dp:
+                return dp[(i, curr_sum)]
+            total = 0
+
+            total += dfs(i, curr_sum+coins[i])
+            total += dfs(i+1, curr_sum)
+            
+
+            dp[(i, curr_sum)] = total
+            return total
+        
+        return dfs(0, 0)
+
+        
