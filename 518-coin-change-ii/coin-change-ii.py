c76dbbdce0684931e73f6959856faf08c7d94c83
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-06 16:20:27 -0400

    Time: 119 ms (88.64%) | Memory: 18.1 MB (72.00%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,21 +1,32 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
 
-        dp = {}
-        def dfs(i, curr_sum):
-            if (i, curr_sum) in dp:
-                return dp[(i, curr_sum)]
-            if curr_sum == amount:
-                return 1
-            if i >= len(coins) or curr_sum > amount:
-                return 0
-            total = 0
-            total += dfs(i, curr_sum+coins[i])
-            total += dfs(i+1, curr_sum)
+        # # Top - down DP
+        # dp = {}
+        # def dfs(i, curr_sum):
+        #     if curr_sum == amount:
+        #         return 1
+        #     if i >= len(coins) or curr_sum > amount:
+        #         return 0
+        #     if (i, curr_sum) in dp:
+        #         return dp[(i, curr_sum)]
+        #     total = 0
+        #     total += dfs(i, curr_sum+coins[i])
+        #     total += dfs(i+1, curr_sum)
 
-            dp[(i, curr_sum)] = total
-            return total
+        #     dp[(i, curr_sum)] = total
+        #     return total
         
-        return dfs(0, 0)
+        # return dfs(0, 0)
 
+        # Bottom-up DP
+        dp = [0]*(amount+1)
+        dp[0] = 1
+        
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] += dp[i-coin]
+    
+
+        return dp[amount]
         
