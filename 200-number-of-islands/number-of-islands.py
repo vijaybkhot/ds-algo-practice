fc6b8b2dc4bb7661233801ff5b4fb3e073675bcb
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-07 06:55:22 -0400

    Time: 271 ms (30.35%) | Memory: 25.1 MB (33.22%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -2,22 +2,48 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows, cols = len(grid), len(grid[0])
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        def dfs(i, j):
-            visited.add((i, j))
-            for dr, dc in directions:
-                r, c = i+dr, j+dc
-                if 0 <= r < rows and 0 <= c < cols and (r, c) not in visited and grid[r][c] == '1':
-                    dfs(r, c)
         num_islands = 0
         visited = set()
 
+        # # DFS Sloution:
+        # def dfs(i, j):
+        #     visited.add((i, j))
+        #     for dr, dc in directions:
+        #         r, c = i+dr, j+dc
+        #         if 0 <= r < rows and 0 <= c < cols and (r, c) not in visited and grid[r][c] == '1':
+        #             dfs(r, c)
+        # num_islands = 0
+        # visited = set()
+
+        # for row in range(rows):
+        #     for col in range(cols):
+        #         if grid[row][col] == '1' and (row, col) not in visited:
+        #             num_islands += 1
+        #             dfs(row, col)
+        
+        # return num_islands
+
+        # BFS Solution:
+        def bfs(i, j):
+            q = deque()
+            q.append((i, j))
+            while q:
+                i, j = q.popleft()
+                for dr, dc in directions:
+                    r, c = i+dr, j+dc
+                    if 0 <= r < rows and 0 <= c < cols and (r, c) not in visited and grid[r][c] == '1':
+                        q.append((r, c))
+                        visited.add((r, c))
+        
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == '1' and (row, col) not in visited:
                     num_islands += 1
-                    dfs(row, col)
+                    bfs(row, col)
         
         return num_islands
+ 
+            
 
 
 
