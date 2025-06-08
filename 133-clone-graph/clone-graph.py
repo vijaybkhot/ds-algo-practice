29bca42b97801e55b1e1ec0287565bd2eb2a0dfc
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-08 08:07:28 -0400

    Time: 41 ms (72.56%) | Memory: 18.3 MB (16.13%) - LeetSync

--- 133-clone-graph/clone-graph.py
+++ 133-clone-graph/clone-graph.py
@@ -11,39 +11,60 @@ class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return None
+        visited = set()
+        graph_dict = {}
 
-        # # DFS Solution
+        def dfs(vertex):
+            new_node = Node(val=vertex.val)
+            graph_dict[vertex] = new_node
+            for nei in vertex.neighbors:
+                if nei not in visited:
+                    visited.add(nei)
+                    dfs(nei)
+                new_node.neighbors.append(graph_dict[nei])
+
+        visited.add(node)
+        dfs(node)
+        return graph_dict[node]
+
+
+
+
+
+
+
+        # # # DFS Solution
+        # # old_to_new = {}
+        # # def dfs(curr_node):
+        # #     if curr_node in old_to_new:
+        # #         return old_to_new[curr_node]
+            
+        # #     # Clone current Node
+        # #     clone = Node(curr_node.val)
+        # #     old_to_new[curr_node] = clone
+
+        # #     # Recursively clone neighbors
+        # #     for neighbor in curr_node.neighbors:
+        # #         clone.neighbors.append(dfs(neighbor))
+            
+        # #     return clone
+        
+        # # return dfs(node)
+
+        # # BFS Solution
         # old_to_new = {}
-        # def dfs(curr_node):
-        #     if curr_node in old_to_new:
-        #         return old_to_new[curr_node]
-            
-        #     # Clone current Node
-        #     clone = Node(curr_node.val)
-        #     old_to_new[curr_node] = clone
+        # old_to_new[node] = Node(node.val)
+        # queue = deque()
+        # queue.append(node)
 
-        #     # Recursively clone neighbors
+        # while queue:
+        #     curr_node = queue.popleft()
+
         #     for neighbor in curr_node.neighbors:
-        #         clone.neighbors.append(dfs(neighbor))
-            
-        #     return clone
+        #         if neighbor not in old_to_new:
+        #             old_to_new[neighbor] = Node(neighbor.val)
+        #             queue.append(neighbor)
+        #         # Append the clone neighbor to the neighbors list of the clone of the current node
+        #         old_to_new[curr_node].neighbors.append(old_to_new[neighbor])
         
-        # return dfs(node)
-
-        # BFS Solution
-        old_to_new = {}
-        old_to_new[node] = Node(node.val)
-        queue = deque()
-        queue.append(node)
-
-        while queue:
-            curr_node = queue.popleft()
-
-            for neighbor in curr_node.neighbors:
-                if neighbor not in old_to_new:
-                    old_to_new[neighbor] = Node(neighbor.val)
-                    queue.append(neighbor)
-                # Append the clone neighbor to the neighbors list of the clone of the current node
-                old_to_new[curr_node].neighbors.append(old_to_new[neighbor])
-        
-        return old_to_new[node]
+        # return old_to_new[node]
