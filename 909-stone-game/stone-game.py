ece223f29c5e40a5df61254157be7778e941284a
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-08 10:41:15 -0400

    Time: 479 ms (22.43%) | Memory: 127.8 MB (24.11%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -4,18 +4,25 @@ class Solution:
         # return True     # The answer is alway true
 
         dp = {}
-        def dfs(i, j, alice):
+        def dfs(i, j):
             if i > j:
                 return 0
-            if (i, j, alice) in dp:
-                return dp[(i, j, alice)]
-            total = 0
-            if alice:
-                total = max(piles[i] + dfs(i+1, j, not alice), piles[j] + dfs(i, j-1, not alice))
-            else:
-                total = max((dfs(i+1, j, not alice) - piles[i]), (dfs(i, j-1, not alice) - piles[j]))
-            dp[(i, j, alice)] = total
-            return total
+            if (i, j) in dp:
+                return dp[(i, j)]
+            # total = 0
+            # if alice:
+            #     total = max(piles[i] + dfs(i+1, j, not alice), piles[j] + dfs(i, j-1, not alice))
+            # else:
+            #     total = max((dfs(i+1, j, not alice) - piles[i]), (dfs(i, j-1, not alice) - piles[j]))
+            # dp[(i, j, alice)] = total
+            # return total
 
-        return dfs(0, len(piles)-1, True) > 0
+            left = piles[i] - dfs(i+1, j)
+            right = piles[j] - dfs(i, j-1)
+            
+            dp[(i, j)] = max(left, right)
+            return dp[(i, j)]
+
+
+        return dfs(0, len(piles)-1) > 0
 
