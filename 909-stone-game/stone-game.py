199dcad0ed864cd1a92395e9871c89fed027f54c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-08 08:49:57 -0400

    Time: 507 ms (17.66%) | Memory: 128.9 MB (14.77%) - LeetSync

---
+++ 909-stone-game/stone-game.py
@@ -0,0 +1,19 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        dp = {}
+
+        def dfs(i, j, alice):
+            if i > j:
+                return 0
+            if (i, j, alice) in dp:
+                return dp[(i, j, alice)]
+            total = 0
+            if alice:
+                total = max(piles[i] + dfs(i+1, j, not alice), piles[j] + dfs(i, j-1, not alice))
+            else:
+                total = max((dfs(i+1, j, not alice) - piles[i]), (dfs(i, j-1, not alice) - piles[j]))
+            dp[(i, j, alice)] = total
+            return total
+            
+        return dfs(0, len(piles)-1, True) > 0
+
