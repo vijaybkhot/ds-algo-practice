d692803a5346d6354a74fcf291ae979c0c9f7c91
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-08 10:54:06 -0400

    Time: 173 ms (46.61%) | Memory: 23.2 MB (49.09%) - LeetSync

--- 909-stone-game/stone-game.py
+++ 909-stone-game/stone-game.py
@@ -2,27 +2,34 @@ class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
 
         # return True     # The answer is alway true
+        # # Top-down DP
+        # dp = {}
+        # def dfs(i, j):
+        #     if i > j:
+        #         return 0
+        #     if (i, j) in dp:
+        #         return dp[(i, j)]
 
-        dp = {}
-        def dfs(i, j):
-            if i > j:
-                return 0
-            if (i, j) in dp:
-                return dp[(i, j)]
-            # total = 0
-            # if alice:
-            #     total = max(piles[i] + dfs(i+1, j, not alice), piles[j] + dfs(i, j-1, not alice))
-            # else:
-            #     total = max((dfs(i+1, j, not alice) - piles[i]), (dfs(i, j-1, not alice) - piles[j]))
-            # dp[(i, j, alice)] = total
-            # return total
-
-            left = piles[i] - dfs(i+1, j)
-            right = piles[j] - dfs(i, j-1)
+        #     left = piles[i] - dfs(i+1, j)
+        #     right = piles[j] - dfs(i, j-1)
             
-            dp[(i, j)] = max(left, right)
-            return dp[(i, j)]
+        #     dp[(i, j)] = max(left, right)
+        #     return dp[(i, j)]
 
 
-        return dfs(0, len(piles)-1) > 0
+        # return dfs(0, len(piles)-1) > 0
 
+        #  Bottom-up DP
+        n = len(piles)
+        dp = [[0]*n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    dp[i][j] = piles[i]
+        
+        for length in range(2, n+1):
+            for i in range(n-length+1):
+                j = i + length - 1  
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1])
+
+        return dp[0][n-1] > 0
