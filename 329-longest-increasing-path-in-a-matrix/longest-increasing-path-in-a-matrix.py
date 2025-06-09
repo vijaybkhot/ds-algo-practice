7582577a3631afa461c91e0b59506e7f7a2c70d2
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-09 10:49:06 -0400

    Time: 114 ms (94.22%) | Memory: 19.8 MB (59.10%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+
+        rows, cols = len(matrix), len(matrix[0])
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+
+        dp = [[0]*cols for _ in range(rows)]
+        
+        def dfs(i, j):
+            max_from_4 = 0
+            for dr, dc in directions:
+                r, c = i+dr, j+dc
+                if 0 <= r < rows and 0 <= c < cols:
+                    if matrix[r][c] > matrix[i][j]:
+                        max_from_rc = dp[r][c] if dp[r][c] != 0 else dfs(r, c)
+                        max_from_4 = max(max_from_4, max_from_rc)
+            dp[i][j] = 1+max_from_4
+            return dp[i][j]
+        res = 1
+        for i in range(rows):
+            for j in range(cols):
+                if dp[i][j] == 0:
+                    max_from_4 = 0
+                    for dr, dc in directions:
+                        r, c = i+dr, j+dc
+                        if 0 <= r < rows and 0 <= c < cols:
+                            if matrix[r][c] > matrix[i][j]:
+                                max_from_rc = dp[r][c] if dp[r][c] != 0 else dfs(r, c)
+                                max_from_4 = max(max_from_4, max_from_rc)
+                    dp[i][j] = 1+max_from_4
+                    res = max(res, dp[i][j])
+        return res
