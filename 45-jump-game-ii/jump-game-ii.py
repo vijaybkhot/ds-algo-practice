9834e2fb81757604c7c922d0d32aaa20bc9250f4
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-12 11:44:48 -0400

    Time: 9 ms (52.50%) | Memory: 18.5 MB (74.98%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,37 +1,39 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
 
-        # Dynamic Programming appraoch:
-        dp = {}
+        # # Dynamic Programming appraoch:
+        # dp = {}
         
-        def dfs(i):
-            if i >= len(nums)-1:
-                return 0
+        # def dfs(i):
+        #     if i >= len(nums)-1:
+        #         return 0
             
-            if i in dp:
-                return dp[i]
+        #     if i in dp:
+        #         return dp[i]
             
-            min_jumps = float('inf')
-            for j in range(nums[i]):
-                min_jumps = min(min_jumps, 1 + dfs(i+j+1))
+        #     min_jumps = float('inf')
+        #     for j in range(nums[i]):
+        #         min_jumps = min(min_jumps, 1 + dfs(i+j+1))
             
-            dp[i] = min_jumps
-            return min_jumps
+        #     dp[i] = min_jumps
+        #     return min_jumps
         
-        return dfs(0)
+        # return dfs(0)
 
 
 
-        # # Greedy Approach
-        # i, last, jumps = len(nums)-2, len(nums)-1, 0
-        # while i > -1 and last > -1:
-        #     while i >= 0 and i + nums[i] >= last:
-        #         i -= 1
-        #     last = i
-        #     i -= 1
-        #     jumps += 1
-        
-        # return jumps
+        # Greedy Approach
+        jumps = 0
+        curr_end = 0
+        farthest = 0
+
+        for i in range(len(nums) - 1):
+            farthest = max(farthest, i + nums[i])
+            if i == curr_end:
+                jumps += 1
+                curr_end = farthest
+
+        return jumps
         
 
         
