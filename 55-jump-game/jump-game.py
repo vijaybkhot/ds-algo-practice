f61ab091dc23e4e7215c39cd07994becdc766b84
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-12 09:55:31 -0400

    Time: 17 ms (69.67%) | Memory: 18.8 MB (24.89%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,27 +1,33 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = {}
+        # # Dynamic Programming approach:
+        # dp = {}
+        # def dfs(i):
+        #     if i >= len(nums)-1:
+        #         return True
+        #     if nums[i] == 0:
+        #         return False
+        #     if i in dp:
+        #         return dp[i]
 
-        def dfs(i):
-            if i >= len(nums)-1:
-                return True
-            
-            if nums[i] == 0:
-                return False
+        #     can_reach = False
+        #     for j in range(nums[i], 0, -1):
+        #         if dfs(i+j):
+        #             can_reach = True
+        #             break
 
-            if i in dp:
-                return dp[i]
-
-            can_reach = False
-
-            for j in range(nums[i], 0, -1):
-                if dfs(i+j):
-                    can_reach = True
-                    dp[i] = True
-                    return True
-                    break
-
-            dp[i] = can_reach
-            return can_reach
+        #     dp[i] = can_reach
+        #     return can_reach
         
-        return dfs(0)
+        # return dfs(0)
+
+        # Greedy Approach:
+        i = len(nums)-2
+        last = len(nums)-1
+        while last > -1 and i > -1:
+            while i >= 0 and i + nums[i] < last:
+                i -= 1
+            if i >= 0:
+                last = i
+                i -= 1
+        return last <= 0
