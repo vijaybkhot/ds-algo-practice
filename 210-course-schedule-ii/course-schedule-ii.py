3cc74083b1b74d220903051b8daf417c3f8dbb25
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-12 07:13:09 -0400

    Time: 3 ms (77.39%) | Memory: 19.3 MB (48.12%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,11 +1,45 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # # Topological sorting using Kahn's algorithm
         graph = defaultdict(set)
-        in_degree = [0] * numCourses
-        for a, b in prerequisites:
-            graph[b].add(a)
-            in_degree[a] += 1
+        indegree = defaultdict(int)
+
+        for v, u in prerequisites:
+            graph[u].add(v)
+            indegree[v] += 1
+        
+        topo = []
+        q = deque()
+        # start with 0 indegree nodes
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        
+        while q:
+            curr_node = q.popleft()
+            topo.append(curr_node)
+            for nei in graph[curr_node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        
+        return [] if len(topo)!= numCourses else topo
+
+
+
+
+
+
+
+
+
+
+
+        # # # Topological sorting using Kahn's algorithm
+        # graph = defaultdict(set)
+        # in_degree = [0] * numCourses
+        # for a, b in prerequisites:
+        #     graph[b].add(a)
+        #     in_degree[a] += 1
         
         # queue = deque()
 
@@ -26,30 +60,30 @@ class Solution:
         
         # return topo_order if len(topo_order) == numCourses else []
 
-        # Topological sorting using DFS
-        visited = set()
-        rec_stack = set()
-        hasCycle = False
-        topo_order = []
-        def dfs(node):
-            nonlocal hasCycle
-            nonlocal topo_order
-            if not hasCycle:
-                visited.add(node)
-                rec_stack.add(node)
-                for neighbor in graph[node]:
-                    if neighbor not in visited:
-                        dfs(neighbor)
-                    elif neighbor in rec_stack:
-                        topo_order = []
-                        hasCycle = True
-                        return
-                if not hasCycle:
-                    topo_order.append(node)
-                    rec_stack.remove(node)
+        # # Topological sorting using DFS
+        # visited = set()
+        # rec_stack = set()
+        # hasCycle = False
+        # topo_order = []
+        # def dfs(node):
+        #     nonlocal hasCycle
+        #     nonlocal topo_order
+        #     if not hasCycle:
+        #         visited.add(node)
+        #         rec_stack.add(node)
+        #         for neighbor in graph[node]:
+        #             if neighbor not in visited:
+        #                 dfs(neighbor)
+        #             elif neighbor in rec_stack:
+        #                 topo_order = []
+        #                 hasCycle = True
+        #                 return
+        #         if not hasCycle:
+        #             topo_order.append(node)
+        #             rec_stack.remove(node)
 
-        for node in range(numCourses):
-            if node not in visited:
-                dfs(node)
+        # for node in range(numCourses):
+        #     if node not in visited:
+        #         dfs(node)
         
-        return topo_order[::-1] if topo_order else []
+        # return topo_order[::-1] if topo_order else []
