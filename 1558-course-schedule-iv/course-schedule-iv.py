9c7ce87e7ccbe3646559a298d78dd1caef431dec
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-13 08:00:32 -0400

    Time: 1449 ms (6.70%) | Memory: 21.2 MB (7.32%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,41 +1,79 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        # Using Kahns algorithm:
-        prereq = defaultdict(set)
+        
         graph = defaultdict(set)
-        # indegree = defaultdict(int)
-        for a, b in prerequisites:
-            graph[a].add(b)
-            # indegree[b] += 1
-        
-        # q = deque()
-        # for i in range(numCourses):
-        #     if indegree[i] == 0:
-        #         q.append(i)
+        indegree = defaultdict(int)
+        index_dict = {}
 
-        # while q:
-        #     node = q.popleft()
+        for u, v in prerequisites:
+            graph[u].add(v)
+            indegree[v] += 1
+        
+        def dfs(node, target):
+            if node == target:
+                return True
+            visited = set()
+            def helper_dfs(curr):
+                visited.add(curr)
+                for nei in graph[curr]:
+                    if nei == target:
+                        return True
+                    if nei not in visited:
+                        if helper_dfs(nei):
+                            return True
+                return False
+            return helper_dfs(node)
+
+
+        return [dfs(u, v) for u, v in queries]
+        
+
+
+
+
+
+
+
+
+
+
+
+        # # Using Kahns algorithm:
+        # prereq = defaultdict(set)
+        # graph = defaultdict(set)
+        # # indegree = defaultdict(int)
+        # for a, b in prerequisites:
+        #     graph[a].add(b)
+        #     # indegree[b] += 1
+        
+        # # q = deque()
+        # # for i in range(numCourses):
+        # #     if indegree[i] == 0:
+        # #         q.append(i)
+
+        # # while q:
+        # #     node = q.popleft()
+        # #     for neighbor in graph[node]:
+        # #         prereq[neighbor].update(prereq[node])
+        # #         prereq[neighbor].add(node)
+        # #         indegree[neighbor] -= 1
+        # #         if indegree[neighbor] == 0:
+        # #             q.append(neighbor)
+
+        # visited = set()
+
+        # def dfs(node):
+        #     if node in visited:
+        #         return
+        #     visited.add(node)
+
         #     for neighbor in graph[node]:
-        #         prereq[neighbor].update(prereq[node])
-        #         prereq[neighbor].add(node)
-        #         indegree[neighbor] -= 1
-        #         if indegree[neighbor] == 0:
-        #             q.append(neighbor)
-
-        visited = set()
-
-        def dfs(node):
-            if node in visited:
-                return
-            visited.add(node)
-
-            for neighbor in graph[node]:
-                dfs(neighbor)
-                prereq[node].add(neighbor)
-                prereq[node].update(prereq[neighbor])
+        #         dfs(neighbor)
+        #         prereq[node].add(neighbor)
+        #         prereq[node].update(prereq[neighbor])
         
-        for i in range(numCourses):
-            dfs(i)
+        # for i in range(numCourses):
+        #     dfs(i)
 
-        return [True if v in prereq[u] else False for u, v in queries]
+        # return [True if v in prereq[u] else False for u, v in queries]
         
