4061bb00126ade3daf63ad42bca8334a189ac309
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-14 10:36:41 -0400

    Time: 3 ms (86.21%) | Memory: 19 MB (65.21%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,21 +1,32 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
 
-        dp = {}
+        # # Top-down DP solution
+        # dp = {}
+        # def dfs(i, j):
+        #     if i == len(triangle):
+        #         return 0
+        #     if (i, j) in dp:
+        #         return dp[(i, j)]
+        #     path_sum = 0
 
-        def dfs(i, j):
-            if i == len(triangle):
-                return 0
-            if (i, j) in dp:
-                return dp[(i, j)]
-            path_sum = 0
+        #     path_sum = (triangle[i][j] + dfs(i+1, j))
+        #     if j+1 < len(triangle[i]):
+        #         path_sum = min(path_sum, (triangle[i][j+1] + dfs(i+1, j+1)))
 
-            path_sum = (triangle[i][j] + dfs(i+1, j))
-            if j+1 < len(triangle[i]):
-                path_sum = min(path_sum, (triangle[i][j+1] + dfs(i+1, j+1)))
-                
-            dp[(i, j)] = path_sum
-            return path_sum
+        #     dp[(i, j)] = path_sum
+        #     return path_sum
         
-        return dfs(0, 0)
-        
+        # return dfs(0, 0)
+
+        # Bottom-up DP
+        n = len(triangle)
+        dp = triangle[n-1]
+
+        for i in range(n-2, -1, -1):
+            new_dp = dp[::]
+            for j in range(i+1):
+                new_dp[j] = min((triangle[i][j] + dp[j]), (triangle[i][j]+dp[j+1]))
+            dp = new_dp
+
+        return dp[0]        
