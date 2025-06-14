4595ebcb4dd2272416089c32efad0a2ac5c57c82
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-14 08:27:22 -0400

    Time: 221 ms (97.72%) | Memory: 82 MB (99.71%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,24 +1,55 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent = [-1]*(size+1)
+    
+    def find(self, x):
+        if self.parent[x] < 0:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        rootX, rootY = self.find(x), self.find(y)
+        if rootX == rootY:
+            return False
+        
+        if self.parent[rootX] < self.parent[rootY]:
+            self.parent[rootX] += self.parent[rootY]
+            self.parent[rootY] = rootX
+        else:
+            self.parent[rootY] += self.parent[rootX]
+            self.parent[rootX] = rootY
+        return True
+
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # # DFS Approach:
+        # graph = defaultdict(set)
+        # visited = set()
+        # for u, v in edges:
+        #     graph[u].add(v)
+        #     graph[v].add(u)
 
-        graph = defaultdict(set)
-        visited = set()
+        # def dfs(node):
+        #     if node in visited:
+        #         return
+        #     if node == destination:
+        #         return True
+        #     visited.add(node)
+        #     for nei in graph[node]:
+        #         if nei not in visited:
+        #             if dfs(nei):
+        #                 return True
+        #     return False
+
+        # return dfs(source)
+
+        # Union Find approach:
+        uf = UnionFind(n)
         for u, v in edges:
-            graph[u].add(v)
-            graph[v].add(u)
-
-        def dfs(node):
-            if node in visited:
-                return
-            if node == destination:
-                return True
-            visited.add(node)
-            for nei in graph[node]:
-                if nei not in visited:
-                    if dfs(nei):
-                        return True
-            return False
-
-        return dfs(source)
+            uf.union(u, v)
+        
+        return not uf.union(source, destination)
 
         
