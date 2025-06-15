ffdcd097814122d7aaa0c7a93481a30e6addf3a4
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-15 08:32:24 -0400

    Time: 73 ms (51.47%) | Memory: 32.6 MB (43.09%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -11,20 +11,29 @@ class Solution:
         
         # return res
 
-        # Top-down DP
-        dp = {}
-        def dfs(i):
-            if i in dp:
-                return dp[i]
+        # # Top-down DP
+        # dp = {}
+        # def dfs(i):
+        #     if i in dp:
+        #         return dp[i]
 
-            if i == len(nums)-1:
-                return nums[i]
+        #     if i == len(nums)-1:
+        #         return nums[i]
         
             
-            dp[i] = max(nums[i], (nums[i]+dfs(i+1)))
-            return dp[i]
+        #     dp[i] = max(nums[i], (nums[i]+dfs(i+1)))
+        #     return dp[i]
         
-        return max(dfs(i) for i in range(len(nums)))
+        # return max(dfs(i) for i in range(len(nums)))
+
+        # Bottom-up DP
+        n = len(nums)
+        dp = [0]*n
+        dp[n-1] = nums[n-1]
+        for i in range(n-2, -1, -1):
+            dp[i] = max(nums[i], nums[i]+dp[i+1])
+        
+        return max(dp)
 
             
 
