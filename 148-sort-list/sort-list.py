2c594f74be88e3c8e90fe8d4f6790786e89b2306
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-17 07:08:29 -0400

    Time: 197 ms (30.65%) | Memory: 32.9 MB (68.51%) - LeetSync

--- 148-sort-list/sort-list.py
+++ 148-sort-list/sort-list.py
@@ -8,22 +8,60 @@ class Solution:
         if not head or not head.next:
             return head
         
-
         dummy = ListNode()
         dummy.next = head
         tail = dummy
-        # Using a heap
-        heap = []
-        curr = head
-        while curr:
-            heapq.heappush(heap, (curr.val, id(curr),curr))
-            curr = curr.next
+        # # Using a heap - O(n logn) time and O(n) space complexity
+        # heap = []
+        # curr = head
+        # while curr:
+        #     heapq.heappush(heap, (curr.val, id(curr),curr))
+        #     curr = curr.next
         
-        while heap:
-            _, _, node = heapq.heappop(heap)
-            tail.next = node
-            tail = tail.next
+        # while heap:
+        #     _, _, node = heapq.heappop(heap)
+        #     tail.next = node
+        #     tail = tail.next
 
-        tail.next = None
+        # tail.next = None
 
-        return dummy.next
+        # return dummy.next
+
+        # Using merge sort
+        def merge_sort_list(l):
+            # Base case
+            if not l or not l.next:
+                return l
+            # divide the list in half
+            slow, fast = l, l
+            prev = None
+            while fast and fast.next:
+                prev = slow
+                slow = slow.next
+                fast = fast.next.next
+
+            # Cut the list
+            prev.next = None
+            l1 = merge_sort_list(l)
+            l2 = merge_sort_list(slow)
+            dummy = ListNode()
+            dummy.next = merge(l1, l2)
+            return dummy.next
+
+        def merge(l1, l2):
+            dummy = ListNode()
+            tail = dummy
+            while l1 and l2:
+                if l1.val < l2.val:
+                    tail.next = l1
+                    l1 = l1.next
+                else:
+                    tail.next = l2
+                    l2 = l2.next
+                tail = tail.next
+            tail.next = l1 or l2
+            return dummy.next
+        
+        return merge_sort_list(head)
+        
+
