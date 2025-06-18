afbf4a5baf72c9889d2aba95ecb22c5dd5b88f58
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-18 17:22:37 -0400

    Time: 38 ms (40.90%) | Memory: 19.1 MB (74.58%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.py
+++ 876-hand-of-straights/hand-of-straights.py
@@ -7,15 +7,20 @@ class Solution:
         frequency_dict = defaultdict(int)
         for num in hand:
             frequency_dict[num] += 1
+        min_heap = list(frequency_dict.keys())
+        heapq.heapify(min_heap)
         
         while frequency_dict:
-            min_card = min(frequency_dict)
+            while min_heap and frequency_dict[min_heap[0]] == 0:
+                heapq.heappop(min_heap)
+            if not min_heap:
+                break
+
+            min_card = min_heap[0]
             for i in range(groupSize):
                 card = min_card + i
                 if frequency_dict[card] == 0:
                     return False
                 frequency_dict[card] -= 1
-                if frequency_dict[card] == 0:
-                    del frequency_dict[card]
         
         return True
