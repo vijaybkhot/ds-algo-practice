178ca66ebd9db34558357e341ab43a971cf797bd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-19 08:35:29 -0400

    Time: 34 ms (25.08%) | Memory: 18.5 MB (7.89%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -1,25 +1,12 @@
 class Solution:
     def predictPartyVictory(self, senate: str) -> str:
 
-        r_count, d_count = 0, 0
         rad, dire = "Radiant", "Dire"
         n = len(senate)
-        for char in senate:
-            if char == 'R':
-                r_count += 1
-            else:
-                d_count += 1
         
-        if r_count == 0:
-            return dire
-        elif d_count == 0:
-            return rad
-        
-        senate_arr = [char for char in senate]
         rad_heap = [idx for idx, char in enumerate(senate) if char == 'R']
         dire_heap = [idx for idx, char in enumerate(senate) if char == 'D']
-        heapq.heapify(rad_heap)
-        heapq.heapify(dire_heap)
+        
 
         while rad_heap and dire_heap:
             if rad_heap[0] < dire_heap[0]:
@@ -27,12 +14,9 @@ class Solution:
                 # Eliminate upcoming dire senator
                 heapq.heappop(dire_heap)
                 if not dire_heap:
-                    return rad
-                # Add curr_player back to heap
-                max_rad_idx = max(rad_heap) if rad_heap else curr_player
-                max_dire_idx = max(dire_heap)
-                new_idx = max(max_rad_idx, max_dire_idx) + 1
-                heapq.heappush(rad_heap, new_idx)
+                    return rad    
+                # Add curr_player back to heap            
+                heapq.heappush(rad_heap, curr_player+n)
             else:
                 curr_player = heapq.heappop(dire_heap)
                 # Eliminate upcoming dire senator
@@ -40,10 +24,7 @@ class Solution:
                 if not rad_heap:
                     return dire
                 # Add curr_player back to heap
-                max_rad_idx = max(rad_heap) 
-                max_dire_idx = max(dire_heap) if dire_heap else curr_player
-                new_idx = max(max_rad_idx, max_dire_idx) + 1
-                heapq.heappush(dire_heap, new_idx)
+                heapq.heappush(dire_heap, curr_player+n)
 
         return rad if rad_heap else dire
         
