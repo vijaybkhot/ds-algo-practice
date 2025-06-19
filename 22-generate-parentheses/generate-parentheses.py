89b021e5f75415d448439bc2470600ad4a0757da
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-19 15:33:27 -0400

    Time: 3 ms (29.95%) | Memory: 18.2 MB (25.79%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -7,16 +7,14 @@ class Solution:
                 self.res.append(path)
                 return
             
-    
             if i > 0 and i >= j:
                 dfs(i-1, j, path+"(")
-                return
+        
             else:
                 if i > 0:
                     dfs(i-1, j, path+"(")
                 if j > 0:
                     dfs(i, j-1, path+")")
-                return
         
         dfs(n, n, "")
         return self.res
