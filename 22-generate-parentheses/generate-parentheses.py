0e8a33071d11ec549931591ccbd4c7a1e72ad287
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-19 15:37:48 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (5.75%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -2,23 +2,34 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
 
         self.res= []
+        # def dfs(i, j, path):
+        #     if i == 0 and j == 0:
+        #         self.res.append(path)
+        #         return
+            
+        #     if i > 0 and i >= j:
+        #         dfs(i-1, j, path+"(")
+        
+        #     else:
+        #         if i > 0:
+        #             dfs(i-1, j, path+"(")
+        #         if j > 0:
+        #             dfs(i, j-1, path+")")
+        
+
         def dfs(i, j, path):
             if i == 0 and j == 0:
                 self.res.append(path)
                 return
             
-            if i > 0 and i >= j:
+            if i > 0:
                 dfs(i-1, j, path+"(")
-        
-            else:
-                if i > 0:
-                    dfs(i-1, j, path+"(")
-                if j > 0:
-                    dfs(i, j-1, path+")")
-        
+            if j > i:
+                dfs(i, j-1, path+")")
+
+
         dfs(n, n, "")
         return self.res
-
 
 
 
