9b7b3278634ad2e4b291a6e2482991086ef10522
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-19 12:11:19 -0400

    Time: 8 ms (13.98%) | Memory: 17.7 MB (83.44%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -1,20 +1,64 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        last_index = {}    # {char: last_index}
-        res = []
-        for index, char in enumerate(s):
-            last_index[char] = index
+        max_idx_map = defaultdict(int)
+        for i in range(len(s)):
+            max_idx_map[s[i]] = i
         
-        end = last_index[s[0]]
+
         start = 0
-        for index, char in enumerate(s):
-            if index == end:
-                res.append(end - start + 1)
+        i = 0
+        res = []
+        end = max_idx_map[s[0]]
+        while i < len(s):
+            if i == end:
+                res.append(end-start+1)
                 start = end + 1
-                end = last_index[s[start]] if start < len(s) else index
-            else:
-                end = max(end, last_index[s[index]])
-            
+                end = max_idx_map[s[start]] if start < len(s) else len(s)-1
+            end = max(end, max_idx_map[s[i]])
+            i += 1
+        
         return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#         last_index = {}    # {char: last_index}
+#         res = []
+#         for index, char in enumerate(s):
+#             last_index[char] = index
+        
+#         end = last_index[s[0]]
+#         start = 0
+#         for index, char in enumerate(s):
+#             if index == end:
+#                 res.append(end - start + 1)
+#                 start = end + 1
+#                 end = last_index[s[start]] if start < len(s) else index
+#             else:
+#                 end = max(end, last_index[s[index]])
+            
+#         return res
 
         
