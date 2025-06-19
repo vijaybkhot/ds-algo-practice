05b4483a0c59790ae76a426e768f3929d572c9c9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-19 11:33:39 -0400

    Time: 23 ms (63.79%) | Memory: 55.1 MB (86.41%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.py
@@ -1,36 +1,44 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
         n = len(triplets)
-        if triplets[0] == target:
-            return True
+        # if triplets[0] == target:
+        #     return True
         
-        for i in range(1, n):
-            if triplets[i][0] > target[0] or triplets[i][1] > target[1] or triplets[i][2] > target[2]:
-                triplets[i] = triplets[i-1]
-                continue
+        # for i in range(1, n):
+        #     if triplets[i][0] > target[0] or triplets[i][1] > target[1] or triplets[i][2] > target[2]:
+        #         triplets[i] = triplets[i-1]
+        #         continue
 
-            max_1, max_2, max_3 = max(triplets[i][0], triplets[i-1][0]), max(triplets[i][1], triplets[i-1][1]), max(triplets[i][2], triplets[i-1][2])
+        #     max_1, max_2, max_3 = max(triplets[i][0], triplets[i-1][0]), max(triplets[i][1], triplets[i-1][1]), max(triplets[i][2], triplets[i-1][2])
 
-            min_1, min_2, min_3 = min(triplets[i][0], triplets[i-1][0]), min(triplets[i][1], triplets[i-1][1]), min(triplets[i][2], triplets[i-1][2])
+        #     min_1, min_2, min_3 = min(triplets[i][0], triplets[i-1][0]), min(triplets[i][1], triplets[i-1][1]), min(triplets[i][2], triplets[i-1][2])
 
-            if [max_1, max_2, max_3] == target:
-                return True
-            new_triplet = []
-            if max_1 <= target[0]:
-                new_triplet.append(max_1)
-            else:
-                new_triplet.append(min_1)
+        #     if [max_1, max_2, max_3] == target:
+        #         return True
+        #     new_triplet = []
+        #     if max_1 <= target[0]:
+        #         new_triplet.append(max_1)
+        #     else:
+        #         new_triplet.append(min_1)
             
-            if max_2 <= target[1]:
-                new_triplet.append(max_2)
-            else:
-                new_triplet.append(min_2)
+        #     if max_2 <= target[1]:
+        #         new_triplet.append(max_2)
+        #     else:
+        #         new_triplet.append(min_2)
             
-            if max_3 <= target[2]:
-                new_triplet.append(max_3)
-            else:
-                new_triplet.append(min_3)
-            triplets[i] = new_triplet
+        #     if max_3 <= target[2]:
+        #         new_triplet.append(max_3)
+        #     else:
+        #         new_triplet.append(min_3)
+        #     triplets[i] = new_triplet
         
-        return False
+        # return False
+        # More readable solution:
+        found = [False, False, False]
+        for t in triplets:
+            if t[0] <= target[0] and  t[1] <= target[1] and  t[2] <= target[2]:
+                found[0] = True if t[0] == target[0] else found[0]
+                found[1] = True if t[1] == target[1] else found[1]
+                found[2] = True if t[2] == target[2] else found[2]
+        return all(found) 
 
