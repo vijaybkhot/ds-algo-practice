0406d2225c6bcaf55c30b7ae2f5b8716ce7667aa
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-19 15:31:50 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (43.76%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,39 +1,76 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
 
-        def dfs(open_paren, closed_paren, path):
-            if len(path) == 2*n:
-                
-                res.append(''.join(path[:]))
+        self.res= []
+        def dfs(i, j, path):
+            if i == 0 and j == 0:
+                self.res.append(path)
+                return
+            
+    
+            if i > 0 and i >= j:
+                dfs(i-1, j, path+"(")
+                return
+            else:
+                if i > 0:
+                    dfs(i-1, j, path+"(")
+                if j > 0:
+                    dfs(i, j-1, path+")")
                 return
         
+        dfs(n, n, "")
+        return self.res
 
-            if open_paren == n:
-                path.append('(')
-                dfs(open_paren-1, closed_paren, path)
-                path.pop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # res = []
+
+        # def dfs(open_paren, closed_paren, path):
+        #     if len(path) == 2*n:
+                
+        #         res.append(''.join(path[:]))
+        #         return
+        
+
+        #     if open_paren == n:
+        #         path.append('(')
+        #         dfs(open_paren-1, closed_paren, path)
+        #         path.pop()
             
-            elif open_paren == 0:
-                path.append(')')
-                dfs(open_paren, closed_paren-1, path)
-                path.pop()
+        #     elif open_paren == 0:
+        #         path.append(')')
+        #         dfs(open_paren, closed_paren-1, path)
+        #         path.pop()
 
-            elif (open_paren <= closed_paren) and (open_paren > 0):
-                path.append('(')
-                dfs(open_paren-1, closed_paren, path)
-                path.pop()
+        #     elif (open_paren <= closed_paren) and (open_paren > 0):
+        #         path.append('(')
+        #         dfs(open_paren-1, closed_paren, path)
+        #         path.pop()
 
-                path.append(')')
-                dfs(open_paren, closed_paren-1, path)
-                path.pop()
+        #         path.append(')')
+        #         dfs(open_paren, closed_paren-1, path)
+        #         path.pop()
             
 
             
 
-        dfs(n, n, [])
+        # dfs(n, n, [])
 
-        return res
+        # return res
 
     
                 
