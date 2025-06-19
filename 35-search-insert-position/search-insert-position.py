8cf3e5d1c4ff97d5d4c572ffcd6a58f47e57d9e8
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-19 06:31:02 -0400

    Time: 0 ms (100.00%) | Memory: 18.3 MB (72.48%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,20 +1,16 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left, right = 0, len(nums)-1
-       
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] > target:
-                right = mid - 1
-            else:
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+
+        while l <= r:
+            mid = (l+r)//2
+            if nums[mid] == target:
                 return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid -1
         
-        return left
+        return l
+
         
