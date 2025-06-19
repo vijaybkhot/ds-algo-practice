fc3b98d66273f89666af5651ba63f00656151882
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-19 08:40:44 -0400

    Time: 15 ms (41.70%) | Memory: 18.2 MB (33.40%) - LeetSync

--- 649-dota2-senate/dota2-senate.py
+++ 649-dota2-senate/dota2-senate.py
@@ -3,30 +3,53 @@ class Solution:
 
         rad, dire = "Radiant", "Dire"
         n = len(senate)
-        
-        rad_heap = [idx for idx, char in enumerate(senate) if char == 'R']
-        dire_heap = [idx for idx, char in enumerate(senate) if char == 'D']
-        
+        # # Solution I: Using a heap to get the player with current round
+        # rad_heap = [idx for idx, char in enumerate(senate) if char == 'R']
+        # dire_heap = [idx for idx, char in enumerate(senate) if char == 'D']
 
-        while rad_heap and dire_heap:
-            if rad_heap[0] < dire_heap[0]:
-                curr_player = heapq.heappop(rad_heap)
+        # while rad_heap and dire_heap:
+        #     if rad_heap[0] < dire_heap[0]:
+        #         curr_player = heapq.heappop(rad_heap)
+        #         # Eliminate upcoming dire senator
+        #         heapq.heappop(dire_heap)
+        #         if not dire_heap:
+        #             return rad    
+        #         # Add curr_player back to heap            
+        #         heapq.heappush(rad_heap, curr_player+n)
+        #     else:
+        #         curr_player = heapq.heappop(dire_heap)
+        #         # Eliminate upcoming dire senator
+        #         heapq.heappop(rad_heap)
+        #         if not rad_heap:
+        #             return dire
+        #         # Add curr_player back to heap
+        #         heapq.heappush(dire_heap, curr_player+n)
+
+        # return rad if rad_heap else dire
+
+        # Solution II - Using two deques instead of heaps
+        rad_q = deque([idx for idx, char in enumerate(senate) if char == 'R'])
+        dire_q = deque([idx for idx, char in enumerate(senate) if char == 'D'])
+        while rad_q and dire_q:
+            if rad_q[0] < dire_q[0]:
+                curr_player = rad_q.popleft()
                 # Eliminate upcoming dire senator
-                heapq.heappop(dire_heap)
-                if not dire_heap:
+                dire_q.popleft()
+                if not dire_q:
                     return rad    
-                # Add curr_player back to heap            
-                heapq.heappush(rad_heap, curr_player+n)
+                # Add curr_player back to q            
+                rad_q.append(curr_player+n)
             else:
-                curr_player = heapq.heappop(dire_heap)
-                # Eliminate upcoming dire senator
-                heapq.heappop(rad_heap)
-                if not rad_heap:
-                    return dire
-                # Add curr_player back to heap
-                heapq.heappush(dire_heap, curr_player+n)
+                curr_player = dire_q.popleft()
+                # Eliminate upcoming rad senator
+                rad_q.popleft()
+                if not rad_q:
+                    return dire    
+                # Add curr_player back to q            
+                dire_q.append(curr_player+n)
+                
+        return rad if rad_q else dire
 
-        return rad if rad_heap else dire
         
 
         
