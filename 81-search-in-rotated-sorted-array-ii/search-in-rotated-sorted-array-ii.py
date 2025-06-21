a4e8a1d1629f8bbb2544941b2164462fc3ec63e1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-21 10:37:32 -0400

    Time: 4 ms (2.77%) | Memory: 18.2 MB (79.06%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -1,35 +1,31 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: bool
-        """
-        left, right = 0, len(nums)-1
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        # Find pivot:
+        n = len(nums)
+        left, right = 0, n-1
 
         while left <= right:
             mid = (left + right) // 2
+            
             if nums[mid] == target:
                 return True
-            
-            # If duplicates found:
+
+            # If we can't decide the sorted half
             if nums[left] == nums[mid] == nums[right]:
                 left += 1
-                continue
-            # If the left part is sorted
-            if nums[left] <= nums[mid]:
-                # If target lies inside this sorted left part
-                if target >= nums[left] and target < nums[mid]:
+                right -= 1
+
+            # Left half is sorted
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-            # If the right part is sorted:
-            elif nums[mid] <= nums[right]:
-                # If target lies inside this sorted right part
-                if target <= nums[right] and target > nums[mid]:
+
+            # Right half is sorted
+            else:
+                if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
-        
         return False
-        
