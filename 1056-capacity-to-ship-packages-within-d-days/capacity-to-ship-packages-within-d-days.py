7ca39673f4e704a5261c3dfc728edb0fb5301d14
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-21 06:08:24 -0400

    Time: 205 ms (33.94%) | Memory: 22.1 MB (55.79%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.py
@@ -1,32 +1,24 @@
-class Solution(object):
-    def shipWithinDays(self, weights, days):
-        """
-        :type weights: List[int]
-        :type days: int
-        :rtype: int
-        """
-        def calcDaysForCapacity(capacity):
-            curr_capacity = 0
-            days = 1
-            for weight in weights:
-                curr_capacity += weight
-                if curr_capacity > capacity:
-                    days += 1
-                    curr_capacity = weight
-            return days
-        
-        low, high = max(weights), sum(weights)
-        res = high
-        while low <= high:
-            mid = (low + high) // 2
-            if calcDaysForCapacity(mid) <= days:
-                res = mid
-                high = mid - 1
-            else:
-                low = mid + 1
-        
-        return res
-        
-            
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
 
+        def canShip(ship_capacity):
+            days_for_curr_speed = 1
+            curr_load = 0
+            for w in weights:
+                if curr_load + w > ship_capacity:
+                    days_for_curr_speed += 1
+                    curr_load = 0
+                curr_load += w
+            return days_for_curr_speed <= days
         
+        l, r = max(weights), sum(weights)
+        while l <= r:
+            mid = (l+r)//2
+            if canShip(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return l
+
+            
