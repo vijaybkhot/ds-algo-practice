5dccb69f223050028624372a0d313bbc01c4fe4a
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-21 08:24:25 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (22.13%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,28 +1,36 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left, right = 0, len(nums)-1
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Find the pivot:
+        n = len(nums)
+        l, r = 0, n-1
+        while l < r:
+            mid = (l+r) // 2
 
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+
+        # since we have pivot, we find the target in both portions
+        # left portion
+        left, right = 0, l-1
         while left <= right:
-            mid = (left + right) // 2
+            mid = (left+right)//2
             if nums[mid] == target:
                 return mid
-            # Left Sorted Part:
-            if nums[left] <= nums[mid]:
-                if target >= nums[left] and target < nums[mid]:
-                    right = mid -1
-                else:
-                    left = mid + 1
-            # Right Sorted part
-            elif nums[mid] <= nums[right]:
-                if target <= nums[right] and target > nums[mid]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-
-        return -1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        # if not found, binary search also on right portion
+        left, right = l, n-1
+        while left <= right:
+            mid = (left+right)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
         
+        return -1
