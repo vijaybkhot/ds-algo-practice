76be6f4e97fdc96ce9267ec1356c916092f715ec
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-22 07:45:23 -0400

    Time: 4 ms (20.66%) | Memory: 19.7 MB (60.57%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -31,16 +31,38 @@ class Solution:
         # return res
             
 
-        # More readable solution O(n) time and O(1) space. O(n) for output list
-        res = []
-        for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
-                res.append(newInterval)
-                return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
-            else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+        # # Greedy Approach: More readable solution O(n) time and O(1) space. O(n) for output list
+        # res = []
+        # for i in range(len(intervals)):
+        #     if newInterval[1] < intervals[i][0]:
+        #         res.append(newInterval)
+        #         return res + intervals[i:]
+        #     elif newInterval[0] > intervals[i][1]:
+        #         res.append(intervals[i])
+        #     else:
+        #         newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         
+        # res.append(newInterval)
+        # return res
+
+        # Linear search
+        n = len(intervals)
+        i = 0
+        res = []
+        while i < n and intervals[i][1] < newInterval[0]:
+            res.append(intervals[i])
+            i += 1
+        
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(intervals[i][0], newInterval[0])
+            newInterval[1] = max(intervals[i][1], newInterval[1])
+            i += 1
+
         res.append(newInterval)
+        
+        while i < n:
+            res.append(intervals[i])
+            i += 1
+        
         return res
+        
