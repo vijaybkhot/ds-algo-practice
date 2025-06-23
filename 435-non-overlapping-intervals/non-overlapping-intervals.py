1e5e83007b1fdb8b7650a943948392f37c898f55
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-23 06:55:07 -0400

    Time: 220 ms (8.25%) | Memory: 51.4 MB (55.00%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -33,6 +33,21 @@ class Solution:
         #     count += 1
         # return count
 
+        # # Greedy approach to sort by start position
+        # count = 0
+        # prev_end = intervals[0][1]
+        # for i in range(1, len(intervals)):
+        #     if intervals[i][0] >= prev_end:
+        #         prev_end = intervals[i][1]
+        #     else:
+        #         count += 1
+        #         prev_end = min(prev_end, intervals[i][1])
+        
+        # return count
+
+        # Greedy approach to sort by end position
+        intervals.sort(key=lambda x: (x[1]))
+
         count = 0
         prev_end = intervals[0][1]
         for i in range(1, len(intervals)):
@@ -40,10 +55,8 @@ class Solution:
                 prev_end = intervals[i][1]
             else:
                 count += 1
-                prev_end = min(prev_end, intervals[i][1])
         
         return count
-
             
 
             
