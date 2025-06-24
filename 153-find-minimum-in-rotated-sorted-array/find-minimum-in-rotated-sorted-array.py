e1ea70918adcdb6582791829acbdfd733e8ca0c0
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-24 06:47:30 -0400

    Time: 0 ms (100.00%) | Memory: 18.2 MB (9.32%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -2,23 +2,7 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         n = len(nums)
         l, r, = 0 , n-1
-
-        # res = nums[0]
-        # while l <= r:
-        #     if nums[l] < nums[r]:
-        #         res = min(res, nums[l])
-        #         break
-
-        #     mid = (l+r)//2
-        #     res = min(res, nums[mid])
-        #     if nums[mid] >= nums[l]:
-        #         l = mid + 1
-        #     else:
-        #         r = mid - 1
-        
-        # return res
-
-        # Pivot solution
+        res = nums[l]
         while l < r:
             mid = (l+r) // 2
             if nums[mid] > nums[r]:
@@ -26,4 +10,28 @@ class Solution:
             else:
                 r = mid
         return nums[l]
+
+        # # res = nums[0]
+        # # while l <= r:
+        # #     if nums[l] < nums[r]:
+        # #         res = min(res, nums[l])
+        # #         break
+
+        # #     mid = (l+r)//2
+        # #     res = min(res, nums[mid])
+        # #     if nums[mid] >= nums[l]:
+        # #         l = mid + 1
+        # #     else:
+        # #         r = mid - 1
+        
+        # # return res
+
+        # # Pivot solution - Always move to the unsorted portion.
+        # while l < r:
+        #     mid = (l+r) // 2
+        #     if nums[mid] > nums[r]:
+        #         l = mid + 1
+        #     else:
+        #         r = mid
+        # return nums[l]
                 
