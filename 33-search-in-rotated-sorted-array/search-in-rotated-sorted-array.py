c1a96f1f26c57dd4cd671a282e9ac1048b1f07b1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-24 06:22:28 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (90.97%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,29 +1,62 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        # Find the pivot:
-        n = len(nums)
-        l, r = 0, n-1
-        while l < r:
-            mid = (l+r) // 2
 
-            if nums[mid] > nums[r]:
-                l = mid + 1
-            else:
-                r = mid
-        pivot = l
-        # since we have pivot, we find the target in correct portions
-        if nums[pivot] <= target <= nums[n - 1]:
-            left, right = pivot, n - 1
-        else:
-            left, right = 0, pivot - 1
+        left, right = 0, len(nums)-1
 
         while left <= right:
-            mid = (left+right)//2
+            mid = (left + right) // 2
+            
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
-                left = mid + 1
+            
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             else:
-                right = mid - 1
-        
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return -1
+
+
+
+
+
+
+
+
+
+
+
+        # # Find the pivot:
+        # n = len(nums)
+        # l, r = 0, n-1
+        # while l < r:
+        #     mid = (l+r) // 2
+
+        #     if nums[mid] > nums[r]:
+        #         l = mid + 1
+        #     else:
+        #         r = mid
+        # pivot = l
+        # # since we have pivot, we find the target in correct portions
+        # if nums[pivot] <= target <= nums[n - 1]:
+        #     left, right = pivot, n - 1
+        # else:
+        #     left, right = 0, pivot - 1
+
+        # while left <= right:
+        #     mid = (left+right)//2
+        #     if nums[mid] == target:
+        #         return mid
+        #     elif nums[mid] < target:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
+        
+        # return -1
+
+
