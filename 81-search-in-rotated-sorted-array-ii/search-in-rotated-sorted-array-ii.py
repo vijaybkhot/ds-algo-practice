5c9cd16771b8112868040a1e05877dd5c5ad9fa4
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-24 06:18:13 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (95.52%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -5,27 +5,60 @@ class Solution:
         left, right = 0, n-1
 
         while left <= right:
-            mid = (left + right) // 2
-            
+            mid = (left+right) // 2
+
             if nums[mid] == target:
                 return True
 
-            # If we can't decide the sorted half
             if nums[left] == nums[mid] == nums[right]:
                 left += 1
                 right -= 1
 
-            # Left half is sorted
             elif nums[left] <= nums[mid]:
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-
-            # Right half is sorted
             else:
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
+        
         return False
+
+
+
+
+
+
+
+
+
+
+
+        # while left <= right:
+        #     mid = (left + right) // 2
+            
+        #     if nums[mid] == target:
+        #         return True
+
+        #     # If we can't decide the sorted half
+        #     if nums[left] == nums[mid] == nums[right]:
+        #         left += 1
+        #         right -= 1
+
+        #     # Left half is sorted
+        #     elif nums[left] <= nums[mid]:
+        #         if nums[left] <= target < nums[mid]:
+        #             right = mid - 1
+        #         else:
+        #             left = mid + 1
+
+        #     # Right half is sorted
+        #     else:
+        #         if nums[mid] < target <= nums[right]:
+        #             left = mid + 1
+        #         else:
+        #             right = mid - 1
+        # return False
