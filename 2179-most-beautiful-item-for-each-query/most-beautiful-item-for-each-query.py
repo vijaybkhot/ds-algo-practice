ad18e311bf6f552ac12c9292359234c6bc2ff207
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-27 08:09:17 -0400

    Time: 204 ms (45.33%) | Memory: 69.5 MB (7.55%) - LeetSync

---
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+
+        items.sort()
+        indexed_queries = [(query, idx)for idx, query in enumerate(queries)]
+        indexed_queries.sort()
+        heap = []
+        res = [0]*len(queries)
+        i = 0
+        for query, idx in indexed_queries:
+            while i < len(items) and items[i][0] <= query:
+                price, beauty = items[i]
+                heapq.heappush(heap, (-beauty))
+                i += 1
+            
+            max_beauty = -heap[0] if heap else 0
+            res[idx] = max_beauty
+        
+        return res
+
+        
