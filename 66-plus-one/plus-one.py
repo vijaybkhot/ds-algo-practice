0853bab884d6da8b30a7b1aa25ce15a5f848b837
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-29 11:29:17 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (49.55%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,6 +1,20 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num_str = ''.join([str(num) for num in digits])
-        new_num = str(int(num_str)+1)
-        return [int(str_num) for str_num in new_num]
+        # num_str = ''.join([str(num) for num in digits])
+        # new_num = str(int(num_str)+1)
+        # return [int(str_num) for str_num in new_num]
+        def dfs(i):
+            if i == -1:
+                return [1] + digits
+            
+            else:
+                if digits[i] + 1 <= 9:
+                    digits[i] += 1
+                    return digits
+                else:
+                    digits[i] = 0
+                    return dfs(i-1)
+        
+        return dfs(len(digits)-1)
+
         
