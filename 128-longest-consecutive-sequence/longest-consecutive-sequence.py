38b6d73c4ac69506272006aa6a5952aae9de66a9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-06-30 07:00:13 -0400

    Time: 43 ms (90.41%) | Memory: 33.3 MB (81.48%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,38 +1,17 @@
-class Solution(object):
-    def longestConsecutive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # if not nums:
-        #     return 0
-        # res = []
-        # unique_nums = list(set(nums))
-        # unique_nums.sort()
-        # curr_sequence = 1
-        # for i in range(0, len(unique_nums)-1):
-        #     if unique_nums[i+1] == unique_nums[i] + 1:
-        #         curr_sequence += 1
-        #     else:
-        #         res.append(curr_sequence)
-        #         curr_sequence = 1
-        # res.append(curr_sequence)
-        # return max(res)
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        res = 0
+        nums_set = set(nums)
 
-        if not nums:
-            return 0
-        longest_streak = 0
-        unique_nums = set(nums)
-        for num in unique_nums:
-            if num-1 not in unique_nums:
-                curr_streak = 1
-                curr_num = num
 
-                while curr_num + 1 in unique_nums:
-                    curr_num += 1
-                    curr_streak += 1
-                
-                if curr_streak > longest_streak:
-                    longest_streak = curr_streak
-                
-        return longest_streak
+        for num in nums_set:
+            if num-1 not in nums_set:
+                current = num
+                streak = 1
+
+                while current+1 in nums_set:
+                    streak += 1
+                    current+= 1
+                res = max(streak, res)
+        
+        return res
