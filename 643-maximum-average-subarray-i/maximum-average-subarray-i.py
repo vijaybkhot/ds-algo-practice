6bf01a387e0d1006e99ccfae6c2c71b945ae917e
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-01 16:53:47 -0400

    Time: 67 ms (83.99%) | Memory: 27.4 MB (39.29%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,39 +1,10 @@
-class Solution(object):
-    def findMaxAverage(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: float
-        """
-        # # Brute force approach
-        # max_sum = 0
-        # if len(nums) == 1:
-        #     max_sum = nums[0]
-        # for i in range(0, len(nums)-k+1):
-        #     curr_sum = 0
-        #     for j in range(i, i+k):
-        #         curr_sum += nums[j]
-        #     if curr_sum > max_sum:
-        #         max_sum = curr_sum
-        
-        # return float(max_sum) / k
-
-        # Using Sliding window approach
-        window_sum = 0
-        max_sum = 0
-        # Set up initial window
-        for i in range(0, k):
-            window_sum += nums[i]
-        
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        window_sum = sum(nums[:k])
         max_sum = window_sum
 
         for i in range(k, len(nums)):
-            window_sum += nums[i] - nums[i-k]
-            if window_sum > max_sum:
-                max_sum = window_sum
-
-        return float(max_sum) / k
-
-
-
+            window_sum = window_sum - nums[i-k] + nums[i]
+            max_sum = max(window_sum, max_sum)
         
+        return max_sum/k
