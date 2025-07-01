c0e52f9d3d9e4f53d461144bb12e246b12f19e56
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-01 06:11:09 -0400

    Time: 11 ms (36.07%) | Memory: 23.7 MB (5.03%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,15 +1,15 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # stripped_str = [char.lower() for char in s if char.lower() in "abcdefghijklmnopqrstuvwxyz0123456789"]
-        # l, r = 0, len(stripped_str)-1
-        # while l <= r:
-        #     if stripped_str[l] != stripped_str[r]:
-        #         return False
-        #     l += 1
-        #     r -= 1
+        stripped_str = [char.lower() for char in s if char.lower() in "abcdefghijklmnopqrstuvwxyz0123456789"]
+        return stripped_str == stripped_str[::-1]
+        l, r = 0, len(stripped_str)-1
+        while l <= r:
+            if stripped_str[l] != stripped_str[r]:
+                return False
+            l += 1
+            r -= 1
         
         # return True
-        # s = s.lower()
         l, r = 0, len(s)-1
         while l <= r:
             while l< r and s[l].lower() not in "abcdefghijklmnopqrstuvwxyz0123456789":
