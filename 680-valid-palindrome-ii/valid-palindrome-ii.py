1c803951dd588f75ca648ec7018ec5ce8b439c1a
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-01 11:48:09 -0400

    Time: 31 ms (92.01%) | Memory: 18.1 MB (20.02%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,24 +1,32 @@
-class Solution(object):
-    def validPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        # dp = {}
         
+        # def dfs(s, hasExcluded):
+        #     if (s, hasExcluded) in dp:
+        #         return dp[(s, hasExcluded)]
+        #     if not s or s == s[::-1]:
+        #         return True
+            
+        #     isValid = False
+        #     if not hasExcluded:
+        #         for i in range(len(s)):
+        #             excluded_str = s[:i]+s[i+1:]
+        #             if dfs(excluded_str, True):
+        #                 isValid = True
+        #                 break
+        #     dp[(s, hasExcluded)] = isValid
+        #     return dp[(s, hasExcluded)]
         
-        def isPalindrome(sub_s, left, right):
-            while left < right:
-                if sub_s[left] != sub_s[right]:
-                    return False
-                left += 1
-                right -= 1
-            return True
+        # return dfs(s, False)
 
-        left_p, right_p = 0, len(s)-1
-        while left_p < right_p:
-            if s[left_p] != s[right_p]:
-               return isPalindrome(s, left_p+1, right_p) or isPalindrome(s, left_p, right_p-1)
-            left_p += 1
-            right_p -= 1
-        
+        # Greedy two pointer
+
+        l, r = 0, len(s)-1
+
+        while l < r:
+            if s[l] != s[r]:
+                return s[l+1 : r+1] == s[l+1 : r+1][::-1] or s[l : r] == s[l : r][::-1]
+            l += 1
+            r -= 1
         return True
