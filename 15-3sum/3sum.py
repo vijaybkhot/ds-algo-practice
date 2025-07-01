03e3617bb75bfdd0b8b872c5da8b2b6e3674906f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-01 06:38:58 -0400

    Time: 753 ms (27.08%) | Memory: 20.6 MB (68.82%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,82 +1,26 @@
-class Solution(object):
-    def threeSum(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-
-        # nums.sort()
-        # n = len(nums)
-        # res = []
-        # for i in range(n):
-        #     curr_num = nums[i]
-        #     curr_target = -curr_num
-        #     left, right = i + 1, n-1
-        #     curr_triplet = []
-        #     while left < right:
-        #         if nums[left] + nums[right] == curr_target:
-        #             curr_triplet = [nums[i], nums[left], nums[right]]
-        #             if curr_triplet in res:
-        #                 curr_triplet = []
-        #             if len(curr_triplet) == 3:
-        #                 res.append(curr_triplet)
-        #             left += 1
-        #             right -= 1 
-        #         elif nums[left] + nums[right] < curr_target:
-        #             left += 1
-        #         else:
-        #             right -= 1
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        # return res
-
-        # # Optimize above approach. Used a set to store first elem in each triplets, to avoid duplicates
-        # nums.sort()
-        # n = len(nums)
-        # res = []
-        # triplet_first = set()
-        # for i in range(n):
-        #     curr_num = nums[i]
-        #     if curr_num in triplet_first:
-        #         continue
-        #     curr_target = -curr_num
-        #     left, right = i + 1, n-1
-        #     curr_triplet = []
-        #     while left < right:
-        #         if nums[left] + nums[right] == curr_target:
-        #             curr_triplet = [nums[i], nums[left], nums[right]]
-        #             if len(curr_triplet) == 3:
-        #                 res.append(curr_triplet)
-        #             left += 1
-        #             while nums[left] == nums[left-1] and left < right:
-        #                 left += 1
-        #         elif nums[left] + nums[right] < curr_target:
-        #             left += 1
-        #         else:
-        #             right -= 1
-        #     triplet_first.add(curr_num)
-        
-        # return res
-
-         # Optimize above approach. without using set
         nums.sort()
-        n = len(nums)
         res = []
-        for i in range(n-2):
+        i = 0
+        while i < (len(nums)-2):
+            j, k = i+1, len(nums)-1
             curr_num = nums[i]
-            if i > 0 and curr_num == nums[i-1]:
-                continue
-            curr_target = -curr_num
-            left, right = i + 1, n-1
-            while left < right:
-                if nums[left] + nums[right] == curr_target:
-                    res.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    while nums[left] == nums[left-1] and left < right:
-                        left += 1
-                elif nums[left] + nums[right] < curr_target:
-                    left += 1
+            while j < k:
+                if curr_num + nums[j] + nums[k] == 0:
+                    res.append([curr_num, nums[j], nums[k]])
+                    while j < k and nums[j+1] == nums[j]:
+                        j += 1
+                    j += 1
+                elif curr_num + nums[j] + nums[k] > 0:
+                    k -= 1
                 else:
-                    right -= 1      
-
+                    j += 1
+            
+            while i < len(nums)-2 and nums[i+1] == nums[i]:
+                i += 1
+            i += 1
+            
         return res
-
+        
