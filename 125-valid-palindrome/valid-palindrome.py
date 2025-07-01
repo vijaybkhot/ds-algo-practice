f9176fe6b5e65d0ffa0f4a106b6b1162552d06ae
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-01 06:06:23 -0400

    Time: 11 ms (36.07%) | Memory: 23.2 MB (17.14%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,24 +1,11 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        left_p = 0
-        right_p = len(s)-1
-
-        while right_p > left_p:
-            if not s[left_p].isalnum() or not s[right_p].isalnum():
-                if not s[left_p].isalnum():
-                    left_p += 1
-                if not s[right_p].isalnum():
-                    right_p -= 1
-                continue
-            if ord(s[left_p].lower()) !=  ord(s[right_p].lower()):
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        stripped_str = [char.lower() for char in s if char.lower() in "abcdefghijklmnopqrstuvwxyz0123456789"]
+        l, r = 0, len(stripped_str)-1
+        while l <= r:
+            if stripped_str[l] != stripped_str[r]:
                 return False
-            left_p += 1
-            right_p -= 1
+            l += 1
+            r -= 1
         
         return True
-                
-        
