7b50884b048bc538c8024f453f0706c739223061
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-01 05:20:17 -0400

    Time: 15 ms (11.32%) | Memory: 21.3 MB (61.63%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,43 +1,30 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
-        # # Sorting approach O(nlogn)
-        # def frequencyIndexMap(arr):
-        #     map = {}
-        #     for i in range(len(arr)):
-        #         map[arr[i]] = map.get(arr[i],0)+1
-        #     return map
-        
-        # freqMap = frequencyIndexMap(nums)
-        # ferqMapTupleArray = tuple(freqMap.items())
-        # sortedTupleArray = sorted(ferqMapTupleArray, key=lambda x: x[1],  reverse=True)
-        # return map(lambda x:x[0], sortedTupleArray[0:k])
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        n = len(nums)
 
-        # Bucket sort approach O(n)
-        def frequencyIndexMap(arr):
-            map = {}
-            for i in range(len(arr)):
-                map[arr[i]] = map.get(arr[i],0)+1
-            return map
-        freqMap = frequencyIndexMap(nums)
-
-        buckets = [[] for _ in range(len(nums)+1)]
-        for num, freq in freqMap.items():
-            buckets[freq].append(num)
+        freq_map = defaultdict(int)
+        max_freq = 0
+        for num in nums:
+            freq_map[num]+= 1
+            max_freq = max(max_freq, freq_map[num])
         
-        res = []
-        for i in range(len(buckets)-1, -1, -1):
-            for num in buckets[i]:
-                res.append(num)
-                if len(res) == k:
-                    return res
+        # bucket = [[]*(max_freq+1)]
+        # for num in nums:
+        #     bucket[freq_map[num]].append(num)
+        # res = []
+
+        # for i in range(len(bucket)-1, -1, -1):
+        #     while len(res) < k and bucket[i]:
+        #         res.append(bucket[i].pop())
             
-        
-        
+        # return res
 
+        # Using a heap
+        heap = [(-freq_map[num], num) for num in freq_map]
+        heapq.heapify(heap)
+        res = []
+        for i in range(k):
+            freq, num = heapq.heappop(heap)
+            res.append(num)
         
-        
+        return res
