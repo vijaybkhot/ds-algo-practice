fc35cbea50d135588e25803aa18163c4c4a596a1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-01 15:18:14 -0400

    Time: 20 ms (39.49%) | Memory: 18.1 MB (12.30%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,23 +1,56 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        # Using dynamic sliding window and a hash set to track the length of longest substring
-        output = 0
-        left = 0
-        unique_set = set()
-
-        for right in range(len(s)):
-            if s[right] in unique_set:
-                while True:
-                    unique_set.remove(s[left])
-                    left += 1
-                    if s[right] not in unique_set:
-                        break
-            unique_set.add(s[right])
-            if len(unique_set) > output:
-                output = len(unique_set)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+        res = 0
+        left, right = 0, 1
+        curr_set = set()
+        curr_set.add(s[left])
+        while left < len(s) and right < len(s):
+            while right < len(s) and s[right] not in curr_set:
+                curr_set.add(s[right])
+                right += 1
+                res = max(res, len(curr_set))
+            while right < len(s) and s[right] in curr_set:
+                curr_set.remove(s[left])
+                left += 1
         
-        return output
+        return max(res, 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # res = 0
+        # i, j = 0, 0
+        # curr_set = set()
+        
+        # index_map = defaultdict(int)
+
+        # for idx, char in enumerate(s):
+        #     s[char] += 1
+        
+        # l, r = 0, len(s)-1
+        
+        # while l < r:
+        #     if index_map[s[l]] > 1:
+        #         l += 1
+        #     if index_map[s[r]] > 1:
+        #         r -= 1
+            
