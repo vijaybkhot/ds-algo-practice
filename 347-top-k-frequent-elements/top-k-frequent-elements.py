697f77744d36a3c35006e497a2e9f90986d8ccf0
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-01 05:25:35 -0400

    Time: 8 ms (34.72%) | Memory: 21 MB (89.52%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -8,23 +8,24 @@ class Solution:
             freq_map[num]+= 1
             max_freq = max(max_freq, freq_map[num])
         
-        # bucket = [[]*(max_freq+1)]
-        # for num in nums:
-        #     bucket[freq_map[num]].append(num)
-        # res = []
+        bucket = [[] for _ in range(max_freq + 1)]
 
-        # for i in range(len(bucket)-1, -1, -1):
-        #     while len(res) < k and bucket[i]:
-        #         res.append(bucket[i].pop())
-            
-        # return res
-
-        # Using a heap
-        heap = [(-freq_map[num], num) for num in freq_map]
-        heapq.heapify(heap)
+        for num in freq_map:
+            bucket[freq_map[num]].append(num)
         res = []
-        for i in range(k):
-            freq, num = heapq.heappop(heap)
-            res.append(num)
-        
+
+        for i in range(len(bucket)-1, -1, -1):
+            while len(res) < k and bucket[i]:
+                res.append(bucket[i].pop())
+            
         return res
+
+        # # Using a heap
+        # heap = [(-freq_map[num], num) for num in freq_map]
+        # heapq.heapify(heap)
+        # res = []
+        # for i in range(k):
+        #     freq, num = heapq.heappop(heap)
+        #     res.append(num)
+        
+        # return res
