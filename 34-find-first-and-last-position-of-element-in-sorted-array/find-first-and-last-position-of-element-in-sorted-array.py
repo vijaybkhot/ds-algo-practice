318ead16b38ec828465e5c62a5a0b6ad24ea758a
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-02 09:52:24 -0400

    Time: 0 ms (100.00%) | Memory: 13.2 MB (30.86%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -5,86 +5,127 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        # # O(n) solution
-        # result = []
-        # for i in range(len(nums)):
-        #     if nums[i] == target and len(result)== 0:
-        #         result.append(i)
-        #     elif nums[i] == target and len(result)>= 1:
-        #         if(len(result)==1):
-        #             result.append(i)
-        #         else:
-        #             result[1] = i
-        
-        # if len(result)== 1:
-        #     if nums[-1] == target:
-        #         result.append(len(nums)-1)
-        #     else:
-        #         result.append(result[0])
-        # if len(result)==0:
-        #     result = [-1, -1]
-        
-        # return result
 
-        # # O(log n) + O(n) solution
-        # low, high = 0, len(nums)-1
-        # target_index = -1
-        # while low <= high:
-        #     mid = (low + high) // 2
-        #     if nums[mid] < target:
-        #         low = mid + 1
-        #     elif nums[mid] > target:
-        #         high = mid - 1
-        #     else:
-        #         target_index = mid
-        #         break
-        
-        # if target_index == -1:
-        #     return [-1, -1]
-        
-        # low_index, high_index = target_index, target_index
-        # output = [low_index, high_index]
-        # while low_index > -1 and nums[low_index] == target:
-        #     output[0] = low_index
-        #     low_index -= 1
-        
-        # while high_index < len(nums) and nums[high_index] == target:
-        #     output[1] = high_index
-        #     high_index += 1
-        
-        # return output
-        
-        # Optimized O(log n) solution
-
-        def findFirstOccurrence(nums, target):
-            low, high = 0, len(nums)-1
+        def first_occurence(nums, target):
+            l, r = 0, len(nums)-1
             first = -1
-            while low <= high:
-                mid = (low + high) // 2
+            while l <= r:
+                mid = (l+r)//2
                 if nums[mid] >= target:
                     if nums[mid] == target:
                         first = mid
-                    high = mid - 1
+                    r = mid-1
                 else:
-                    low = mid + 1
+                    l = mid + 1
             return first
+
         
-        def findLastOccurrence(nums, target):
-            low, high = 0, len(nums)-1
+        def last_occurence(nums, target):
             last = -1
-            while low <= high:
-                mid = (low + high) // 2
+            l, r = 0, len(nums)-1
+            while l <= r:
+                mid = (l+r)//2
                 if nums[mid] <= target:
                     if nums[mid] == target:
                         last = mid
-                    low = mid + 1
+                    l = mid +1
                 else:
-                    high = mid - 1
+                    r = mid - 1
+            
             return last
         
-        first = findFirstOccurrence(nums, target)
-        last = findLastOccurrence(nums, target)
+        first, last = first_occurence(nums, target), last_occurence(nums, target)
         return [first, last]
+
+
+
+
+
+
+
+
+
+
+        # # # O(n) solution
+        # # result = []
+        # # for i in range(len(nums)):
+        # #     if nums[i] == target and len(result)== 0:
+        # #         result.append(i)
+        # #     elif nums[i] == target and len(result)>= 1:
+        # #         if(len(result)==1):
+        # #             result.append(i)
+        # #         else:
+        # #             result[1] = i
+        
+        # # if len(result)== 1:
+        # #     if nums[-1] == target:
+        # #         result.append(len(nums)-1)
+        # #     else:
+        # #         result.append(result[0])
+        # # if len(result)==0:
+        # #     result = [-1, -1]
+        
+        # # return result
+
+        # # # O(log n) + O(n) solution
+        # # low, high = 0, len(nums)-1
+        # # target_index = -1
+        # # while low <= high:
+        # #     mid = (low + high) // 2
+        # #     if nums[mid] < target:
+        # #         low = mid + 1
+        # #     elif nums[mid] > target:
+        # #         high = mid - 1
+        # #     else:
+        # #         target_index = mid
+        # #         break
+        
+        # # if target_index == -1:
+        # #     return [-1, -1]
+        
+        # # low_index, high_index = target_index, target_index
+        # # output = [low_index, high_index]
+        # # while low_index > -1 and nums[low_index] == target:
+        # #     output[0] = low_index
+        # #     low_index -= 1
+        
+        # # while high_index < len(nums) and nums[high_index] == target:
+        # #     output[1] = high_index
+        # #     high_index += 1
+        
+        # # return output
+        
+        # # Optimized O(log n) solution
+
+        # def findFirstOccurrence(nums, target):
+        #     low, high = 0, len(nums)-1
+        #     first = -1
+        #     while low <= high:
+        #         mid = (low + high) // 2
+        #         if nums[mid] >= target:
+        #             if nums[mid] == target:
+        #                 first = mid
+        #             high = mid - 1
+        #         else:
+        #             low = mid + 1
+        #     return first
+        
+        # def findLastOccurrence(nums, target):
+        #     low, high = 0, len(nums)-1
+        #     last = -1
+        #     while low <= high:
+        #         mid = (low + high) // 2
+        #         if nums[mid] <= target:
+        #             if nums[mid] == target:
+        #                 last = mid
+        #             low = mid + 1
+        #         else:
+        #             high = mid - 1
+        #     return last
+        
+        # first = findFirstOccurrence(nums, target)
+        # last = findLastOccurrence(nums, target)
+        # return [first, last]
 
 
 
