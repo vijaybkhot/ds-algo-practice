0fb522c1e9f96f6db3b33575a0a598f09c5d8590
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-02 05:30:05 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (32.32%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,14 +1,47 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        for i in range(len(s)):
-            if s[i] == "(" or s[i] == "{" or s[i] == "[":
-                stack.append(s[i])
-            else:
-                if stack and ((s[i] == ")" and stack[-1] == "(") or (s[i] == "}" and stack[-1] == "{") or (s[i] == "]" and stack[-1] == "[")):
-                    stack.pop()
-                else:
+        for paren in s:
+            if paren in ["(", "{", "["]:
+                stack.append(paren)
+            elif paren in [")", "}", "]"]:
+                if not stack or paren == ")" and stack[-1] != "(" or paren == "}" and stack[-1] != "{" or paren == "]" and stack[-1] != "[":
                     return False
+                else:
+                    stack.pop()
+        
+        return True if not stack else False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack = []
+        # for i in range(len(s)):
+        #     if s[i] == "(" or s[i] == "{" or s[i] == "[":
+        #         stack.append(s[i])
+        #     else:
+        #         if stack and ((s[i] == ")" and stack[-1] == "(") or (s[i] == "}" and stack[-1] == "{") or (s[i] == "]" and stack[-1] == "[")):
+        #             stack.pop()
+        #         else:
+        #             return False
                 
-        return not stack
+        # return not stack
         
