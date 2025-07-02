e241be839220c872e15b7bbba21d23bcf44fe0b7
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-02 07:05:52 -0400

    Time: 51 ms (85.42%) | Memory: 23.1 MB (18.11%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -1,32 +1,16 @@
-class StockSpanner(object):
+class StockSpanner:
 
     def __init__(self):
         self.stack = []
         
 
-    def next(self, price):
-        """
-        :type price: int
-        :rtype: int
-        """
-        # curr_index = len(self.stack)
-        # self.stack.append([price, curr_index])
-        # self.backup_stack = []
-        # while self.stack and self.stack[-1][0] <= price:
-        #     self.backup_stack.append(self.stack.pop())
-        # res = len(self.backup_stack)
-        # self.stack.extend(self.backup_stack[::-1])
-        # self.backup_stack = []
-        # return res
-
-        # Monotonically decreasing stack
+    def next(self, price: int) -> int:
         curr_span = 1
         while self.stack and self.stack[-1][0] <= price:
             curr_span += self.stack.pop()[1]
-        self.stack.append([price, curr_span])
+            
+        self.stack.append((price, curr_span))
         return curr_span
-
-        
 
 
 # Your StockSpanner object will be instantiated and called as such:
