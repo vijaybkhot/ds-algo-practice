d3b4aec7f7a6e0d066a5989589859fa7266b7663
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-02 04:24:44 -0400

    Time: 11 ms (97.36%) | Memory: 18 MB (39.44%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,38 +1,27 @@
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
             return False
-        # using fixed size sliding window
-        # Create frequency map of s1 to account for all permutations of s1
-        s1_map = {}
+        s1_map = defaultdict(int)
+        s2_map = defaultdict(int)
         for char in s1:
-            s1_map[char] = s1_map.get(char, 0) + 1
-        # Create a starting widow in s2 and check 
-        left = 0
-        window_map = {}
-        for i in range(len(s1)):
-            window_map[s2[i]] = window_map.get(s2[i], 0) + 1
+            s1_map[char] += 1
         
-        if s1_map == window_map:
+        left = 0
+        for i in range(len(s1)):
+            s2_map[s2[i]] += 1
+        if s1_map == s2_map:
             return True
         
         for right in range(len(s1), len(s2)):
-            # Remove the leftmost element from window
-            window_map[s2[left]] = window_map.get(s2[left]) - 1
-            if window_map[s2[left]] == 0:
-                del window_map[s2[left]]
-            # Add the new right element
-            window_map[s2[right]] = window_map.get(s2[right], 0) + 1
-            if s1_map == window_map:
-                return True
+            # remove left pointer element
+            s2_map[s2[left]] -= 1
+            if s2_map[s2[left]] == 0:
+                del s2_map[s2[left]]
             left += 1
+            # add right element
+            s2_map[s2[right]] += 1
+            if s1_map == s2_map:
+                return True
         
         return False
-
-
-        
