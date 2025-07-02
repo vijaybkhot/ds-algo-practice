88445a7c08bf73e1284508d2b10f761d8b270415
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-02 11:51:00 -0400

    Time: 83 ms (67.62%) | Memory: 27.7 MB (94.37%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.py
@@ -1,40 +1,17 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-    def getIntersectionNode(self, headA, headB):
-        """
-        :type head1, head1: ListNode
-        :rtype: ListNode
-        """
-        # # Set approach. O(n+m) time and O(n) or O(m) extra space
-        # node_set = set()
-        # while headA or headB:
-        #     if headA is not None and headA in node_set:
-        #         return headA
-        #     if headA is not None:
-        #         node_set.add(headA)
-        #     if headB is not None and headB in node_set:
-        #         return headB
-        #     if headB is not None:
-        #         node_set.add(headB)
-        #     headA = headA.next if headA else None
-        #     headB = headB.next if headB else None
-        
-        # return None
-
-        # Two pointer approach. O(n+m) time and O(1) extra space
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
         if not headA or not headB:
             return None
+        x, y = headA, headB
 
-        ptrA, ptrB = headA, headB
-
-        while ptrA != ptrB:
-            ptrA = ptrA.next if ptrA else headB
-            ptrB = ptrB.next if ptrB else headA
+        while x != y:
+            x = x.next if x else headB
+            y = y.next if y else headA
         
-        return ptrA
-        
+        return x
