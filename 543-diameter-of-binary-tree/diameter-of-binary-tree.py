3dd717d7fd4115ffa9dfd57aba95cc4951cc7ab2
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-02 13:39:02 -0400

    Time: 4 ms (73.91%) | Memory: 20.9 MB (49.43%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -7,56 +7,73 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         # BFS approach -> Not correct
-        if not root:
+        if not root or (not root.left and not root.right):
             return 0
-
-        # q = deque([root])
-        # dia = 0
-        # level = 0
-        # while q:
-        #     if level > 0:
-        #         if len(q) > 2 ** (level-1):
-        #             dia += 2
-        #         else:
-        #             dia += 1
-        #     for i in range(len(q)):
-        #         node = q.popleft()
-        #         if node.left:
-        #             q.append(node.left)
-        #         if node.right:
-        #             q.append(node.right)
-        #     level += 1
-        
-        # return dia
-
-        # # DFS - Recursive Approach
-        # self.maxDia = 0
-
-        # def height(node):
-        #     if not node:
-        #         return 0
-            
-        #     left_height = height(node.left)
-        #     right_height = height(node.right)
-        #     self.maxDia = max(self.maxDia, left_height+right_height)
-
-        #     return 1 + max(left_height, right_height)
-        
-        # height(root)
-        # return self.maxDia
-        
-        self.maxDia = 0
+        self.max_dia = 0
         def height(node):
             if not node:
                 return 0
+            
             left = height(node.left)
             right = height(node.right)
-            self.maxDia = max(self.maxDia, left+right)
+            self.max_dia = max(self.max_dia, left+right)
             return 1 + max(left, right)
+        height(root)
+        return self.max_dia
+        
+
+
+        
+
+
+
+        # # q = deque([root])
+        # # dia = 0
+        # # level = 0
+        # # while q:
+        # #     if level > 0:
+        # #         if len(q) > 2 ** (level-1):
+        # #             dia += 2
+        # #         else:
+        # #             dia += 1
+        # #     for i in range(len(q)):
+        # #         node = q.popleft()
+        # #         if node.left:
+        # #             q.append(node.left)
+        # #         if node.right:
+        # #             q.append(node.right)
+        # #     level += 1
+        
+        # # return dia
+
+        # # # DFS - Recursive Approach
+        # # self.maxDia = 0
+
+        # # def height(node):
+        # #     if not node:
+        # #         return 0
+            
+        # #     left_height = height(node.left)
+        # #     right_height = height(node.right)
+        # #     self.maxDia = max(self.maxDia, left_height+right_height)
+
+        # #     return 1 + max(left_height, right_height)
+        
+        # # height(root)
+        # # return self.maxDia
+        
+        # self.maxDia = 0
+        # def height(node):
+        #     if not node:
+        #         return 0
+        #     left = height(node.left)
+        #     right = height(node.right)
+        #     self.maxDia = max(self.maxDia, left+right)
+        #     return 1 + max(left, right)
     
         
-        height(root)
-        return self.maxDia
+        # height(root)
+        # return self.maxDia
 
 
 
