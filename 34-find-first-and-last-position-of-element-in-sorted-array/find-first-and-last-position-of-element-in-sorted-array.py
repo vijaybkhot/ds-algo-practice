1e58f0cdaaea69e503711cdbde02c285534edb66
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-02 09:57:30 -0400

    Time: 0 ms (100.00%) | Memory: 13.1 MB (65.24%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -33,15 +33,24 @@ class Solution(object):
         def last_occurence(nums, target):
             last = -1
             l, r = 0, len(nums)-1
+            # while l <= r:
+            #     mid = (l+r)//2
+            #     if nums[mid] <= target:
+            #         if nums[mid] == target:
+            #             last = mid
+            #         l = mid +1
+            #     else:
+            #         r = mid - 1
+            
             while l <= r:
                 mid = (l+r)//2
-                if nums[mid] <= target:
-                    if nums[mid] == target:
-                        last = mid
-                    l = mid +1
+                if nums[mid] == target:
+                    last = mid
+                    l = mid+1
+                elif nums[mid] <= target:
+                    l = mid + 1
                 else:
                     r = mid - 1
-            
             return last
         
         first, last = first_occurence(nums, target), last_occurence(nums, target)
