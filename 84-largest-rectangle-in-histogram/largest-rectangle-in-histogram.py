0fbf4373579b5f73a958670cfdc4fbe87c84441c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-02 08:06:08 -0400

    Time: 171 ms (37.00%) | Memory: 36.2 MB (13.52%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -1,46 +1,16 @@
-class Solution(object):
-    def largestRectangleArea(self, heights):
-        """
-        :type heights: List[int]
-        :rtype: int
-        """
-        # # Brute force approach:
-        # stack = []
-        # max_area = 0
-        
-        # for i in range(len(heights)):
-        #     curr_height = heights[i]
-        #     # Calculate max area starting from the current graph
-        #     curr_area = curr_height
-        #     curr_max = curr_height
-        #     curr_min = curr_height
-        #     j = 1
-        #     while stack:
-        #         prev_height = stack.pop()
-        #         j += 1
-        #         curr_max = max(curr_max, prev_height)
-        #         curr_min = min(curr_min, prev_height)
-        #         area = curr_min * j
-        #         curr_area = max(curr_area, area)
-            
-        #     max_area = max(curr_area, max_area)
-        #     stack = heights[:i+1]
-        
-        # return max_area
-
-        # Optimized monotonic stack approach
-        max_area = 0
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
         stack = []
+        max_area = 0
 
         for i, h in enumerate(heights):
             start = i
             while stack and stack[-1][1] > h:
-                index, height = stack.pop()
-                max_area = max(max_area, (height * (i - index)))
-                start = index
+                idx, height = stack.pop()
+                max_area = max(max_area, (height*(i-idx)))
+                start = idx
             stack.append((start, h))
-        
-        for i, h in stack:
-            max_area = max(max_area, h * (len(heights) - i))
+        for idx, h in stack:
+            max_area = max(max_area, (h*(len(heights)-idx)))
         
         return max_area
