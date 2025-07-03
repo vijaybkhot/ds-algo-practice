f7d2266a0951e48e482546388e4afd2c45293be0
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-03 16:49:26 -0400

    Time: 19 ms (1.59%) | Memory: 19.7 MB (13.15%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -3,41 +3,61 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
-        
-        # # Bottom-up Dynamic programming approach
-        # nums_record = [(0, 0)]*(n+1)    # with_curr, without_curr
-        # nums_record[0] = (nums[0], 0)
-        # nums_record[1] = (nums[1], nums_record[0][0])
+        res = 0
+        dp = {}
+        def dfs(i, prev):
+            if (i, prev) in dp:
+                return dp[i, prev]
 
-        # for i in range(2, n):
-        #     nums_record[i] = ((nums[i]+nums_record[i-1][1]), max(nums_record[i-1]))
-        
-        # return max(nums_record[n-1])
+            if i == len(nums):
+                return 0
+            
+            total = 0
+            if prev == i-1:
+                total = dfs(i+1, prev)
+            else:
+                total = max(nums[i]+dfs(i+1, i), dfs(i+1, prev))
+            
+            dp[i, prev]= total
+            return dp[i, prev]
+        return dfs(0, None)
+                
 
-        # # Space optimized DP
-        # max_num_1 = (nums[1], nums[0])
-        # for i in range(2, n):
-        #     curr_max_set = ((nums[i]+ max_num_1[1]), max(max_num_1))
-        #     max_num_1 = curr_max_set
         
-        # return max(max_num_1)
+        # # # Bottom-up Dynamic programming approach
+        # # nums_record = [(0, 0)]*(n+1)    # with_curr, without_curr
+        # # nums_record[0] = (nums[0], 0)
+        # # nums_record[1] = (nums[1], nums_record[0][0])
 
-        # # Recursion:
+        # # for i in range(2, n):
+        # #     nums_record[i] = ((nums[i]+nums_record[i-1][1]), max(nums_record[i-1]))
+        
+        # # return max(nums_record[n-1])
+
+        # # # Space optimized DP
+        # # max_num_1 = (nums[1], nums[0])
+        # # for i in range(2, n):
+        # #     curr_max_set = ((nums[i]+ max_num_1[1]), max(max_num_1))
+        # #     max_num_1 = curr_max_set
+        
+        # # return max(max_num_1)
+
+        # # # Recursion:
+        # # def dfs(i):
+        # #     if i >= n:
+        # #         return 0
+        # #     return max(nums[i]+dfs(i+2), dfs(i+1))
+        # # return dfs(0)
+
+        # # Memoization
+        # memo = [-1]*(n+1)
         # def dfs(i):
         #     if i >= n:
         #         return 0
-        #     return max(nums[i]+dfs(i+2), dfs(i+1))
+        #     if memo[i] != -1:
+        #         return memo[i]
+        #     memo[i] = max(nums[i]+dfs(i+2), dfs(i+1))
+        #     return memo[i]
         # return dfs(0)
-
-        # Memoization
-        memo = [-1]*(n+1)
-        def dfs(i):
-            if i >= n:
-                return 0
-            if memo[i] != -1:
-                return memo[i]
-            memo[i] = max(nums[i]+dfs(i+2), dfs(i+1))
-            return memo[i]
-        return dfs(0)
 
         
