baca67f69c4b025cadc87f1fee79d246ae779cf8
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-03 06:50:29 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (75.34%) - LeetSync

--- 778-reorganize-string/reorganize-string.py
+++ 778-reorganize-string/reorganize-string.py
@@ -43,31 +43,55 @@ class Solution:
         # return res
 
 
-        freq_count = defaultdict(int)
-        for char in s:
-            freq_count[char] += 1
+        # freq_count = defaultdict(int)
+        # for char in s:
+        #     freq_count[char] += 1
         
-        heap = [(-val, key) for key, val in freq_count.items()]
-        heapq.heapify(heap)
+        # heap = [(-val, key) for key, val in freq_count.items()]
+        # heapq.heapify(heap)
+
+        # res = ""
+
+        # while len(heap) > 1:
+        #     count1, char1 = heapq.heappop(heap)
+        #     count2, char2 = heapq.heappop(heap)
+        #     res += char1 + char2
+        #     count1 += 1
+        #     count2 += 1
+        #     if count1 < 0:
+        #         heapq.heappush(heap, (count1, char1))
+        #     if count2 < 0:
+        #         heapq.heappush(heap, (count2, char2))
+        
+        # if heap:
+        #     if heap[0][0] < -1 or (res and res[-1] == heap[0][1]):
+        #         return ""
+        #     else:
+        #         res += heap[0][1]
+        
+        # return res
+        
+        freq = Counter(s)
+        max_heap = [(-freq, char) for char, freq in freq.items()]
+        heapq.heapify(max_heap)
 
         res = ""
+        prev_char = ""
 
-        while len(heap) > 1:
-            count1, char1 = heapq.heappop(heap)
-            count2, char2 = heapq.heappop(heap)
-            res += char1 + char2
-            count1 += 1
-            count2 += 1
-            if count1 < 0:
-                heapq.heappush(heap, (count1, char1))
-            if count2 < 0:
-                heapq.heappush(heap, (count2, char2))
-        
-        if heap:
-            if heap[0][0] < -1 or (res and res[-1] == heap[0][1]):
-                return ""
-            else:
-                res += heap[0][1]
+        while max_heap:
+            count1, char1 = heapq.heappop(max_heap)
+            if res and prev_char == char1:
+                if max_heap:
+                    count2, char2 = heapq.heappop(max_heap)
+                    res += char2
+                    if count2 < -1:
+                        heapq.heappush(max_heap, (count2+1, char2))
+                else:
+                    return ""
+            res += char1
+            prev_char = char1
+            if count1 < -1:
+                heapq.heappush(max_heap, (count1+1, char1))
         
         return res
 
