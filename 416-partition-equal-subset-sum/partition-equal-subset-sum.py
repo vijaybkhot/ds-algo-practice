30c774cac559a52c6dd90718aaaf36e6eea40d73
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-03 18:59:30 -0400

    Time: 2722 ms (7.22%) | Memory: 493.9 MB (5.01%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,79 +1,102 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # # First attempt
-        # l_sum, r_sum = 0, 0
-        # l_sum = nums[0]
-        # for i in range(1, len(nums)):
+        total = sum(nums)
+        if total%2:
+            return False
+        target_sum = total//2
+        self.possible = False
+        dp = {}
+        def dfs(i, curr_sum):
+            if (i, curr_sum) in dp:
+                return dp[(i, curr_sum)]
+            if curr_sum == target_sum or self.possible:
+                self.possible = True
+                return True
+            if i == len(nums):
+                return 0
+            if self.possible:
+                return True
+            dp[(i, curr_sum)] =  dfs(i+1, curr_sum+nums[i]) or dfs(i+1, curr_sum)
+            return dp[(i, curr_sum)]
+        
+        dfs(0, 0)
+        return self.possible
 
-        #     if nums[i] >= l_sum+r_sum:
-        #         l_sum = l_sum + r_sum
-        #         r_sum = nums[i]
-        #     elif l_sum < r_sum:
-        #         l_sum += nums[i]
-        #     else:
-        #         r_sum += nums[i]
-        # return l_sum == r_sum
 
-        # # Second attempt
+        # # # First attempt
+        # # l_sum, r_sum = 0, 0
+        # # l_sum = nums[0]
+        # # for i in range(1, len(nums)):
+
+        # #     if nums[i] >= l_sum+r_sum:
+        # #         l_sum = l_sum + r_sum
+        # #         r_sum = nums[i]
+        # #     elif l_sum < r_sum:
+        # #         l_sum += nums[i]
+        # #     else:
+        # #         r_sum += nums[i]
+        # # return l_sum == r_sum
+
+        # # # Second attempt
+        # # total = sum(nums)
+        # # if total % 2:
+        # #     return False
+        
+        # # l_sum, r_sum = 0, 0
+        # # l_sum = nums[0]
+
+        # # for i in range(1, len(nums)):
+        # #     if l_sum + nums[i] <= total//2:
+        # #         l_sum += nums[i]
+        # #     else:
+        # #         r_sum += nums[i]
+        # # return l_sum == r_sum
+        
+
         # total = sum(nums)
         # if total % 2:
         #     return False
+        # target = total // 2
+
+        # # # Top-down memoization approach
+        # # memo = {}
+        # # def dfs(i, curr_sum):
+        # #     if (i, curr_sum) in memo:
+        # #         return memo[(i, curr_sum)]
+        # #     if curr_sum == target:
+        # #         return True
+        # #     if i == len(nums) or curr_sum > target:
+        # #         return False
+
+        # #     memo[(i, curr_sum)] = dfs(i + 1, curr_sum + nums[i]) or dfs(i + 1, curr_sum)
+        # #     return memo[(i, curr_sum)]
         
-        # l_sum, r_sum = 0, 0
-        # l_sum = nums[0]
+        # # return dfs(0, 0)
 
-        # for i in range(1, len(nums)):
-        #     if l_sum + nums[i] <= total//2:
-        #         l_sum += nums[i]
-        #     else:
-        #         r_sum += nums[i]
-        # return l_sum == r_sum
+        # # # Bottom up DP using a set to store all the sums
+        # # dp = set()
+        # # dp.add(0)
+
+        # # for i in range(len(nums)-1, -1, -1):
+        # #     nextDP = set()
+        # #     for t in dp:
+        # #         if (t + nums[i]) == target:
+        # #             return True
+        # #         nextDP.add(t+nums[i])
+        # #         nextDP.add(t)
+        # #     dp = nextDP
         
+        # # return False
 
-        total = sum(nums)
-        if total % 2:
-            return False
-        target = total // 2
-
-        # # Top-down memoization approach
-        # memo = {}
-        # def dfs(i, curr_sum):
-        #     if (i, curr_sum) in memo:
-        #         return memo[(i, curr_sum)]
-        #     if curr_sum == target:
-        #         return True
-        #     if i == len(nums) or curr_sum > target:
-        #         return False
-
-        #     memo[(i, curr_sum)] = dfs(i + 1, curr_sum + nums[i]) or dfs(i + 1, curr_sum)
-        #     return memo[(i, curr_sum)]
+        # # Bottom-up DP using an array
+        # dp = [False]*(target+1)
+        # dp[0] = True
         
-        # return dfs(0, 0)
-
-        # # Bottom up DP using a set to store all the sums
-        # dp = set()
-        # dp.add(0)
-
-        # for i in range(len(nums)-1, -1, -1):
-        #     nextDP = set()
-        #     for t in dp:
-        #         if (t + nums[i]) == target:
-        #             return True
-        #         nextDP.add(t+nums[i])
-        #         nextDP.add(t)
-        #     dp = nextDP
+        # for num in nums:
+        #     for i in range(target, num-1, -1):
+        #         dp[i] = dp[i] or dp[i-num]
         
-        # return False
-
-        # Bottom-up DP using an array
-        dp = [False]*(target+1)
-        dp[0] = True
-        
-        for num in nums:
-            for i in range(target, num-1, -1):
-                dp[i] = dp[i] or dp[i-num]
-        
-        return dp[target]
+        # return dp[target]
         
             
             
