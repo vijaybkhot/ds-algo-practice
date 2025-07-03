aab1b8d9d4c4937cd2de69114bd2a2b9313b7dba
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-03 05:39:24 -0400

    Time: 11 ms (61.70%) | Memory: 20.2 MB (54.87%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -8,67 +8,102 @@ class Solution:
         if not lists:
             return None
         
-        # # Appraoch - I: Using a function to merge two lists and iteratively merging two lists in the lists array
-        # def mergeTwoLists(l1, l2):
-        #     dummy = ListNode()
-        #     tail = dummy
-        #     while l1 and l2:
-        #         if l1.val < l2.val:
-        #             tail.next = l1
-        #             l1 = l1.next
-        #             tail = tail.next
-        #         else:
-        #             tail.next = l2
-        #             l2 = l2.next
-        #             tail = tail.next
-        #     if l1:
-        #         tail.next = l1
-        #     if l2:
-        #         tail.next = l2
-            
-        #     return dummy.next
-            
-        # dummy = ListNode()
-        # tail = dummy
 
-        # while len(lists) > 1:
-        #     merged_list = []
-        #     for i in range(0, len(lists), 2):
-        #         list1 = lists[i]
-        #         list2 = lists[i+1] if i+1 < len(lists) else None
-        #         merged_list.append(mergeTwoLists(list1, list2))
-        #     lists = merged_list
+
         
-        # return lists[0]
+        # # # Appraoch - I: Using a function to merge two lists and iteratively merging two lists in the lists array
+        # # def mergeTwoLists(l1, l2):
+        # #     dummy = ListNode()
+        # #     tail = dummy
+        # #     while l1 and l2:
+        # #         if l1.val < l2.val:
+        # #             tail.next = l1
+        # #             l1 = l1.next
+        # #             tail = tail.next
+        # #         else:
+        # #             tail.next = l2
+        # #             l2 = l2.next
+        # #             tail = tail.next
+        # #     if l1:
+        # #         tail.next = l1
+        # #     if l2:
+        # #         tail.next = l2
+            
+        # #     return dummy.next
+            
+        # # dummy = ListNode()
+        # # tail = dummy
 
-        # # Approach II: Counting the maximum length of a list and iterating that many number of times to merge all lists together
+        # # while len(lists) > 1:
+        # #     merged_list = []
+        # #     for i in range(0, len(lists), 2):
+        # #         list1 = lists[i]
+        # #         list2 = lists[i+1] if i+1 < len(lists) else None
+        # #         merged_list.append(mergeTwoLists(list1, list2))
+        # #     lists = merged_list
+        
+        # # return lists[0]
+
+        # # # Approach II: Counting the maximum length of a list and iterating that many number of times to merge all lists together
+        # # dummy = ListNode()
+        # # tail = dummy
+
+        # # while any(lists):
+        # #     # Get node with smallest value
+        # #     holder = ListNode()
+        # #     min_value = float('inf')
+        # #     min_val_idx = len(lists)+1
+        # #     for j in range(len(lists)):
+        # #         if lists[j] and lists[j].val < min_value:
+        # #             min_value = lists[j].val
+        # #             min_val_idx = j
+        # #     if min_val_idx < len(lists):
+        # #         tail.next = lists[min_val_idx]
+        # #         tail = tail.next
+        # #         lists[min_val_idx] = lists[min_val_idx].next
+        # #     else:
+        # #         break
+        # # return dummy.next
+
+        # # Approach III: Using Priority Queue
+        # pq = []
+
+        # for idx, l in enumerate(lists):
+        #     if l:
+        #         heapq.heappush(pq, (l.val, idx))
+
         # dummy = ListNode()
         # tail = dummy
-
-        # while any(lists):
-        #     # Get node with smallest value
-        #     holder = ListNode()
-        #     min_value = float('inf')
-        #     min_val_idx = len(lists)+1
-        #     for j in range(len(lists)):
-        #         if lists[j] and lists[j].val < min_value:
-        #             min_value = lists[j].val
-        #             min_val_idx = j
-        #     if min_val_idx < len(lists):
-        #         tail.next = lists[min_val_idx]
-        #         tail = tail.next
-        #         lists[min_val_idx] = lists[min_val_idx].next
-        #     else:
-        #         break
+        # while pq:
+        #     val, idx = heapq.heappop(pq)
+        #     node = lists[idx]
+        #     tail.next = node
+        #     tail = tail.next
+        #     node = node.next
+        #     if node is not None:
+        #         heapq.heappush(pq, (node.val, idx))
+        #     lists[idx] = node
+        
         # return dummy.next
 
-        # Approach III: Using Priority Queue
+
+
+
+
+
+
+
+
+
+
+
+        if not lists:
+            return []
         pq = []
-
         for idx, l in enumerate(lists):
-            if l:
+            if l is not None:
                 heapq.heappush(pq, (l.val, idx))
-
+        
         dummy = ListNode()
         tail = dummy
         while pq:
@@ -77,8 +112,8 @@ class Solution:
             tail.next = node
             tail = tail.next
             node = node.next
-            if node is not None:
+            if node:
                 heapq.heappush(pq, (node.val, idx))
             lists[idx] = node
-        
         return dummy.next
+
