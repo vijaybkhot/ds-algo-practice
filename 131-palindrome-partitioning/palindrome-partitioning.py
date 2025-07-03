a5e7137fe36a6862050ae630a6040996a2fc9d14
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-03 09:48:27 -0400

    Time: 58 ms (26.88%) | Memory: 34.4 MB (72.49%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,48 +1,63 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
 
-        # def isPalindrome(word):
-        #     # return word == word[::-1]
-        #     left, right = 0, len(word)-1
-        #     while left <= right:
-        #         if word[left] != word[right]:
-        #             return False
-        #         left += 1
-        #         right -= 1
-        #     return True
+        res = []
+
+        def dfs(start, path):
+            if start == len(s):
+                res.append(path[::])
+            
+            for i in range(start, len(s)):
+                if s[start:i+1] == s[start:i+1][::-1]:
+                    path.append(s[start:i+1])
+                    dfs(i+1, path)
+                    path.pop()
+        
+        dfs(0, [])
+        return res
+
+        # # def isPalindrome(word):
+        # #     # return word == word[::-1]
+        # #     left, right = 0, len(word)-1
+        # #     while left <= right:
+        # #         if word[left] != word[right]:
+        # #             return False
+        # #         left += 1
+        # #         right -= 1
+        # #     return True
+        
+        # # res = []
+        # # def backtrack(start, path):
+        # #     if start == len(s):
+        # #         res.append(path[:])
+        # #         return
+            
+        # #     for i in range(start, len(s)):
+        # #         if isPalindrome(s[start:i+1]):
+        # #             path.append(s[start:i+1])
+        # #             backtrack(i+1, path)
+        # #             path.pop()
+
+        # # backtrack(0, [])
+        # # return res
+
+
+        # def is_palindrome(word):
+        #     return word == word[::-1]
         
         # res = []
-        # def backtrack(start, path):
+        # def dfs(start, path):
         #     if start == len(s):
         #         res.append(path[:])
         #         return
             
         #     for i in range(start, len(s)):
-        #         if isPalindrome(s[start:i+1]):
-        #             path.append(s[start:i+1])
-        #             backtrack(i+1, path)
+        #         left = s[start:i+1]
+        #         right = s[i+1:]
+        #         if is_palindrome(left):
+        #             path.append(left)
+        #             dfs(i+1, path)
         #             path.pop()
-
-        # backtrack(0, [])
+        # dfs(0, [])
         # return res
-
-
-        def is_palindrome(word):
-            return word == word[::-1]
-        
-        res = []
-        def dfs(start, path):
-            if start == len(s):
-                res.append(path[:])
-                return
-            
-            for i in range(start, len(s)):
-                left = s[start:i+1]
-                right = s[i+1:]
-                if is_palindrome(left):
-                    path.append(left)
-                    dfs(i+1, path)
-                    path.pop()
-        dfs(0, [])
-        return res
             
