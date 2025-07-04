71b5def378dd8938a0a1ab1f5f6d33de1daadd0e
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-04 05:57:14 -0400

    Time: 251 ms (88.97%) | Memory: 42.8 MB (39.16%) - LeetSync

--- 502-ipo/ipo.py
+++ 502-ipo/ipo.py
@@ -1,44 +1,5 @@
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        # # First attempt - O(NlogN + KlogN) - Inefficient
-        # max_profit = []
-        # for i in range(len(profits)):
-        #     profit = profits[i]
-        #     investment = capital[i]
-        #     heapq.heappush(max_profit, (-profit, investment))
-        
-        # for i in range(k):
-        #     stack = []
-        #     while max_profit and max_profit[0][1] > w:
-        #         stack.append(heapq.heappop(max_profit))
-
-        #     if max_profit:
-        #         net_profit, investment = heapq.heappop(max_profit)
-        #         w += -net_profit
-        #     else:
-        #         break
-
-        #     while stack:
-        #         heapq.heappush(max_profit, stack.pop())
-        # return w
-
-        # # Using sorting 
-        # projects = list(zip(capital, profits))
-        # projects.sort()
-
-        # max_profit_heap = []
-        # i = 0
-        # n = len(projects)
-
-        # for _ in range(k):
-        #     while i < n and projects[i][0] <= w:
-        #         heapq.heappush(max_profit_heap, -projects[i][1])
-        #         i += 1
-        #     if not max_profit_heap:
-        #         break
-        #     w += -heapq.heappop(max_profit_heap)
-
-        # return w 
 
         i = 0
         max_heap = []
@@ -53,8 +14,66 @@ class Solution:
             if not max_heap:
                 break
             w += -heapq.heappop(max_heap)
-        
+
         return w
+            
+            
+
+        # # # First attempt - O(NlogN + KlogN) - Inefficient
+        # # max_profit = []
+        # # for i in range(len(profits)):
+        # #     profit = profits[i]
+        # #     investment = capital[i]
+        # #     heapq.heappush(max_profit, (-profit, investment))
+        
+        # # for i in range(k):
+        # #     stack = []
+        # #     while max_profit and max_profit[0][1] > w:
+        # #         stack.append(heapq.heappop(max_profit))
+
+        # #     if max_profit:
+        # #         net_profit, investment = heapq.heappop(max_profit)
+        # #         w += -net_profit
+        # #     else:
+        # #         break
+
+        # #     while stack:
+        # #         heapq.heappush(max_profit, stack.pop())
+        # # return w
+
+        # # # Using sorting 
+        # # projects = list(zip(capital, profits))
+        # # projects.sort()
+
+        # # max_profit_heap = []
+        # # i = 0
+        # # n = len(projects)
+
+        # # for _ in range(k):
+        # #     while i < n and projects[i][0] <= w:
+        # #         heapq.heappush(max_profit_heap, -projects[i][1])
+        # #         i += 1
+        # #     if not max_profit_heap:
+        # #         break
+        # #     w += -heapq.heappop(max_profit_heap)
+
+        # # return w 
+
+        # i = 0
+        # max_heap = []
+        # projects = list(zip(capital, profits))
+        # n = len(projects)
+        # projects.sort()
+
+        # for _ in range(k):
+        #     while i < n and projects[i][0] <= w:
+        #         heapq.heappush(max_heap, -projects[i][1])
+        #         i += 1
+        #     if not max_heap:
+        #         break
+        #     w += -heapq.heappop(max_heap)
+        
+        # return w
 
 
 
