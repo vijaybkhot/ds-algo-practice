e3426d1e5e4bf4651d3d01204de881e9bac69d73
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-06 14:37:01 -0400

    Time: 226 ms (34.94%) | Memory: 19.1 MB (25.44%) - LeetSync

--- 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.py
+++ 2040-minimum-cost-to-reach-destination-in-time/minimum-cost-to-reach-destination-in-time.py
@@ -1,38 +1,39 @@
 class Solution:
     def minCost(self, maxTime: int, edges: List[List[int]], passingFees: List[int]) -> int:
-        # graph = defaultdict(set)
-        # for u, v, time in edges:
-        #     graph[u].add((v, time))
-        #     graph[v].add((u, time))
+        graph = defaultdict(set)
+        for u, v, time in edges:
+            graph[u].add((v, time))
+            graph[v].add((u, time))
         
-        # n = len(passingFees)
-        # costs = [float('inf')]*n
-        # costs[0] = passingFees[0]
-        # times = [float('inf')]*n
-        # times[0] = 0
+        n = len(passingFees)
+        
+        # min_time[node] = smallest time at which we visited node
+        min_time = [float('inf')] * n
     
-        # heap = []
-        # heapq.heappush(heap, (0, passingFees[0], 0)) # (time, node)
-        # min_cost = float('inf')
+        heap = []
+        heapq.heappush(heap, (passingFees[0], 0, 0)) # (time, node)
 
-        # while heap:
-        #     curr_time, curr_cost, curr_node = heapq.heappop(heap)
+        while heap:
+            curr_cost, curr_time, curr_node = heapq.heappop(heap)
 
-        #     if curr_time > maxTime:
-        #         continue
+            if curr_time > maxTime:
+                continue
             
-        #     if curr_node == n-1:
-        #         min_cost = min(min_cost, curr_cost)
+            if curr_node == n-1:
+                return curr_cost
             
-        #     for nxt_node, time in graph[curr_node]:
-        #         nxt_time = curr_time + time
-        #         nxt_cost = curr_cost + passingFees[nxt_node]
-        #         if nxt_time <= maxTime and nxt_cost < costs[nxt_node]:
-        #             heapq.heappush(heap, (nxt_time, nxt_cost, nxt_node))
-        #             times[nxt_node] = nxt_time
-        #             costs[nxt_node] = nxt_cost
+            if curr_time >= min_time[curr_node]:
+                continue
+            min_time[curr_node] = curr_time
+            
+            
+            for nxt_node, time in graph[curr_node]:
+                nxt_time = curr_time + time
+                nxt_cost = curr_cost + passingFees[nxt_node]
+                if nxt_time <= maxTime:
+                    heapq.heappush(heap, (nxt_cost, nxt_time, nxt_node))
         
-        # return min_cost if min_cost != float('inf') else -1
+        return -1
 
 
         graph = defaultdict(list)
