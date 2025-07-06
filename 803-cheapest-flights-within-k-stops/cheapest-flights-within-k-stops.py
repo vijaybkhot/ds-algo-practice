34ed5c2af4915b36f8b2ad39b9b632a9588b8557
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-06 09:45:34 -0400

    Time: 31 ms (43.21%) | Memory: 19.6 MB (16.77%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -21,22 +21,46 @@ class Solution:
         
         # return -1
 
-        # Using Bellman-Ford algorithm
-        prices = [float('inf')] * n
-        prices[src] = 0
+        # # Using Bellman-Ford algorithm
+        # prices = [float('inf')] * n
+        # prices[src] = 0
 
-        for i in range(k+1):
-            temp = prices[:]
-            for u, v, w in flights:
-                if prices[u] == float('inf'):
-                    continue
-                if temp[v] > (prices[u] + w):
-                    temp[v] = prices[u] + w
+        # for i in range(k+1):
+        #     temp = prices[:]
+        #     for u, v, w in flights:
+        #         if prices[u] == float('inf'):
+        #             continue
+        #         if temp[v] > (prices[u] + w):
+        #             temp[v] = prices[u] + w
         
-            prices = temp
+        #     prices = temp
         
-        return prices[dst] if prices[dst] != float('inf') else -1
+        # return prices[dst] if prices[dst] != float('inf') else -1
         
 
 
+        graph = defaultdict(set)
+        for u, v, w in flights:
+            graph[u].add((v, w))
+        
+        heap = []
+        heapq.heappush(heap, (0, src, 0)) # arrival_price, source, stops
+        prices = dict() #(node, stops) : price
+       
 
+        while heap:
+            price, node, stops = heapq.heappop(heap)
+        
+            if node == dst:
+                return price
+            if stops > k:
+                continue
+
+            for nei, cost in graph[node]:
+                next_state = (nei, stops+1)
+                next_price = price+cost
+                if next_state not in prices or next_price < prices[next_state]:
+                    heapq.heappush(heap, (price + cost, nei, stops + 1))
+                    prices[next_state] = next_price
+            
+        return -1
