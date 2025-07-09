39a11d07cb41c0dc4d315cabe0d9011d1b98c479
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-09 10:29:29 -0400

    Time: 151 ms (58.78%) | Memory: 53.5 MB (9.86%) - LeetSync

---
+++ 354-russian-doll-envelopes/russian-doll-envelopes.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
+
+        
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        widths = [w for w, h in envelopes]
+        heights = [h for w, h in envelopes]
+        n = len(envelopes)
+
+        lis = []
+        for h in heights:
+            idx = bisect_left(lis, h)
+            if idx == len(lis):
+                lis.append(h)
+            else:
+                lis[idx] = h
+        return len(lis)
+
+        # def next_idx(i):
+        #     for j in range(i + 1, n):
+        #         if widths[j] > widths[i] and heights[j] > heights[i]:
+        #             return j
+        #     return n
+
+        # @lru_cache(None)
+        # def dfs(i):
+        #     if i >= n:
+        #         return 0
+        #     next_valid_idx = next_idx(i)
+        #     take = 1 + dfs(next_valid_idx)
+        #     skip = dfs(i+1)
+        #     return max(take, skip)
+                
+        # return dfs(0)
