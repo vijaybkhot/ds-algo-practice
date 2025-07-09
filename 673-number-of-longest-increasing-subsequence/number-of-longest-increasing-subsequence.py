105bf5ae48e77de09f20f3d5cfc2239dd498c5bd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-09 10:18:27 -0400

    Time: 455 ms (91.16%) | Memory: 18 MB (87.88%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.py
@@ -22,23 +22,65 @@ class Solution:
         
         # return res
 
-        # Using DP based solution
-        n = len(nums)
-        if n == 0:
-            return 0
-        length = [1] * n
-        count = [1] * n
+        # # Using DP based solution
+        # n = len(nums)
+        # if n == 0:
+        #     return 0
+        # length = [1] * n
+        # count = [1] * n
         
-        for i in range(1, len(nums)):
-            for j in range(i):
-                if nums[j] < nums[i]:
-                    if length[j] + 1 > length[i]:
-                        length[i] = length[j] + 1
+        # for i in range(1, len(nums)):
+        #     for j in range(i):
+        #         if nums[j] < nums[i]:
+        #             if length[j] + 1 > length[i]:
+        #                 length[i] = length[j] + 1
+        #                 count[i] = count[j]
+        #             elif length[j] + 1 == length[i]:
+        #                 count[i] += count[j]
+        
+        # LIS = max(length)
+        
+        # return sum(c for l, c in zip(length, count) if l == LIS)
+        # # Inefficient DFS appraoch
+        # n = len(nums)
+        # @lru_cache(None)
+        # def length(i, prev_index):
+        #     if i == n:
+        #         return 0
+        #     taken = 0
+        #     if prev_index == -1 or nums[i] > nums[prev_index]:
+        #         taken = 1 + length(i+1, i)
+        #     not_taken = length(i+1, prev_index)
+        #     return max(taken, not_taken)
+
+        # LIS_len = length(0, -1)
+
+        # @lru_cache(None)
+        # def count(i, prev_index, curr_len):
+        #     if i == n:
+        #         return 1 if curr_len == LIS_len else 0
+        #     total = 0
+        #     if prev_index == -1 or nums[i] > nums[prev_index]:
+        #         total += count(i+1, i, curr_len + 1)
+        #     total += count(i+1, prev_index, curr_len)
+        #     return total
+
+        # return count(0, -1, 0)
+
+        n = len(nums)
+        length = [1]*n
+        count = [1]*n
+        for i in range(n-2, -1, -1):
+            for j in range(i+1, n):
+                if nums[i] < nums[j]:
+                    if length[j]+1 > length[i]:
+                        length[i] = length[j]+1
                         count[i] = count[j]
-                    elif length[j] + 1 == length[i]:
+                    elif length[j]+1 == length[i]:
                         count[i] += count[j]
         
         LIS = max(length)
-        
-        return sum(c for l, c in zip(length, count) if l == LIS)
-        
+
+        return sum(c for c, l in zip(count, length) if l == LIS)
+
+            
