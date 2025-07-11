e71c32c80021a700354213f7a9b87ff2fa8fff1f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-11 08:45:41 -0400

    Time: 95 ms (56.64%) | Memory: 18.5 MB (45.92%) - LeetSync

---
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -0,0 +1,84 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word: str)-> None:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.endOfWord = True
+    
+    def search(self, word: str)-> bool:
+        curr = self.root
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return curr.endOfWord
+    
+    def startsWith(self, prefix:str)-> bool:
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
+
+class Solution:
+    def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        # trie = TrieNode()
+        # extra_chars = 0
+
+        # def insert(word: str, node: TrieNode)-> None:
+        #     curr = node
+        #     for c in word:
+        #         if c not in curr.children:
+        #             curr.children[c] = TrieNode()
+        #         curr = curr.children[c]
+        #     curr.endOfWord = True
+        
+        # for word in dictionary:
+        #     insert(word, trie)
+        
+        # curr = trie
+        # for char in s:
+        #     if char not in curr.children:
+        #         curr = trie
+        #         extra_chars += 1
+        #     else:
+        #         curr = curr.children[char]
+        #         if curr.endOfWord:
+        #             curr = trie
+        
+        # return extra_chars
+        dict_set = set(dictionary)
+        dp = {}
+        def dfs(i):
+            if i == len(s):
+                return 0
+            
+            if i in dp:
+                return dp[i]
+            
+            # skip
+            res = 1 + dfs(i+1)
+
+            for j in range(i, len(s)):
+                if s[i:j+1] in dict_set:
+                    res = min(res, dfs(j+1))
+
+            dp[i] = res
+            return res
+        
+        return dfs(0)
+
+            
+
+        
