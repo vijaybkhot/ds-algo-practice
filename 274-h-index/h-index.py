0f7875f4256b07bde2c26944f0f4d7893ddddf28
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-13 11:51:50 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (88.75%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -1,17 +1,36 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         
-        def citations_greater_than(num):
-            return sum(1 for citation in citations if citation >= num)
+        # # Using Binary Search
+        # def citations_greater_than(num):
+        #     return sum(1 for citation in citations if citation >= num)
         
-        low, high = 0, len(citations)
-        res = low
-        while low <= high:
-            mid = (low+high)//2
-            if mid <= citations_greater_than(mid):
-                res = mid
-                low = mid + 1
-            else:
-                high = mid - 1
+        # low, high = 0, len(citations)
+        # res = low
+        # while low <= high:
+        #     mid = (low+high)//2
+        #     if mid <= citations_greater_than(mid):
+        #         res = mid
+        #         low = mid + 1
+        #     else:
+        #         high = mid - 1
+        
+        # return res
+
+        # Using count sort
+        count = [0]*(max(citations)+1)
+
+        for citation in citations:
+            count[citation] += 1
+        
+        remaining = len(citations)
+        res = 0
+        for i in range(len(count)):
+            if remaining >= i:
+                res = i
+            if count[i] > 0:
+                remaining -= count[i]
         
         return res
+
+            
