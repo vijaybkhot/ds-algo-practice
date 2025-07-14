1b34489a1de94484e9cee4dd1eab3d78c4465958
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-14 17:48:54 -0400

    Time: 43 ms (31.57%) | Memory: 17.6 MB (81.70%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -2,9 +2,14 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         
         res = 0
+        # # Solution - I
+        # for i in range(32):
+        #     bit = (n >> i) & 1  # Shift n to the right by i and  get the i-th bit from left .
+        #     res = res | (bit << (31-i)) # Take the current bit and place it at position (31 - i) in the result integer res.
 
+        # Solution - II - More readable
         for i in range(32):
-            bit = (n >> i) & 1  # Get the i-th bit . Shift n to the right by i
-            res = res | (bit << (31-i))
+            res = (res << 1) | (n & 1)
+            n = n >> 1
         
         return res
