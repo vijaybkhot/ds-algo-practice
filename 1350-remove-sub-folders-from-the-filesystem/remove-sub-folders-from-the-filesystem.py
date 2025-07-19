90a5c5a1b972d95b38317986a48978ccc476ca68
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-19 06:34:16 -0400

    Time: 383 ms (5.08%) | Memory: 30.9 MB (48.94%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,57 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def insert(self, word):
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.endOfWord = True
+    
+    def search(self, word):
+        curr = self.root
+        for char in word:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return curr.endOfWord
+    
+    def isPrefix(self, prefix):
+        curr = self.root
+        for char in prefix:
+            if char not in curr.children:
+                return False
+            curr = curr.children[char]
+        return True
+
+
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        trie = Trie()
+        res = []
+
+        # folder.sort(key = lambda x: len(x))
+        folder.sort()
+
+        for subfolder in folder:
+            i = len(subfolder)-1
+            while i >= 0:
+                while i >= 0 and subfolder[i] != '/':
+                    i -= 1
+                parent_folder = subfolder[:i]
+                if trie.search(parent_folder):
+                    break
+                i -= 1
+            if i < 0:    
+                trie.insert(subfolder)
+                res.append(subfolder)
+            
+        return res
+            
