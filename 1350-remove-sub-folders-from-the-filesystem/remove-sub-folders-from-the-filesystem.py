7fda65a64252bf58342cedbd8a24765d18e12a86
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-19 06:42:25 -0400

    Time: 55 ms (61.78%) | Memory: 30.8 MB (53.03%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -14,6 +14,18 @@ class Trie:
             curr = curr.children[char]
         curr.endOfWord = True
     
+    def insertIfNoPrefix(self, parts):
+        curr = self.root
+        for part in parts:
+            if curr.endOfWord:
+                return False
+            if part not in curr.children:
+                curr.children[part] = TrieNode()
+            
+            curr = curr.children[part]
+        curr.endOfWord = True
+        return True
+    
     def search(self, word):
         curr = self.root
         for char in word:
@@ -36,22 +48,29 @@ class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
         trie = Trie()
         res = []
-
-        # folder.sort(key = lambda x: len(x))
         folder.sort()
 
-        for subfolder in folder:
-            i = len(subfolder)-1
-            while i >= 0:
-                while i >= 0 and subfolder[i] != '/':
-                    i -= 1
-                parent_folder = subfolder[:i]
-                if trie.search(parent_folder):
-                    break
-                i -= 1
-            if i < 0:    
-                trie.insert(subfolder)
-                res.append(subfolder)
+        # for subfolder in folder:
+        #     i = len(subfolder)-1
+        #     while i >= 0:
+        #         while i >= 0 and subfolder[i] != '/':
+        #             i -= 1
+        #         parent_folder = subfolder[:i]
+        #         if trie.search(parent_folder):
+        #             break
+        #         i -= 1
+        #     if i < 0:    
+        #         trie.insert(subfolder)
+        #         res.append(subfolder)
             
+        # return res
+
+        # Optimized approach
+        for subfolder in folder:
+            parts = subfolder.split('/')[1:]
+            if trie.insertIfNoPrefix(parts):
+                res.append(subfolder)
+        
         return res
+
             
