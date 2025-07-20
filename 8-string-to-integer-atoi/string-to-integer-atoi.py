d341990877b02c1ebe02aa93da2d75be4c381420
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-20 18:39:44 -0400

    Time: 3 ms (39.17%) | Memory: 17.7 MB (77.33%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,31 +1,23 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        MIN = -1*(2**31)
-        MAX = (2**31)-1
+        MIN = -2**31
+        MAX = 2**31 - 1
 
         s = s.strip()
-        i = 0
-        if i < len(s)  and s[i] not in "+-0123456789":
+        if not s:
             return 0
-        sign = "+"
-        if i < len(s) and s[i] in "+-":
-            sign = "-" if s[i]=="-" else "+"
+
+        i = 0
+        sign = 1
+
+        if s[i] in '+-':
+            sign = -1 if s[i] == '-' else 1
             i += 1
-        
+
         res = 0
-        exceeded = False
-        while i < len(s) and s[i] in "0123456789":
-            res = res*10+int(s[i])
-            if res > MAX:
-                res = MAX
-                exceeded = True
-                break
+        while i < len(s) and s[i].isdigit():
+            res = res * 10 + int(s[i])
             i += 1
-        
-        if sign == '-':
-            res = (res*-1)
-        
-        if res == -MAX and exceeded:
-            res = MIN
-        
-        return res
+
+        res *= sign
+        return max(MIN, min(MAX, res))
