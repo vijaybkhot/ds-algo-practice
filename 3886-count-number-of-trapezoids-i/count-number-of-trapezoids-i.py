cb6028d26d25b646eae10a7a0f790b6f3753f530
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-20 07:13:33 -0400

    Time: 210 ms (0.00%) | Memory: 63.9 MB (0.00%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.py
@@ -10,10 +10,14 @@ class Solution:
         
         y_count_list = [comb(count, 2) for _, count in y_map.items() if count > 1]
         res = 0
+        # Brute force ineffieient approach
         # for i in range(len(y_count_list)):
         #     for j in range(i+1, len(y_count_list)):
         #         res += y_count_list[i]*y_count_list[j]
         
+        # The double sum: sum_{i=0}^{n-1} sum_{j=i+1}^{n-1} (a[i] * a[j])
+        # is equal to: (sum(a)^2 - sum(a^2)) // 2
+        # i.e., total_pairs = (total_sum^2 - sum_of_squares) // 2
         total = sum(y_count_list)
         total_sq = sum(x * x for x in y_count_list)
         
