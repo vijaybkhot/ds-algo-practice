971a7b44293be53c9a9f63e3e7ead778d6edfc00
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-20 10:10:27 -0400

    Time: 2086 ms (5.85%) | Memory: 542.4 MB (5.23%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,25 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        # # Dynamic programming - memoization approach
-        # dp = {}
-        # def dfs(i, hold, count):
-        #     if (i, hold, count) in dp:
-        #         return dp[(i, hold, count)]
-        #     if i == n:
-        #         return 0
+        # Dynamic programming - memoization approach
+        dp = {}
+        def dfs(i, hold, count):
+            if (i, hold, count) in dp:
+                return dp[(i, hold, count)]
+            if i == n:
+                return 0
             
-        #     profit = 0
-        #     if hold:
-        #         profit = max(prices[i] + dfs(i+1, False, count), dfs(i+1, hold, count))
-        #     else:
-        #         if count < 2:
-        #             profit = max(-prices[i]+dfs(i+1, True, count+1), dfs(i+1, hold, count))
+            profit = 0
+            skip = dfs(i+1, hold, count)
+            if hold:
+                profit = max(prices[i] + dfs(i+1, False, count), skip)
+            else:
+                if count < 2:
+                    profit = max(-prices[i]+dfs(i+1, True, count+1), skip)
             
-        #     dp[(i, hold, count)] = profit
-        #     return profit
+            dp[(i, hold, count)] = profit
+            return profit
         
-        # return dfs(0, False, 0)
+        return dfs(0, False, 0)
 
         # Divide and conquer - at any index, calculate max profit in a single transaction for left side and also max profit in a single transaction for right side and then add the two to get max possible profit at that point
         left_profit = [0]*n
