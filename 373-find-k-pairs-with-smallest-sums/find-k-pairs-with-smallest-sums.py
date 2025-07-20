d654aa30d8fc762430975e49f6df770accb6452e
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-19 20:16:16 -0400

    Time: 87 ms (75.24%) | Memory: 35.1 MB (26.65%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.py
@@ -3,19 +3,38 @@ class Solution:
         # Brute Force approach
         res = []
         pairs = []
-        for i in range(min(len(nums1), k)):
-            heapq.heappush(pairs, (nums1[i]+nums2[0], i, 0))
-    
-        
-        
-        while pairs and len(res) < k:
+
+        # # Optimized approach:
+        # for i in range(min(len(nums1), k)):
+        #     heapq.heappush(pairs, (nums1[i]+nums2[0], i, 0))
+
+        # while pairs and len(res) < k:
+        #     curr_sum, i, j = heapq.heappop(pairs)
+        #     res.append([nums1[i], nums2[j]])
+
+        #     if j+1 < len(nums2):
+        #         next_sum = nums1[i]+nums2[j+1]
+        #         heapq.heappush(pairs, (next_sum, i, j+1))
+
+        # return res 
+
+        # Appraoch Similar to Djikstras
+        visited = set()
+        heapq.heappush(pairs, (nums1[0]+nums2[0], 0, 0))
+        visited.add((0, 0))
+
+        while pairs and len(res)<k:
             curr_sum, i, j = heapq.heappop(pairs)
             res.append([nums1[i], nums2[j]])
+            if j+1 < len(nums2) and (i, j+1) not in visited:
+                visited.add((i, j+1))
+                heapq.heappush(pairs, (nums1[i]+nums2[j+1], i, j+1))
+            
+            if i+1 < len(nums1) and (i+1, j) not in visited:
+                visited.add((i+1, j))
+                heapq.heappush(pairs, (nums1[i+1]+nums2[j], i+1, j))
+        
+        return res
 
-            if j+1 < len(nums2):
-                next_sum = nums1[i]+nums2[j+1]
-                heapq.heappush(pairs, (next_sum, i, j+1))
-
-        return res 
 
         
