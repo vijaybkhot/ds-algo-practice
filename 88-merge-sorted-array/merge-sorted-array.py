e58347c29603a9ff5d32db2ed08deffd12062a98
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-22 09:59:39 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (43.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,49 +1,21 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        # # Solution using an extra array: O(n) time and space complexity
-        # nums3 = nums1[:]
-        # i, j, k = 0, 0, 0
+        l, r, k = m-1, n-1, len(nums1)-1
 
-        # while i < m and j < n:
-        #     if nums3[i] < nums2[j]:
-        #         nums1[k] = nums3[i]
-        #         k += 1
-        #         i += 1
-        #     else:
-        #         nums1[k] = nums2[j]
-        #         k += 1
-        #         j += 1
-        # while i < m:
-        #     nums1[k] = nums3[i]
-        #     k += 1
-        #     i += 1
-        # while j < n:
-        #     nums1[k] = nums2[j]
-        #     k += 1
-        #     j += 1
+        while l >= 0 and r >=0:
 
-        # In-place merging without an extra array - O(n) time and O(1) space complexity
-
-        last = m + n -1
-
-        while m > 0 and n > 0:
-            if nums1[m-1] > nums2[n-1]:
-                nums1[last] = nums1[m-1]
-                m -= 1
+            if nums1[l] > nums2[r]:
+                nums1[k] = nums1[l]
+                l -= 1
             else:
-                nums1[last] = nums2[n-1]
-                n -= 1
-            last -= 1
+                nums1[k] = nums2[r]
+                r -= 1
+            k -= 1
         
-        while n > 0:
-            nums1[last] = nums2[n-1]
-            n -= 1
-            last -= 1
-
+        while r >= 0:
+            nums1[k] = nums2[r]
+            r -= 1
+            k -= 1
