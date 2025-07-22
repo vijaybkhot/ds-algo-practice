8825a03bc2a35c2ee9fc2505e0a35b8b2d7e5055
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-22 09:47:55 -0400

    Time: 984 ms (5.02%) | Memory: 289.7 MB (8.33%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        profit = 0
-        left = 0
-        for i in range(1, len(prices)):
-            if prices[i] > prices[left]:
-                profit = max(profit , prices[i]-prices[left])
-            else:
-                left = i
+        # profit = 0
+        # left = 0
+        # for i in range(1, len(prices)):
+        #     if prices[i] > prices[left]:
+        #         profit = max(profit , prices[i]-prices[left])
+        #     else:
+        #         left = i
 
         
-        return profit
+        # return profit
+        dp = {}
+        def dfs(i, hold):
+            if (i, hold) in dp:
+                return dp[(i, hold)]
+            if i == len(prices):
+                return 0
+            
+            if not hold:
+                dp[(i, hold)] = max((dfs(i+1, True)-prices[i]), dfs(i+1, False))
+            else:
+                dp[(i, hold)] = max(prices[i], dfs(i+1, True))
+            
+            return dp[(i, hold)]
+        
+        return dfs(0, False)
+
