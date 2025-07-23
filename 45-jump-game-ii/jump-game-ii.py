c5a1c7e7205566366216597a4952ac9a98bc2beb
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-23 13:47:33 -0400

    Time: 7 ms (79.06%) | Memory: 18.6 MB (75.34%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -10,22 +10,33 @@ class Solution:
         
         # return dp[0]
 
-        dp = {}
-        def dfs(i):
-            if i == n-1:
-                return 0
-            if i in dp:
-                return dp[i]
+        # dp = {}
+        # def dfs(i):
+        #     if i == n-1:
+        #         return 0
+        #     if i in dp:
+        #         return dp[i]
 
-            min_jump = float('inf')
-            max_jump = min(n-1, i+nums[i])
-            for j in range(i+1, max_jump+1):
-                min_jump = min(min_jump, 1+dfs(j))
+        #     min_jump = float('inf')
+        #     max_jump = min(n-1, i+nums[i])
+        #     for j in range(i+1, max_jump+1):
+        #         min_jump = min(min_jump, 1+dfs(j))
             
-            dp[i] = min_jump
-            return min_jump
+        #     dp[i] = min_jump
+        #     return min_jump
         
-        return dfs(0)
+        # return dfs(0)
+
+        jumps = 0
+        farthest = 0
+        curr_end = 0
+        for i in range(n-1):
+            farthest = max(farthest, i+nums[i])
+            if i == curr_end:
+                jumps += 1
+                curr_end = farthest
+        
+        return jumps
 
            
 
