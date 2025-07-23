5dfe3777d3ed839613b3e0f4c822454d178cd351
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-23 12:06:34 -0400

    Time: 0 ms (100.00%) | Memory: 18.6 MB (98.44%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,22 +1,34 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        dp = {}
-        def dfs(i, hold):
-            if i == n:
-                return 0
-            if (i, hold) in dp:
-                return dp[(i, hold)]
-            profit = 0
-            skip = dfs(i+1, hold)
-            if hold:
-                sell = prices[i] + dfs(i+1, not hold)
-                profit = max(sell, skip)
-            else:
-                buy = -prices[i] + dfs(i+1, not hold)
-                profit = max(buy, skip)
+        # dp = {}
+        # def dfs(i, hold):
+        #     if i == n:
+        #         return 0
+        #     if (i, hold) in dp:
+        #         return dp[(i, hold)]
+        #     profit = 0
+        #     skip = dfs(i+1, hold)
+        #     if hold:
+        #         sell = prices[i] + dfs(i+1, not hold)
+        #         profit = max(sell, skip)
+        #     else:
+        #         buy = -prices[i] + dfs(i+1, not hold)
+        #         profit = max(buy, skip)
             
-            dp[(i, hold)] = profit
-            return profit
+        #     dp[(i, hold)] = profit
+        #     return profit
         
-        return dfs(0, False)
+        # return dfs(0, False)
+        # Greedy solution
+
+        min_stock = prices[0]
+        profit = 0
+        for i in range(n):
+            curr_price = prices[i]
+            curr_profit = curr_price - min_stock
+            if curr_profit > 0:
+                profit += curr_profit
+            min_stock = curr_price
+        
+        return profit
