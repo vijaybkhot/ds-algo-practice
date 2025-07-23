d1b3e42cdf77398a61b30aa7a695819a589d8707
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-23 08:18:34 -0400

    Time: 4 ms (12.75%) | Memory: 18 MB (46.00%) - LeetSync

---
+++ 443-string-compression/string-compression.py
@@ -0,0 +1,21 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        insert = 0
+        i = 0
+        while i < len(chars):
+            curr_char = chars[i]
+            i += 1
+            repeat = 0
+            while i < len(chars) and chars[i] == curr_char:
+                repeat += 1
+                i += 1
+            
+            chars[insert] = curr_char
+            insert += 1
+            if repeat:
+                repeat = str(repeat+1)
+                for char in repeat:
+                    chars[insert] = char
+                    insert += 1
+        
+        return insert
