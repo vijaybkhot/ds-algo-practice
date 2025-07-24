2bba94cc0632d186c5088fd3a4bd0bafb6eea9ff
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-24 07:47:00 -0400

    Time: 28 ms (23.56%) | Memory: 19.2 MB (68.64%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -1,56 +1,27 @@
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        # # Solution using extra space - O(n) time and O(n) space complexity
-        # n = len(height)
-        # left_max_arr = [0] * n
-        # right_max_arr = [0] * n
-        # min_of_l_and_r = [0] * n
-        # left_max_arr[0], right_max_arr[-1] = 0, 0
-        # # Calculate left max for each position
-        # for i in range(1, n):
-        #     left_max_arr[i] = max(height[i-1], left_max_arr[i-1])
-        # # Calculate right max for each position
-        # for i in range(n-2, -1, -1):
-        #     right_max_arr[i] = max(height[i+1], right_max_arr[i+1])
-        # # Caclulate the min of left_max and right_max at each position
-        # for i in range(n):
-        #     min_of_l_and_r[i] = min(left_max_arr[i], right_max_arr[i])
-        # rain_water = 0
-        # # Count Rain water trapped at each position by subtracting input from min_of_l_and_r
-        # for i in range(n):
-        #     curr_water = min_of_l_and_r[i] - height[i]
-        #     if curr_water > 0:
-        #         rain_water += curr_water
-        # return rain_water
-
-        # Solution using constant extra space - O(n) time and O(1) space complexity
-        if not height or len(height) == 1:
-            return 0
-
+class Solution:
+    def trap(self, height: List[int]) -> int:
         n = len(height)
-        left, right = 0, n-1
-        max_l, max_r = height[left], height[right]
-        total_rain_trapped = 0
-        while left < right:
-            if max_l < max_r:
-                left += 1
-                max_l = max(max_l, height[left])
-                curr_rain_trapped = max_l - height[left]
-                if curr_rain_trapped > 0:
-                    total_rain_trapped += curr_rain_trapped
+        l_max = [0]*n
+        r_max = [0]*n
+        for i in range(n):
+            l_max[i] = max(l_max[i-1] if i-1 >= 0 else 0, height[i])
+        
+        for i in range(n-1, -1, -1):
+            r_max[i] = max(r_max[i+1] if i+1 < n else 0, height[i])
+        
+        rain_water = 0
+
+        for i in range(1, n-1):
+            left_max = l_max[i-1]
+            right_max = r_max[i+1]
+            curr_height = height[i]
+            if left_max > curr_height and curr_height < right_max:
+                rain_water += min(left_max, right_max) - curr_height
+                
+        
+        return rain_water
 
             
-            else:
-                right -= 1
-                max_r = max(max_r, height[right])
-                curr_rain_trapped = max_r - height[right]
-                if curr_rain_trapped > 0:
-                    total_rain_trapped += curr_rain_trapped
-        return total_rain_trapped
+
             
         
-       
