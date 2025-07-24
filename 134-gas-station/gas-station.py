d74b94ec07b5c4403ecbca1bb0a2c9a7e0c80ed0
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-24 05:57:51 -0400

    Time: 9 ms (99.71%) | Memory: 23.4 MB (19.25%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -2,32 +2,6 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
 
-        # # Brute force approach
-        # isPossible = False
-        # res = -1
-        # if sum(cost) > sum(gas):
-        #     return -1
-        # for i in range(n):
-        #     if gas[i] < cost[i] or gas[i]==0:
-        #         continue
-
-        #     if not isPossible:
-        #         curr_gas = 0
-        #         for j in range(i, i+n):
-        #             idx = j % n
-        #             curr_gas += gas[idx]
-                    
-        #             if curr_gas < cost[idx]:
-        #                 break
-        #             curr_gas = curr_gas - cost[idx]
-        #             if idx == (i-1)%n and curr_gas >= 0:
-        #                 isPossible = True
-        #                 res = i
-        #                 break
-        #     else:
-        #         break
-        
-        # return res if isPossible else -1
         if sum(gas) < sum(cost):
             return -1
         start = 0
