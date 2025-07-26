54e6b609f48081b783ba98724047736fcddb914d
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-26 08:08:28 -0400

    Time: 106 ms (41.73%) | Memory: 28.4 MB (67.39%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,38 +1,13 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # Greedy two pointer approach
         l, r = 0, len(height)-1
         res = 0
-
         while l < r:
-            res = max(res, (min(height[l], height[r]) * (r-l)))
+            curr_water = min(height[l], height[r]) * (r-l)
+            res = max(res, curr_water)
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
+                
         return res
-
-        # # Brute Force solution:
-        # for i in range(len(height)):
-        #     for j in range(i+1, len(height)):
-        #         res = max(res, min(height[i], height[j])*(j-i))
-        # return res
-
-        # Dynamic programming:
-        # dp = {}
-        # def dfs(i, j):
-        #     if (i, j) in dp:
-        #         return dp[(i, j)]
-        #     if i >= j or i >= len(height) or j < 0:
-        #         return 0
-            
-
-        #     dp[(i, j)] = max((min(height[i], height[j])*(j-i)), dfs(i+1, j), dfs(i, j-1))
-
-        #     return dp[(i, j)]
-        
-        # return dfs(0, len(height)-1)
-
-        # 
-    
-        
