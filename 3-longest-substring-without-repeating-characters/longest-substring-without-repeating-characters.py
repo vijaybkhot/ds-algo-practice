6e7bc6929f3d938b76a242367b7cf3f185971e32
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-26 14:48:32 -0400

    Time: 11 ms (95.28%) | Memory: 18 MB (33.90%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,56 +1,13 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if s == "":
-            return 0
+        char_set = set()
         res = 0
-        left, right = 0, 1
-        curr_set = set()
-        curr_set.add(s[left])
-        while left < len(s) and right < len(s):
-            while right < len(s) and s[right] not in curr_set:
-                curr_set.add(s[right])
-                right += 1
-                res = max(res, len(curr_set))
-            while right < len(s) and s[right] in curr_set:
-                curr_set.remove(s[left])
+        left = 0
+        for right in range(len(s)):
+            while left <= right and s[right] in char_set:
+                char_set.remove(s[left])
                 left += 1
+            char_set.add(s[right])
+            res = max(res, len(char_set))
         
-        return max(res, 1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # res = 0
-        # i, j = 0, 0
-        # curr_set = set()
-        
-        # index_map = defaultdict(int)
-
-        # for idx, char in enumerate(s):
-        #     s[char] += 1
-        
-        # l, r = 0, len(s)-1
-        
-        # while l < r:
-        #     if index_map[s[l]] > 1:
-        #         l += 1
-        #     if index_map[s[r]] > 1:
-        #         r -= 1
-            
+        return res
