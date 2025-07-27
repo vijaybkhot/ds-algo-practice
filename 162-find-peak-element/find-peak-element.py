50e0dc7d4b4e85c84ee9b6f887c80290aed394a3
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-27 07:22:21 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (88.59%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,22 +1,15 @@
-class Solution(object):
-    def findPeakElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        left, right = 0, len(nums)-1
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
 
-        while left <= right:
-            mid = (left + right) // 2
-            left_mid_num = float('-infinity') if mid == 0 else nums[mid-1]
-            right_mid_num = float('-infinity') if mid == len(nums)-1 else nums[mid+1]
+        i = 0
 
-            if nums[mid] > left_mid_num and nums[mid] > right_mid_num:
-                return mid
-            elif right_mid_num > nums[mid]:
-                left = mid + 1
-            else:
-                right = mid - 1
-        
+        while i < len(nums):
+            left_nei = nums[i-1] if i-1 >= 0 else float('-inf')
+            right_nei = nums[i+1] if i+1 < len(nums) else float('-inf')
+
+            if left_nei < nums[i] > right_nei:
+                return i
+            
+            i += 1
         
         
