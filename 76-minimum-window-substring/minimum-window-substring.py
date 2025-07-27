c3c68ea26bf654a300c3766de85f61720da5b2ca
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-27 08:23:30 -0400

    Time: 853 ms (5.01%) | Memory: 18.2 MB (54.47%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,27 +2,25 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
-        t_set = set(t)
-        
         
         t_map = Counter(t)
-
+        curr_map = Counter()
+        required_chars = len(t_map)
         res = ""
         len_res = float('inf')
         left = 0
-        curr_map = Counter()
+        
         for right in range(len(s)):
             curr_char = s[right]
-            if curr_char in t_set:
-                curr_map[curr_char] += 1
+            curr_map[curr_char] += 1
+            
             while all(curr_map[c] >= t_map[c] for c in t_map) and left <= right:
                 if (right - left + 1) < len_res:
                     res = s[left:right+1]
                     len_res = right - left + 1
-
+                
                 left_char = s[left]
-                if left_char in curr_map:
-                    curr_map[left_char] -= 1
+                curr_map[left_char] -= 1
                 left += 1
         
         return res
