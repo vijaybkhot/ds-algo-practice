24e7e0d078ad992216d2fa8e6ed85ec141cce7f9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-27 11:59:08 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.59%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,30 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
 
-        # i = 0
-        # while i < len(nums):
-        #     left_nei = nums[i-1] if i-1 >= 0 else float('-inf')
-        #     right_nei = nums[i+1] if i+1 < len(nums) else float('-inf')
+        l, r = 0, len(nums)-1
 
-        #     if left_nei < nums[i] > right_nei:
-        #         return i
-            
-        #     i += 1
-        # l  mid    r
-        # [1, 2, 3, 1]
-        left, right = 0, len(nums)-1
-        while left <= right:
-            mid = (left+right) // 2
-            num = nums[mid]
-            left_num = nums[mid-1] if mid > 0 else float('-inf')
-            right_num = nums[mid+1] if mid < len(nums)-1 else float('-inf')
-            
-            # 1 - 2 - 3
-            if left_num < num > right_num:
+        while l <= r:
+            mid = (l+r)//2
+            left_mid = nums[mid-1] if mid-1 >= 0 else float('-inf')
+            right_mid = nums[mid+1] if mid+1 < len(nums) else float('-inf')
+
+            if left_mid < nums[mid] > right_mid:
                 return mid
-
-            elif left_num < num < right_num:
-                left = mid + 1
+            elif left_mid < nums[mid] < right_mid:
+                l = mid+1
             else:
-                right = mid - 1
+                r = mid-1
         
