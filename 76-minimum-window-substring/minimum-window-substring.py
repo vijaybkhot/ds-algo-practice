1f1d6ce71db673a1ead324ced960c783e3598656
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-27 08:43:18 -0400

    Time: 87 ms (46.82%) | Memory: 18.3 MB (54.47%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -2,25 +2,35 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(t) > len(s):
             return ""
+        if not s or not t:
+            return ""
         
         t_map = Counter(t)
-        curr_map = Counter()
-        required_chars = len(t_map)
-        res = ""
-        len_res = float('inf')
+        s_map = Counter()
+        required = len(t_map)
+        have = 0
+
         left = 0
-        
+        res = [-1, -1]
+        len_res = float('inf')
+
         for right in range(len(s)):
-            curr_char = s[right]
-            curr_map[curr_char] += 1
+            char = s[right]
+            if char in t_map:
+                s_map[char] += 1
+                if s_map[char] == t_map[char]:
+                    have += 1
             
-            while all(curr_map[c] >= t_map[c] for c in t_map) and left <= right:
-                if (right - left + 1) < len_res:
-                    res = s[left:right+1]
-                    len_res = right - left + 1
-                
+            while have == required:
+                if right-left+1 < len_res:
+                    len_res = right-left+1
+                    res = [left,right]
                 left_char = s[left]
-                curr_map[left_char] -= 1
+                if left_char in s_map:
+                    s_map[left_char] -= 1
+                    if s_map[left_char] < t_map[left_char]:
+                        have -= 1
                 left += 1
         
-        return res
+        l, r = res
+        return s[l:r+1] if len_res != float('inf') else ""
