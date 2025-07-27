28a6c9c9b0974fb22f7b48aea499b3ef77785c93
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-27 10:11:55 -0400

    Time: 4 ms (58.35%) | Memory: 18 MB (24.85%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,20 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row_set = [set() for _ in range(9)]
-        col_set = [set() for _ in range(9)]
-        box_set = [set() for _ in range(9)]
+        row_set = [set() for _ in range(9)]     # [{8, 0, 7}, {6,1}, {}, {}, {}, {}, {}, {}, {}]
+        col_set = [set() for _ in range(9)]     # [{8, 6}, {0}, {}, {1}, {7}, {}, {}, {}, {}]
+        box_set = [set() for _ in range(9)]     # [{8, 0, 6}, {7, 1}, {}, {}, {}, {}, {}, {}, {}]
+
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+                #r: 1, c: 4
+                curr_num = board[r][c]  # "1"
+                if curr_num == '.':
                     continue
-                
-                box_index = (r//3)*3 + (c//3)
-                num = board[r][c]
-                if num in row_set[r] or num in col_set[c] or num in box_set[box_index]:
+                box_index = ((r//3)*3) + (c//3) # 1
+
+                if curr_num in row_set[r] or curr_num in col_set[c] or curr_num in box_set[box_index]:
                     return False
-                row_set[r].add(num)
-                col_set[c].add(num)
-                box_set[box_index].add(num)
-        
+                row_set[r].add(curr_num)
+                col_set[c].add(curr_num)
+                box_set[box_index].add(curr_num)
+
         return True
+
