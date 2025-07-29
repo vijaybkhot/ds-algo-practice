4321c56df58c04d88661f52436b2b6ddb5f0ece7
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-29 07:02:16 -0400

    Time: 114 ms (97.64%) | Memory: 29.9 MB (39.37%) - LeetSync

--- 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
@@ -23,6 +23,18 @@ class Solution:
         #             break
         
         # return res
+        n = len(nums)
+        res = [1]*n
+
+        for i in range(n):
+            curr = nums[i]
+            prev = i-1
+            while prev >= 0 and (nums[prev] | curr) != nums[prev]:
+                res[prev] = i-prev+1
+                nums[prev] |= curr
+                prev -= 1
+        
+        return res
 
         n = len(nums)
         pos = [-1] * 31
