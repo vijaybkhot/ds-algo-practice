3e76fdf2b5a930b366cd285681c03bc60e3b37a7
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-30 07:37:17 -0400

    Time: 7 ms (53.45%) | Memory: 18.2 MB (17.39%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,42 +1,16 @@
-class Solution(object):
-    def isIsomorphic(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # char_map = {}
-        # for i in range(len(s)):
-        #     char_map[s[i]] = t[i]
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
         
-        # # Check if multiple characters map to same character
-        # val_set = set(char_map.values())
-        # if len(val_set) != len(char_map):
-        #     return False
-        # # Using the mapped words, create a new string from s
-        # new_str = ""
-        # for char in s:
-        #     new_str = new_str + char_map[char]
+        s_t_map = dict()
+        t_s_map = dict()
         
-        # return new_str == t
-        
-        # More optimized solution using two dictionaries
-        if len(s) != len(t):
-            return False
-        
-        map_s_to_t = {}
-        map_t_to_s = {}
-        
-        for i in range(len(s)):
-            # Check if there's a conflict in mapping from s to t
-            if s[i] in map_s_to_t and map_s_to_t[s[i]] != t[i]:
-                return False
-            # Check if there's a conflict in mapping from t to s
-            if t[i] in map_t_to_s and map_t_to_s[t[i]] != s[i]:
-                return False
-            
-            # Map characters from s to t and t to s
-            map_s_to_t[s[i]] = t[i]
-            map_t_to_s[t[i]] = s[i]
-        
+        for idx, char in enumerate(s):
+            t_char = t[idx]
+            if char not in s_t_map and t[idx] not in t_s_map:
+                s_t_map[char] = t[idx]
+                t_s_map[t[idx]] = char
+            else:
+                if char not in s_t_map or t_char not in t_s_map or s_t_map[char] != t_char or t_s_map[t_char] != char:
+                    return False
+
         return True
