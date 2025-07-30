cd9624876922ae058c9d9c77959bbafe7a1f6af7
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-30 07:21:31 -0400

    Time: 15 ms (75.27%) | Memory: 18.1 MB (34.71%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,21 +1,10 @@
-class Solution(object):
-    def canConstruct(self, ransomNote, magazine):
-        """
-        :type ransomNote: str
-        :type magazine: str
-        :rtype: bool
-        """
-        # Create ransomNote map:
-        r_note_map = {}
-        magazine_map = {}
-        for char in ransomNote:
-            r_note_map[char] = r_note_map.get(char, 0) + 1
-        for char in magazine:
-            magazine_map[char] = magazine_map.get(char, 0) + 1
-        
-        
-        for key in r_note_map.keys():
-            if key not in magazine_map or r_note_map[key] > magazine_map[key]:
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        note_counter = Counter(ransomNote)
+        mag_counter = Counter(magazine)
+
+        for char in note_counter:
+            if note_counter[char] > mag_counter[char]:
                 return False
         
         return True
