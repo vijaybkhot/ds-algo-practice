098732b23c7440b9ad358771735dd0e3269b83a1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-31 07:34:45 -0400

    Time: 11 ms (82.53%) | Memory: 21.5 MB (37.72%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,23 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # anagram_strs = [(''.join(sorted(s)), s) for s in strs]
 
-        # anagram_strs.sort()
-        # res = []
-        # res.append([anagram_strs[0][1]])
-        # for i in range(1 ,len(strs)):
-        #     anagram, s = anagram_strs[i]
-        #     if anagram == anagram_strs[i-1][0]:
-        #         res[-1].append(s)
-        #     else:
-        #         res.append([s])
-        
-        # return res
-
-        # Using a hash map:
         anagram_map = defaultdict(list)
-        for s in strs:
-            key = ''.join(sorted(s))
-            anagram_map[key].append(s)
+
+        for word in strs:
+            anagram = ''.join(sorted(word))
+            anagram_map[anagram].append(word)
         
-        return [anagram_map[key] for key in anagram_map]
+        return [list(anagrams) for anagrams in anagram_map.values()]
