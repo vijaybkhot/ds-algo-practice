0adaf90ccf54243f7759b2f0d9816fb8130bfed1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-31 07:47:43 -0400

    Time: 3 ms (40.40%) | Memory: 18 MB (18.33%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,18 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        num_set = set()
+        nums_set = set()
         while True:
+            num = n
             new_num = 0
-            str_num = str(n)
-            for c in str_num:
-                new_num += int(c)*int(c)
-            
+            while num:
+                last_digit = num%10
+                new_num += last_digit**2
+                num = num // 10
             if new_num == 1:
                 return True
-            elif new_num in num_set:
+            elif new_num in nums_set:
                 return False
-            num_set.add(new_num)
+            nums_set.add(new_num)
             n = new_num
-
-
-        
