081b2dc5d958258a9671a9acf246885b8ddc8ad0
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-31 09:03:23 -0400

    Time: 275 ms (5.10%) | Memory: 32.6 MB (5.78%) - LeetSync

---
+++ 2355-maximum-consecutive-floors-without-special-floors/maximum-consecutive-floors-without-special-floors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxConsecutive(self, bottom: int, top: int, special: List[int]) -> int:
+        if not special:
+            return top-bottom+1
+        res = 0
+        special.sort()
+
+        res = special[0] - bottom
+        for i in range(1, len(special)):
+            curr_bottom = special[i-1]+1
+            curr_top = special[i]-1
+            print(res)
+            res = max(res, curr_top-curr_bottom+1)
+        print(res)
+        res = max(res, top - special[-1])
+        print(res)
+    
+        return res
