7d70ec5edc303970d174aaf46215e254ac9df2c8
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-01 11:55:50 -0400

    Time: 5 ms (10.18%) | Memory: 20.5 MB (5.52%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -1,68 +1,26 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # # First attempt:
-        # if not intervals:
-        #     return [newInterval]
-        # temp, res = [], []
-        # new_start, new_end = newInterval
-        # hasAppended = False
-        # curr_start, curr_end = intervals[0] if intervals[0][0] < newInterval[0] else newInterval
-        # for start, end in intervals:
-        #     if start < new_start or hasAppended:
-        #         temp.append([start, end])
-        #     else:
-        #         temp.append([new_start, new_end])
-        #         temp.append([start, end])
-        #         hasAppended = True
-        # if not hasAppended:
-        #     temp.append(newInterval)
-
-        # res.append(temp[0])
-        # # merge overlapping intervals
-        # for i in range(1, len(temp)):
-        #     prev = res[-1]
-        #     curr = temp[i]
-        #     if prev[1] >= curr[0]:
-        #         res.pop()
-        #         res.append([min(prev[0], curr[0]), max(prev[1], curr[1])])
-        #     else:
-        #         res.append([curr[0], curr[1]])
-        
-        # return res
-            
-
-        # # Greedy Approach: More readable solution O(n) time and O(1) space. O(n) for output list
-        # res = []
-        # for i in range(len(intervals)):
-        #     if newInterval[1] < intervals[i][0]:
-        #         res.append(newInterval)
-        #         return res + intervals[i:]
-        #     elif newInterval[0] > intervals[i][1]:
-        #         res.append(intervals[i])
-        #     else:
-        #         newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
-        
-        # res.append(newInterval)
-        # return res
-
-        # Linear search
-        n = len(intervals)
-        i = 0
+        newStart, newEnd = newInterval
+        used = False
+        insert = []
         res = []
-        while i < n and intervals[i][1] < newInterval[0]:
-            res.append(intervals[i])
-            i += 1
-        
-        while i < n and intervals[i][0] <= newInterval[1]:
-            newInterval[0] = min(intervals[i][0], newInterval[0])
-            newInterval[1] = max(intervals[i][1], newInterval[1])
-            i += 1
 
-        res.append(newInterval)
-        
-        while i < n:
-            res.append(intervals[i])
-            i += 1
+        # insert newInterval in order
+        for start, end in intervals:
+            if newStart < start and not used:
+                insert.append([newStart, newEnd])
+                used = True
+            insert.append([start, end])
+
+        if not used:
+            insert.append([newStart, newEnd])
+        res = [insert[0]]
+        # Merge overlapping intervals
+        for start, end in insert[1:]:
+            prev_start, prev_end = res[-1]
+            if start <= prev_end and prev_start <= end:
+                res[-1] = [min(start, prev_start), max(end, prev_end)]
+            else:
+                res.append([start, end])
         
         return res
-        
