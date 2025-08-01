b5e156b7a3042dba9e90716990f3f088cb2b88f1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-01 07:31:49 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.01%) - LeetSync

--- 228-summary-ranges/summary-ranges.py
+++ 228-summary-ranges/summary-ranges.py
@@ -1,29 +1,45 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
         
-        res = []
-        if not nums:
-            return []
-        prev, start, end, = nums[0], nums[0], None
+        # res = []
+        # if not nums:
+        #     return []
+        # prev, start, end, = nums[0], nums[0], None
        
-        i = 1
-        while i < len(nums):
-            while i < len(nums) and nums[i] == prev+1:
-                prev = nums[i]
-                end = nums[i]
-                i += 1
-            if end is not None:
-                res.append(str(start) + "->" + str(end))
-            else:
-                res.append(str(start))
-            if i < len(nums):
-                start = nums[i]
-                end = None
-                prev = start
-                if i+1 == len(nums):
-                    res.append(str(start))
-            i += 1
-        if not res:
-            res.append(str(start))
+        # i = 1
+        # while i < len(nums):
+        #     while i < len(nums) and nums[i] == prev+1:
+        #         prev = nums[i]
+        #         end = nums[i]
+        #         i += 1
+        #     if end is not None:
+        #         res.append(str(start) + "->" + str(end))
+        #     else:
+        #         res.append(str(start))
+        #     if i < len(nums):
+        #         start = nums[i]
+        #         end = None
+        #         prev = start
+        #         if i+1 == len(nums):
+        #             res.append(str(start))
+        #     i += 1
+        # if not res:
+        #     res.append(str(start))
         
+        # return res
+
+        res = []
+        i = 0
+        while i < len(nums):
+            start, prev = nums[i], nums[i]
+            end = start
+            while i < len(nums)-1 and nums[i+1] == prev+1:
+                prev = nums[i+1]
+                end = prev
+                i += 1
+            if start == end:
+                res.append(str(start))
+            else:
+                res.append(str(start) + "->" + str(end))
+            i += 1
         return res
