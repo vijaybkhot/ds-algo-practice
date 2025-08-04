41ec20be87fb8c7529e81fda458678a4557a6881
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-04 10:26:15 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (9.92%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,47 +1,25 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
+
         for paren in s:
-            if paren in ["(", "{", "["]:
+            if paren in "([{":
                 stack.append(paren)
-            elif paren in [")", "}", "]"]:
-                if not stack or paren == ")" and stack[-1] != "(" or paren == "}" and stack[-1] != "{" or paren == "]" and stack[-1] != "[":
-                    return False
-                else:
-                    stack.pop()
-        
-        return True if not stack else False
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # stack = []
-        # for i in range(len(s)):
-        #     if s[i] == "(" or s[i] == "{" or s[i] == "[":
-        #         stack.append(s[i])
-        #     else:
-        #         if stack and ((s[i] == ")" and stack[-1] == "(") or (s[i] == "}" and stack[-1] == "{") or (s[i] == "]" and stack[-1] == "[")):
-        #             stack.pop()
-        #         else:
-        #             return False
+            else:
+                if paren == ')':
+                    if stack and stack[-1] == '(':
+                        stack.pop()
+                    else:
+                        return False
+                if paren == '}':
+                    if stack and stack[-1] == '{':
+                        stack.pop()
+                    else:
+                        return False
                 
-        # return not stack
-        
+                if paren == ']':
+                    if stack and stack[-1] == '[':
+                        stack.pop()
+                    else:
+                        return False   
+        return not stack
