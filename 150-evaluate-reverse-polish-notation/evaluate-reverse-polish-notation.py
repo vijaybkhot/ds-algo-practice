016535f451701f20631bc7ff6caca762b18aee1b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-04 11:04:09 -0400

    Time: 4 ms (28.84%) | Memory: 19.3 MB (17.39%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -6,14 +6,12 @@ class Solution:
             if token in "+-/*":
                 num1 = int(stack.pop())
                 num2 = int(stack.pop())
-                print(num1, num2, token)
                 if token == "+":
                     stack.append(num1+num2)
                 elif token == "-":
                     stack.append(num2-num1)
                 elif token == "/":
                     stack.append(int(num2/num1))
-                    print(stack[-1])
                 else:
                     stack.append(num1*num2)
             else:
