d56d0cfcd1e48b1fde396669ddd99c17b5a94326
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-05 07:32:53 -0400

    Time: 183 ms (5.17%) | Memory: 18.2 MB (5.55%) - LeetSync

---
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        basket_heap = [(idx, capacity) for idx, capacity in enumerate(baskets)]
+        heapq.heapify(basket_heap)
+        i, j, = 0, 0
+        unplaced = 0
+
+        n = len(fruits)
+        for fruit_count in fruits:
+            backup = []
+            while basket_heap and basket_heap[0][1] < fruit_count:
+                backup.append(heapq.heappop(basket_heap))
+            if basket_heap:
+                heapq.heappop(basket_heap)
+            elif not basket_heap:
+                unplaced += 1
+            while backup:
+                heapq.heappush(basket_heap, heapq.heappop(backup))
+        
+        return unplaced
+            
+            
