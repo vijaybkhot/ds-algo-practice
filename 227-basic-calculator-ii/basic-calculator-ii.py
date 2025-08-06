56224b274d8c7109e09a51d21691a4ce0f143ef2
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-06 10:08:39 -0400

    Time: 165 ms (5.49%) | Memory: 26.3 MB (5.05%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.py
@@ -0,0 +1,56 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        n = len(s)
+
+        def parse_num(i):
+            curr_num = ""
+            while i < n and s[i].isdigit():
+                curr_num += s[i]
+                i += 1
+            return curr_num, i
+        
+        stack = []
+        i = 0
+        while i < len(s):
+            if s[i] in "+-*/":
+                stack.append(s[i])
+                i += 1
+            elif s[i] == ' ':
+                i += 1
+                continue
+            else:
+                curr_num, i = parse_num(i)
+                stack.append(curr_num)
+        i = 0
+        new_stack = []
+        while i < len(stack):
+            if stack[i] in '*/':
+                sign = stack[i]
+                num1 = int(new_stack.pop())
+                i += 1
+                num2 = int(stack[i])
+                new_num = str(int(num1 / num2)) if sign == '/' else str(num1*num2)
+                new_stack.append(new_num)
+            else:
+                new_stack.append(stack[i])
+            i += 1
+        
+        final_stack = []
+        i = 0
+        while i < len(new_stack):
+            if new_stack[i] in '+-':
+                sign = new_stack[i]
+                i += 1
+                num1 = final_stack.pop()
+                num2 = int(new_stack[i])
+                new_num = num1 + num2 if sign == '+' else num1-num2
+                final_stack.append(new_num)
+            else:
+                final_stack.append(int(new_stack[i]))
+            i += 1
+        
+        return final_stack[0]
+
+
+
+        
