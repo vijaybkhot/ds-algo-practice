e1b5aaf888e46ea582efdac449e5d778b714479f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-08 08:16:26 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (85.72%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -8,50 +8,25 @@ class Solution:
         if not head or left == right:
             return head
 
+        
         dummy = ListNode(0)
         dummy.next = head
         prev = dummy
 
-        # while curr and count != left:
-        #     prev = curr
-        #     curr = curr.next
-        #     count += 1
-        
-        # tail1 = prev
-        # head2 = curr
-        # prev = None
-
-        # while curr and count != right:
-        #     nxt = curr.next
-        #     curr.next = prev
-        #     prev = curr
-        #     curr = nxt
-        #     count += 1
-
-        # if curr:
-        #     head3 = curr.next
-        #     curr.next = prev
-        # if tail1:
-        #     tail1.next = curr
-        # if head2:
-        #     head2.next = head3
-
-        # return dummy.next
-
-        # More readable solution
         for _ in range(left-1):
             prev = prev.next
         
-        # `start` is the first node to reverse
-        start = prev.next
-        then = start.next
+        rev_prev = None
+        curr = prev.next
+        tail = curr
 
-        for _ in range(right-left):
-            start.next = then.next
-            then.next = prev.next
-            prev.next = then
-            then = start.next
+        for _ in range(right-left+1):
+            next_node = curr.next
+            curr.next = rev_prev
+            rev_prev = curr
+            curr = next_node
         
+        tail.next = curr
+        prev.next = rev_prev
+
         return dummy.next
-        
-        
