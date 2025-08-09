e611a860105bc5c81b5618e8a8860853fadb86f9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-09 08:01:56 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (97.89%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -5,42 +5,36 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        
-        # Return the head if head is none or head is the only node in the list
-        if not head or not head.next:
+        if not head:
             return head
 
-        #  Calculate the length of list
-        list_len = 0
+        num_nodes = 0
+
         curr = head
         while curr:
-            list_len += 1
+            num_nodes += 1
             curr = curr.next
-        # Calculate the rotations if rotations exceed the length of the list
-        k = k%list_len
-        # If no rotations, return list
+        
+        k = k % num_nodes
+        
         if k == 0:
             return head
-        
-        # Get the pointer to the new head after rotation. It would be k nodes away from the end
-        fast = head
-        for _ in range(k):
-            fast = fast.next if fast else None
-            
-    
-        slow = head
-        while fast.next:
-            slow = slow.next
-            fast = fast.next
-        # Get a dummy node to return
+
         dummy = ListNode()
-        # Attach the slow.next which would be the start of the new LL to dummy
-        dummy.next = slow.next
-        curr= slow.next
-        slow.next = None
+        dummy.next = head
+
+        curr = head
+        for _ in range(k):
+            curr = curr.next
+        
+        prev = head
 
         while curr and curr.next:
+            prev = prev.next
             curr = curr.next
-    
+        dummy.next = prev.next
+        prev.next = None
         curr.next = head
         return dummy.next
+
+       
