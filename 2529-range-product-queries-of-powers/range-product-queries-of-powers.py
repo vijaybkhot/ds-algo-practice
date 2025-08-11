eb7312fbc1c5b930907b603228d92e294b405d06
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-11 08:34:45 -0400

    Time: 48 ms (76.40%) | Memory: 47.7 MB (52.17%) - LeetSync

--- 2529-range-product-queries-of-powers/range-product-queries-of-powers.py
+++ 2529-range-product-queries-of-powers/range-product-queries-of-powers.py
@@ -6,9 +6,9 @@ class Solution:
         for i in range(1, len(powers)):
             prefix[i] = powers[i]*prefix[i-1]
 
-        res = []
-        for start, end in queries:
+        res = [0]*len(queries)
+        for idx, (start, end) in enumerate(queries):
             product = prefix[end] // prefix[start-1] if start > 0 else prefix[end]
-            res.append(product% (10**9 + 7))
+            res[idx] = (product% (10**9 + 7))
         
         return res
