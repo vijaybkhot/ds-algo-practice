10f8f167735897044f8602ee84440c87e1446ddb
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-11 09:53:49 -0400

    Time: 1 ms (20.85%) | Memory: 17.8 MB (39.11%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.py
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.py
@@ -6,45 +6,52 @@
 #         self.right = right
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
-        h = 0
+        # h = 0
+        # q = deque([root])
+
+        # while q:
+        #     missing = False
+        #     count = 0
+        #     for _ in range(len(q)):
+        #         node = q.popleft()
+        #         count += 1
+        #         if node.right and not node.left:
+        #             return False
+        #         if not node.left:
+        #             missing = True
+        #         if node.left:
+        #             if missing:
+        #                 return False
+        #             q.append(node.left)
+        #         if not node.right:
+        #             missing = True
+        #         if node.right:
+        #             if missing:
+        #                 return False
+        #             q.append(node.right)
+        #     if q and count < 2**h:
+        #         return False
+            
+        #     h += 1
+
+    
+        # return True
+        
+
+        # Clean approach
+
         q = deque([root])
 
+        found_null = False
+
         while q:
-            missing = False
-            count = 0
-            for _ in range(len(q)):
-                node = q.popleft()
-                count += 1
-                if node.right and not node.left:
-                    print(1)
+            node = q.popleft()
+            if not node:
+                found_null = True
+            else:
+                if found_null:
                     return False
-                if not node.left:
-                    missing = True
-                if node.left:
-                    if missing:
-                        print(2)
-                        return False
-                    
-                    q.append(node.left)
-                if not node.right:
-                    missing = True
-                if node.right:
-                    if missing:
-                        print(3)
-                        return False
-                    
-                    q.append(node.right)
-            if q and count < 2**h:
-                print(4)
-                print(q)
-                print(2**h)
-                print(count)
-                return False
-            
-            h += 1
-
+                q.append(node.left)
+                q.append(node.right)
         
-
-
         return True
-        
