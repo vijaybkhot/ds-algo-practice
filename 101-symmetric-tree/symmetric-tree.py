6dd1efc016a319819f0c5e77ea3e8b90ae8ae389
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-11 08:43:34 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (34.50%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.py
+++ 101-symmetric-tree/symmetric-tree.py
@@ -7,46 +7,12 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
 
-        # def dfs_left(node, arr):
-        #     if node:
-        #         arr.append(node.val)
-        #         if node.left and not node.right:
-        #             node.right = TreeNode(float('inf'))
-        #         if node.right and not node.left:
-        #             node.left = TreeNode(float('inf'))
-
-        #         dfs_left(node.left, arr)
-        #         dfs_left(node.right, arr)
-        #     return arr
-        
-        # def dfs_right(node, arr):
-        #     if node:
-        #         arr.append(node.val)
-        #         if node.left and not node.right:
-        #             node.right = TreeNode(float('inf'))
-        #         if node.right and not node.left:
-        #             node.left = TreeNode(float('inf'))
-        #         dfs_right(node.right, arr)
-        #         dfs_right(node.left, arr)
-        #     return arr
-
-        # if not root or (root.left is None and root.right is None):
-        #     return True
-        
-        # left_subtree = dfs_left(root.left, [])
-        # right_subtree = dfs_right(root.right, [])
-
-        # return left_subtree == right_subtree
-
-        # More optimal approach
-        def isMirror(t1, t2):
-            if (not t1 and t2) or (not t2 and t1):
+        def isMirror(r1, r2):
+            if not r1 and r2 or not r2 and r1:
                 return False
-            if not t1 and not t2:
+            if not r1 and not r2:
                 return True
             
-            return t1.val == t2.val and isMirror(t1.left, t2.right) and isMirror(t2.left, t1.right)
-        
+            return r1.val == r2.val and isMirror(r1.right, r2.left) and isMirror(r1.left, r2.right)
+
         return isMirror(root.left, root.right) if root else True
-
-
