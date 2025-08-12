3f139a96d6ce89eba936a8de40cd9352b27d30ec
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-12 11:28:40 -0400

    Time: 0 ms (100.00%) | Memory: 17.5 MB (97.59%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.py
@@ -6,100 +6,12 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # # # Recursive
-        # # def dfs(node, arr):
-        # #     if node:
-        # #         arr.append(node.val)
-        # #         dfs(node.left, arr)
-        # #         dfs(node.right, arr)
-        # #     return arr
+        res = []
+        def preorder(node):
+            if node:
+                res.append(node.val)
+                preorder(node.left)
+                preorder(node.right)
         
-        # # return dfs(root, [])
-
-        # # # Iterative
-        # # if not root:
-        # #     return []
-            
-        # # stack = [root]
-        # # res = []
-        # # while stack:
-        # #     node = stack.pop()
-        # #     res.append(node.val)
-        # #     if node.right:
-        # #         stack.append(node.right)
-        # #     if node.left:
-        # #         stack.append(node.left)
-        
-        # # return res
-
-        # # # Recursive approach:
-        # # def dfs(node):
-        # #     if node:
-        # #         res.append(node.val)
-        # #         dfs(node.left)
-        # #         dfs(node.right)
-        
-        # # res = []
-        # # dfs(root)
-        # # return res
-
-        # # Iterative approach:
-        # if not root:
-        #     return []
-        # curr, stack, res = root, [root], []
-        # while stack:
-        #     node = stack.pop()
-        #     res.append(node.val)
-        #     if node.right:
-        #         stack.append(node.right)
-        #     if node.left:
-        #         stack.append(node.left)
-        
-        # return res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # res = []
-        # def preorder(node):
-        #     if node:
-        #         res.append(node.val)
-        #         preorder(node.left)
-        #         preorder(node.right)
-        
-        # preorder(root)
-        # return res
-        if not root:
-            return []
-        stack, res = [root], []
-
-        while stack:
-            curr = stack.pop()
-            res.append(curr.val)
-            if curr.right:
-                stack.append(curr.right)
-            if curr.left:
-                stack.append(curr.left)
-        
+        preorder(root)
         return res
-
-
-
-
-
-
-
-
-
-
-        
