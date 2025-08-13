bcdd1f663852389c6cd8b110d2a3bf26c14cf441
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-13 09:54:52 -0400

    Time: 56 ms (44.81%) | Memory: 22.1 MB (72.42%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -5,43 +5,43 @@
 #         self.left = None
 #         self.right = None
 
+
+# p=5, q=4
+#                       3
+#               5               1
+#           6       2       0       8
+#               7       4
+
 class Solution:
+
+    def find(self, node, value):
+        if not node:
+            return False
+        if node.val == value:
+            return True
+        else:
+            return self.find(node.left, value) or self.find(node.right, value)
+
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # # Approach using set - Inefficient
-        # self.res = None
-        # def dfs(node):
-        #     if not node or self.res:  # Stop recursion if LCA is found
-        #         return set()
 
-        #     left_set = dfs(node.left)
-        #     right_set = dfs(node.right)
-
-        #     # If either left_set or right_set is None, treat it as an empty set
-        #     if left_set is None: left_set = set()
-        #     if right_set is None: right_set = set()
-
-        #     merged_set = left_set | {node.val} | right_set
-
-        #     if p.val in merged_set and q.val in merged_set and self.res is None:
-        #         self.res = node
-        #         return None  # Signal to stop further recursion
-
-        #     return merged_set
-
-
-        # dfs(root)
-        # return self.res
-
-        # Optimized approach
+        # if p.val == root.val or q.val == root.val:
+        #     return root
+        # if self.find(root.left, p.val) and self.find(root.left, q.val):
+        #     return self.lowestCommonAncestor(root.left, p, q)
+        
+        # if self.find(root.right, p.val) and self.find(root.right, q.val):
+        #     return self.lowestCommonAncestor(root.right, p, q)
+        
+        # if (self.find(root.left, p.val) and self.find(root.right, q.val)) or (self.find(root.right, p.val) and self.find(root.left, q.val)):
+        #     return root 
 
         if not root or root == p or root == q:
             return root
         
         left = self.lowestCommonAncestor(root.left, p, q)
         right = self.lowestCommonAncestor(root.right, p, q)
-
+        
         if left and right:
-            return root
+            return root  # p in one side, q in the other
         
-        return left or right
-        
+        return left if left else right
