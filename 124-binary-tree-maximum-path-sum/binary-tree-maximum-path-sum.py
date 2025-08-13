218c3198ca736db053dd06dbcf38fc848b206bd3
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-13 07:25:40 -0400

    Time: 31 ms (5.26%) | Memory: 29.3 MB (13.08%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -6,77 +6,29 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
+
         self.max_sum = float('-inf')
-
+        @lru_cache(maxsize=None)
         def dfs(node):
-            if not node:
-                return 0
-            if not node.left and not node.right:
-                self.max_sum = max(self.max_sum, node.val)
-                return node.val
-
-
-            left_tree_sum = dfs(node.left) if node.left else float('-inf')
-            right_tree_sum = dfs(node.right) if node.right else float('-inf')
-
-            self.max_sum = max(self.max_sum, left_tree_sum, right_tree_sum, node.val,left_tree_sum+right_tree_sum+node.val, right_tree_sum+node.val, left_tree_sum+node.val)
-
-            if node.left and node.right:
-                return max(left_tree_sum+node.val, right_tree_sum+node.val, node.val)
-            elif node.left:
-                return max(left_tree_sum+node.val, node.val)
+            if node:
+                center = node.val
+                left, right, left_and_right = float('-inf'), float('-inf'), float('-inf')
+                
+                if node.left:
+                    left = node.val+dfs(node.left)
+                
+                if node.right:
+                    right = node.val+dfs(node.right)
+                
+                if node.left and node.right:
+                    left_and_right = node.val+dfs(node.right)+dfs(node.left)
+                
+                self.max_sum = max(self.max_sum, left, right, left_and_right, center)
+                
+                return max(center, left, right)
             else:
-                return max(right_tree_sum+node.val, node.val)
-
+                return float('-inf')
         
         dfs(root)
-        
+
         return self.max_sum
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-#         # # First Attempt:
-#         # self.curr_max = float('-inf')
-#         # def dfs(node):
-#         #     if node is None:
-#         #         return 0
-                
-#         #     left_path_sum = dfs(node.left)
-#         #     right_path_sum = dfs(node.right)
-#         #     curr_path_sum = max(node.val, node.val + left_path_sum + right_path_sum, node.val + left_path_sum, node.val + right_path_sum)
-#         #     self.curr_max = max(self.curr_max, curr_path_sum)
-#         #     return_path_sum = max(node.val, node.val+left_path_sum, node.val + right_path_sum)
-#         #     return return_path_sum
-        
-#         # dfs(root)
-#         # return self.curr_max
-
-#         # More readable code:
-#         self.curr_max = float('-inf')
-
-#         def dfs(node):
-#             if not node:
-#                 return 0
-#             leftTreeSum = max(0, dfs(node.left))
-#             rightTreeSum = max(0, dfs(node.right))
-#             curr_node_sum = node.val + leftTreeSum + rightTreeSum
-#             self.curr_max = max(self.curr_max, curr_node_sum)
-
-#             return node.val + max(leftTreeSum, rightTreeSum)
-        
-#         dfs(root)
-#         return self.curr_max
-            
