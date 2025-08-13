6e18c9cb7b564440778a23a2dfe366e7c7c5ae4b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-13 09:28:06 -0400

    Time: 0 ms (100.00%) | Memory: 23.3 MB (82.38%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.py
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.py
@@ -8,25 +8,48 @@ class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        count = 0
-        next_level = True
-        q = deque([root])
+        # count = 0
+        # next_level = True
+        # q = deque([root])
 
-        while q and next_level:
-            count += len(q)
-            for _ in range(len(q)):
-                node = q.popleft()
+        # while q and next_level:
+        #     count += len(q)
+        #     for _ in range(len(q)):
+        #         node = q.popleft()
 
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-                if not node.left or not node.right:
-                    next_level = False
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #         if not node.left or not node.right:
+        #             next_level = False
             
-            if not next_level:
-                count += len(q)
-                break
+        #     if not next_level:
+        #         count += len(q)
+        #         break
         
-        return count
+        # return count
+
+        def left_height(node):
+            h = 0
+            while node:
+                h += 1
+                node = node.left
+            return h
+        
+        def right_height(node):
+            h = 0
+            while node:
+                h += 1
+                node = node.right
+            return h
+        
+        lh = left_height(root)
+        rh = right_height(root)
+        if lh == rh:
+            return 2**lh - 1
+        
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+    
+    
                 
