4da343a0cb5f16ca3efb1e6872b66cbd16e31ea5
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-14 07:37:08 -0400

    Time: 4 ms (16.26%) | Memory: 20.9 MB (93.32%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,79 +6,22 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        heap = []
 
-        stack, curr = [], root
+        stack = []
 
-        while stack or curr:
+        curr = root
+
+        while curr or stack:
             while curr:
                 stack.append(curr)
                 curr = curr.left
+            
             curr = stack.pop()
-            k -= 1
-            if k == 0:
-                return curr.val
+            heapq.heappush(heap, -curr.val)
+            if len(heap) > k:
+                heapq.heappop(heap)
+            
             curr = curr.right
         
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-#         # # Solution using inorder traversal. Uses O(n) extra space
-#         # def inorder(root, arr):
-#         #     if root:
-#         #         inorder(root.left, arr)
-#         #         arr.append(root.val)
-#         #         inorder(root.right, arr)
-#         #     return arr
-        
-#         # sorted_bst_arr = inorder(root, [])
-
-#         # return sorted_bst_arr[k-1]
-
-#         # # Optimized solution for follow up question. Using iterative inorder traversal
-#         # stack = []
-#         # curr = root
-
-#         # while True:
-#         #     # Go left as much as possible
-#         #     while curr:
-#         #         stack.append(curr)
-#         #         curr = curr.left
-            
-#         #     # Process the leftmost node
-#         #     curr = stack.pop()
-#         #     k -= 1
-#         #     if k == 0:
-#         #         return curr.val  # Found kth smallest
-            
-#         #     # Move to the right subtree
-#         #     curr = curr.right
-
-#         # Recursive inorder traversal
-#         self.count = 0
-#         self.result = None
-
-#         def inorder(node):
-#             if node and self.result is None:
-#                 inorder(node.left)
-#                 self.count += 1
-#                 if self.count == k:
-#                     self.result = node.val
-#                     return
-#                 inorder(node.right)
-            
-#         inorder(root)
-#         return self.result
-
-        
+        return -heap[0]
