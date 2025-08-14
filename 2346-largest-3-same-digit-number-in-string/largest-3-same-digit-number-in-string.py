56ede2bff1dea10cb9c2eeae48f1609b2ac6d33f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-14 07:27:38 -0400

    Time: 8 ms (7.30%) | Memory: 17.6 MB (98.45%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.py
@@ -4,11 +4,12 @@ class Solution:
         max_num = float('-inf')
         for i in range(len(num)-2):
             curr_num = num[i:i+3]
+            curr_int = int(curr_num)
             
             num_nums = set(curr_num)
-            if len(num_nums) == 1 and int(curr_num) > max_num:
+            if len(num_nums) == 1 and curr_int > max_num:
                 res = curr_num
-                max_num = int(curr_num)
+                max_num = curr_int
             
 
         
