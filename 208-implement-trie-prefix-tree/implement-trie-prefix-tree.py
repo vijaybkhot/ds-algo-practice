e664ae9e233124049fd7aad45ea7bb105978a1e7
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-15 15:48:23 -0400

    Time: 70 ms (18.12%) | Memory: 32.2 MB (40.64%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -10,40 +10,31 @@ class Trie:
         
 
     def insert(self, word: str) -> None:
-            i = 0
-            curr_trie = self.trie
-            for c in word:
-                if c not in curr_trie.children:
-                    curr_trie.children[c] = TrieNode()
-                curr_trie = curr_trie.children[c]
-            curr_trie.endOfWord = True
-
-
-
+        curr = self.trie
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        
+        curr.endOfWord = True
+    
+    def search(self, word: str) -> bool:
+        curr = self.trie
+        for c in word:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        
+        return curr.endOfWord
         
 
-    def search(self, word: str) -> bool:
-        i = 0
-        curr_trie = self.trie
-        while i < len(word):
-            char = word[i]
-            if char not in curr_trie.children:
-                return False                
-            curr_trie = curr_trie.children[char]
-            i += 1
-        if not curr_trie.endOfWord:
-            return False
-        return True
-
     def startsWith(self, prefix: str) -> bool:
-        i = 0
-        curr_trie = self.trie
-        while i < len(prefix):
-            char = prefix[i]
-            if char not in curr_trie.children:
+        curr = self.trie
+        for c in prefix:
+            if c not in curr.children:
                 return False
-            curr_trie = curr_trie.children[char]
-            i += 1
+            curr = curr.children[c]
+        
         return True
         
 
