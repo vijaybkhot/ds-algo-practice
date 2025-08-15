6fdb06a0625b9926e3d2050f874c35a4b6ef22e9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-15 07:37:22 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (73.27%) - LeetSync

--- 399-evaluate-division/evaluate-division.py
+++ 399-evaluate-division/evaluate-division.py
@@ -1,93 +1,34 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        # Create a weighted edge graph with adjacency list
-        graph = defaultdict(set)
-        for idx, edge in enumerate(equations):
-            u, v = edge
-            val = values[idx]
-            graph[u].add((v, val))
-            graph[v].add((u, 1/val))
-        
 
-        def bfs(node, dest):
-            if node not in graph or dest not in graph:
-                return -1
-            if node == dest:
-                return 1
-            
-            q = deque()
-            q.append((node, 1))
+        graph = defaultdict(list)
+
+        for i in range(len(equations)):
+            u, v = equations[i]
+            graph[u].append((v, values[i]))
+            graph[v].append((u, 1/values[i]))
+        
+        res = []
+        for u, v in queries:
+            if u not in graph or v not in graph:
+                res.append(-1)
+                continue
+            q = deque([(u, 1)])
             visited = set()
-            visited.add(node)
+            found = False
             while q:
-                curr, curr_dist = q.popleft()
-                if curr == dest:
-                    return curr_dist
-                
-                for nei, dist in graph[curr]:
-                    if nei not in visited:
-                        visited.add(nei)
-                        q.append((nei, curr_dist*dist))
-            
-            return -1
+                curr_node, curr_dist = q.popleft()
+                visited.add(curr_node)
+                if curr_node == v:
+                    found = True
+                    res.append(curr_dist)
+                    break
+                for nxt_node, nxt_dist in graph[curr_node]:
+                    if nxt_node not in visited:
+                        q.append((nxt_node, curr_dist*nxt_dist))
+            if not found:
+                res.append(-1)
         
-        return [bfs(u, v) for u, v in queries]
-
-
+        return res
                 
-                
-                    
-                        
-                    
-                
-                
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#         adj = defaultdict(list) # Map a -> list of [b, a/b]
-#         for i, eq in enumerate(equations):
-#             a, b = eq
-#             adj[a].append((b, values[i]))
-#             adj[b].append((a, 1/values[i]))
-        
-#         def bfs(src, target):
-#             if src not in adj or target not in adj:
-#                 return -1
-            
-#             q = deque()
-#             visited = set()
-#             q.append((src, 1))
-#             visited.add(src)
-
-#             while q:
-#                 node, w = q.popleft()
-#                 if node == target:
-#                     return w
-                
-#                 for nei, weight in adj[node]:
-#                     if nei not in visited:
-#                         visited.add(nei)
-#                         q.append((nei, w*weight))
-            
-#             return -1
-        
-#         return [bfs(a, b) for a, b in queries]
-
 
