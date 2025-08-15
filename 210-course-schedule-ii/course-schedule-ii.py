599c4c8490cd36db90aa38cb017e37e27bddd036
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-15 15:29:34 -0400

    Time: 7 ms (26.62%) | Memory: 20.6 MB (6.29%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -6,22 +6,46 @@ class Solution:
             graph[v].add(u)
             indegree[u] += 1
         
-        q = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                q.append(i)
-        
         res = []
-        while q:
-            node = q.popleft()
-            res.append(node)
+        visited = set()
+        stack = set()
+        def dfs(node):
+            if node in stack:
+                return False
+            if node in visited:
+                return True
+            stack.add(node)
             for nei in graph[node]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
-        
+                if nei not in visited and not dfs(nei):
+                    return False
+            
+            stack.remove(node)
+            visited.add(node)
+            res.append(node)
+            return True
+
         for i in range(numCourses):
-            if indegree[i] > 0:
+            if i not in visited and not dfs(i):
                 return []
         
-        return res
+        return res[::-1]
+
+        # q = deque()
+        # for i in range(numCourses):
+        #     if indegree[i] == 0:
+        #         q.append(i)
+        
+        # res = []
+        # while q:
+        #     node = q.popleft()
+        #     res.append(node)
+        #     for nei in graph[node]:
+        #         indegree[nei] -= 1
+        #         if indegree[nei] == 0:
+        #             q.append(nei)
+        
+        # for i in range(numCourses):
+        #     if indegree[i] > 0:
+        #         return []
+        
+        # return res
