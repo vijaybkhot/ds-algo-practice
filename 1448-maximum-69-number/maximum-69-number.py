17ccfb1475428e8084d82cdd822b7f5d650b950c
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-16 08:39:35 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.80%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        max_num = num
+        str_num = [char for char in str(num)]
+
+        for idx, num_char in enumerate(str_num):
+            if num_char == '6':
+                str_num[idx] = '9'
+                max_num = max(max_num, int(''.join(str_num)))
+                str_num[idx] = '6'
+            
+            if num_char == '9':
+                str_num[idx] = '6'
+                max_num = max(max_num, int(''.join(str_num)))
+                str_num[idx] = '9'
+        
+        return max_num
