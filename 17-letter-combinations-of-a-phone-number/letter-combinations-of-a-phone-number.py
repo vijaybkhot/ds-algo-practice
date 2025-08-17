35a1e85061302551fa9b853aa07ae9b52293d317
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-17 13:43:04 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (69.08%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,84 +1,32 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        num_to_char_map = {
-            '1':[],
-            '2': ['a', 'b', 'c'],
-            '3': ['d', 'e', 'f'],
-            '4': ['g', 'h', 'i'],
-            '5': ['j', 'k', 'l'],
-            '6': ['m', 'n', 'o'],
-            '7': ['p', 'q', 'r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z']
+        digit_char_map = {
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': "tuv",
+            '9': "wxyz"
         }
-
-        if not digits:
-            return []
+        n = len(digits)
         res = []
-        def dfs(start, path):
-            if start == len(digits):
-                if len(path) == len(digits):
-                    res.append(path[::])
-                    return
+        if len(digits) == 0:
+            return []
+        @lru_cache(None)
+        def dfs(i, curr_str):
+            if i == n:
+                res.append(curr_str)
+                return
             
-            for i in range(start, len(digits)):
-                curr_chars = num_to_char_map[digits[i]]
-                for char in curr_chars:
-                    dfs(i+1, path+char)
+            # for j in range(i, n):
+            digit = digits[i]
+            if digit in digit_char_map:
+                for char in digit_char_map[digit]:
+                    dfs(i+1, curr_str+char)
 
-        
+
         dfs(0, "")
+
         return res
-
-        # # # Attempt I
-        # # res = []
-        # # def backtrack(start, path):
-        # #     if start >= len(digits):
-        # #         if len(path)>0 and len(path) == len(digits):
-        # #             res.append(''.join(path[:]))
-        # #         return
-            
-        # #     for i in range(start, len(digits)):
-        # #         curr_chars = num_to_char_map[digits[i]]
-        # #         for char in curr_chars:
-        # #             path.append(char)
-        # #             backtrack(i+1, path)
-        # #             path.pop()
-
-        # # Simplified code
-        # if not digits:
-        #     return []
-
-        # res = []
-        
-
-        # def backtrack(index, path):
-        #     if index == len(digits):
-        #         res.append(''.join(path[:]))
-        #         return
-            
-        #     for char in num_to_char_map[digits[index]]:
-        #         path.append(char)
-        #         backtrack(index+1, path)
-        #         path.pop()
-
-        
-        # backtrack(0, [])
-        # return res
-
-        # res = []
-
-        # def dfs(start, path):
-        #     if start == len(digits):
-        #         res.append(''.join(path[:]))
-        #         return
-        #     curr_digit_chars = num_to_char_map[digits[start]]
-        #     for char in curr_digit_chars:
-        #         path.append(char)
-        #         dfs(start+1, path)
-        #         path.pop()
-        
-        # dfs(0, [])
-        # return res if digits else []
-
-        
