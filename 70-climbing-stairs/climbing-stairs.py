2fd8f17aba13bc9a6a406382f5de97dd713243ed
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-17 15:10:54 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.54%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,47 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-
-        # # Recursion:
-        # def dfs(i):
-        #     if i >=n:
-        #         return i == n
-            
-        #     return dfs(i+1) + dfs(i+2)
-
-        ## Memoization
-        # memo = [-1] * n
-        # def dfs(i):
-        #     if i >= n:
-        #         return i == n
-        #     if memo[i] != -1:
-        #         return memo[i]
-        #     memo[i] = dfs(i + 1) + dfs(i + 2)
-        #     return memo[i]
-        # return dfs(0)
-
-        # # Dynamic Programming:
-        # if n <= 2:
-        #     return n
-
-        # dp = [0]*(n+1)
-        # dp[1], dp[2] = 1, 2
-
-        # for i in range(3, n+1):
-        #     dp[i] = dp[i-1] + dp[i-2]
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [0]*(n+1)
+        dp[1] = 1
+        dp[2] = 2
         
-        # return dp[n]
-
-        # Space optimized DP
-        if n <= 2:
-            return n
-        
-        dp_1, dp_2 = 1, 2
-
         for i in range(3, n+1):
-            dp_curr = dp_1 + dp_2
-            dp_1 = dp_2
-            dp_2 = dp_curr
-
-        return dp_2
-            
-            
+            dp[i] = dp[i-1]+dp[i-2]
+        
+        return dp[n]
