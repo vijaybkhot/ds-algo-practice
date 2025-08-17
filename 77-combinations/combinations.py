b03231aa72d8a2e80e8f1ac86c14b2bbf3bf769a
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-17 14:03:41 -0400

    Time: 143 ms (36.97%) | Memory: 59.7 MB (55.74%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,20 +1,27 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
+        # n = 4
+        # res = [[12], [13], ]
+        # 1, ""
+        # 2, "1" / 2, ""
+        # 3, "12" / 3, "1" // 3, "2"/ 3, ""
+        # 4, "1" // 4, "23" / 4, "2" // 4, "3" / 4, ""
 
-        result = []
-
-        def dfs(i, path):
-            if len(path) == k:
-                result.append(path[:])
-                return 
-            if len(path) > k:
+        
+        res = []
+        def dfs(i, curr_comb):
+            if len(curr_comb) == k:
+                res.append(curr_comb[::])
                 return
-            if i <= n:
-                path.append(i)
-                dfs(i+1, path)
-                path.pop()
-                dfs(i+1, path)
+            if i >= n+1:
+                return
+
+            curr_comb.append(i)
+            # take curr num
+            dfs(i+1, curr_comb)
+            # skip curr num
+            curr_comb.pop()
+            dfs(i+1, curr_comb)
         
         dfs(1, [])
-        return result
-        
+        return res
