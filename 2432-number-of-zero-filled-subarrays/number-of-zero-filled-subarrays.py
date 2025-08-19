5ca32424931dbf487764509e7b6769eebf6e3fcd
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-19 05:37:43 -0400

    Time: 22 ms (97.28%) | Memory: 28.4 MB (42.33%) - LeetSync

--- 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -1,23 +1,37 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
-        left = 0
-        count = 0
-        curr_map = defaultdict(int)
+        # left = 0
+        # count = 0
+        # curr_map = defaultdict(int)
 
-        for right in range(len(nums)):
-            if len(curr_map) == 1 and 0 in curr_map and nums[right] != 0:
-                curr_len = curr_map[0]
-                count += (curr_len*(curr_len+1))//2
-            curr_map[nums[right]] += 1
-            while left < right and len(curr_map) > 1:
-                curr_map[nums[left]] -= 1
-                if curr_map[nums[left]] == 0:
-                    del curr_map[nums[left]]
-                left += 1
+        # for right in range(len(nums)):
+        #     if len(curr_map) == 1 and 0 in curr_map:
+        #         curr_len = curr_map[0]
+        #         prev_len = curr_len - 1
+        #         count -= (prev_len*(prev_len+1))//2
+        #         count += (curr_len*(curr_len+1))//2
+        #     curr_map[nums[right]] += 1
+        #     while left < right and len(curr_map) > 1:
+        #         curr_map[nums[left]] -= 1
+        #         if curr_map[nums[left]] == 0:
+        #             del curr_map[nums[left]]
+        #         left += 1
         
-        if len(curr_map) == 1 and 0 in curr_map:
-            curr_len = curr_map[0]
-            count += (curr_len*(curr_len+1))//2
+        # if len(curr_map) == 1 and 0 in curr_map:
+        #     curr_len = curr_map[0]
+        #     count += (curr_len*(curr_len+1))//2
+        
+        # return count
+
+        count = 0
+        streak = 0
+
+        for num in nums:
+            if num == 0:
+                streak += 1
+                count += streak
+            else:
+                streak = 0
         
         return count
                 
