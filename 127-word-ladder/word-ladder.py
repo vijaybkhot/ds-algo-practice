6cbcfa2c450627394e4c9eb67d406fa8d98012e5
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-20 08:38:50 -0400

    Time: 6521 ms (8.55%) | Memory: 22.1 MB (18.00%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,114 +1,35 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        wordListSet = set(wordList)
-        if endWord not in wordListSet:
-            return 0
-        
-        wordListSet.add(beginWord)
+        words = set(wordList)
 
-        graph = defaultdict(set)
+        q = deque([(beginWord, 1)])
+        num_words = 0
+        visited = set()
 
-        def get_neighbors(word, wordListSet):
-            neighbors = set()
-            for i in range(len(word)):
-                for c in 'abcdefghijklmnopqrstuvwxyz':
-                    if c != word[i]:
-                        new_word = list(word)      
-                        new_word[i] = c            
-                        new_word = ''.join(new_word)  
-                        if new_word in wordListSet:
-                            neighbors.add(new_word)
-            return neighbors    
-        
-        left, right = deque(), deque()
-        left.append(beginWord)
-        right.append(endWord)
-        visited_left, visited_right = set(), set()
-        visited_right.add(endWord)
-        visited_left.add(beginWord)
-        level = 1
-        while left and right:
-            if len(left) > len(right):
-                left, right = right, left
-                visited_left, visited_right = visited_right, visited_left
-                
+        def get_adjacent_words(word):
+            word_list = [char for char in word]
+            adj_words = []
+            for idx, char in enumerate(word_list):
+                for new_char in "abcdefghijklmnopqrstuvwxyz":
+                    new_word = word_list[::]
+                    new_word[idx] = new_char
+                    new_word = ''.join(new_word)
+                    if new_word in words:
+                        adj_words.append(new_word)
             
-            for _ in range(len(left)):
-                curr_word = left.popleft()
-                adjacents = get_neighbors(curr_word, wordListSet)
-                for nei in adjacents:
-                    if nei in visited_right:
-                        return level + 1
-                    if nei not in visited_left:
-                        left.append(nei)
-                        visited_left.add(nei)
-            level += 1
+            return adj_words
+
+
+        while q:
+            word, num_words = q.popleft()
+            if word == endWord:
+                return num_words
+            visited.add(word)
+            
+            adj_words = get_adjacent_words(word)
+            for new_word in adj_words:
+                if new_word not in visited:
+                    q.append((new_word, num_words+1))
         
         return 0
-
-
-
-#         if endWord not in wordList:
-#             return 0
-#         # char_sets = [set()] * len(beginWord)
-#         # Accumulate unique index wise character for all words in wordList
-#         # for word in wordList:
-#         #     for idx, char in enumerate(word):
-#         #         char_sets[idx].add(char)
-        
-#         wordSet = set(wordList)
-#         wordSet.add(beginWord)
-#         visited = set()
-
-#         def getAdjacentWords(word):
-#             adj = []
-#             for i in range(len(beginWord)):
-#                 for char in 'abcdefghijklmnopqrstuvwxyz':
-#                     if char != word[i]:
-#                         curr_word = word[:i] + char + word[i+1:]
-#                         if curr_word in wordSet:
-#                             adj.append(curr_word)
-#             return adj
-#         # One directional BFS
-#         q = deque()
-#         q.append((beginWord, 0))
-#         dist = 0
-#         while q:
-#             curr_word, curr_dist = q.popleft()
-#             if curr_word == endWord:
-#                 return curr_dist+1
-#             for nei in getAdjacentWords(curr_word):
-#                 if nei not in visited:
-#                     visited.add(nei)
-#                     q.append((nei, curr_dist+1))
-        
-#         return 0
-
-#         # # Bi-directional BFS
-#         # begin = {beginWord}
-#         # end = {endWord}
-#         # dist = 1
-
-        
-#         # while begin and end:
-#         #     if len(begin) > len(end):
-#         #         begin, end = end, begin
-
-#         #     temp = set()
-#         #     for word in begin:
-#         #         visited.add(word)
-#         #         for nei in getAdjacentWords(word):
-#         #             if nei in end:
-#         #                 return dist + 1
-#         #             if nei not in visited:
-#         #                 visited.add(nei)
-#         #                 temp.add(nei)
-
-#         #     begin = temp
-#         #     dist += 1
-                        
-        
-#         # return 0
-
-
-        
+            
