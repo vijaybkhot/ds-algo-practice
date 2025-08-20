7770d17fcc3e7f706368353dc538b539b26f233f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-20 08:41:10 -0400

    Time: 8207 ms (5.00%) | Memory: 21.6 MB (19.48%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -6,18 +6,17 @@ class Solution:
         num_words = 0
         visited = set()
 
-        def get_adjacent_words(word):
-            word_list = [char for char in word]
-            adj_words = []
-            for idx, char in enumerate(word_list):
-                for new_char in "abcdefghijklmnopqrstuvwxyz":
-                    new_word = word_list[::]
-                    new_word[idx] = new_char
-                    new_word = ''.join(new_word)
-                    if new_word in words:
-                        adj_words.append(new_word)
-            
-            return adj_words
+        def get_neighbors(word, wordListSet):
+            neighbors = set()
+            for i in range(len(word)):
+                for c in 'abcdefghijklmnopqrstuvwxyz':
+                    if c != word[i]:
+                        new_word = list(word)      
+                        new_word[i] = c            
+                        new_word = ''.join(new_word)  
+                        if new_word in wordListSet:
+                            neighbors.add(new_word)
+            return neighbors    
 
 
         while q:
@@ -26,7 +25,7 @@ class Solution:
                 return num_words
             visited.add(word)
             
-            adj_words = get_adjacent_words(word)
+            adj_words = get_neighbors(word, words)
             for new_word in adj_words:
                 if new_word not in visited:
                     q.append((new_word, num_words+1))
