8c7af68307531ce0080d8a377f850aaba17c1d43
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-20 07:43:42 -0400

    Time: 60 ms (52.01%) | Memory: 20.2 MB (74.47%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
@@ -1,7 +1,6 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         rows, cols = len(matrix), len(matrix[0])
-
         for r in range(rows-1, -1, -1):
             for c in range(cols-1, -1, -1):
                 if matrix[r][c] == 0:
@@ -11,10 +10,7 @@ class Solution:
                 diag = matrix[r+1][c+1] if (0 <= r+1 < rows and 0 <= c+1 < cols) else 0
 
                 if right and bottom and diag:
-                    if right == bottom == diag:
-                        matrix[r][c] = right+1
-                    else:
-                        matrix[r][c] = min(right, bottom, diag) + 1
+                    matrix[r][c] = min(right, bottom, diag) + 1
         
         total = 0
 
