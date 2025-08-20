7e720ec37aba0224a3c9fe70241e00852cee8cd6
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-20 10:29:01 -0400

    Time: 35 ms (97.76%) | Memory: 18.8 MB (71.24%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,10 +1,8 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         words = set(wordList)
-
-        q = deque([(beginWord, 1)])
-        num_words = 0
-        visited = set()
+        if endWord not in words:
+            return 0
 
         def get_neighbors(word, wordListSet):
             neighbors = set()
@@ -18,17 +16,34 @@ class Solution:
                             neighbors.add(new_word)
             return neighbors    
 
+        q = deque([(beginWord, 1)])
+        num_words = 0
+        visited = set()
 
-        while q:
-            word, num_words = q.popleft()
-            if word == endWord:
-                return num_words
-            visited.add(word)
+        left, right = deque(), deque()
+        left.append(beginWord)
+        right.append(endWord)
+        left_visited_set = {beginWord}
+        right_visited_set = {endWord}
+        level = 1
+
+        while left and right:
+            if len(left) > len(right):
+                left, right = right, left
+                left_visited_set, right_visited_set = right_visited_set, left_visited_set
             
-            adj_words = get_neighbors(word, words)
-            for new_word in adj_words:
-                if new_word not in visited:
-                    q.append((new_word, num_words+1))
+            
+            for _ in range(len(left)):
+                curr_word = left.popleft()
+                adjacents = get_neighbors(curr_word, words)
+                for nei in adjacents:
+                    if nei in right_visited_set:
+                        return level + 1
+                    if nei not in left_visited_set:
+                        left_visited_set.add(nei)
+                        left.append(nei)
+            level += 1
         
         return 0
+        
             
