d284f22766555ecd48dee93c3ebfd1e2349421b2
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-21 11:38:04 -0400

    Time: 19 ms (81.05%) | Memory: 19.8 MB (16.48%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,37 +1,22 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        #   2   3   4   -1  -1
+        #             -1     4
+        #  [1,  2,  3,  4,  3]
+
         n = len(nums)
+        res = [-1]* n
+        
 
-        # # Brute force approach
-        # max_idx = 0
-        # max_num = max(nums)
-        # res = [0]*n
+        stack = []  #(num, idx)
 
-        # for i in range(len(nums)):
-        #     if nums[i] == max_num:
-        #         res[i] = -1 
-        # for i in range(len(nums)):
-        #     if res[i] == -1:
-        #         continue
-        #     j = i+1
-        #     while j%n != i and nums[j%n] <= nums[i]:
-        #         j += 1
-        #     if i != j%n:
-        #         res[i] = nums[j%n]
-
-        # return res
-
-        # Using monotonic stack
-        n = len(nums)
-        stack = []
-        res = [-1]*n
-        # Loop through array twice to simulate circularity
-        for i in range(2 * n):
-            current = nums[i % n]
-            while stack and nums[stack[-1]] < current:
-                idx = stack.pop()
-                res[idx] = current
+        for i in range(2*n):
+            num = nums[i%n]
+            while stack and stack[-1][0] < num:
+                back_num, back_idx = stack.pop()
+                res[back_idx] = num
             if i < n:
-                stack.append(i)
+                stack.append((num, i))
 
+        
         return res
