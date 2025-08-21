3239bfdd21a8825c270ee1aeee72a262f6d33a3d
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-21 11:09:24 -0400

    Time: 3 ms (41.33%) | Memory: 18.2 MB (21.44%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,40 +1,22 @@
-class Solution(object):
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
-        # Solution without external help
-        # Using monotonically decreasing stack approach
-        # res = [-1] * len(nums2)
-        # stack = []
-        # for index, num in enumerate(nums2):
-        #     while stack and stack[-1][0] < num:
-        #         stack_num, stack_idx = stack.pop()
-        #         res[stack_idx] = num
-        #     stack.append([num, index])
-        # output = []
-        # for num in nums1:
-        #     idx = nums2.index(num)
-        #     output.append(res[idx])
-        # return output
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []  #(num, index)
+        next_greater = defaultdict(int)
+        for i in range(len(nums2)):
+            curr_num = nums2[i]
+            while stack and stack[-1][0] < curr_num:
+                num, index = stack.pop()
+                next_greater[num] = curr_num
+            stack.append((curr_num, i))
 
-        # More optimal solution.# Instead of using the index() method O(n), to get index of an element in array, We can create a index map to lookup index in O(1)
-        index_map = {}
-        for index, num in enumerate(nums2):
-            index_map[num] = index
-        
-        next_greater = [-1] * len(nums2)
-        stack = []
-        for index, num in enumerate(nums2):
-            while stack and stack[-1][0] < num:
-                stack_num, stack_idx = stack.pop()
-                next_greater[stack_idx] = num
-            stack.append([num, index])
-        output = []
+        print(next_greater)
+        res = []
         for num in nums1:
-            output.append(next_greater[index_map[num]])
-        return output
-
+            print(num, next_greater[num])
+            if next_greater[num] > 0:
+                res.append(next_greater[num])
+            else:
+                res.append(-1)
         
+        return res
+                
