678a96155813d3cae9f0028f0a29fa266786f45d
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-22 08:36:15 -0400

    Time: 44 ms (93.69%) | Memory: 23 MB (34.53%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -3,12 +3,11 @@ class StockSpanner:
     def __init__(self):
         self.stack = []
         
-
     def next(self, price: int) -> int:
         curr_span = 1
         while self.stack and self.stack[-1][0] <= price:
             curr_span += self.stack.pop()[1]
-            
+
         self.stack.append((price, curr_span))
         return curr_span
 
