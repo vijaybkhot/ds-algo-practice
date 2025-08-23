b54a29f999486e4fd10d8a5bb13795b543b339bc
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-23 09:53:57 -0400

    Time: 89 ms (70.87%) | Memory: 30.9 MB (11.93%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.py
@@ -8,8 +8,6 @@ class Solution:
         #               ^
         #  0    1   2   3   4   5   6   7   8   9   10  11
         # [71,  18, 52, 29, 55, 73, 24, 42, 66, 8,  80, 2]
-
-
         for idx, num in enumerate(nums):
             while stack and stack[-1] > num and n-idx >= k-len(stack)+1:
                 stack.pop()
