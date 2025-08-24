4729b2a2332907a18cc6b62a6d0a6b543efd8d62
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-24 13:53:18 -0400

    Time: 116 ms (13.86%) | Memory: 32.9 MB (6.25%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
@@ -3,12 +3,6 @@ class Solution:
         n = len(heights)
         res = [0]*n
 
-        # (11, 4)
-        # 3,    1,  2,  1,  0,  0
-        #                       ^
-        # 10,   6,  8,  5,  11, 9
-
-
         stack = []
         for idx, num in enumerate(heights):
             while stack and stack[-1][0] < num:
