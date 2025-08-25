352656e6b2594aa2535c0b499114711175c231c8
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-25 16:30:22 -0400

    Time: 43 ms (13.04%) | Memory: 32 MB (5.34%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.py
@@ -1,26 +1,12 @@
+from typing import List
+
 class Solution:
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        
-        # @lru_cache(None)
-        # def dfs(left, right, curr_cards):
-        #     if curr_cards == k:
-        #         return 0
-        #     if left > right:
-        #         return float('-inf')
-            
-        #     # take left
-        #     max_score = cardPoints[left] + dfs(left+1, right, curr_cards+1)
-        #     # take right
-        #     max_score = max(max_score, cardPoints[right] + dfs(left, right-1, curr_cards+1))
-
-        #     return max_score
-        
-        # return dfs(0, len(cardPoints)-1, 0)
-        
         n = len(cardPoints)
         if n == k:
             return sum(cardPoints)
-       # build left prefix
+        
+        # build left prefix
         left_prefix = [0]
         for point in cardPoints:
             left_prefix.append(left_prefix[-1] + point)
@@ -31,12 +17,10 @@ class Solution:
             right_prefix[i] = right_prefix[i+1] + cardPoints[i]
         
         max_score = 0
-        print(left_prefix)
-        print(right_prefix)
+        # try taking i from left, k-i from right
         for i in range(k+1):
             left_score = left_prefix[i]
             right_score = right_prefix[n-(k-i)]
-            print(left_score+right_score)
-            max_score = max(max_score, left_score+right_score)
+            max_score = max(max_score, left_score + right_score)
         
         return max_score
