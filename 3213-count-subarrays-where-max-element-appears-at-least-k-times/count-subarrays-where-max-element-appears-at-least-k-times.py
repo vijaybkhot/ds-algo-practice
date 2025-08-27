c3e91b5872e7584d6616d68612f5671232de78e9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-27 07:30:07 -0400

    Time: 99 ms (44.87%) | Memory: 29.6 MB (78.13%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,38 +1,21 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        # # First attempt: Using a dictionary to track the frequency of each number in sub-array and tracking the frequency of max element
-        # freq_map = defaultdict(int)
-        # left = 0
-        # max_elem = max(nums)
-        # count = 0
-
-        # for right in range(len(nums)):
-        #     freq_map[nums[right]] += 1
-        #     while freq_map[max_elem] >= k:
-        #         count += (len(nums) - right)
-        #         freq_map[nums[left]] -= 1
-        #         left += 1
-        
-        # return count
-
-        # More optimized approach - No dictionary. Just keep track of frequency of max element in a counter
-
         left = 0
-        max_elem = max(nums)
-        count = 0
-        freq = 0
+        max_num = max(nums)
+        max_num_freq = 0
+
+        num_subarrays = 0
 
         for right in range(len(nums)):
-            if nums[right] == max_elem:
-                freq += 1
-            while freq >= k:
-                count += (len(nums) - right)
-                if nums[left] == max_elem:
-                    freq -= 1
+            num = nums[right]
+            if num == max_num:
+                max_num_freq += 1
+            
+            while max_num_freq >= k:
+                num_subarrays += len(nums) - right
+                if nums[left] == max_num:
+                    max_num_freq -= 1
                 left += 1
         
-        return count
-
-
-
+        return num_subarrays
         
