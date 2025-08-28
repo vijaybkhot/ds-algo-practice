cf9b8b07630f410a50d7780a929744853c1bbd34
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-28 10:43:29 -0400

    Time: 211 ms (37.36%) | Memory: 18.5 MB (36.19%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,27 +1,20 @@
+from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        len_p, len_s = len(p), len(s)
-        if len_p > len_s:
-            return []
-        anagrams = []
         left = 0
-        p_map, s_map = defaultdict(int), defaultdict(int)
-        for i in range(len_p):
-            p_map[p[i]] += 1
-            s_map[s[i]] += 1
-        left = 0
-        if p_map == s_map:
-            anagrams.append(left)
+        counter = Counter()
+        p_counter = Counter(p)
+
+        res = []
+
+        for right in range(len(s)):
+            counter[s[right]] += 1
+
+            while right-left+1 > len(p):
+                counter[s[left]] -= 1
+                left += 1
+            
+            if counter == p_counter:
+                res.append(left)
         
-        for right in range(len_p, len_s):
-            # remove left char from s_map
-            s_map[s[left]] -= 1
-            if s_map[s[left]] == 0:
-                del s_map[s[left]]
-            left += 1
-            # add right char to s_map
-            s_map[s[right]] += 1
-            if s_map == p_map:
-                anagrams.append(left)
-        
-        return anagrams
+        return res
