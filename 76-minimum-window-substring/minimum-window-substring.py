f611f7c2938bea4138f584ebf552d751a83bc45f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-28 09:48:17 -0400

    Time: 164 ms (21.86%) | Memory: 18.3 MB (34.19%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,36 +1,24 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s):
-            return ""
-        if not s or not t:
-            return ""
-        
-        t_map = Counter(t)
-        s_map = Counter()
-        required = len(t_map)
-        have = 0
-
         left = 0
-        res = [-1, -1]
-        len_res = float('inf')
+        counter = Counter()
+        t_counter = Counter(t)
+        curr_hold = 0
+        min_len = len(s)
+        min_str = ""
 
         for right in range(len(s)):
-            char = s[right]
-            if char in t_map:
-                s_map[char] += 1
-                if s_map[char] == t_map[char]:
-                    have += 1
-            
-            while have == required:
-                if right-left+1 < len_res:
-                    len_res = right-left+1
-                    res = [left,right]
-                left_char = s[left]
-                if left_char in s_map:
-                    s_map[left_char] -= 1
-                    if s_map[left_char] < t_map[left_char]:
-                        have -= 1
+            counter[s[right]] += 1
+            if counter[s[right]] == t_counter[s[right]]:
+                curr_hold += 1
+
+            while left <= right and  curr_hold >= len(t_counter):
+                if right - left + 1 <= min_len:
+                    min_len = right - left + 1
+                    min_str = s[left:right+1]
+                counter[s[left]] -= 1
+                if counter[s[left]] < t_counter[s[left]]:
+                    curr_hold -= 1
                 left += 1
         
-        l, r = res
-        return s[l:r+1] if len_res != float('inf') else ""
+        return min_str
