5ac6e7df43540e6791327744b892ae723be762b1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-28 10:19:31 -0400

    Time: 353 ms (7.30%) | Memory: 18 MB (87.06%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,24 +1,19 @@
-class Solution(object):
-    def characterReplacement(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: int
-        """
-        freq_map = {}
-        left = 0
-        max_freq = 0
-        max_length = 0
-
-        for right in range(len(s)):
-            freq_map[s[right]] = freq_map.get(s[right], 0) + 1 # Extend window
-            max_freq = max(max_freq, freq_map[s[right]])
-
-            while (right - left + 1) - max_freq > k:
-                freq_map[s[left]] -= 1
-                if freq_map[s[left]] == max_freq:
-                    max_freq = max(freq_map.values())
-                left += 1 # Shrink window until a valid substring remains
-            max_length = max(max_length, (right - left + 1))
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        counter = Counter(s)
+        res = 0
+        for char in counter:
+            left = 0
+            non_char = 0
+            for right in range(len(s)):
+                if s[right] != char:
+                    non_char += 1
+                
+                while left <= right and non_char > k:
+                    if s[left] != char:
+                        non_char -= 1
+                    left += 1
+                
+                res = max(res, right-left+1)
         
-        return max_length
+        return res
