b26bed7b0f84cf8690c013f298dd2b443284ecab
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-29 08:35:53 -0400

    Time: 293 ms (16.26%) | Memory: 28.8 MB (54.06%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.py
+++ 1813-maximum-erasure-value/maximum-erasure-value.py
@@ -1,31 +1,20 @@
 class Solution:
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        res = 0
-        curr_set = set()
-        curr_sum, max_sum = 0, 0
-        res_left, res_right = 0, 0
-
+        curr_sum = 0
         left = 0
-        for right in range(len(nums)):
-            if nums[right] not in curr_set:
-                curr_sum += nums[right]
-                curr_set.add(nums[right])
-                if curr_sum > max_sum:
-                    max_sum = curr_sum
-                    res_left = left
-                    res_right = right
-            else:
-                while left < right and nums[right] in curr_set:
-                    curr_set.remove(nums[left])
-                    curr_sum -= nums[left]
-                    left += 1
-                curr_sum += nums[right]
-                curr_set.add(nums[right])
-                if curr_sum > max_sum:
-                    max_sum = curr_sum
-                    res_left = left
-                    res_right = right
-                
-        
-        return max_sum
+        curr_map = defaultdict(int)
+        res = 0
 
+        for right in range(len(nums)):
+            curr_map[nums[right]] += 1
+            curr_sum += nums[right]
+            while left <= right and curr_map[nums[right]] > 1:
+                curr_map[nums[left]] -= 1
+                curr_sum -= nums[left]
+                left += 1
+            
+            res = max(res, curr_sum)
+        
+        return res
+
+            
