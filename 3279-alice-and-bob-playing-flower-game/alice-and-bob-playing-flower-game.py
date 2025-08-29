aac3d683b6dd91cbd51610e4f9d18c226f8635c2
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-29 07:56:27 -0400

    Time: 1 ms (15.97%) | Memory: 17.8 MB (43.75%) - LeetSync

---
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.py
@@ -0,0 +1,15 @@
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        # 4, 4
+        # 4, 4
+        # 1,2/ 2,1/ 2,3/ 3,2/ 3,4/ 4,3/ 
+        n_odd = math.ceil(n/2)
+        n_even = n//2
+        m_odd = math.ceil(m/2)
+        m_even = m//2
+
+        print( n_odd, n_even,
+        m_odd,
+        m_even)
+
+        return (n_odd*m_even + n_even*m_odd)
