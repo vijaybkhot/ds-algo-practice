86afadb116567ebb0e10441731aea19e925eb79b
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-29 08:01:24 -0400

    Time: 278 ms (7.79%) | Memory: 23.5 MB (80.10%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,15 +1,17 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        res = 0 
+        curr_fruit_map = Counter()
         left = 0
-        curr_dict = dict()    
+        res = 0
+        
         for right in range(len(fruits)):
-            curr_dict[fruits[right]] = curr_dict.get(fruits[right], 0) + 1
-            while left < right and len(curr_dict) > 2:
-                curr_dict[fruits[left]] -= 1
-                if curr_dict[fruits[left]] == 0:
-                    del curr_dict[fruits[left]]
+            curr_fruit_map[fruits[right]] += 1
+            while left <= right and len(curr_fruit_map) > 2:
+                curr_fruit_map[fruits[left]] -= 1
+                if curr_fruit_map[fruits[left]] == 0:
+                    del curr_fruit_map[fruits[left]]
                 left += 1
-            res = max(res, right-left+1)
-        return res
             
+            res = max(res, right-left+1)
+        
+        return res
