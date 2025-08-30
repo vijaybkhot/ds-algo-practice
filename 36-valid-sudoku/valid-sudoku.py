ac750b82254db23bdfd9f95e8a481d7939e1c801
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-30 07:36:41 -0400

    Time: 4 ms (58.73%) | Memory: 18.2 MB (5.46%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,23 +1,22 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row_set = [set() for _ in range(9)]     # [{8, 0, 7}, {6,1}, {}, {}, {}, {}, {}, {}, {}]
-        col_set = [set() for _ in range(9)]     # [{8, 6}, {0}, {}, {1}, {7}, {}, {}, {}, {}]
-        box_set = [set() for _ in range(9)]     # [{8, 0, 6}, {7, 1}, {}, {}, {}, {}, {}, {}, {}]
+        row_set = [set() for _ in range(9)]
+        col_set = [set() for _ in range(9)]
+        box_set = [set() for _ in range(9)]
 
+        # r//3 + c/3
 
         for r in range(9):
             for c in range(9):
-                #r: 1, c: 4
-                curr_num = board[r][c]  # "1"
-                if curr_num == '.':
-                    continue
-                box_index = ((r//3)*3) + (c//3) # 1
-
-                if curr_num in row_set[r] or curr_num in col_set[c] or curr_num in box_set[box_index]:
-                    return False
-                row_set[r].add(curr_num)
-                col_set[c].add(curr_num)
-                box_set[box_index].add(curr_num)
-
+                if board[r][c] != '.':
+                    curr_num = board[r][c]
+                    box_idx = ((r//3)*3) + int(c/3)
+                    print(r//3, int(c/3), box_idx)
+                    if curr_num in row_set[r] or curr_num in col_set[c] or curr_num in box_set[box_idx]:
+                        return False
+                    
+                    row_set[r].add(curr_num)
+                    col_set[c].add(curr_num)
+                    box_set[box_idx].add(curr_num)
+        
         return True
-
