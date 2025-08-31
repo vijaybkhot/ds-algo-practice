1a420e5c6fe11b243911771be202be961cb069d4
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-31 14:45:31 -0400

    Time: 242 ms (66.34%) | Memory: 20 MB (95.81%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,6 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        # directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         rows, cols = len(grid), len(grid[0])
 
         def bfs(r, c):
@@ -10,7 +10,7 @@ class Solution:
             while q:
                 row, col = q.popleft()
 
-                for dr, dc in directions:
+                for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
                     nr, nc = row+dr, col+dc
                     if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == "1":
                         grid[nr][nc] = "0"
