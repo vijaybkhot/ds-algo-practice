546d435e1fd6229c20205779abb0232f5ffe36cf
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-31 10:31:52 -0400

    Time: 38 ms (14.66%) | Memory: 21.2 MB (35.50%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.py
@@ -1,48 +1,16 @@
-class Solution(object):
-    def subarraysDivByK(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
-        # # This solution was developed independently by me without external help.
-        # # edge cases
-        # if not nums:
-        #     return 0
-        # if len(nums)== 1:
-        #     if nums[0] % k != 0:
-        #         return 0
-        #     else:
-        #         return 1
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
         
-        # res = 0
-        # # Remainder frequency hash map
-        # remainder = {}
-        # prefix = 0
-        # for index, num in enumerate(nums, start=0):
-        #     prefix += num
-        #     curr_remainder = prefix % k
-        #     remainder.setdefault(curr_remainder, []).append(index)
-        #     res += len(remainder[curr_remainder]) - 1 if curr_remainder !=0 else len(remainder[curr_remainder])
-                    
-        # return res
+        remainder_count = defaultdict(int)
+        remainder_count[0] = 1
+
+        curr_sum = 0
+        num_subarrays = 0
+
+        for idx, num in enumerate(nums):
+            curr_sum += num
+            if curr_sum % k in remainder_count:
+                num_subarrays += remainder_count[curr_sum % k]
+            remainder_count[curr_sum % k] += 1
         
-        # More optimal solution
-
-        res = 0
-        remainder_count = {0: 1}  # To count the number of times a remainder has appeared
-        prefix = 0
-        for num in nums:
-            prefix += num
-            curr_remainder = prefix % k
-            if curr_remainder < 0:
-                curr_remainder += k
-            # If the remainder has appeared before, it means there are `remainder_count[curr_remainder]` subarrays ending here that are divisible by k
-            res += remainder_count.get(curr_remainder, 0)
-            
-            # Update the count of this remainder
-            remainder_count[curr_remainder] = remainder_count.get(curr_remainder, 0) + 1
-        
-        return res
-
-
+        return num_subarrays
