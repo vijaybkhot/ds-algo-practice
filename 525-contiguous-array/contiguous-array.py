c8d0a52671176f2c66d918024e8a80015e36fe0a
Vijay Khot <vijaysinh.khot@gmail.com>
2025-08-31 14:10:00 -0400

    Time: 127 ms (11.67%) | Memory: 23.4 MB (51.38%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        # [0,   1,  0,  1]
+        #  1,   1,  2,  2
+        #   0,  1,  1,  2
+        #   1,  0,  
+
+        diff_map = defaultdict(int)
+        diff_map[0] = -1
+        zero, one = 0, 0
+        res = 0
+        for idx, num in enumerate(nums):
+            if num:
+                one += 1
+            else:
+                zero += 1
+            diff = zero-one
+
+            if diff in diff_map:
+                res = max(res, idx - diff_map[diff])
+            if diff not in diff_map:
+                diff_map[diff] = idx
+        
+        return res
