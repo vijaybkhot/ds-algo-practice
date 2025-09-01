25769d66d3234792c306054483d56d2ae6e299a1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-01 06:20:41 -0400

    Time: 51 ms (65.53%) | Memory: 38 MB (74.92%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,23 +1,21 @@
-class Solution(object):
-    def checkSubarraySum(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
-        # Edge cases
-        if len(nums) < 2:
-            return False
-            
-        # Creating a hash map to store remainders
-        hash_map = {0: -1}
-        prefix = 0
-        for i in range(len(nums)):
-            prefix = prefix + nums[i]
-            curr_remainder = prefix % k
-            if curr_remainder not in hash_map:
-                hash_map[curr_remainder] = i
-            elif i - hash_map[curr_remainder] >= 2:
-                return True 
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        # 23,   2,  6,  4,  7
+        # 23,   25, 31, 35, 42
+        # 5:0, 1:1, 
+        prefix_map = {0: -1} 
+        prefix_map[0] = -1
+        curr_sum = 0
+
+        for i, num in enumerate(nums):
+            curr_sum += num
+            rem = curr_sum % k
+
+            if rem in prefix_map:
+                if i - prefix_map[rem] >= 2:
+                    return True
+            else:
+                prefix_map[rem] = i
 
         return False
+        
