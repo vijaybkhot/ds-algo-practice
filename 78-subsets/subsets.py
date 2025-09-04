0ee5c020e486de3d1a633e80a192a4b8ec958ed5
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-04 11:33:20 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (21.72%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,83 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         self.res = []
-        def dfs(index, path):
-            # if i == len(nums):
-            #     self.res.append(path)
-            #     return
+        def dfs(i, curr_set):
+            if i == len(nums):
+                self.res.append(curr_set[::])
+                return
             
-            # # Include current Num:
-            # dfs(i + 1, path + [nums[i]])
-            # # skip current num
-            # dfs(i+1, path)
-            # return
-            self.res.append(path[::])
-
-            for i in range(index, len(nums)):
-                path.append(nums[i])
-                dfs(i+1, path)
-                path.pop()
+            curr_set.append(nums[i])
+            dfs(i+1, curr_set)
+            curr_set.pop()
+            dfs(i+1, curr_set)
         
         dfs(0, [])
+
         return self.res
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # # def dfs(index, output):
-        # #     if index == len(nums):
-        # #         result.append(output)
-        # #         return
-        # #     include_subset = output + [nums[index]]
-        # #     exclude_subset = output
-        # #     dfs(index + 1, include_subset)
-        # #     dfs(index + 1, exclude_subset)
-
-        # # result = []
-        # # dfs(0, [])
-        # # return result
-
-        # # Solution using a for loop in dfs function
-
-        # # res = []
-        # # def dfs(start, path):
-        # #     res.append(path[:])
-
-        # #     for i in range(start, len(nums)):
-        # #         path.append(nums[i])
-        # #         dfs(i+1, path)
-        # #         path.pop()
-        # # dfs(0, [])
-        # # return res
-
-        # # # Solution using BFS
-        # # q = deque([[]])
-        # # for num in nums:
-        # #     for _ in range(len(q)):
-        # #         curr_path = q.popleft()
-        # #         q.append(curr_path + [num])
-        # #         q.append(curr_path)
-        
-        # # return list(q)
-
-        # res = []
-
-        # def dfs(index, path):
-        #     res.append(path[:])
-        #     for i in range(index, len(nums)):
-        #         path.append(nums[i])
-        #         dfs(i + 1, path)
-        #         path.pop()
-
-    
