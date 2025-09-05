ccc06e386a352513a32cf00bafd3f9fb06eb65b6
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-05 07:04:11 -0400

    Time: 12 ms (18.49%) | Memory: 20.4 MB (5.47%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,37 +1,29 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        self.parent = [-1] * len(isConnected)
-
-        def find(x):
-            if self.parent[x] < 0:
-                return x
-            self.parent[x] = find(self.parent[x])
-            return self.parent[x]
         
-        def union(x, y):
-            if x == y:
+        graph = defaultdict(set)
+        n = len(isConnected)
+
+        for i in range(n):
+            for j in range(n):
+                if i != j and isConnected[i][j] == 1:
+                    graph[i].add(j)
+        
+        visited = set()
+        def dfs(node):
+            if node in visited:
                 return
-            rootX = find(x)
-            rootY = find(y)
-            if rootX == rootY:
-                return False
-            if self.parent[rootX] > self.parent[rootY]:
-                rootX, rootY = rootY, rootX
-            self.parent[rootX] += self.parent[rootY]  
-            self.parent[rootY] = rootX
-
-            return True
+            visited.add(node)
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei)
         
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected[0])):
-                if isConnected[i][j] == 1:
-                    union(i, j)
-        
-        provinces = 0
-        for province in self.parent:
-            if province < 0:
-                provinces += 1
-        return provinces
+        num_components = 0
 
-
+        for i in range(n):
+            if i not in visited:
+                num_components += 1
+                dfs(i)
+        return num_components
         
+
