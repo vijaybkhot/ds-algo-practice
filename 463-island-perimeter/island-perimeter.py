c665d0a5b7f5dc1a65f959fb4556fd17cf63b076
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-06 12:57:40 -0400

    Time: 88 ms (46.46%) | Memory: 19.2 MB (33.67%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -2,23 +2,22 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         directions = [(1,0), (0,1), (-1,0), (0,-1)]
-        visited = set()
 
         def dfs(r, c):
             total = 0
             for dr, dc in directions:
                 nr, nc = r+dr, c+dc
                 if nr < 0 or nr >= rows or nc < 0 or nc >= cols:
-                    total += 1   # edge touches boundary
+                    total += 1   # touches boundary
                 elif grid[nr][nc] == 0:
-                    total += 1   # edge touches water
-                elif (nr, nc) not in visited:
-                    visited.add((nr, nc))
+                    total += 1   # touches water
+                elif grid[nr][nc] == 1:
+                    grid[nr][nc] = -1   # mark visited
                     total += dfs(nr, nc)
             return total
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    visited.add((r, c))
+                    grid[r][c] = -1
                     return dfs(r, c)
