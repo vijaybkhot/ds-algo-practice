c39a9206ba1b4cb90a7c5580fab9049d2b957f6d
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-07 14:52:37 -0400

    Time: 1314 ms (0.00%) | Memory: 20.4 MB (0.00%) - LeetSync

--- 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.py
+++ 3999-minimum-operations-to-transform-string/minimum-operations-to-transform-string.py
@@ -1,15 +1,21 @@
 class Solution:
     def minOperations(self, s: str) -> int:
         
-        min_char = 26
-        for char in s:
-            if char == 'a':
-                continue
-            min_char = min(min_char, ord(char) - ord('a'))
+        # min_char = 26
+        # for char in s:
+        #     if char == 'a':
+        #         continue
+        #     min_char = min(min_char, ord(char) - ord('a'))
         
-        if min_char == 26:
-            return 0
+        # if min_char == 26:
+        #     return 0
         
-        num_operations = 26 - min_char
+        # num_operations = 26 - min_char
 
-        return num_operations if min_char else 0
+        # return num_operations if min_char else 0
+
+        max_ops = 0
+        for ch in s:
+            dist = (26 - (ord(ch) - ord('a'))) % 26
+            max_ops = max(max_ops, dist)
+        return max_ops
