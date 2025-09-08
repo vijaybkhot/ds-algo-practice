50a3c3169526f89f7f827182c4171dfafbc25aaa
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-08 07:42:34 -0400

    Time: 39 ms (40.24%) | Memory: 19.3 MB (64.70%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -7,24 +7,39 @@ class Solution:
         pacific = set()
         atlantic = set()
 
-        def dfs(row, col, curr_set):
-            if (row, col) in curr_set:
-                return 
-            curr_set.add((row, col))
+        # def dfs(row, col, curr_set):
+        #     if (row, col) in curr_set:
+        #         return 
+        #     curr_set.add((row, col))
 
-            for dr, dc in directions:
-                nr, nc = dr+row, dc+col
-                if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in curr_set and heights[nr][nc] >= heights[row][col]:
-                    dfs(nr, nc, curr_set)
+        #     for dr, dc in directions:
+        #         nr, nc = dr+row, dc+col
+        #         if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in curr_set and heights[nr][nc] >= heights[row][col]:
+        #             dfs(nr, nc, curr_set)
+
+        def bfs(row, col, curr_set):
+            if (row, col) in curr_set:
+                return
+            
+            curr_set.add((row, col))
+            q = deque()
+            q.append((row, col))
+            while q:
+                r, c = q.popleft()
+                for dr, dc in directions:
+                    nr, nc = dr+r, dc+c
+                    if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) not in curr_set and heights[nr][nc] >= heights[r][c]:
+                        q.append((nr, nc))
+                        curr_set.add((nr, nc))
 
 
         for r in range(rows):
             for c in range(cols):
                 if (r == 0 or c == 0) and (r, c) not in pacific:
-                    dfs(r, c, pacific)
+                    bfs(r, c, pacific)
                 
                 if (r == rows-1 or c == cols-1) and (r, c) not in atlantic:
-                    dfs(r, c, atlantic)
+                    bfs(r, c, atlantic)
 
         res = []
         for r in range(rows):
