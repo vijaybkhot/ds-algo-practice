8e54774f8b03f06ba51cada9d55b5e794077c6e9
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-11 06:36:43 -0400

    Time: 73 ms (78.14%) | Memory: 19.9 MB (88.31%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.py
@@ -2,15 +2,28 @@ class Solution:
     def sortVowels(self, s: str) -> str:
         s_map = Counter(s)
         vowel_count = [[char, count] for char, count in s_map.items() if char in "aeiouAEIOU"]
-        vowel_count.sort(key=lambda x: x[0], reverse=True)
+        # vowel_count.sort(key=lambda x: x[0], reverse=True)
+        sorted_vowels = [['A', 0], ['E', 0], ['I', 0], ['O', 0], ['U', 0], ['a', 0], ['e', 0], ['i', 0], ['o', 0], ['u', 0]]
+        for char, count in vowel_count:
+            for i in range(len(sorted_vowels)):
+                if sorted_vowels[i][0] == char:
+                    sorted_vowels[i][1] = count
+                    break
+        
+
 
         res = [char for char in s]
+        vowel_pointer = 0
         for i in range(len(res)):
             if res[i] in "AEIOUaeiou":
-                res[i] = vowel_count[-1][0]
-                vowel_count[-1][1] -= 1
-                if vowel_count[-1][1] == 0:
-                    vowel_count.pop()
+                while sorted_vowels[vowel_pointer][1] == 0:
+                    vowel_pointer += 1
+                
+                res[i] = sorted_vowels[vowel_pointer][0]
+                sorted_vowels[vowel_pointer][1] -= 1
+                # vowel_count[-1][1] -= 1
+                # if vowel_count[-1][1] == 0:
+                #     vowel_count.pop()
 
         return ''.join(res)
 
