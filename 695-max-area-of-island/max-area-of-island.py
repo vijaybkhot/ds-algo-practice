169242e7bc1a80b2c700294f1dc71ecf1f3260d6
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-11 07:34:20 -0400

    Time: 17 ms (74.71%) | Memory: 18.6 MB (33.17%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -2,81 +2,30 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        visited = set()
-        max_area = 0
 
-        def bfs(i, j):
-            curr_area = 0
-            q = deque()
-            q.append((i, j))
-            visited.add((i, j))
-
-            while q:
-                r, c = q.popleft()
-                curr_area += 1
+        def dfs_helper(row, col):
+            
+            def dfs(r, c):
+                nonlocal count
                 for dr, dc in directions:
-                    m, n = r+dr, c+dc
-                    if 0 <= m < rows and 0 <= n < cols and (m, n) not in visited and grid[m][n] == 1:
-                        q.append((m, n))
-                        visited.add((m, n))
-            return curr_area
+                    nr, nc = r+dr, c+dc
+
+                    if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                        grid[nr][nc] = -1
+                        count += 1
+                        dfs(nr, nc)
+
+            grid[row][col] = -1
+            count = 1
+            dfs(row, col)
+            return count
         
-        for row in range(rows):
-            for col in range(cols):
-                if (row, col) not in visited and grid[row][col]==1:
-                    max_area = max(max_area, bfs(row, col))
+        res = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    res = max(res, dfs_helper(r, c))
         
-        return max_area
+        return res
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#         max_area = 0
-
-#         visited = set()
-
-#         def dfs(pos):
-#             i, j = pos
-#             if grid[i][j] == 0:
-#                 return 0
-
-#             visited.add((i, j))
-#             area = 1
-            
-#             top = (i-1, j) if i-1 > -1 else None
-#             bottom = (i+1, j) if i+1 < len(grid) else None
-#             left = (i, j-1) if j-1 > -1 else None
-#             right = (i, j+1) if j+1 < len(grid[0]) else None
-
-#             top_area, bottom_area, left_area, right_area = 0, 0, 0, 0
-            
-#             if top and top not in visited:
-#                 top_area = dfs(top)
-#             if bottom and bottom not in visited:
-#                 bottom_area = dfs(bottom)
-#             if left and left not in visited:
-#                 left_area = dfs(left)
-#             if right and right not in visited:
-#                 right_area = dfs(right)
-            
-#             return area + top_area + bottom_area + left_area + right_area
-
-
-#         for r in range(len(grid)):
-#             for c in range(len(grid[0])):
-#                 if grid[r][c] == 1 and (r, c) not in visited:
-#                     area = dfs((r, c))
-#                     max_area = max(max_area, area)
-        
-#         return max_area
