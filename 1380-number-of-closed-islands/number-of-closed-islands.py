213ac39ff90f626ca9f73c79daa0ffe0a5013fab
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-11 07:23:51 -0400

    Time: 15 ms (76.45%) | Memory: 18.6 MB (35.59%) - LeetSync

--- 1380-number-of-closed-islands/number-of-closed-islands.py
+++ 1380-number-of-closed-islands/number-of-closed-islands.py
@@ -4,27 +4,45 @@ class Solution:
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         res = 0
 
-        def bfs(r, c):
-            q = deque()
-            q.append((r, c))
-            grid[r][c] = -1
-            closed_island = True
-            while q:
-                row, col = q.popleft()
-                if row == rows-1 or row == 0 or col == cols-1 or col == 0:
-                    closed_island = False
+        # def bfs(r, c):
+        #     q = deque()
+        #     q.append((r, c))
+        #     grid[r][c] = -1
+        #     closed_island = True
+        #     while q:
+        #         row, col = q.popleft()
+        #         if row == rows-1 or row == 0 or col == cols-1 or col == 0:
+        #             closed_island = False
                 
+        #         for dr, dc in directions:
+        #             nr, nc = row+dr, col+dc
+        #             if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 0:
+        #                 q.append((nr, nc))
+        #                 grid[nr][nc] = -1
+
+        #     return 1 if closed_island else 0
+
+        def dfs_helper(r, c):
+            closed = True
+            
+            def dfs(row, col):
+                nonlocal closed
+                if row == rows-1 or col == cols-1 or row == 0 or col == 0:
+                    closed = False
                 for dr, dc in directions:
                     nr, nc = row+dr, col+dc
                     if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 0:
-                        q.append((nr, nc))
                         grid[nr][nc] = -1
+                        dfs(nr, nc)
+            grid[r][c] = -1
+            dfs(r, c)
 
-            return 1 if closed_island else 0
+            return 1 if closed else 0
+
         
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 0:
-                    res += bfs(r, c)
+                    res += dfs_helper(r, c)
         
         return res
