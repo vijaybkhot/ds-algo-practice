db4a691a6400fbf14b8347534f18545bae4ef707
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-12 10:47:15 -0400

    Time: 39 ms (14.78%) | Memory: 17.9 MB (84.18%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.py
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -1,46 +1,45 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-
-       
         n = len(board)
-
-        def get_coordinates(pos):
-            row = n - 1 - (pos - 1) // n
-            col_offset = (pos - 1) % n
-
-            if (n - 1 - row) % 2 == 0:  # even row index from bottom → left to right
-                col = col_offset
-            else:  # odd row index from bottom → right to left
-                col = n - 1 - col_offset
-
+        
+        def get_row_col(num):
+            row = n - (((num-1) // n)+1)
+            col = 0
+            if (n-1)%2 == 0:
+                if row % 2 == 0:
+                    col = (num-1)%n
+                else:
+                    col = n - 1 - (num-1)%n
+            else:
+                if row % 2 != 0:
+                    col = (num-1)%n
+                else:
+                    col = n - 1 - (num-1)%n
             return (row, col)
 
-        # Edge case: if starting cell has a ladder/snake
-        if board[n - 1][0] != -1:
-            return -1
+                    
+        
 
         q = deque()
-        q.append((1, 0))  # (square_number, num_rolls)
-        visited = set()
-        visited.add(1)
-
+        q.append((1, 0))
+        dest = n*n
+        visited = set([1])
         while q:
-            pos, num_rolls = q.popleft()
-            if pos == n * n:
-                return num_rolls
+            curr, moves = q.popleft()
+            if curr == dest:
+                return moves
+            
             for i in range(1, 7):
-                next_pos = pos + i
-                if next_pos > n * n:
+                nxt = curr+i
+                if nxt > n*n:
                     continue
-                row, col = get_coordinates(next_pos)
-                dest = board[row][col] if board[row][col] != -1 else next_pos
-                if dest not in visited:
-                    visited.add(dest)
-                    q.append((dest, num_rolls + 1))
-
+                # get row, col for the number
+                row, col = get_row_col(nxt)
+                target = nxt if board[row][col] == -1 else board[row][col]
+                if target not in visited:
+                   visited.add(target)
+                   q.append((target, moves+1))
+                
         return -1
+            
         
-            
-                
-                
-            
