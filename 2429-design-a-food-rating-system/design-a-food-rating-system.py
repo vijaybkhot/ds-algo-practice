ab0bb7a0936db65344bd8e984be20c397a576d9f
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-17 07:36:14 -0400

    Time: 250 ms (31.13%) | Memory: 50.4 MB (95.51%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.py
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.py
@@ -1,26 +1,20 @@
-import heapq
+from sortedcontainers import SortedList
 from collections import defaultdict
 
 class FoodRatings:
     def __init__(self, foods, cuisines, ratings):
-        self.food_rating_map = {}   # food -> (rating, cuisine)
-        self.cuisine_q = defaultdict(list)
+        self.food_rating_map = {}
+        self.cuisine_map = defaultdict(lambda: SortedList())
 
         for f, c, r in zip(foods, cuisines, ratings):
             self.food_rating_map[f] = [r, c]
-            heapq.heappush(self.cuisine_q[c], (-r, f))
+            self.cuisine_map[c].add((-r, f))
 
     def changeRating(self, food: str, newRating: int) -> None:
         oldRating, cuisine = self.food_rating_map[food]
+        self.cuisine_map[cuisine].remove((-oldRating, food))
+        self.cuisine_map[cuisine].add((-newRating, food))
         self.food_rating_map[food][0] = newRating
-        # push new rating
-        heapq.heappush(self.cuisine_q[cuisine], (-newRating, food))
 
     def highestRated(self, cuisine: str) -> str:
-        # pop until valid
-        heap = self.cuisine_q[cuisine]
-        while heap:
-            rating, food = heap[0]
-            if -rating == self.food_rating_map[food][0]:
-                return food   # ✅ valid top
-            heapq.heappop(heap)   # discard stale
+        return self.cuisine_map[cuisine][0][1]
