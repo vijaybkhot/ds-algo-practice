2a992f47ae70707e7cdda66500b7293ed8e73228
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-20 08:25:48 -0400

    Time: 308 ms (58.92%) | Memory: 89.9 MB (17.74%) - LeetSync

--- 3827-implement-router/implement-router.py
+++ 3827-implement-router/implement-router.py
@@ -1,51 +1,56 @@
 class Router:
+
     def __init__(self, memoryLimit: int):
-        self.size = memoryLimit
-        self.packets = {}  # key -> [source, destination, timestamp]
-        self.counts = defaultdict(list)  # destination -> sorted list of timestamps
-        self.queue = deque()  # FIFO order of packets
+        self.size = 0
+        self.memoryLimit = memoryLimit
+        self.time_queue = deque([])
+        self.time_set = defaultdict(set)
+        self.destination_map = defaultdict(list)
 
     def addPacket(self, source: int, destination: int, timestamp: int) -> bool:
-        key = self._encode(source, destination, timestamp)
-
-        # Duplicate check
-        if key in self.packets:
+        if timestamp in self.time_set and (source, destination) in self.time_set[timestamp]:
             return False
+        elif self.size == self.memoryLimit:
+            # Remove the oldest packer
+            self.removeOldest()
 
-        # If memory full, forward oldest packet
-        if len(self.packets) >= self.size:
-            self.forwardPacket()
-
-        # Add packet
-        self.packets[key] = [source, destination, timestamp]
-        self.queue.append(key)
-        self.counts[destination].append(timestamp)
-
+        self.time_set[timestamp].add((source, destination))
+        self.size += 1
+        self.time_queue.append((source, destination, timestamp))
+        self.destination_map[destination].append(timestamp)
         return True
 
-    def forwardPacket(self):
-        if not self.packets:
+    def forwardPacket(self) -> List[int]:
+        if self.size == 0:
             return []
-
-        key = self.queue.popleft()
-        packet = self.packets.pop(key)
-
-        dest = packet[1]
-        self.counts[dest].pop(0)  # remove the earliest timestamp
-
-        return packet
+        return self.removeOldest()
+    
+    def removeOldest(self) -> List[int]:
+        source, destination, oldest_time = self.time_queue.popleft()
+        self.time_set[oldest_time].remove((source, destination))
+        self.destination_map[destination].pop(0)
+        self.size -= 1
+        return [source, destination, oldest_time]
+        
 
     def getCount(self, destination: int, startTime: int, endTime: int) -> int:
-        timestamps = self.counts.get(destination, [])
-        if not timestamps:
-            return 0
+        packets = 0
+        # for i in range(startTime, endTime+1):
+        #     if i in self.time_set:
+        #         for s, d in self.time_set[i]:
+        #             if d == destination:
+        #                 packets += 1
+        # for source, timestamp in self.destination_map[destination]:
+        #     if startTime <= timestamp <= endTime:
+        #         packets += 1  
+        destinations = self.destination_map[destination]
+        startIndex = bisect_left(destinations, startTime)
+        endIndex = bisect_right(destinations, endTime)
+        return endIndex-startIndex
 
-        # Binary search for range
-        left = bisect.bisect_left(timestamps, startTime)
-        right = bisect.bisect_right(timestamps, endTime)
 
-        return right - left
-
-    def _encode(self, source: int, destination: int, timestamp: int) -> int:
-        # Encode uniquely into 1 number
-        return (source << 40) | (destination << 20) | timestamp
+# Your Router object will be instantiated and called as such:
+# obj = Router(memoryLimit)
+# param_1 = obj.addPacket(source,destination,timestamp)
+# param_2 = obj.forwardPacket()
+# param_3 = obj.getCount(destination,startTime,endTime)
