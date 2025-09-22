3ff05aadd2108c8ed5cd0202414ceff38662f224
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-22 10:32:38 -0400

    Time: 3 ms (8.67%) | Memory: 18 MB (20.87%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.py
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -1,34 +1,33 @@
-from collections import deque
-from typing import List
-
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
         bank = set(bank)
-        if startGene == endGene:
-            return 0
-        if endGene not in bank:
-            return -1
-
         q = deque([(startGene, 0)])
-        visited = {startGene}
+        visited = set(startGene)
 
         def getMutations(gene):
             mutations = []
-            for idx, ch in enumerate(gene):
+            for idx, char in enumerate(gene):
                 for new_char in "ACGT":
-                    if ch == new_char:
+                    if char == new_char:
                         continue
-                    new_gene = gene[:idx] + new_char + gene[idx+1:]
+                    new_gene = [char for char in gene]
+                    new_gene[idx] = new_char
+                    new_gene = ''.join(new_gene)
                     if new_gene in bank and new_gene not in visited:
-                        visited.add(new_gene)      # mark when discovered
+                        visited.add(new_gene)
                         mutations.append(new_gene)
+                    
             return mutations
+        
 
         while q:
             curr_gene, level = q.popleft()
             if curr_gene == endGene:
                 return level
-            for m in getMutations(curr_gene):
-                q.append((m, level + 1))
+            
+            mutations = getMutations(curr_gene)
 
+            for mutation in mutations:
+                q.append((mutation, level+1))
+        
         return -1
