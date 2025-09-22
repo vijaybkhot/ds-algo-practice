7d28a8c91f94c6bf5238303e41139c2515010985
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-22 10:25:02 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (39.84%) - LeetSync

--- 433-minimum-genetic-mutation/minimum-genetic-mutation.py
+++ 433-minimum-genetic-mutation/minimum-genetic-mutation.py
@@ -1,31 +1,33 @@
 class Solution:
     def minMutation(self, startGene: str, endGene: str, bank: List[str]) -> int:
-        bank_set = set(bank)
-       
+        bank = set(bank)
+        q = deque([(startGene, 0)])
+        visited = set(startGene)
 
-        q = deque()
-        q.append((startGene, 0))    # Gene, mutations
-        visited = set()
-        visited.add(startGene)
+        def getMutations(gene):
+            mutations = []
+            for idx, char in enumerate(gene):
+                for new_char in "ACGT":
+                    if char == new_char:
+                        continue
+                    new_gene = [char for char in gene]
+                    new_gene[idx] = new_char
+                    new_gene = ''.join(new_gene)
+                    if new_gene in bank and new_gene not in visited:
+                        visited.add(new_gene)
+                        mutations.append(new_gene)
+                    
+            return mutations
+        
 
         while q:
-            gene, mutation = q.popleft()
-            if gene == endGene:
-                return mutation
+            curr_gene, level = q.popleft()
+            if curr_gene == endGene:
+                return level
             
-            for idx in range(len(gene)):
-                new_gene = [char for char in gene]
-                char_set = set(["A", "C", "G", "T"])
-                curr_char = new_gene[idx]
-                char_set.remove(curr_char)
-                for new_char in char_set:
-                    new_gene[idx] = new_char
-                    new_gene_str = ''.join(new_gene)
-                    print(new_gene_str)
-                    if new_gene_str in bank_set and new_gene_str not in visited:
-                        print(new_gene_str)
-                        visited.add(new_gene_str)
-                        q.append((new_gene_str, mutation+1))
+            mutations = getMutations(curr_gene)
+
+            for mutation in mutations:
+                q.append((mutation, level+1))
         
         return -1
-
