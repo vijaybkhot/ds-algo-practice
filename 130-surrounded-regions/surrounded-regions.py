a2274ab3c0db67b18754b57bed5fb731440d6722
Vijay Khot <vijaysinh.khot@gmail.com>
2025-09-26 08:15:54 -0400

    Time: 6 ms (60.98%) | Memory: 21.8 MB (42.62%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,28 +3,32 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+
         rows, cols = len(board), len(board[0])
-        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
-
-        def dfs(row, col):
-            board[row][col] = "M"
+        def dfs(r, c, mark):
+            board[r][c] = mark
             for dr, dc in directions:
-                r, c = row+dr, col+dc
-                if 0 <= r < rows and 0 <= c < cols and board[r][c] == "O":
-                    # board[r][c] = "O"
-                    dfs(r, c)
-        
+                nr, nc = r+dr, c+dc
+                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == "O":
+                    dfs(nr, nc, mark)
+
+
+
+        # Get uncaptured border regions
         for r in range(rows):
             for c in range(cols):
-                if (r in (0, rows-1) or c in (0, cols-1)) and board[r][c] == "O":
-                    dfs(r, c)
+                if r == 0 or c == 0 or r == rows-1 or c == cols-1:
+                    if board[r][c] == "O":
+                        dfs(r, c, "N")
         
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "N":
+                    board[r][c] = "O"
         
-        for i in range(rows):
-            for j in range(cols):
-                if board[i][j] == "O":
-                     board[i][j] = "X"
-                elif board[i][j] == "M":
-                     board[i][j] = "O"
-        
+
