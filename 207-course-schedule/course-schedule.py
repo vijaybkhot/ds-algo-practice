f36953798c4d75f9fa0dcd6d5d257c2152cc8d26
Vijay Khot <vijaysinh.khot@gmail.com>
2025-10-02 07:04:42 -0400

    Time: 7 ms (46.27%) | Memory: 19.3 MB (51.02%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,93 +1,21 @@
-
-class UnionFind:
-    def __init__(self):
-        self.parent = {}
-        self.rank = {}
-    
-    def find(self, x):
-        if x not in self.parent:
-            self.parent[x] = x
-            self.rank[x] = 1
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-
-        if rootX == rootY:
-            return False
-        
-        if self.rank[rootX] > self.rank[rootY]:
-            self.parent[rootY] = rootX
-        elif self.rank[rootY] > self.rank[rootX]:
-            self.parent[rootX] = rootY
-        else:
-            self.parent[rootX] = rootY
-            self.rank[rootY] += 1
-        
-        return True
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-
-    
-        indegree = defaultdict(int)
         graph = defaultdict(set)
-
+        inorder = defaultdict(int)
         for u, v in prerequisites:
             graph[v].add(u)
-            indegree[u] += 1
+            inorder[u] += 1
         
-        # q = deque([])
-        # for i in range(numCourses):
-        #     if indegree[i] == 0:
-        #         q.append(i)
+        q = deque([i for i in range(numCourses) if inorder[i] == 0])
 
-        # while q:
-        #     curr_node = q.popleft()
-        #     for nei in graph[curr_node]:
-        #         indegree[nei] -= 1
-        #         if indegree[nei] == 0:
-        #             q.append(nei)
-        
-        # for i in range(numCourses):
-        #     if indegree[i] > 0:
-        #         return False
-        
-        # return True
-
-        # 1 -> 0 ->1
-        # 1-> 0
-        # 0, 1
-        # graph = { }
-        
-        # stack {0}
-        # vijsited = {}
-
-        stack = set()
-        visited = set()
-
-        def dfs(node):
-            if node in stack:
-                return False
-            if node in visited:
-                return True
-            stack.add(node)
-            for nei in graph[node]:
-                if nei not in visited:
-                    if not dfs(nei):
-                        return False
-            visited.add(node)
-            stack.remove(node)
-            return True
-        
+        while q:
+            node = q.popleft()
+            for u in graph[node]:
+                inorder[u] -= 1
+                if inorder[u] == 0:
+                    q.append(u)
         for i in range(numCourses):
-            if i not in visited:
-                if not dfs(i):
-                    return False
+            if inorder[i] > 0:
+                return False
+        
         return True
-
-
-
