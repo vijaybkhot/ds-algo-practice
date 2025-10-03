b87ab7cc4a873c201d67839f9de9097b7166507d
Vijay Khot <vijaysinh.khot@gmail.com>
2025-10-03 07:52:23 -0400

    Time: 7 ms (25.92%) | Memory: 19.3 MB (47.77%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.py
+++ 210-course-schedule-ii/course-schedule-ii.py
@@ -1,51 +1,25 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        indegree = defaultdict(int)
         graph = defaultdict(set)
+        inorder = defaultdict(int)
+
         for u, v in prerequisites:
             graph[v].add(u)
-            indegree[u] += 1
+            inorder[u] += 1
         
+        q = deque([i for i in range(numCourses) if inorder[i] == 0])
         res = []
-        visited = set()
-        stack = set()
-        def dfs(node):
-            if node in stack:
-                return False
-            if node in visited:
-                return True
-            stack.add(node)
-            for nei in graph[node]:
-                if nei not in visited and not dfs(nei):
-                    return False
-            
-            stack.remove(node)
-            visited.add(node)
-            res.append(node)
-            return True
 
+        while q:
+            node = q.popleft()
+            res.append(node)
+            for v in graph[node]:
+                inorder[v] -= 1
+                if inorder[v] == 0:
+                    q.append(v)
+        
         for i in range(numCourses):
-            if i not in visited and not dfs(i):
+            if inorder[i] > 0:
                 return []
         
-        return res[::-1]
-
-        # q = deque()
-        # for i in range(numCourses):
-        #     if indegree[i] == 0:
-        #         q.append(i)
-        
-        # res = []
-        # while q:
-        #     node = q.popleft()
-        #     res.append(node)
-        #     for nei in graph[node]:
-        #         indegree[nei] -= 1
-        #         if indegree[nei] == 0:
-        #             q.append(nei)
-        
-        # for i in range(numCourses):
-        #     if indegree[i] > 0:
-        #         return []
-        
-        # return res
+        return res
