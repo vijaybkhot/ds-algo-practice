bc6ec3bf71d29dafdc9b88d7fa6c8a2b19abfffb
Vijay Khot <vijaysinh.khot@gmail.com>
2025-10-08 17:55:41 -0400

    Time: 710 ms (30.51%) | Memory: 42.5 MB (60.38%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -6,17 +6,32 @@ class Solution:
 
         # success = 7
 
+        def bs_left(arr, target):
+            left, right = 0, len(arr)-1
+            while left <= right:
+                mid = (left+right)//2
+                if arr[mid] >= target:
+                    right = mid - 1
+                else:
+                    left = mid+1
+            return left
         
-
+        # 2 - > 5, 8, 8
+        #     l    m   r
+        
 
         pairs = []
         potions.sort()
         # return bisect_left(potions, 7/5)
+
         for idx, spell in enumerate(spells):
             required_potion_strength = success/spell
-            potion_idx = bisect_left(potions, required_potion_strength)
+            # potion_idx = bisect_left(potions, required_potion_strength)
+            potion_idx = bs_left(potions, required_potion_strength)
             if potion_idx > len(potions):
                 continue
             pairs.append(len(potions)-potion_idx)
 
         return pairs
+
+        
