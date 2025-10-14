475e8d6f10a63aa8bf69ff4ac772cdfba9c1184d
Vijay Khot <vijaysinh.khot@gmail.com>
2025-10-14 16:46:43 -0400

    Time: 8 ms (23.10%) | Memory: 19.7 MB (16.94%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -8,22 +8,22 @@ class Solution:
 
         def dfs(node):
             if state[node] == 1:
+                return True
+            if state[node] == 2:
                 return False
             
-            if state[node] == 2:
-                return True
             state[node] = 1
 
             for nei in graph[node]:
-                if not dfs(nei):
-                    return False
+                if dfs(nei):
+                    return True
             
             state[node] = 2
-            return True
+            return False
         
         for i in range(numCourses):
             if state[i] == 0:
-                if not dfs(i):
+                if dfs(i):
                     return False
         
         return True
