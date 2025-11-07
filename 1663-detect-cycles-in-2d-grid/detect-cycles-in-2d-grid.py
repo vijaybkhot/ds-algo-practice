b4e853f2e55e9430ed292d829ea80fd6e440fc62
Vijay Khot <vijaysinh.khot@gmail.com>
2025-11-07 08:53:55 -0500

    Time: 655 ms (26.02%) | Memory: 93.2 MB (59.97%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
@@ -1,17 +1,18 @@
 class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
+        # Variables to check bound and traverse in 4 directions
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         rows, cols = len(grid), len(grid[0])
 
+        # A set to track visited cells
         visited = set()
 
+        # BFS function to traverse the grid for one iteration
         def bfs(r, c, char):
             curr_visited = set()
             nonlocal visited
             curr_visited.add((r, c))
-            
             q = deque([(r, c, None, None)])
-
             while q:
                 row, col, p_r, p_c = q.popleft()
                 
@@ -20,16 +21,20 @@ class Solution:
                     nr, nc = row+dr, col+dc
                     if 0 <= nr < rows and 0 <= nc < cols and (nr, nc) != (p_r, p_c) and grid[nr][nc] == char:
                         if (nr, nc) in curr_visited and len(curr_visited) >= 4:
+                            # return true if cycle is found and the current path contains atleast 4 cells
                             return True
                         visited.add((nr, nc))
                         curr_visited.add((nr, nc))
                         q.append((nr, nc, row, col))
-            
+            # No cycle found, return False
             return False
+        
+        # Traverse through all cells to find cycle
         for r in range(rows):
             for c in range(cols):
                 if (r, c) not in visited:
+                    # Return True if Cycle found
                     if bfs(r, c, grid[r][c]):
                         return True
-        
+        # Return False, cycle not found.
         return False
