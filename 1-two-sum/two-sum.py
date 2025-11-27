9058116c2bbf9cc5a9c10c7b9ba234f81f782111
Vijay Khot <vijaysinh.khot@gmail.com>
2025-11-27 06:18:44 -0500

    Time: 0 ms (100.00%) | Memory: 18.8 MB (51.55%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        complement_map = dict()
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_map = {}
         for idx, num in enumerate(nums):
-            complement = target - num
-            if complement in complement_map:
-                return [idx, complement_map[complement]]
-            else:
-                complement_map[num] = idx
+            curr_target = target - num
+            if curr_target in nums_map:
+                return [nums_map[curr_target] ,idx]
+            nums_map[num] = idx
+        
