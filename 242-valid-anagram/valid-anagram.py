6bc33fc82a3540f669cee79712e767d903ef3dd4
Vijay Khot <vijaysinh.khot@gmail.com>
2025-11-27 06:11:46 -0500

    Time: 19 ms (14.79%) | Memory: 18.7 MB (11.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,21 +1,7 @@
-import unicodedata
-from collections import Counter
-
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # s_counter = Counter(s)
-        # t_counter = Counter(t)
-
-        # for char in s_counter:
-        #     if char not in t_counter or s_counter[char] != t_counter[char]:
-        #         return False
-        
-        # for char in t_counter:
-        #     if char not in s_counter or s_counter[char] != t_counter[char]:
-        #         return False
-        
-        # return True
-
-        s = unicodedata.normalize('NFC', s)
-        t = unicodedata.normalize('NFC', t)
-        return Counter(s) == Counter(t)
+        s_arr = [char for char in s]
+        s_arr.sort()
+        t_arr = [char for char in t]
+        t_arr.sort()
+        return ''.join(s_arr) == ''.join(t_arr)
