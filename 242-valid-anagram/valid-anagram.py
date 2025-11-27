3067877a1390aa811286d60c85c8844f08ce91a1
Vijay Khot <vijaysinh.khot@gmail.com>
2025-11-27 06:15:00 -0500

    Time: 7 ms (89.67%) | Memory: 17.7 MB (85.90%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_arr = [char for char in s]
-        s_arr.sort()
-        t_arr = [char for char in t]
-        t_arr.sort()
-        return ''.join(s_arr) == ''.join(t_arr)
+        # s_arr = [char for char in s]
+        # s_arr.sort()
+        # t_arr = [char for char in t]
+        # t_arr.sort()
+        # return ''.join(s_arr) == ''.join(t_arr)
+        s_map = defaultdict(int)
+        t_map = defaultdict(int)
+        for char in s:
+            s_map[char] += 1
+        for char in t:
+            t_map[char] += 1
+        
+        for char in s_map:
+            if s_map[char] != t_map[char]:
+                return False
+        
+        for char in t_map:
+            if s_map[char] != t_map[char]:
+                return False
+        
+        return True
